0c8ae5f2f8095bfdc0649466f1049fc6d4e364bc
yarpo <jar.patryk@gmail.com>
2017-08-23 14:00:59 +0100

    FIX: Payment object moved out from claimant and serialise / deserialise correctly (#20)

--- src/main/app/claims/models/claimData.ts
+++ src/main/app/claims/models/claimData.ts
@@ -4,6 +4,7 @@ import InterestDate from 'app/claims/models/interestDate'
 import Interest from 'app/forms/models/interest'
 import { Claimant } from 'claims/models/claimant'
 import { Defendant } from 'claims/models/defendant'
+import Payment from 'app/pay/payment'
 
 export default class ClaimData implements Serializable<ClaimData> {
   claimant: Claimant
@@ -13,16 +14,19 @@ export default class ClaimData implements Serializable<ClaimData> {
   reason: string
   interest: Interest
   interestDate: InterestDate
+  payment: Payment = new Payment()
 
   deserialize (input: any): ClaimData {
     if (input) {
       this.claimant = new Claimant().deserialize(input.claimant)
       this.defendant = new Defendant().deserialize(input.defendant)
-      this.paidFeeAmount = this.claimant.payment.amount / 100
+      this.payment = new Payment().deserialize(input.payment)
       this.amount = new ClaimAmountBreakdown().deserialize(input.amount).totalAmount()
-      this.reason = input.reason
       this.interest = new Interest().deserialize(input.interest)
       this.interestDate = new InterestDate().deserialize(input.interestDate)
+
+      this.reason = input.reason
+      this.paidFeeAmount = this.payment.amount / 100
     }
     return this
   }
