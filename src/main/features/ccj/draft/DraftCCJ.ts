53733e027bab95b9f5c52a5b4b7c547d0e41df23
yarpo <jar.patryk@gmail.com>
2017-08-30 12:45:32 +0100

    Feature/roc 1530 ccj amount (#21)
    * ROC-1530: front end
    
    * ROC-1530: added tests, css, img and refactored code
    
    * ROC-1530: after review
    
    * ROC-1530: paid-amount-summary page added
    
    * ROC-1530: refactored code, added paid-amount-summary page and tests
    
    * ROC-1530: supporting custom rate
    
    * ROC-1530: after review
    
    * ROC-1530: after review
    
    * ROC-1530: pound input refactored
    
    * ROC-1530: additional route tests
    
    * ROC-1530: ccj guard tests
    
    * ROC-1530: romoved '| safe' filter
    
    * ROC-1530: more tests for routes

--- src/main/features/ccj/draft/DraftCCJ.ts
+++ src/main/features/ccj/draft/DraftCCJ.ts
@@ -1,13 +1,15 @@
 import { Defendant } from 'app/drafts/models/defendant'
+import { PaidAmount } from 'ccj/form/models/paidAmount'
 
 export class DraftCCJ {
   defendant: Defendant = new Defendant()
+  paidAmount?: PaidAmount
 
   deserialize (input: any): DraftCCJ {
     if (input) {
       this.defendant = new Defendant().deserialize(input.defendant)
+      this.paidAmount = new PaidAmount().deserialize(input.paidAmount)
     }
     return this
   }
-
 }
---
+++ src/test/features/ccj/form/models/paidAmount.ts
@@ -0,0 +1,63 @@
+import { expect } from 'chai'
+
+import { Validator } from 'class-validator'
+import { expectValidationError } from '../../../../app/forms/models/validationUtils'
+import { PaidAmount, ValidationErrors } from 'ccj/form/models/paidAmount'
+import { PaidAmountOption } from 'ccj/form/models/yesNoOption'
+
+describe('PaidAmount', () => {
+  describe('validation', () => {
+    const validator: Validator = new Validator()
+
+    describe('should reject when', () => {
+      it('undefined option', () => {
+        const errors = validator.validateSync(new PaidAmount(undefined))
+
+        expect(errors.length).to.equal(1)
+        expectValidationError(errors, ValidationErrors.OPTION_REQUIRED)
+      })
+
+      it('invalid option', () => {
+        const errors = validator.validateSync(new PaidAmount(new PaidAmountOption('maybe')))
+
+        expect(errors.length).to.equal(1)
+        expectValidationError(errors, ValidationErrors.OPTION_REQUIRED)
+      })
+
+      it('valid option, but undefined amount', () => {
+        const errors = validator.validateSync(new PaidAmount(PaidAmountOption.YES, undefined))
+
+        expect(errors.length).to.equal(1)
+        expectValidationError(errors, ValidationErrors.AMOUNT_REQUIRED)
+      })
+
+      it('valid option, but invalid amount (negative)', () => {
+        const errors = validator.validateSync(new PaidAmount(PaidAmountOption.YES, -10))
+
+        expect(errors.length).to.equal(1)
+        expectValidationError(errors, ValidationErrors.AMOUNT_NOT_VALID)
+      })
+
+      it('valid option, but invalid amount (zero)', () => {
+        const errors = validator.validateSync(new PaidAmount(PaidAmountOption.YES, 0))
+
+        expect(errors.length).to.equal(1)
+        expectValidationError(errors, ValidationErrors.AMOUNT_NOT_VALID)
+      })
+    })
+
+    describe('should accept when', () => {
+      it('option is NO', () => {
+        const errors = validator.validateSync(new PaidAmount(PaidAmountOption.NO))
+
+        expect(errors.length).to.equal(0)
+      })
+
+      it('option is YES and amount is valid', () => {
+        const errors = validator.validateSync(new PaidAmount(PaidAmountOption.YES, 100))
+
+        expect(errors.length).to.equal(0)
+      })
+    })
+  })
+})
