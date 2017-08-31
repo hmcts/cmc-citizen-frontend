088908e186cd75089b722dd916c91952dc03a20d
Ranjit Rattu <>
2017-08-31 11:01:17 +0100

    Update test cases after review comments

--- src/test/features/ccj/routes/date-of-birth.ts
+++ src/test/features/ccj/routes/date-of-birth.ts
@@ -13,10 +13,11 @@ import * as idamServiceMock from '../../../http-mocks/idam'
 import * as claimStoreServiceMock from '../../../http-mocks/claim-store'
 import * as draftStoreServiceMock from '../../../http-mocks/draft-store'
 import { checkAuthorizationGuards } from './checks/authorization-check'
+import { sampleClaimObj } from '../../../http-mocks/claim-store'
+const externalId = sampleClaimObj.externalId
 
 const cookieName: string = config.get<string>('session.cookieName')
-
-const dateOfBirthPage = Paths.dateOfBirthPage.uri.replace(':externalId', 'b17af4d2-273f-4999-9895-bce382fa24c8')
+const dateOfBirthPage = Paths.dateOfBirthPage.uri.replace(':externalId', externalId)
 
 describe('CCJ - defendant date of birth', () => {
   attachDefaultHooks()
@@ -27,6 +28,25 @@ describe('CCJ - defendant date of birth', () => {
     context('when user authorised', () => {
       beforeEach(() => {
         idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta')
+      })
+
+      it('should return 500 and render error page when cannot retrieve claims', async () => {
+        claimStoreServiceMock.rejectRetrieveClaimByExternalId('HTTP error')
+
+        await request(app)
+          .get(dateOfBirthPage)
+          .set('Cookie', `${cookieName}=ABC`)
+          .expect(res => expect(res).to.be.serverError.withText('Error'))
+      })
+
+      it('should return 500 and render error page when cannot retrieve CCJ draft', async () => {
+        draftStoreServiceMock.rejectRetrieve('ccj', 'Error')
+        claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+
+        await request(app)
+          .get(dateOfBirthPage)
+          .set('Cookie', `${cookieName}=ABC`)
+          .expect(res => expect(res).to.be.serverError.withText('Error'))
       })
 
       it('should render page when everything is fine', async () => {
@@ -48,8 +68,19 @@ describe('CCJ - defendant date of birth', () => {
         idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta')
       })
       context('when form is invalid', async () => {
-        it('should return 500 and render error page and cannot save draft', async () => {
+        it('should return 500 and render error page when cannot retrieve claim', async () => {
           claimStoreServiceMock.rejectRetrieveClaimByExternalId('HTTP error')
+          await request(app)
+            .post(dateOfBirthPage)
+            .set('Cookie', `${cookieName}=ABC`)
+            .send({ known: 'true', date: { day: '31', month: '12', year: '1900' } })
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+
+        it('should return 500 when cannot retrieve CCJ draft', async () => {
+          draftStoreServiceMock.rejectRetrieve('ccj', 'Error')
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+
           await request(app)
             .post(dateOfBirthPage)
             .set('Cookie', `${cookieName}=ABC`)
@@ -67,6 +98,18 @@ describe('CCJ - defendant date of birth', () => {
             .set('Cookie', `${cookieName}=ABC`)
             .send({ known: 'true', date: { day: '31', month: '12', year: '1980' } })
             .expect(res => expect(res).to.be.redirect.toLocation('toDo'))
+        })
+
+        it('should return 500 and render error page when cannot save ccj draft', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+          draftStoreServiceMock.resolveRetrieve('ccj')
+          draftStoreServiceMock.rejectSave('ccj', 'Error')
+
+          await request(app)
+            .post(dateOfBirthPage)
+            .set('Cookie', `${cookieName}=ABC`)
+            .send({ known: 'true', date: { day: '31', month: '12', year: '1980' } })
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
         })
       })
     })
