4fb002b6c606ef2f293293c55941a10901f1cd4f
Ranjit Rattu <ranjitrattu@Ranjits-MacBook-Pro.local>
2017-09-14 11:15:36 +0100

    Change to use const instead of let after review comments

--- src/test/app/forms/validation/validators/IsTodayOrInFutureConstraint.ts
+++ src/test/app/forms/validation/validators/IsTodayOrInFutureConstraint.ts
@@ -17,12 +17,12 @@ describe('DateTodayOrInFutureConstraint', () => {
       })
 
       it('given a valid date in the future', () => {
-        let inFuture = moment().add(10, 'years')
+        const inFuture = moment().add(10, 'years')
         expect(constraint.validate(new LocalDate(inFuture.year(), 1, 1))).to.equal(true)
       })
 
       it('given today date', () => {
-        let now = moment()
+        const now = moment()
         expect(constraint.validate(new LocalDate(now.year(), now.month() + 1, now.date()))).to.equal(true)
       })
     })
@@ -33,7 +33,7 @@ describe('DateTodayOrInFutureConstraint', () => {
       })
 
       it('given a date in the past', () => {
-        let inPast = moment().subtract(10, 'years')
+        const inPast = moment().subtract(10, 'years')
         expect(constraint.validate(new LocalDate(inPast.year(), 8, 8))).to.equal(false)
       })
     })
