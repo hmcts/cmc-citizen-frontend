44a2f465e0bdc48b89638455d2b9c243c4c92bf5
Ranjit Rattu <ranjitrattu@Ranjits-MacBook-Pro.local>
2017-09-14 14:40:31 +0100

    Fix typo

--- src/test/app/forms/validation/validators/IsTodayOrInFutureConstraint.ts
+++ src/test/app/forms/validation/validators/IsTodayOrInFutureConstraint.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import * as moment from 'moment'
 
-import { DateTodayOrInFutureConstraint } from 'forms/validation/validators/isTodayOrInFuture'
+import { DateTodayOrInFutureConstraint } from 'forms/validation/validators/IsTodayOrInFuture'
 
 import { LocalDate } from 'app/forms/models/localDate'
 
