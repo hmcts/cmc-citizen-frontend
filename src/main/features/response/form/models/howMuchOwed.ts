19d44e1a95b92443e1579aaa45b40f3e91e692c5
Ranjit Rattu <ranjitrattu@Ranjits-MacBook-Pro.local>
2017-09-21 16:43:17 +0100

    Remove old readonly value

--- src/main/features/response/form/models/howMuchOwed.ts
+++ src/main/features/response/form/models/howMuchOwed.ts
@@ -5,7 +5,7 @@ import { IsNotBlank } from 'app/forms/validation/validators/isBlank'
 export class ValidationErrors {
   static readonly NOT_OWE_FULL_AMOUNT_REQUIRED: string = 'Explain why you don’t owe the full amount'
   static readonly REASON_NOT_OWE_MONEY_TOO_LONG: string = 'Enter reason no longer than $constraint1 characters'
-  static readonly VALID_AMOUNT_REQUIRED: string = 'Enter an amount between £1 and $constraint1'
+  static readonly VALID_AMOUNT_REQUIRED: string = 'Enter a valid amount between £1 and $constraint1'
   static readonly AMOUNT_REQUIRED: string = 'Enter an amount'
 }
 
