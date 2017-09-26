d5b5c277cfb87086ca58fb62d93312e2f7abd5e6
Tim Jacomb <tim.jacomb@hmcts.net>
2017-09-26 07:43:05 +0100

    More test fixes

--- src/main/features/response/routes/response-type.ts
+++ src/main/features/response/routes/response-type.ts
@@ -11,37 +11,34 @@ import { ResponseDraftMiddleware } from 'response/draft/responseDraftMiddleware'
 import { ErrorHandling } from 'common/errorHandling'
 import User from 'idam/user'
 
-function renderView (form: Form<Response>, res: express.Response, next: express.NextFunction) {
-  try {
-    res.render(Paths.responseTypePage.associatedView, {
-      form: form
-    })
-  } catch (err) {
-    next(err)
-  }
+function renderView (form: Form<Response>, res: express.Response) {
+  res.render(Paths.responseTypePage.associatedView, {
+    form: form
+  })
 }
 
 export default express.Router()
-  .get(Paths.responseTypePage.uri, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    renderView(new Form(res.locals.user.responseDraft.response), res, next)
-  })
+  .get(Paths.responseTypePage.uri, ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    renderView(new Form(res.locals.user.responseDraft.response), res)
+  }))
   .post(
     Paths.responseTypePage.uri,
     FormValidator.requestHandler(Response, Response.fromObject),
     ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+      const { externalId } = req.params
       const form: Form<Response> = req.body
 
       if (form.hasErrors()) {
-        renderView(form, res, next)
+        renderView(form, res)
       } else {
         const user: User = res.locals.user
         user.responseDraft.response = form.model
         await ResponseDraftMiddleware.save(res, next)
 
         if (ResponseType.OWE_NONE === form.model.type) {
-          res.redirect(Paths.defenceOptionsPage.evaluateUri({ externalId: user.claim.externalId }))
+          res.redirect(Paths.defenceOptionsPage.evaluateUri({ externalId: externalId }))
         } else {
-          res.redirect(Paths.taskListPage.evaluateUri({ externalId: user.claim.externalId }))
+          res.redirect(Paths.taskListPage.evaluateUri({ externalId: externalId }))
         }
       }
     }))
--- src/test/features/response/routes/response-type.ts
+++ src/test/features/response/routes/response-type.ts
@@ -14,22 +14,24 @@ import { app } from '../../../../main/app'
 import * as idamServiceMock from '../../../http-mocks/idam'
 import * as draftStoreServiceMock from '../../../http-mocks/draft-store'
 import * as claimStoreServiceMock from '../../../http-mocks/claim-store'
+import { sampleClaimObj } from '../../../http-mocks/claim-store'
 import { ResponseType } from 'response/form/models/responseType'
 
 const cookieName: string = config.get<string>('session.cookieName')
+const pagePath = ResponsePaths.responseTypePage.evaluateUri({ externalId: sampleClaimObj.externalId })
 
 describe('Defendant response: response type page', () => {
   attachDefaultHooks()
 
   describe('on GET', () => {
-    checkAuthorizationGuards(app, 'get', ResponsePaths.responseTypePage.uri)
+    checkAuthorizationGuards(app, 'get', pagePath)
 
     context('when user authorised', () => {
       beforeEach(() => {
         idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta', 'defendant')
       })
 
-      checkAlreadySubmittedGuard(app, 'get', ResponsePaths.responseTypePage.uri)
+      checkAlreadySubmittedGuard(app, 'get', pagePath)
 
       context('when response not submitted', () => {
         beforeEach(() => {
@@ -40,7 +42,7 @@ describe('Defendant response: response type page', () => {
           draftStoreServiceMock.resolveRetrieve('response')
 
           await request(app)
-            .get(ResponsePaths.responseTypePage.uri)
+            .get(pagePath)
             .set('Cookie', `${cookieName}=ABC`)
             .expect(res => expect(res).to.successful.withText('Do you owe the money claimed?'))
         })
@@ -49,26 +51,23 @@ describe('Defendant response: response type page', () => {
   })
 
   describe('on POST', () => {
-    checkAuthorizationGuards(app, 'post', ResponsePaths.responseTypePage.uri)
+    checkAuthorizationGuards(app, 'post', pagePath)
 
     context('when user authorised', () => {
       beforeEach(() => {
         idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta', 'defendant')
       })
 
-      checkAlreadySubmittedGuard(app, 'post', ResponsePaths.responseTypePage.uri)
+      checkAlreadySubmittedGuard(app, 'post', pagePath)
 
       context('when response not submitted', () => {
-        beforeEach(() => {
-          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
-        })
-
         context('when form is invalid', () => {
           it('should render page when everything is fine', async () => {
+            claimStoreServiceMock.resolveRetrieveClaimByExternalIdWithResponse()
             draftStoreServiceMock.resolveRetrieve('response')
 
             await request(app)
-              .post(ResponsePaths.responseTypePage.uri)
+              .post(pagePath)
               .set('Cookie', `${cookieName}=ABC`)
               .expect(res => expect(res).to.be.successful.withText('Do you owe the money claimed?', 'div class="error-summary"'))
           })
@@ -80,7 +79,7 @@ describe('Defendant response: response type page', () => {
             draftStoreServiceMock.rejectSave('response', 'HTTP error')
 
             await request(app)
-              .post(ResponsePaths.responseTypePage.uri)
+              .post(pagePath)
               .set('Cookie', `${cookieName}=ABC`)
               .send({ type: ResponseType.OWE_NONE })
               .expect(res => expect(res).to.be.serverError.withText('Error'))
@@ -91,7 +90,7 @@ describe('Defendant response: response type page', () => {
             draftStoreServiceMock.resolveSave('response')
 
             await request(app)
-              .post(ResponsePaths.responseTypePage.uri)
+              .post(pagePath)
               .set('Cookie', `${cookieName}=ABC`)
               .send({ type: ResponseType.OWE_SOME_PAID_NONE })
               .expect(res => expect(res).to.be.redirect.toLocation(ResponsePaths.taskListPage.uri))
@@ -102,7 +101,7 @@ describe('Defendant response: response type page', () => {
             draftStoreServiceMock.resolveSave('response')
 
             await request(app)
-              .post(ResponsePaths.responseTypePage.uri)
+              .post(pagePath)
               .set('Cookie', `${cookieName}=ABC`)
               .send({ type: ResponseType.OWE_NONE })
               .expect(res => expect(res).to.be.redirect.toLocation(ResponsePaths.defenceOptionsPage.uri))
