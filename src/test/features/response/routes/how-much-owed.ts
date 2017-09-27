b6407e6afd77080036ebed728ee249705cfa8021
Ranjit Rattu <ranjitrattu@Ranjits-MacBook-Pro.local>
2017-09-27 16:46:37 +0100

    update test case

--- src/test/features/response/routes/how-much-owed.ts
+++ src/test/features/response/routes/how-much-owed.ts
@@ -14,37 +14,42 @@ import { app } from '../../../../main/app'
 import * as idamServiceMock from '../../../http-mocks/idam'
 import * as draftStoreServiceMock from '../../../http-mocks/draft-store'
 import * as claimStoreServiceMock from '../../../http-mocks/claim-store'
+import { sampleClaimObj } from '../../../http-mocks/claim-store'
 
 const cookieName: string = config.get<string>('session.cookieName')
+const defendantHowMuchOwedPage = ResponsePaths.defendantHowMuchOwed.evaluateUri({ externalId: sampleClaimObj.externalId })
 
 describe('Defendant response: how much money do you believe you owe', () => {
   attachDefaultHooks()
 
   describe('on GET', () => {
-    checkAuthorizationGuards(app, 'get', ResponsePaths.defendantHowMuchOwed.uri)
+    checkAuthorizationGuards(app, 'get', defendantHowMuchOwedPage)
 
     context('when user authorised', () => {
       beforeEach(() => {
         idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta', 'defendant')
       })
 
-      checkAlreadySubmittedGuard(app, 'get', ResponsePaths.defendantHowMuchOwed.uri)
+      checkAlreadySubmittedGuard(app, 'get', defendantHowMuchOwedPage)
 
       context('when response not submitted', () => {
-        beforeEach(() => {
-          claimStoreServiceMock.resolveRetrieveByDefendantId('000MC001')
-          // claimStoreServiceMock.resolveRetrieveResponsesByDefendantIdToEmptyList()
+        it('should return 500 and render error page when cannot retrieve claim', async () => {
+          claimStoreServiceMock.rejectRetrieveClaimByExternalId('HTTP error')
+
+          await request(app)
+            .get(defendantHowMuchOwedPage)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
         })
 
         it('should render page when everything is fine', async () => {
           draftStoreServiceMock.resolveRetrieve('response')
-          claimStoreServiceMock.resolveRetrieveByDefendantId('000MC001')
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
 
           await request(app)
-            .get(ResponsePaths.defendantHowMuchOwed.uri)
+            .get(defendantHowMuchOwedPage)
             .set('Cookie', `${cookieName}=ABC`)
-            .expect(res => expect(res).to.be.successful.withText('How much money do you believe you owe?'))
-        })
+            .expect(res => expect(res).to.be.successful.withText('How much money do you believe you owe?'))        })
       })
     })
   })
@@ -57,18 +62,16 @@ describe('Defendant response: how much money do you believe you owe', () => {
         idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta', 'defendant')
       })
 
-      checkAlreadySubmittedGuard(app, 'post', ResponsePaths.defendantHowMuchOwed.uri)
+      checkAlreadySubmittedGuard(app, 'post', defendantHowMuchOwedPage)
 
       context('when response not submitted', () => {
         beforeEach(() => {
-          claimStoreServiceMock.resolveRetrieveByDefendantId('000MC001')
-          // claimStoreServiceMock.resolveRetrieveResponsesByDefendantIdToEmptyList()
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
         })
 
         context('when form is invalid', () => {
           it('should render page when everything is fine', async () => {
             draftStoreServiceMock.resolveRetrieve('response')
-            claimStoreServiceMock.resolveRetrieveByDefendantId('000MC001')
 
             await request(app)
               .post(ResponsePaths.defendantHowMuchOwed.uri)
@@ -81,6 +84,7 @@ describe('Defendant response: how much money do you believe you owe', () => {
           it('should return 500 and render error page when form is valid and cannot save draft', async () => {
             draftStoreServiceMock.resolveRetrieve('response')
             draftStoreServiceMock.rejectSave('response', 'HTTP error')
+            claimStoreServiceMock.resolveRetrieveClaimByExternalId()
 
             await request(app)
               .post(ResponsePaths.defendantHowMuchOwed.uri)
@@ -92,6 +96,7 @@ describe('Defendant response: how much money do you believe you owe', () => {
           it('should redirect to free mediation page when form is valid and everything is fine', async () => {
             draftStoreServiceMock.resolveRetrieve('response')
             draftStoreServiceMock.resolveSave('response')
+            claimStoreServiceMock.resolveRetrieveClaimByExternalId()
 
             await request(app)
               .post(ResponsePaths.defendantHowMuchOwed.uri)
