e04f9aa27b8d2734bc55532c185bd75350831224
Damian Dunajski <damian.dunajski@hmcts.net>
2017-10-03 23:00:23 +0100

    Reimplement draft middleware to handle legacy drafts scenario (drafts without external ID)

--- src/main/common/draft/draftMiddleware.ts
+++ src/main/common/draft/draftMiddleware.ts
@@ -3,6 +3,31 @@ import DraftStoreClient from 'common/draft/draftStoreClient'
 import { Draft, DraftDocument } from 'models/draft'
 import { DraftStoreClientFactory } from 'common/draft/draftStoreClientFactory'
 
+/**
+ * Extracts external ID from paths in the '/case/<external-id>.*' format
+ */
+function extractExternalId (path: string): string {
+  const pattern = /\/case\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})\/.*/
+
+  const externalIds: RegExpMatchArray = path.match(pattern)
+  if (externalIds !== null) {
+    return externalIds[1]
+  }
+  return undefined
+}
+
+/**
+ * Filters list of drafts to return only these matching external ID. If none of the drafts has external ID set
+ * then unchanged list is returned so that they can be migrated to new format with external ID (legacy drafts scenario).
+ */
+function tryFilterByExternalId <T extends DraftDocument> (drafts: Draft<T>[], externalId: string): Draft<T>[] {
+  if (drafts.filter(draft => draft.document.externalId !== undefined).length === 0) {
+    return drafts
+  }
+
+  return drafts.filter(item => item.document.externalId === externalId)
+}
+
 export class DraftMiddleware {
 
   static requestHandler<T extends DraftDocument> (draftType: string, deserializeFn: (value: any) => T = (value) => value): express.RequestHandler {
@@ -11,33 +36,32 @@ export class DraftMiddleware {
         try {
           const client: DraftStoreClient<T> = await DraftStoreClientFactory.create<T>()
 
-          const query: { [key: string]: string } = { type: draftType }
-
-          const externalIds = req.path.match(/\/case\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})\/.*/)
-          if (externalIds !== null) {
-            query.externalId = externalIds[1]
-          }
-
           client
-            .find(query, res.locals.user.bearerToken, deserializeFn)
+            .find({ type: draftType }, res.locals.user.bearerToken, deserializeFn)
             .then((drafts: Draft<T>[]) => {
-              const matchingDrafts = drafts.filter(item => item.type === draftType)
+              const externalId = extractExternalId(req.path)
 
-              if (matchingDrafts.length > 1) {
+              if (externalId !== undefined) {
+                drafts = tryFilterByExternalId(drafts, externalId)
+              }
+
+              if (drafts.length > 1) {
                 throw new Error('More then one draft has been found')
               }
 
-              if (matchingDrafts.length === 0) {
-                const draft = new Draft<T>()
+              let draft: Draft<T>
+              if (drafts.length === 1) {
+                draft = drafts[0]
+              } else {
+                draft = new Draft<T>()
                 draft.type = draftType
                 draft.document = deserializeFn(undefined)
-                if (draft.document.externalId === undefined && externalIds !== null) {
-                  draft.document.externalId = externalIds[1]
-                }
-                res.locals.user[`${draftType}Draft`] = draft
-              } else {
-                res.locals.user[`${draftType}Draft`] = matchingDrafts[0]
               }
+
+              if (draft.document.externalId === undefined && externalId !== undefined) {
+                draft.document.externalId = externalId
+              }
+              res.locals.user[`${draftType}Draft`] = draft
 
               next()
             })
