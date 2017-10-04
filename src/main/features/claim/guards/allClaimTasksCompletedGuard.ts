a5bd613ac728c3312e1da42e5dd9b65a3ce0b389
Tim Jacomb <tim.jacomb@hmcts.net>
2017-10-04 09:45:35 +0100

    Add response

--- src/main/features/claim/guards/allClaimTasksCompletedGuard.ts
+++ src/main/features/claim/guards/allClaimTasksCompletedGuard.ts
@@ -1,13 +1,8 @@
 import * as express from 'express'
 
 import DraftClaim from 'app/drafts/models/draftClaim'
-import { ResolveDispute } from 'app/drafts/tasks/resolveDispute'
-import { CompletingYourClaim } from 'app/drafts/tasks/completingYourClaim'
-import { YourDetails } from 'app/drafts/tasks/yourDetails'
-import { TheirDetails } from 'app/drafts/tasks/theirDetails'
-import { ClaimAmount } from 'app/drafts/tasks/claimAmount'
-import { ClaimDetails } from 'app/drafts/tasks/claimDetails'
 import { Paths } from 'claim/paths'
+import { TaskListBuilder } from 'claim/helpers/taskListBuilder'
 
 const logger = require('@hmcts/nodejs-logging').getLogger('claim/guards/allTasksCompletedGuard')
 
@@ -15,12 +10,7 @@ export default class AllClaimTasksCompletedGuard {
 
   static requestHandler (req: express.Request, res: express.Response, next: express.NextFunction): void {
     const claim: DraftClaim = res.locals.user.claimDraft
-    const allTasksCompleted: boolean = ResolveDispute.isCompleted(claim) &&
-      CompletingYourClaim.isCompleted(claim) &&
-      YourDetails.isCompleted(claim) &&
-      TheirDetails.isCompleted(claim) &&
-      ClaimAmount.isCompleted(claim) &&
-      ClaimDetails.isCompleted(claim)
+    const allTasksCompleted: boolean = TaskListBuilder.buildRemainingTasks(claim).length === 0
     if (allTasksCompleted) {
       return next()
     }
--- src/main/features/response/routes/task-list.ts
+++ src/main/features/response/routes/task-list.ts
@@ -3,59 +3,11 @@ import { Moment } from 'moment'
 
 import { Paths } from 'response/paths'
 
-import { MomentFactory } from 'common/momentFactory'
-
-import TaskList from 'drafts/tasks/taskList'
-import TaskListItem from 'drafts/tasks/taskListItem'
-
 import Claim from 'claims/models/claim'
 
-import { ResponseDraft } from 'response/draft/responseDraft'
-import { OweMoneyTask } from 'response/tasks/oweMoneyTask'
-
-import { YourDefenceTask } from 'response/tasks/yourDefenceTask'
-import { MoreTimeNeededTask } from 'response/tasks/moreTimeNeededTask'
-import { YourDetails } from 'response/tasks/yourDetails'
 import User from 'app/idam/user'
 import { isAfter4pm } from 'common/dateUtils'
-
-export function buildBeforeYouStartSection (responseDraft: ResponseDraft, externalId: string): TaskList {
-  const tasks: TaskListItem[] = []
-  tasks.push(new TaskListItem('Confirm your details', Paths.defendantYourDetailsPage
-    .evaluateUri({ externalId: externalId }), YourDetails.isCompleted(responseDraft)))
-
-  return new TaskList(1, 'Before you start', tasks)
-}
-
-export function buildRespondToClaimSection (draft: ResponseDraft, responseDeadline: Moment, externalId: string): TaskList {
-  const tasks: TaskListItem[] = []
-  const now: Moment = MomentFactory.currentDateTime()
-  if (responseDeadline.isAfter(now)) {
-    tasks.push(new TaskListItem('More time needed to respond', Paths.moreTimeRequestPage
-        .evaluateUri({ externalId: externalId }),
-      MoreTimeNeededTask.isCompleted(draft)))
-  }
-
-  tasks.push(new TaskListItem('Do you owe the money claimed', Paths.responseTypePage
-      .evaluateUri({ externalId: externalId }),
-    OweMoneyTask.isCompleted(draft)))
-
-  if (draft.requireDefence()) {
-    tasks.push(new TaskListItem('Your defence', Paths.defencePage
-        .evaluateUri({ externalId: externalId }),
-      YourDefenceTask.isCompleted(draft)))
-  }
-
-  return new TaskList(2, 'Respond to claim', tasks)
-}
-
-function buildSubmitSection (externalId: string): TaskList {
-  const tasks: TaskListItem[] = []
-  tasks.push(new TaskListItem('Check and submit your response', Paths.checkAndSendPage
-    .evaluateUri({ externalId: externalId }), false))
-
-  return new TaskList(3, 'Submit', tasks)
-}
+import { TaskListBuilder } from 'response/helpers/taskListBuilder'
 
 export default express.Router()
   .get(Paths.taskListPage.uri, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -63,16 +15,15 @@ export default express.Router()
       const user: User = res.locals.user
       const claim: Claim = user.claim
       const responseDeadline: Moment = claim.responseDeadline
-      const beforeYouStartSection = buildBeforeYouStartSection(user.responseDraft, claim.externalId)
-      const respondToClaimSection = buildRespondToClaimSection(user.responseDraft, responseDeadline, claim.externalId)
-      const submitSection = buildSubmitSection(claim.externalId)
+      const beforeYouStartSection = TaskListBuilder.buildBeforeYouStartSection(user.responseDraft, claim.externalId)
+      const respondToClaimSection = TaskListBuilder.buildRespondToClaimSection(user.responseDraft, responseDeadline, claim.externalId)
+      const submitSection = TaskListBuilder.buildSubmitSection(claim.externalId)
 
       res.render(Paths.taskListPage.associatedView,
         {
           beforeYouStartSection: beforeYouStartSection,
           submitSection: submitSection,
           respondToClaimSection: respondToClaimSection,
-          allTasksCompleted: beforeYouStartSection.isCompleted() && respondToClaimSection.isCompleted(),
           claim: claim,
           isAfter4pm: isAfter4pm()
         })
