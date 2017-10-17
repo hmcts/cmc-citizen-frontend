559c24ea8935d7b535ac3ee86bc172f668b3fdff
Anirudha Deshpande <anirudha.deshpande@hmcts.net>
2017-10-17 20:07:41 +0100

    review comments

--- src/main/features/offer/form/models/offer.ts
+++ src/main/features/offer/form/models/offer.ts
@@ -4,12 +4,10 @@ import { IsNotBlank } from 'app/forms/validation/validators/isBlank'
 import { IsFutureDate } from 'forms/validation/validators/dateFutureConstraint'
 import { LocalDate } from 'forms/models/localDate'
 import { IsValidLocalDate } from 'forms/validation/validators/isValidLocalDate'
-import { IsValidYearFormat } from 'app/forms/validation/validators/isValidYearFormat'
 
 export class ValidationErrors {
   static readonly DATE_REQUIRED: string = 'Please enter a valid date'
   static readonly DATE_NOT_VALID: string = 'Please enter a valid date'
-  static readonly DATE_INVALID_YEAR: string = 'Enter a 4 digit year'
   static readonly FUTURE_DATE: string = 'Enter a offer date in the future'
   static readonly OFFER_REQUIRED: string = "You haven't made your offer"
   static readonly OFFER_TEXT_TOO_LONG: string = 'Enter offer no longer than $constraint1 characters'
@@ -24,7 +22,6 @@ export default class Offer implements Serializable<Offer> {
   @ValidateNested()
   @IsDefined({ message: ValidationErrors.DATE_REQUIRED })
   @IsValidLocalDate({ message: ValidationErrors.DATE_NOT_VALID })
-  @IsValidYearFormat({ message: ValidationErrors.DATE_INVALID_YEAR })
   @IsFutureDate({ message: ValidationErrors.FUTURE_DATE })
   date?: LocalDate
 
