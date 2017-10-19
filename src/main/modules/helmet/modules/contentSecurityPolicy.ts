d96af3636e92a8c4b7c482a839139a7eaa77f4d9
jacmurphy <jack.murphy@hmcts.net>
2017-10-19 11:47:46 +0100

    ROC-2527 - Added webchat to claim/start page.

--- src/main/modules/helmet/modules/contentSecurityPolicy.ts
+++ src/main/modules/helmet/modules/contentSecurityPolicy.ts
@@ -1,31 +1,48 @@
 import * as express from 'express'
 import * as helmet from 'helmet'
+import * as config from 'config'
+import * as toBoolean from 'to-boolean'
 
 const none = '\'none\''
 const self = '\'self\''
+
+const webchatEnabled: boolean = toBoolean(config.get('featureToggles.webchat'))
 
 export class ContentSecurityPolicy {
 
   constructor (public developmentMode: boolean) {}
 
   enableFor (app: express.Express) {
-    const scriptSrc = [self, 'https://vcc-eu3.8x8.com/' , '*.google-analytics.com']
-    const connectSrc = [self]
+
+    const imgSrc = [self, '*.google-analytics.com'];
+    const scriptSrc = [self, '*.google-analytics.com']
+    const connectSrc = imgSrc
+    const frameSrc = []
 
     if (this.developmentMode) {
       scriptSrc.push('http://localhost:35729')
       connectSrc.push('ws://localhost:35729')
     }
 
+    if (webchatEnabled) {
+      const webChatUrl: string = config.get('webchat.url');
+      scriptSrc.push(webChatUrl)
+      frameSrc.push(webChatUrl)
+      imgSrc.push(webChatUrl)
+    }
+
+
+
     app.use(helmet.contentSecurityPolicy({
       directives: {
         defaultSrc: [none],
         fontSrc: [self, 'data:'],
-        imgSrc: [self, '*.google-analytics.com'],
+        imgSrc: imgSrc,
         styleSrc: [self],
         scriptSrc: scriptSrc,
         connectSrc: connectSrc,
-        objectSrc: [self]
+        objectSrc: [self],
+        frameSrc: frameSrc
       }
     }))
   }
