065bdf89baa14a4da2a2a58f00eb6a4d22708e30
jacmurphy <jack.murphy@hmcts.net>
2017-10-25 14:53:56 +0100

    Updated tests to redirect to task list. Added missing '='

--- src/main/features/response/draft/responseDraft.ts
+++ src/main/features/response/draft/responseDraft.ts
@@ -91,7 +91,7 @@ export class ResponseDraft extends DraftDocument implements Serializable<Respons
       return true
     }
     return this.response.type === ResponseType.OWE_SOME_PAID_NONE && this.rejectPartOfClaim &&
-      (this.rejectPartOfClaim.option == RejectPartOfClaimOption.PAID_WHAT_BELIEVED_WAS_OWED ||
+      (this.rejectPartOfClaim.option === RejectPartOfClaimOption.PAID_WHAT_BELIEVED_WAS_OWED ||
         this.rejectPartOfClaim.option === RejectPartOfClaimOption.AMOUNT_TOO_HIGH)
   }
 
