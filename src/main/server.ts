463c9759763ba79699e8bc70e51b7d3e7fd7e316
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-10-30 00:33:06 +0100

    Add typings

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,6 +2,7 @@
 
 import * as tsConfigPaths from 'tsconfig-paths'
 import { cpus } from 'os'
+import * as express from 'express'
 import * as fs from 'fs'
 import * as path from 'path'
 import * as https from 'https'
@@ -12,14 +13,14 @@ const numberOfCores = cpus().length
 const port: number = parseInt(process.env.PORT, 10) || 3000
 const logger = require('@hmcts/nodejs-logging').getLogger('server')
 
-function bootstrapTypeScriptPaths (tsConfig) {
+function bootstrapTypeScriptPaths (tsConfig: any) {
   tsConfigPaths.register({
     baseUrl: tsConfig.compilerOptions.baseUrl,
     paths: tsConfig.compilerOptions.paths
   })
 }
 
-function listen (app, port) {
+function listen (app: express.Application, port: number) {
   if (app.locals.ENV === 'development' || app.locals.ENV === 'dockertests') {
     const server = https.createServer(getSSLOptions(), app)
     server.listen(port, () => {
@@ -32,7 +33,7 @@ function listen (app, port) {
   }
 }
 
-function getSSLOptions () {
+function getSSLOptions (): any {
   const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
   return {
     key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
@@ -40,7 +41,7 @@ function getSSLOptions () {
   }
 }
 
-function forkListenerProcesses (numberOfCores) {
+function forkListenerProcesses (numberOfCores: number) {
   for (let i = 0; i < numberOfCores; i++) {
     cluster.fork()
   }
