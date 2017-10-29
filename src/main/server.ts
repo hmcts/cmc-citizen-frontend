67cd9b755f742b47e0959a94c44e264b2db81c85
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-10-30 00:29:20 +0100

    Use cluster module to spawn one node process per cpu core

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,26 +1,63 @@
 #!/usr/bin/env node
 
-import { app } from './app'
+import * as tsConfigPaths from 'tsconfig-paths'
+import { cpus } from 'os'
 import * as fs from 'fs'
 import * as path from 'path'
 import * as https from 'https'
+import * as cluster from 'cluster'
 
+const tsConfig = require('../../tsconfig.json')
+const numberOfCores = cpus().length
 const port: number = parseInt(process.env.PORT, 10) || 3000
-
 const logger = require('@hmcts/nodejs-logging').getLogger('server')
 
-if (app.locals.ENV === 'development' || app.locals.ENV === 'dockertests') {
+function bootstrapTypeScriptPaths (tsConfig) {
+  tsConfigPaths.register({
+    baseUrl: tsConfig.compilerOptions.baseUrl,
+    paths: tsConfig.compilerOptions.paths
+  })
+}
+
+function listen (app, port) {
+  if (app.locals.ENV === 'development' || app.locals.ENV === 'dockertests') {
+    const server = https.createServer(getSSLOptions(), app)
+    server.listen(port, () => {
+      logger.info(`Listener started: https://localhost:${port}`)
+    })
+  } else {
+    app.listen(port, () => {
+      logger.info(`Listener started: http://localhost:${port}`)
+    })
+  }
+}
+
+function getSSLOptions () {
   const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
-  const sslOptions = {
+  return {
     key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
     cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt'))
   }
-  const server = https.createServer(sslOptions, app)
-  server.listen(port, () => {
-    logger.info(`Application started: https://localhost:${port}`)
+}
+
+function forkListenerProcesses (numberOfCores) {
+  for (let i = 0; i < numberOfCores; i++) {
+    cluster.fork()
+  }
+  cluster.on('online', (worker) => {
+    logger.info(`Worker ${worker.process.pid} is online`)
   })
+  cluster.on('exit', (worker, code, signal) => {
+    logger.info(`Worker ${worker.process.pid} exited with ${code ? code : signal}`)
+  })
+}
+
+bootstrapTypeScriptPaths(tsConfig)
+
+import { app } from './app'
+
+if (cluster.isMaster) {
+  forkListenerProcesses(numberOfCores)
 } else {
-  app.listen(port, () => {
-    logger.info(`Application started: http://localhost:${port}`)
-  })
+  listen(app, port)
 }
