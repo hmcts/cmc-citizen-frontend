c99e2f4a1b1d9648626929028978d3f56d89268c
Rafal Kondratowicz <rafalko@lemon.kainos.com>
2017-10-30 13:59:02 +0000

    Encrypt drafts

--- src/main/services/draftService.ts
+++ src/main/services/draftService.ts
@@ -1,11 +1,28 @@
 import * as config from 'config'
 import request from 'client/request'
-import { DraftService as BaseDraftService } from '@hmcts/draft-store-client'
+import { Draft, DraftService as BaseDraftService, Secrets } from '@hmcts/draft-store-client'
 
 import { ServiceAuthTokenFactoryImpl } from 'common/security/serviceTokenFactoryImpl'
 
 export class DraftService extends BaseDraftService {
+  private secrets?: Secrets
+
   constructor () {
     super(config.get<string>('draft-store.url'), request, new ServiceAuthTokenFactoryImpl())
+
+    const primarySecret: string = config.get<string>('draft-store.secrets.primary')
+    const secondarySecret: string = config.get<string>('draft-store.secrets.secondary')
+
+    if (primarySecret) {
+      this.secrets = new Secrets(primarySecret, secondarySecret)
+    }
+  }
+
+  find<T> (draftType: string, limit: string | any, userToken: string, deserializationFn: (value: any) => T): Promise<Draft<T>[]> {
+    return super.find(draftType, limit, userToken, deserializationFn, this.secrets)
+  }
+
+  save<T> (draft: Draft<T>, userToken: string): Promise<void> {
+    return super.save(draft, userToken, this.secrets)
   }
 }
