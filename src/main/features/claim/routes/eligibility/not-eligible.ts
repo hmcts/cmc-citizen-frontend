8c8f64370f1884c837ac5409a928cdeff7ad53ed
Tim Jacomb <tim.jacomb@hmcts.net>
2017-11-01 15:50:11 +0000

    Add content

--- src/main/features/claim/routes/eligibility/not-eligible.ts
+++ src/main/features/claim/routes/eligibility/not-eligible.ts
@@ -1,11 +1,10 @@
 import * as express from 'express'
 
 import { Paths } from 'claim/paths'
-import { FailureReason } from 'claim/helpers/eligibility/failureReason'
 
 export default express.Router()
   .get(Paths.eligibilityNotEligiblePage.uri, (req: express.Request, res: express.Response): void => {
     res.render(Paths.eligibilityNotEligiblePage.associatedView, {
-      reason_not_eligible: FailureReason.lookup(req.query.reason)
+      reason: req.query.reason
     })
   })
