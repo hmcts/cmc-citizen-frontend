c2cc1db6dd6967291af8469775172fc93d6c00d4
yarpo <jar.patryk@gmail.com>
2017-11-07 10:44:40 +0000

    ROC-2476: Offer accepted and rejected - FE only, no calls to API (yet) (#174)
    * ROC-2476: refactored existing code - using enum constants
    
    * ROC-2476: refactored code
    
    * ROC-2476: Make a legal agreement and declaration pages
    
    * ROC-2476: more unit tests
    
    * ROC-2476: removed  from Settlement class definition
    
    * ROC-2476: accepted and rejected confirmation pages

--- src/test/features/offer/form/models/defendantResponse.ts
+++ src/test/features/offer/form/models/defendantResponse.ts
@@ -5,7 +5,9 @@ import { DefendantResponse, ValidationErrors } from 'offer/form/models/defendant
 import { StatementType } from 'offer/form/models/statementType'
 
 describe('DefendantResponse', () => {
+
   describe('constructor', () => {
+
     it('should set the fields to undefined', () => {
       const defendantResponse = new DefendantResponse()
       expect(defendantResponse.option).to.be.equal(undefined)
@@ -13,6 +15,7 @@ describe('DefendantResponse', () => {
   })
 
   describe('form object deserialization', () => {
+
     it('should return undefined when value is undefined', () => {
       expect(DefendantResponse.fromObject(undefined)).to.be.equal(undefined)
     })
@@ -27,6 +30,7 @@ describe('DefendantResponse', () => {
   })
 
   describe('deserialization', () => {
+
     it('should return instance initialised with defaults given undefined', () => {
       expect(new DefendantResponse().deserialize(undefined)).to.deep.equal(new DefendantResponse())
     })
@@ -37,9 +41,11 @@ describe('DefendantResponse', () => {
   })
 
   describe('validation', () => {
+
     const validator: Validator = new Validator()
 
     describe('should reject when', () => {
+
       it('undefined option', () => {
         const errors = validator.validateSync(new DefendantResponse(undefined))
         expect(errors.length).to.equal(1)
@@ -48,15 +54,19 @@ describe('DefendantResponse', () => {
     })
 
     describe('should accept when', () => {
+
       context('valid form with selected option', () => {
+
         it('Acceptation', () => {
           const errors = validator.validateSync(new DefendantResponse(StatementType.ACCEPTATION))
           expect(errors.length).to.equal(0)
         })
+
         it('Rejection', () => {
           const errors = validator.validateSync(new DefendantResponse(StatementType.REJECTION))
           expect(errors.length).to.equal(0)
         })
+
         it('Make Counter Offer', () => {
           const errors = validator.validateSync(new DefendantResponse(StatementType.OFFER))
           expect(errors.length).to.equal(0)
