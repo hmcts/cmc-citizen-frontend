259f82187c21deb6edec467cdfb3ad7581968c8a
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-11-07 17:05:28 +0000

    Impact of dispute is actually optional

--- src/main/features/response/form/models/impactOfDispute.ts
+++ src/main/features/response/form/models/impactOfDispute.ts
@@ -1,17 +1,10 @@
 import { Serializable } from 'models/serializable'
-import { IsDefined, MaxLength } from 'class-validator'
-import { IsNotBlank } from 'forms/validation/validators/isBlank'
 import { ValidationConstraints } from 'forms/validation/validationConstraints'
 import { ValidationErrors as DefaultValidationErrors } from 'forms/validation/validationErrors'
-
-export class ValidationErrors {
-  static readonly IMPACT_OF_DISPUTE_REQUIRED: string = 'Explain how this dispute has affected you'
-}
+import { MaxLength } from 'forms/validation/validators/maxLengthValidator'
 
 export class ImpactOfDispute implements Serializable<ImpactOfDispute> {
 
-  @IsDefined({ message: ValidationErrors.IMPACT_OF_DISPUTE_REQUIRED })
-  @IsNotBlank({ message: ValidationErrors.IMPACT_OF_DISPUTE_REQUIRED })
   @MaxLength(ValidationConstraints.FREE_TEXT_MAX_LENGTH, { message: DefaultValidationErrors.FREE_TEXT_TOO_LONG })
   text?: string
 
--- src/test/features/response/form/models/impactOfDispute.ts
+++ src/test/features/response/form/models/impactOfDispute.ts
@@ -2,7 +2,7 @@
 /* tslint:disable:no-unused-expression */
 
 import { expect } from 'chai'
-import { ImpactOfDispute, ValidationErrors } from 'response/form/models/impactOfDispute'
+import { ImpactOfDispute } from 'response/form/models/impactOfDispute'
 import { ValidationErrors as DefaultValidationErrors } from 'forms/validation/validationErrors'
 import { Validator } from 'class-validator'
 import { expectValidationError } from '../../../../app/forms/models/validationUtils'
@@ -13,30 +13,22 @@ describe('ImpactOfDispute', () => {
   context('validation', () => {
     const validator: Validator = new Validator()
 
-    it('should be valid when given a non-empty string', () => {
-      const impactOfDispute: ImpactOfDispute = new ImpactOfDispute('non empty text')
-      const errors = validator.validateSync(impactOfDispute)
-      expect(errors).to.be.empty
-    })
-
     it('should be valid when given max amount of characters', () => {
       const impactOfDispute: ImpactOfDispute = new ImpactOfDispute(randomstring.generate(ValidationConstraints.FREE_TEXT_MAX_LENGTH))
       const errors = validator.validateSync(impactOfDispute)
       expect(errors).to.be.empty
     })
 
-    it('should be invalid when given empty string', () => {
+    it('should be valid when given empty string', () => {
       const impactOfDispute: ImpactOfDispute = new ImpactOfDispute('')
       const errors = validator.validateSync(impactOfDispute)
-      expect(errors).to.have.lengthOf(1)
-      expectValidationError(errors, ValidationErrors.IMPACT_OF_DISPUTE_REQUIRED)
+      expect(errors).to.be.empty
     })
 
-    it('should be invalid when given undefined', () => {
+    it('should be valid when given undefined', () => {
       const impactOfDispute: ImpactOfDispute = new ImpactOfDispute(undefined)
       const errors = validator.validateSync(impactOfDispute)
-      expect(errors).to.have.lengthOf(1)
-      expectValidationError(errors, ValidationErrors.IMPACT_OF_DISPUTE_REQUIRED)
+      expect(errors).to.be.empty
     })
 
     it('should be invalid when given too many characters', () => {
