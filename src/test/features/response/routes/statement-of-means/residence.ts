2a51c8ebe511d3d07b9a4c3b45c9cb52d48a226d
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-11-13 18:37:29 +0000

    Add POST route tests

--- src/test/features/response/routes/statement-of-means/residence.ts
+++ src/test/features/response/routes/statement-of-means/residence.ts
@@ -11,10 +11,12 @@ import * as draftStoreServiceMock from '../../../../http-mocks/draft-store'
 
 import { StatementOfMeansPaths as Paths } from 'response/paths'
 import { app } from '../../../../../main/app'
+import { ResidenceType } from 'response/form/models/statement-of-means/residenceType'
+import { ValidationErrors } from 'forms/validation/validationErrors'
 
 const cookieName: string = config.get<string>('session.cookieName')
 
-const whatYouNeedPage = Paths.residencePage.evaluateUri({
+const residencePage = Paths.residencePage.evaluateUri({
   externalId: claimStoreServiceMock.sampleClaimObj.externalId
 })
 
@@ -23,7 +25,7 @@ describe('Statement of means', () => {
     attachDefaultHooks(app)
 
     describe('on GET', () => {
-      checkAuthorizationGuards(app, 'get', whatYouNeedPage)
+      checkAuthorizationGuards(app, 'get', residencePage)
 
       context('when user authorised', () => {
         beforeEach(() => {
@@ -34,7 +36,7 @@ describe('Statement of means', () => {
           claimStoreServiceMock.rejectRetrieveClaimByExternalId('Error')
 
           await request(app)
-            .get(whatYouNeedPage)
+            .get(residencePage)
             .set('Cookie', `${cookieName}=ABC`)
             .expect(res => expect(res).to.be.serverError.withText('Error'))
         })
@@ -44,7 +46,7 @@ describe('Statement of means', () => {
           draftStoreServiceMock.rejectFind()
 
           await request(app)
-            .get(whatYouNeedPage)
+            .get(residencePage)
             .set('Cookie', `${cookieName}=ABC`)
             .expect(res => expect(res).to.be.serverError.withText('Error'))
         })
@@ -54,9 +56,79 @@ describe('Statement of means', () => {
           draftStoreServiceMock.resolveFind('response')
 
           await request(app)
-            .get(whatYouNeedPage)
+            .get(residencePage)
             .set('Cookie', `${cookieName}=ABC`)
             .expect(res => expect(res).to.be.successful.withText('Where are you living?'))
+        })
+      })
+    })
+
+    describe('on POST', () => {
+      const validFormData = {
+        type: ResidenceType.OWN_HOME.value
+      }
+
+      checkAuthorizationGuards(app, 'get', residencePage)
+
+      context('when user authorised', () => {
+        beforeEach(() => {
+          idamServiceMock.resolveRetrieveUserFor('1', 'cmc-private-beta')
+        })
+
+        it('should return error page when unable to retrieve claim', async () => {
+          claimStoreServiceMock.rejectRetrieveClaimByExternalId('Error')
+
+          await request(app)
+            .post(residencePage)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+
+        it('should return error page when unable to retrieve draft', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+          draftStoreServiceMock.rejectFind()
+
+          await request(app)
+            .post(residencePage)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+
+        it('should return error page when unable to save draft', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+          draftStoreServiceMock.resolveFind('response')
+          draftStoreServiceMock.rejectSave()
+
+          await request(app)
+            .post(residencePage)
+            .set('Cookie', `${cookieName}=ABC`)
+            .send(validFormData)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+
+        it('should redirect to dependants page when all is fine and form is valid', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+          draftStoreServiceMock.resolveFind('response')
+          draftStoreServiceMock.resolveSave()
+
+          await request(app)
+            .post(residencePage)
+            .set('Cookie', `${cookieName}=ABC`)
+            .send(validFormData)
+            .expect(res => expect(res).to.be.redirect
+              .toLocation(Paths.dependantsPage
+                .evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId })))
+        })
+
+        it('should trigger validation when all is fine and form is invalid', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+          draftStoreServiceMock.resolveFind('response')
+
+          await request(app)
+            .post(residencePage)
+            .set('Cookie', `${cookieName}=ABC`)
+            .send({ })
+            .expect(res => expect(res).to.be.successful.withText(ValidationErrors.SELECT_AN_OPTION))
         })
       })
     })
