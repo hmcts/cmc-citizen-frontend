d5268c8fefde348a568f6f0eee43035d8908eb0c
Damian Dunajski <damian.dunajski@hmcts.net>
2017-11-15 12:14:51 +0000

    Add new test case for calculateInterest method

--- src/test/app/common/calculateInterest.ts
+++ src/test/app/common/calculateInterest.ts
@@ -7,9 +7,7 @@ import { Interest, InterestType } from 'claim/form/models/interest'
 describe('calculateInterest', () => {
 
   function buildInterest (type: InterestType = InterestType.STANDARD, rate: number = 0, reason: string = undefined): Interest {
-    let interest: Interest = new Interest()
-
-    return interest.deserialize({
+    return new Interest().deserialize({
       type: type,
       rate: rate,
       reason: reason
@@ -17,15 +15,13 @@ describe('calculateInterest', () => {
   }
 
   describe('should return 0 when', () => {
-    describe('amount 0 and', () => {
-      const AMOUNT: number = 0;
-
+    describe('amount is 0 and', () => {
       [100, 0].forEach((rate) => {
-        it(`rate ${rate}`, () => {
+        it(`rate is ${rate}`, () => {
           const interest = buildInterest(InterestType.DIFFERENT, rate)
-          const interestDate = moment().subtract(5, 'years')
+          const interestFromDate = moment().subtract(5, 'years')
 
-          expect(calculateInterest(AMOUNT, interest, interestDate)).to.equal(0)
+          expect(calculateInterest(0, interest, interestFromDate)).to.equal(0)
         })
       })
     })
@@ -33,42 +29,29 @@ describe('calculateInterest', () => {
     describe('interest type is NO_INTEREST and', () => {
       it('amount is greater than 0', () => {
         const interest = buildInterest(InterestType.NO_INTEREST)
-        const interestDate = moment().subtract(5, 'years')
+        const interestFromDate = moment().subtract(5, 'years')
 
-        expect(calculateInterest(100, interest, interestDate)).to.equal(0)
+        expect(calculateInterest(100, interest, interestFromDate)).to.equal(0)
       })
     })
 
-    describe('date is today', () => {
+    describe('date is today and', () => {
       it('amount is greater than 0', () => {
         const interest = buildInterest()
-        const interestDate = moment()
+        const interestFromDate = moment()
 
-        expect(calculateInterest(100, interest, interestDate)).to.equal(0)
-      })
-    })
-
-    describe('interest type is STANDARD', () => {
-      it('amount is greater than 0 and the date is NOT current date', () => {
-        const interest = buildInterest(InterestType.STANDARD)
-        const interestDate = moment().subtract(5, 'years')
-        const interestToDate = moment().subtract(5, 'years')
-        expect(calculateInterest(100, interest, interestDate, interestToDate)).to.equal(0)
+        expect(calculateInterest(100, interest, interestFromDate)).to.equal(0)
       })
     })
   })
 
   describe('should return positive number when', () => {
-
-    let amount: number = 100
-    let EXPECTED_RESULT: number = 40.02
-
     describe('interest type is STANDARD and', () => {
       it('amount is greater than 0', () => {
         const interest = buildInterest(InterestType.STANDARD)
         const interestDate = moment().subtract(5, 'years')
 
-        expect(calculateInterest(amount, interest, interestDate)).to.equal(EXPECTED_RESULT)
+        expect(calculateInterest(100, interest, interestDate)).to.equal(40.02)
       })
     })
 
@@ -77,18 +60,23 @@ describe('calculateInterest', () => {
         const interest = buildInterest(InterestType.DIFFERENT, 8)
         const interestDate = moment().subtract(5, 'years')
 
-        expect(calculateInterest(amount, interest, interestDate)).to.equal(EXPECTED_RESULT)
+        expect(calculateInterest(100, interest, interestDate)).to.equal(40.02)
       })
 
       it('amount is greater than 0 (rate = 20)', () => {
         const interest = buildInterest(InterestType.DIFFERENT, 20)
         const interestDate = moment().subtract(5, 'years')
 
-        let result: number = calculateInterest(amount, interest, interestDate)
-        expect(result).not.to.equal(EXPECTED_RESULT)
-        expect(result).to.equal(100.05)
+        expect(calculateInterest(100, interest, interestDate)).to.equal(100.05)
       })
     })
   })
 
+  it('should calculate interest for any day', () => {
+    const interest = buildInterest(InterestType.DIFFERENT, 8)
+    const interestFromDate = moment().subtract(5, 'years')
+    const interestToDate = moment().subtract(5, 'year').add(1, 'day')
+
+    expect(calculateInterest(100, interest, interestFromDate, interestToDate)).to.equal(0.02)
+  })
 })
