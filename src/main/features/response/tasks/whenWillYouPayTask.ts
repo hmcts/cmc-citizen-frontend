c4574a08d985fe95966ddc7be53862dbe0b162f0
Damian Dunajski <damian.dunajski@hmcts.net>
2017-11-17 18:51:59 +0000

    Use validator in WhenWillYouPayTask and correct test cases

--- src/main/features/response/tasks/whenWillYouPayTask.ts
+++ src/main/features/response/tasks/whenWillYouPayTask.ts
@@ -1,7 +1,12 @@
+import { Validator } from 'class-validator'
+
 import { ResponseDraft } from 'response/draft/responseDraft'
+
+const validator = new Validator()
 
 export class WhenWillYouPayTask {
   static isCompleted (responseDraft: ResponseDraft): boolean {
-    return !!(responseDraft.defendantPaymentOption && responseDraft.defendantPaymentOption.option)
+    return responseDraft.defendantPaymentOption !== undefined
+      && validator.validateSync(responseDraft.defendantPaymentOption).length === 0
   }
 }
--- src/test/features/response/tasks/whenWillYouPayTask.ts
+++ src/test/features/response/tasks/whenWillYouPayTask.ts
@@ -3,33 +3,29 @@ import { expect } from 'chai'
 import { WhenWillYouPayTask } from 'response/tasks/whenWillYouPayTask'
 
 import { ResponseDraft } from 'response/draft/responseDraft'
-import { DefendantPaymentType, DefendantPaymentOption } from 'response/form/models/defendantPaymentOption'
+import { DefendantPaymentOption, DefendantPaymentType } from 'response/form/models/defendantPaymentOption'
 
 describe('WhenWillYouPayTask', () => {
+  it('should not be completed when object is undefined', () => {
+    const draft: ResponseDraft = new ResponseDraft()
+    draft.defendantPaymentOption = undefined
 
-  describe('when response', () => {
-    it('should be completed when option is selected', () => {
-      const draft: ResponseDraft = new ResponseDraft()
-      draft.defendantPaymentOption = new DefendantPaymentOption().deserialize({ option: { value: DefendantPaymentType.INSTALMENTS.value } })
-      expect(WhenWillYouPayTask.isCompleted(draft)).to.equal(true)
-    })
-
-    it('should be completed when option is undefined', () => {
-      const draft: ResponseDraft = new ResponseDraft()
-      draft.defendantPaymentOption = new DefendantPaymentOption().deserialize(undefined)
-
-      expect(WhenWillYouPayTask.isCompleted(draft)).to.equal(false)
-    })
+    expect(WhenWillYouPayTask.isCompleted(draft)).to.equal(false)
   })
 
-  describe('when response', () => {
-    it('should be completed when option is selected', () => {
-      DefendantPaymentType.all().forEach(option => {
-        const draft = new ResponseDraft()
-        draft.defendantPaymentOption = new DefendantPaymentOption(option)
-        expect(WhenWillYouPayTask.isCompleted(draft)).to.equal(true)
-      })
-    })
+  it('should not be completed when payment option is undefined', () => {
+    const draft: ResponseDraft = new ResponseDraft()
+    draft.defendantPaymentOption = new DefendantPaymentOption(undefined)
 
+    expect(WhenWillYouPayTask.isCompleted(draft)).to.equal(false)
+  })
+
+  it('should be completed when option is selected', () => {
+    DefendantPaymentType.all().forEach(defendantPaymentType => {
+      const draft: ResponseDraft = new ResponseDraft()
+      draft.defendantPaymentOption = new DefendantPaymentOption(defendantPaymentType)
+
+      expect(WhenWillYouPayTask.isCompleted(draft)).to.equal(true)
+    })
   })
 })
