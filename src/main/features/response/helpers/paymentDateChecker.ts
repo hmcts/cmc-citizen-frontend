a4642318c10069717dd5ae05c4ec189a44e2802c
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-11-20 11:54:48 +0000

    Use MomentFactory

--- src/main/features/response/helpers/paymentDateChecker.ts
+++ src/main/features/response/helpers/paymentDateChecker.ts
@@ -1,8 +1,9 @@
 import * as moment from 'moment'
+import { MomentFactory } from 'common/momentFactory'
 
 export class PaymentDateChecker {
   static isLaterThan28DaysFromNow (date: moment.Moment): boolean {
-    const twentyEightDaysFromNow: moment.Moment = moment().add(28, 'days')
+    const twentyEightDaysFromNow: moment.Moment = MomentFactory.currentDate().add(28, 'days')
     return date.isAfter(twentyEightDaysFromNow)
   }
 }
