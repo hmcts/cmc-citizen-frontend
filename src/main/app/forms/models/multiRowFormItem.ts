94ff5f7181fd034f9f8f972ee19fb0257e696a69
Patryk Jar <jar.patryk@gmail.com>
2017-11-24 13:09:22 +0000

    ROC-2559: refactored MultiRowFormItem.isEmpty

--- src/main/app/forms/models/multiRowFormItem.ts
+++ src/main/app/forms/models/multiRowFormItem.ts
@@ -3,10 +3,6 @@ export abstract class MultiRowFormItem {
   abstract deserialize (input?: any): MultiRowFormItem
 
   isEmpty (): boolean {
-    const that = this
-    let result: boolean = true
-    Object.keys(this).forEach(key => result = result && !that[key])
-
-    return result
+    return Object.values(this).every(value => !value)
   }
 }
