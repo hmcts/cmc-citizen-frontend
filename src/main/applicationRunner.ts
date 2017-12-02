684a510f8b4292ebf5ca1cea632fc62046b8e543
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-12-02 00:49:19 +0000

    Extract listening code to ApplicationRunner

---
+++ src/main/applicationRunner.ts
@@ -0,0 +1,44 @@
+import * as express from 'express'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as https from 'https'
+
+const logger = require('@hmcts/nodejs-logging').getLogger('applicationRunner')
+
+export class ApplicationRunner {
+  static run (app: express.Application): void {
+    const port = ApplicationRunner.applicationPort()
+    if (app.locals.ENV === 'development' || app.locals.ENV === 'dockertests') {
+      const server = https.createServer(ApplicationRunner.getSSLOptions(), app)
+      server.listen(port, () => {
+        logger.info(`Listener started (PID ${process.pid}): https://localhost:${port}`)
+      })
+    } else {
+      app.listen(port, () => {
+        logger.info(`Listener started (PID ${process.pid}): http://localhost:${port}`)
+      })
+    }
+  }
+
+  private static getSSLOptions (): any {
+    const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
+    return {
+      key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
+      cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt'))
+    }
+  }
+
+  /**
+   * Return type number or string because Azure PaaS communicates with the application
+   * through a named pipe and not a TCP port.
+   */
+  private static applicationPort (): number | string {
+    const defaultPort = 3000
+    let port: number | string = process.env.PORT
+    if (port === undefined) {
+      logger.info(`Port value was not set using PORT env variable, using the default of ${defaultPort}`)
+      port = defaultPort
+    }
+    return port
+  }
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -3,52 +3,13 @@
 import './ts-paths-bootstrap'
 import * as config from 'config'
 import * as toBoolean from 'to-boolean'
-import * as express from 'express'
-import * as fs from 'fs'
-import * as path from 'path'
-import * as https from 'https'
 
 import { app } from './app'
 import { ApplicationCluster } from './applicationCluster'
-
-const logger = require('@hmcts/nodejs-logging').getLogger('server')
-
-const defaultPort = 3000
-
-function listen (app: express.Application, port: number) {
-  if (app.locals.ENV === 'development' || app.locals.ENV === 'dockertests') {
-    const server = https.createServer(getSSLOptions(), app)
-    server.listen(port, () => {
-      logger.info(`Listener started (PID ${process.pid}): https://localhost:${port}`)
-    })
-  } else {
-    app.listen(port, () => {
-      logger.info(`Listener started (PID ${process.pid}): http://localhost:${port}`)
-    })
-  }
-}
-
-function getSSLOptions (): any {
-  const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
-  return {
-    key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
-    cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt'))
-  }
-}
-
-function applicationPort (): any {
-  let port: any = process.env.PORT
-  if (port === undefined) {
-    logger.info(`Port value was not set using PORT env variable, using the default of ${defaultPort}`)
-    port = defaultPort
-  }
-  return port
-}
-
-const entryPoint = () => listen(app, applicationPort())
+import { ApplicationRunner } from './applicationRunner'
 
 if (toBoolean(config.get<boolean>('featureToggles.clusterMode'))) {
-  ApplicationCluster.execute(entryPoint)
+  ApplicationCluster.execute(() => ApplicationRunner.run(app))
 } else {
-  entryPoint()
+  ApplicationRunner.run(app)
 }
