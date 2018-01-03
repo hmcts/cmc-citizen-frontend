46dd35e4a431a15d9a0115eab0666f44b077eb4c
Damian Dunajski <damian.dunajski@hmcts.net>
2018-01-03 17:12:38 +0100

    Add function to assert number of validation errors with readable error message

--- src/test/app/forms/models/validationUtils.ts
+++ src/test/app/forms/models/validationUtils.ts
@@ -9,14 +9,18 @@ class Violation {
   }
 }
 
+export function expectNumberOfValidationErrors (errors: ValidationError[], expectation: number) {
+  expect(errors.length).to.be.equal(expectation, `Number of errors found (${errors.length}) is not equal ${expectation}. The following errors has been triggered: ${stringifyViolations(extractViolationsFrom(errors))}\n`)
+}
+
 export function expectValidationError (errors: ValidationError[], message: string) {
-  const messages: string[] = extractViolationsFrom(errors).map(violation => violation.message)
-  expect(messages).to.contain(message, `Error '${message}' has not been triggered. The following errors has been triggered instead: ${messages}`)
+  const violations: Violation[] = extractViolationsFrom(errors)
+  expect(violations.map(violation => violation.message)).to.include(message, `Error '${message}' has not been found. The following errors has been triggered: ${stringifyViolations(violations)}\n`)
 }
 
 export function expectPropertyValidationError (errors: ValidationError[], property: string, message: string) {
   const violations: Violation[] = extractViolationsFrom(errors)
-  expect(violations).to.deep.include(new Violation(property, message))
+  expect(violations).to.deep.include(new Violation(property, message), `Error '${message}' on property '${property}' has not been found. The following errors has been triggered: ${stringifyViolations(violations)}\n`)
 }
 
 function extractViolationsFrom (errors: ValidationError[], parentProperty?: string): Violation[] {
@@ -33,6 +37,32 @@ function extractViolationsFrom (errors: ValidationError[], parentProperty?: stri
       }
     })
   )
+}
+
+/**
+ * Returns string containing list of violations grouped by property e.g.
+ *
+ * <ul>
+ *  <li>property 'date':
+ *   <ul>
+ *     <li>'Enter date before 3 January 2018'</li>
+ *   </ul>
+ *  </li>
+ *  <li>property 'text':
+ *   <ul>
+ *     <li>'Explain why you don’t owe the full amount'</li>
+ *     <li>'You’ve entered too many characters'</li>
+ *   </ul>
+ *  </li>
+ * </ul>
+ */
+function stringifyViolations (violations: Violation[]): string {
+  return _(violations)
+    .groupBy((violation: Violation) => violation.property)
+    .map((violations: Violation[], property: string) => {
+      return `\n - property '${property}': ${violations.map(violation => `\n  - '${violation.message}'`)}`
+    })
+    .join('')
 }
 
 export function generateString (length: number): string {
