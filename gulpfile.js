6a3f509c5d5471d9965e5014272fffbf8be14cfe
Damian Dunajski <damian.dunajski@hmcts.net>
2018-01-07 14:19:03 +0100

    Stop restaring application after JS file change

--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,7 @@ gulp.task('develop', () => {
       cert: fs.readFileSync(path.join(__dirname, 'src', 'main', 'resources', 'localhost-ssl', 'localhost.crt'), 'utf-8'),
     })
     nodemon({
-      ext: 'ts js po',
+      ext: 'ts po',
       stdout: true
     }).on('readable', () => {
       this.stdout.on('data', function (chunk) {
