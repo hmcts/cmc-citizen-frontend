45c2c6a50deb16a843b140a531ad483b52a79313
Tim Jacomb <tim.jacomb@hmcts.net>
2018-01-08 09:58:32 +0000

    Remove hopefully unnecessary code

--- src/main/app/idam/jwtExtractor.ts
+++ src/main/app/idam/jwtExtractor.ts
@@ -3,12 +3,7 @@ import * as config from 'config'
 export class JwtExtractor {
 
   static extract (req: any): string {
-    if (!req) {
-      return undefined
-    }
-    if (req.cookies) {
-      return req.cookies[config.get<string>('session.cookieName')]
-    }
+    return req.cookies[config.get<string>('session.cookieName')]
   }
 
 }
