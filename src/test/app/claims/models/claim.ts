a285f23920dd0bac60b96d44acd70a39eaf9de6b
Patryk Jar <jar.patryk@gmail.com>
2018-01-11 15:37:48 +0000

    ROC-2572: calculate totla amount on server side

--- src/test/app/claims/models/claim.ts
+++ src/test/app/claims/models/claim.ts
@@ -5,72 +5,8 @@ import { Settlement } from 'claims/models/settlement'
 import { StatementType } from 'offer/form/models/statementType'
 import { MadeBy } from 'offer/form/models/madeBy'
 import { Offer } from 'claims/models/offer'
-import { InterestDate } from 'claims/models/interestDate'
-import { ClaimData } from 'claims/models/claimData'
-import { paymentOf } from '../../../data/entity/payment'
-import { breakdownOf } from '../../../data/entity/amount-breakdown'
-import { Interest, InterestType } from 'claim/form/models/interest'
-import { InterestDateType } from 'app/common/interestDateType'
-import moment = require('moment')
 
 describe('Claim', () => {
-
-  context('totalAmountTillToday', () => {
-    const claim = new Claim()
-    claim.claimData = new ClaimData()
-    claim.claimData.amount = breakdownOf(100)
-    claim.claimData.payment = paymentOf(25)
-
-    it('should return total of claim amount and payment amount if interest is not claimed', () => {
-      claim.claimData.interest = new Interest(InterestType.NO_INTEREST)
-
-      expect(claim.totalAmountTillToday).to.be.equal(125)
-    })
-
-    it('should return total of claim amount, payment amount and interest calculated from date of issue till today if interest is claimed from submission date', () => {
-      claim.claimData.interest = new Interest(InterestType.STANDARD)
-      claim.claimData.interestDate = new InterestDate(InterestDateType.SUBMISSION)
-      claim.createdAt = moment().subtract(10, 'days')
-
-      expect(claim.totalAmountTillToday).to.equal(125.22)
-    })
-
-    it('should return total of claim amount, payment amount and interest calculated from selected date till today if interest is claimed from selected date', () => {
-      claim.claimData.interest = new Interest(InterestType.STANDARD)
-      claim.claimData.interestDate = new InterestDate(InterestDateType.CUSTOM, moment().subtract(10, 'days'))
-
-      expect(claim.totalAmountTillToday).to.equal(125.22)
-    })
-  })
-
-  context('totalAmountTillDateOfIssue', () => {
-    const claim = new Claim()
-    claim.claimData = new ClaimData()
-    claim.claimData.amount = breakdownOf(100)
-    claim.claimData.payment = paymentOf(25)
-
-    it('should return total of claim amount and payment amount if interest is not claimed', () => {
-      claim.claimData.interest = new Interest(InterestType.NO_INTEREST)
-
-      expect(claim.totalAmountTillDateOfIssue).to.be.equal(125)
-    })
-
-    it('should return total of claim amount and payment amount if interest is claimed from submission date', () => {
-      claim.claimData.interest = new Interest(InterestType.STANDARD)
-      claim.claimData.interestDate = new InterestDate(InterestDateType.SUBMISSION)
-      claim.createdAt = moment().subtract(10, 'days')
-
-      expect(claim.totalAmountTillDateOfIssue).to.equal(125)
-    })
-
-    it('should return total of claim amount, payment amount and interest calculated from selected date till today if interest is claimed from selected date', () => {
-      claim.claimData.interest = new Interest(InterestType.STANDARD)
-      claim.claimData.interestDate = new InterestDate(InterestDateType.CUSTOM, moment().subtract(10, 'days'))
-      claim.createdAt = moment()
-
-      expect(claim.totalAmountTillDateOfIssue).to.equal(125.22)
-    })
-  })
 
   describe('eligibleForCCJ', () => {
     const claim = new Claim()
