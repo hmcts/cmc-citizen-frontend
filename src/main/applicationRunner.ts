05697af0755813ed52bbd6fd14ce8cfcd8719c1e
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-01-12 11:57:39 +0000

    Force HTTP listener

--- src/main/applicationRunner.ts
+++ src/main/applicationRunner.ts
@@ -1,7 +1,7 @@
 import * as express from 'express'
-import * as fs from 'fs'
-import * as path from 'path'
-import * as https from 'https'
+// import * as fs from 'fs'
+// import * as path from 'path'
+// import * as https from 'https'
 import { Logger } from '@hmcts/nodejs-logging'
 
 const logger = Logger.getLogger('applicationRunner')
@@ -9,25 +9,25 @@ const logger = Logger.getLogger('applicationRunner')
 export class ApplicationRunner {
   static run (app: express.Application): void {
     const port = ApplicationRunner.applicationPort()
-    if (app.locals.ENV === 'development' || app.locals.ENV === 'dockertests') {
-      const server = https.createServer(ApplicationRunner.getSSLOptions(), app)
-      server.listen(port, () => {
-        logger.info(`Listener started (PID ${process.pid}): https://localhost:${port}`)
-      })
-    } else {
-      app.listen(port, () => {
-        logger.info(`Listener started (PID ${process.pid}): http://localhost:${port}`)
-      })
-    }
+    // if (app.locals.ENV === 'development' || app.locals.ENV === 'dockertests') {
+    //   const server = https.createServer(ApplicationRunner.getSSLOptions(), app)
+    //   server.listen(port, () => {
+    //     logger.info(`Listener started (PID ${process.pid}): https://localhost:${port}`)
+    //   })
+    // } else {
+    app.listen(port, () => {
+      logger.info(`Listener started (PID ${process.pid}): http://localhost:${port}`)
+    })
+    // }
   }
 
-  private static getSSLOptions (): any {
-    const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
-    return {
-      key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
-      cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt'))
-    }
-  }
+  // private static getSSLOptions (): any {
+  //   const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
+  //   return {
+  //     key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
+  //     cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt'))
+  //   }
+  // }
 
   /**
    * Return type is string because Azure PaaS communicates with the application
