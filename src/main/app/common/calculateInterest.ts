4354864894a4afe967484ccecd27b30b5173541d
Patryk Jar <jar.patryk@gmail.com>
2018-01-12 16:13:54 +0000

    ROC-2572: refactoring

--- src/main/app/common/calculateInterest.ts
+++ src/main/app/common/calculateInterest.ts
@@ -3,7 +3,7 @@ import { Moment } from 'moment'
 import { MomentFactory } from 'common/momentFactory'
 import { InterestRateClient } from 'claims/interestRateClient'
 
-const STANDARD_RATE: number = 8.0
+const STANDARD_RATE = 8.0
 
 function getRate (interest: Interest): number {
   if (interest.type === InterestType.NO_INTEREST) {
