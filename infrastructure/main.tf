c6d5e1d41d956dbbdf8f45121dce9adb3e9274b0
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-01-16 11:49:44 +0000

    Inject secrects and values

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,29 @@
+provider "vault" {
+  //  # It is strongly recommended to configure this provider through the
+  //  # environment variables described above, so that each user can have
+  //  # separate credentials set in the environment.
+  //  #
+  //  # This will default to using $VAULT_ADDR
+  //  # But can be set explicitly
+  address = "https://vault.reform.hmcts.net:6200"
+}
+
+data "vault_generic_secret" "s2s_secret" {
+  path = "secret/test/ccidam/service-auth-provider/api/microservice-keys/cmc"
+}
+
+data "vault_generic_secret" "draft_store_secret" {
+  path = "secret/test/cmc/draft-store/encryption-secrets/citizen-frontend"
+}
+
+data "vault_generic_secret" "postcode-lookup-api-key" {
+  path = "secret/test/cmc/postcode-lookup/api-key"
+}
+
+data "vault_generic_secret" "oauth-client-secret" {
+  path = "secret/test/ccidam/idam-api/oauth2/client-secrets/cmc-citizen"
+}
+
 locals {
   aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
 }
@@ -23,10 +49,16 @@ module "citizen-frontend" {
     REFORM_SERVICE_NAME = "${var.microservice}"
     REFORM_ENVIRONMENT = "${var.env}"
 
+    // Application vars
+    GA_TRACKING_ID = "${var.ga-tracking-id}"
+    POSTCODE_LOOKUP_API_KEY = "${data.vault_generic_secret.s2s_secret.data["value"]}"
+
     // IDAM
     IDAM_API_URL = "${var.idam-api-url}"
     IDAM_AUTHENTICATION_WEB_URL = "${var.authentication-web-url}"
     IDAM_S2S_AUTH = "${var.service-2-service-auth-url}"
+    IDAM_S2S_TOTP_SECRET = "${data.vault_generic_secret.postcode-lookup-api-key.data["value"]}"
+    OAUTH_CLIENT_SECRET = "${data.vault_generic_secret.oauth-client-secret.data["value"]}"
 
     // Payments API
     PAY_URL = "${var.payments-api-url}"
@@ -36,9 +68,17 @@ module "citizen-frontend" {
 
     // Draft Store API
     DRAFT_STORE_URL = "${var.draft-store-api-url}"
+    DRAFT_STORE_SECRET_PRIMARY = "${data.vault_generic_secret.draft_store_secret.data["primary"]}"
+    DRAFT_STORE_SECRET_SECONDARY = "${data.vault_generic_secret.draft_store_secret.data["secondary"]}"
 
     // Our service dependencies
     CLAIM_STORE_URL = "http://cmc-claim-store-${var.env}.service.${local.aseName}.internal"
     PDF_SERVICE_URL = "http://cmc-pdf-service-${var.env}.service.${local.aseName}.internal"
+
+    // Feature toggles
+    FEATURE_TESTING_SUPPORT = "true"
+    FEATURE_CCJ = "true"
+    FEATURE_OFFER = "true"
+    FEATURE_STATEMENT_OF_MEANS = "true"
   }
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,25 +1,19 @@
 // Infrastructural variables
 variable "product" {
-  type    = "string"
   default = "cmc"
 }
 
 variable "microservice" {
-  type = "string"
   default = "citizen-frontend"
 }
 
 variable "location" {
-  type    = "string"
   default = "UK South"
 }
 
-variable "env" {
-  type = "string"
-}
+variable "env" { }
 
 variable "ssenv" {
-  type = "string"
   default = "prd"
 }
 
@@ -27,36 +21,33 @@ variable "ilbIp"{}
 
 // Module configuration
 variable "node-uv-threadpool-size" {
-  type = "string"
   default = "64"
 }
 
+variable "ga-tracking-id" {
+  default = "UA-97111056-1"
+}
+
 variable "draft-store-api-url" {
-  type = "string"
   default = "https://testdraftstorelb.moneyclaim.reform.hmcts.net:4302"
 }
 
 variable "payments-api-url" {
-  type = "string"
   default = "https://test.payments.reform.hmcts.net:4421"
 }
 
 variable "fees-api-url" {
-  type = "string"
   default = "https://test.fees-register.reform.hmcts.net:4431"
 }
 
 variable "idam-api-url" {
-  type = "string"
   default = "http://betaDevBccidamAppLB.reform.hmcts.net:4551"
 }
 
 variable "authentication-web-url" {
-  type = "string"
   default = "https://idam-test.dev.ccidam.reform.hmcts.net"
 }
 
 variable "service-2-service-auth-url" {
-  type = "string"
   default = "http://betaDevBccidamAppLB.reform.hmcts.net:4552"
 }
