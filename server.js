cca20df7d7cdf0018eb5b11dd622fdd5f18737a0
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-01-18 12:50:37 +0000

    Add debugging information

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const { execSync } = require('child_process')
+const { StringDecoder } = require('string_decoder')
+const output = execSync('pwd')
+console.log(`>>> ${new StringDecoder('utf8').write(output)}`)
+
 process.env.TS_NODE_FAST = "true"
 require('ts-node/register')
 require('./src/main/server')
