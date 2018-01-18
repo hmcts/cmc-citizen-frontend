2bdaa1751dab8c47adf18910beca1ca224a1be81
Robert Parkinson <robertparkinson@users.noreply.github.com>
2018-01-18 10:10:26 +0000

    ROC-3053 Postcode lookup improvements required (#314)
    * update to use formatted address for postcode lookup
    
    * add line 3 to missing tests
    
    * Add content into line 3 to fix breaking test
    
    * add in missing line 3

--- src/test/features/claim/routes/defendant-soleTrader-details.ts
+++ src/test/features/claim/routes/defendant-soleTrader-details.ts
@@ -17,7 +17,7 @@ const cookieName: string = config.get<string>('session.cookieName')
 const input = {
   type: 'soleTrader',
   name: 'John Smith',
-  address: { line1: 'Apartment 99', line2: '', city: 'London', postcode: 'SE28 0JE' } as Address,
+  address: { line1: 'Apartment 99', line2: '', line3: '', city: 'London', postcode: 'SE28 0JE' } as Address,
   hasCorrespondenceAddress: false,
   businessName: 'businessName'
 } as SoleTraderDetails
@@ -61,7 +61,7 @@ describe('defendant as soleTrader details page', () => {
           draftStoreServiceMock.resolveFind('claim')
         })
         it('line 1 is missing', async () => {
-          const invalidAddressInput = { ...input, ...{ address: { line1: '', line2: '', city: 'London', postcode: 'SE28 0JE' } } }
+          const invalidAddressInput = { ...input, ...{ address: { line1: '', line2: '', line3: '', city: 'London', postcode: 'SE28 0JE' } } }
           await request(app)
             .post(ClaimPaths.defendantSoleTraderOrSelfEmployedDetailsPage.uri)
             .set('Cookie', `${cookieName}=ABC`)
@@ -69,7 +69,7 @@ describe('defendant as soleTrader details page', () => {
             .expect(res => expect(res).to.be.successful.withText('Their details', 'div class="error-summary"', 'Enter first address line'))
         })
         it('postcode is missing', async () => {
-          const invalidAddressInput = { ...input, ...{ address: { line1: 'Apartment 99', line2: '', city: 'London', postcode: '' } } }
+          const invalidAddressInput = { ...input, ...{ address: { line1: 'Apartment 99', line2: '', line3: '', city: 'London', postcode: '' } } }
           await request(app)
             .post(ClaimPaths.defendantSoleTraderOrSelfEmployedDetailsPage.uri)
             .set('Cookie', `${cookieName}=ABC`)
@@ -83,7 +83,7 @@ describe('defendant as soleTrader details page', () => {
           draftStoreServiceMock.resolveFind('claim')
         })
         it('line 1 is missing', async () => {
-          const invalidCorrespondenceAddressInput = { ...input, ...{ hasCorrespondenceAddress: 'true', correspondenceAddress: { line1: '', line2: '', city: 'London', postcode: 'SE28 0JE' } } }
+          const invalidCorrespondenceAddressInput = { ...input, ...{ hasCorrespondenceAddress: 'true', correspondenceAddress: { line1: '', line2: '', line3: '', city: 'London', postcode: 'SE28 0JE' } } }
           await request(app)
             .post(ClaimPaths.defendantSoleTraderOrSelfEmployedDetailsPage.uri)
             .set('Cookie', `${cookieName}=ABC`)
@@ -91,7 +91,7 @@ describe('defendant as soleTrader details page', () => {
             .expect(res => expect(res).to.be.successful.withText('Their details', 'div class="error-summary"', 'Enter first correspondence address line'))
         })
         it('postcode is missing', async () => {
-          const invalidCorrespondenceAddressInput = { ...input, ...{ hasCorrespondenceAddress: 'true', correspondenceAddress: { line1: 'Apartment 99', line2: '', city: 'London', postcode: '' } } }
+          const invalidCorrespondenceAddressInput = { ...input, ...{ hasCorrespondenceAddress: 'true', correspondenceAddress: { line1: 'Apartment 99', line2: '', line3: '', city: 'London', postcode: '' } } }
           await request(app)
             .post(ClaimPaths.defendantSoleTraderOrSelfEmployedDetailsPage.uri)
             .set('Cookie', `${cookieName}=ABC`)
