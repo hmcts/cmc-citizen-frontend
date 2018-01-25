3d8601394e3a92b6045e6dfefcddbeb31c39ae99
Robert Parkinson <robert.parkinson@hmcts.net>
2018-01-25 15:11:34 +0000

    Revert "ROC-3020 Add auth for pdf receipt"
    This reverts commit b168f7ad34c6ccab8075a5bc0d24d95778d95ff9.

--- src/main/app/documents/documentsClient.ts
+++ src/main/app/documents/documentsClient.ts
@@ -2,7 +2,6 @@ import * as config from 'config'
 import { requestNonPromise } from 'client/request'
 import { Request } from 'request'
 import { StringUtils } from 'utils/stringUtils'
-import { User } from 'idam/user'
 
 const claimStoreBaseUrl = config.get<string>('claim-store.url')
 
@@ -11,35 +10,28 @@ export class DocumentsClient {
   constructor (public documentsUrl: string = `${claimStoreBaseUrl}/documents`) {
   }
 
-  getClaimIssueReceiptPDF (claimExternalId: string, user: User): Request {
-    return this.getPDF(claimExternalId, 'claimIssueReceipt', user.bearerToken)
+  getClaimIssueReceiptPDF (claimExternalId: string): Request {
+    return this.getPDF(claimExternalId, 'claimIssueReceipt')
   }
 
-  getDefendantResponseReceiptPDF (claimExternalId: string, user: User): Request {
-    return this.getPDF(claimExternalId, 'defendantResponseReceipt', user.bearerToken)
+  getDefendantResponseReceiptPDF (claimExternalId: string): Request {
+    return this.getPDF(claimExternalId, 'defendantResponseReceipt')
   }
 
-  getSettlementAgreementPDF (claimExternalId: string, user: User): Request {
-    return this.getPDF(claimExternalId, 'settlementAgreement', user.bearerToken)
+  getSettlementAgreementPDF (claimExternalId: string): Request {
+    return this.getPDF(claimExternalId, 'settlementAgreement')
   }
 
-  private getPDF (claimExternalId: string, documentTemplate: string, bearerToken: string): Request {
+  private getPDF (claimExternalId: string, documentTemplate: string): Request {
     if (StringUtils.isBlank(claimExternalId)) {
       throw new Error('Claim external ID cannot be blank')
     }
     if (StringUtils.isBlank(documentTemplate)) {
       throw new Error('Document template cannot be blank')
     }
-    if (StringUtils.isBlank(bearerToken)) {
-      throw new Error('User authorisation cannot be blank')
-    }
-    return requestNonPromise.get(
-      `${this.documentsUrl}/${documentTemplate}/${claimExternalId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${bearerToken}`
-        }
-      }
-    )
+    return requestNonPromise.get({
+      uri: `${this.documentsUrl}/${documentTemplate}/${claimExternalId}`
+    })
   }
+
 }
