decd4ea1a4084146f5b6645e0fe3accf06261ef5
jacmurphy <jack.murphy@hmcts.net>
2018-01-26 11:16:16 +0000

    Fixed failing tests

--- src/main/features/response/form/models/statement-of-means/amountDescriptionRow.ts
+++ src/main/features/response/form/models/statement-of-means/amountDescriptionRow.ts
@@ -8,18 +8,10 @@ import { MaxLength } from 'forms/validation/validators/maxLengthValidator'
 import { IsNotBlank } from 'forms/validation/validators/isBlank'
 
 export class ValidationErrors {
-  static readonly DESCRIPTION_REQUIRED: string = 'Enter a name for the income you added'
-  static readonly AMOUNT_REQUIRED: string = 'Enter an amount for the income you added'
+  static readonly DESCRIPTION_REQUIRED: string = 'Enter a description for the income you added'//   static readonly AMOUNT_REQUIRED: string = 'Enter an amount for the income you added'
   static readonly AMOUNT_INVALID_DECIMALS: string = 'Enter valid amount, maximum two decimal places for the income you added'
   static readonly POSITIVE_NUMBER_REQUIRED: string = 'Enter a number higher than 0 for the field you added'
-}
-
-export class ValidationConstraints {
-  static readonly STANDARD_TEXT_INPUT_MAX_LENGTH: string = 'Enter a name for the income you added'
-  static readonly AMOUNT_REQUIRED: string = 'Enter an amount for the income you added'
-  static readonly AMOUNT_INVALID_DECIMALS: string = 'Enter valid amount, maximum two decimal places for the income you added'
-  static readonly POSITIVE_NUMBER_REQUIRED: string = 'Enter a number higher than 0 for the field you added'
-  static readonly TEXT_TOO_LONG: string = 'You’ve entered too many characters for the income you added'
+  static readonly TEXT_TOO_LONG: string = 'You’ve entered too many characters for the field you added'
 }
 
 export class AmountDescriptionRow extends MultiRowFormItem {
@@ -27,13 +19,13 @@ export class AmountDescriptionRow extends MultiRowFormItem {
   @ValidateIf(o => o.isAtLeastOneFieldPopulated())
   @IsDefined({ message: ValidationErrors.DESCRIPTION_REQUIRED })
   @IsNotBlank({ message: ValidationErrors.DESCRIPTION_REQUIRED })
-  @MaxLength(ValidationConstraints.STANDARD_TEXT_INPUT_MAX_LENGTH, { message: ValidationErrors.TEXT_TOO_LONG })
+  @MaxLength(ValidationConstraints.STANDARD_TEXT_INPUT_MAX_LENGTH, { message: GlobalValidationErrors.TEXT_TOO_LONG })
   description?: string
 
   @ValidateIf(o => o.isAtLeastOneFieldPopulated())
-  @IsDefined({ message: ValidationErrors.AMOUNT_REQUIRED })
-  @Fractions(0, 2, { message: ValidationErrors.AMOUNT_INVALID_DECIMALS })
-  @Min(1, { message: ValidationErrors.POSITIVE_NUMBER_REQUIRED })
+  @IsDefined({ message: GlobalValidationErrors.AMOUNT_REQUIRED })
+  @Fractions(0, 2, { message: GlobalValidationErrors.AMOUNT_INVALID_DECIMALS })
+  @Min(1, { message: GlobalValidationErrors.POSITIVE_NUMBER_REQUIRED })
   amount?: number
 
   constructor (description?: string, amount?: number) {
