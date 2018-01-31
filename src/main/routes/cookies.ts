881dbd219e19069ceb2ba129e1c2c2d63fb28769
jacmurphy <jack.murphy@hmcts.net>
2018-01-31 14:48:05 +0000

    Added blockers so that if the feature toggle is off, they cant access the page

--- src/main/routes/cookies.ts
+++ src/main/routes/cookies.ts
@@ -1,9 +1,12 @@
 import * as express from 'express'
 
 import { Paths as AppPaths } from 'app/paths'
+import { FeatureToggleGuard } from 'guards/featureToggleGuard'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
-  .get(AppPaths.cookiesPage.uri, function (req, res) {
-    res.render(AppPaths.cookiesPage.associatedView)
-  })
+  .get(AppPaths.cookiesPage.uri,
+    FeatureToggleGuard.featureEnabledGuard('finePrint'),
+    function (req, res) {
+      res.render(AppPaths.cookiesPage.associatedView)
+    })
--- src/main/routes/terms-and-conditions.ts
+++ src/main/routes/terms-and-conditions.ts
@@ -1,9 +1,12 @@
 import * as express from 'express'
 
 import { Paths as AppPaths } from 'app/paths'
+import { FeatureToggleGuard } from 'guards/featureToggleGuard'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
-  .get(AppPaths.termsAndConditionsPage.uri, function (req, res) {
-    res.render(AppPaths.termsAndConditionsPage.associatedView)
-  })
+  .get(AppPaths.termsAndConditionsPage.uri,
+    FeatureToggleGuard.featureEnabledGuard('finePrint'), ,
+    function (req, res) {
+      res.render(AppPaths.termsAndConditionsPage.associatedView)
+    })
