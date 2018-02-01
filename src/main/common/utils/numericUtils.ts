0401a808797a46d685704a46817da15a1d4f0f78
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-02-01 15:58:42 +0000

    actoioned review comments

--- src/main/common/utils/numericUtils.ts
+++ src/main/common/utils/numericUtils.ts
@@ -1,4 +1,20 @@
 import * as numeral from 'numeral'
+import * as _ from 'lodash'
+
+function convert (strVal: string) {
+  // checks length, followed by a filter by not containing ',' or valid thousand separators
+
+  if (strVal.length > 0) {
+
+    if ((!strVal.includes(',') || containsAThousandSeparator(strVal))) {
+      return numeral(strVal).value()
+    } else {
+      return _.toNumber(strVal)
+    }
+  } else {
+    return undefined
+  }
+}
 
 export function toNumberOrUndefined (value: any): number {
   if ([undefined, NaN, '', null, false].indexOf(value) !== -1) {
@@ -11,18 +27,14 @@ export function toNumberOrUndefined (value: any): number {
 
   const strVal: string = value && value.toString().trim()
 
-  // checks length, followed by a filter by not containing ',' or valid thousand separators
-  const numberVal: number = strVal.length > 0
-    ? (!strVal.includes(',') || containsAThousandSeparator(strVal))
-      ? numeral(strVal).value()
-      : strVal
-    : undefined
+  // convert to number
+  const numberVal = convert(strVal)
 
   // checks whether is null or not a number then undefined else  number
   return !numberVal || isNaN(numberVal) ? undefined : numberVal
 }
 
 function containsAThousandSeparator (input: string): boolean {
-  const matchedArray = input.match(/^[1-9]\d{0,2}(\,\d{3})+(\.\d*)?$/)
-  return matchedArray ? matchedArray.length > 0 : false
+  const THOUSAND_SEPERATOR_REGULAR_EXPRESSION = /^[1-9]\d{0,2}(\,\d{3})+(\.\d*)?$/
+  return THOUSAND_SEPERATOR_REGULAR_EXPRESSION.test(input.trim())
 }
