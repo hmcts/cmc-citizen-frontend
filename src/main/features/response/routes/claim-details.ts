473a59adc6c0c57d337a2332668ed900a9caa506
Robert Parkinson <robert.parkinson@hmcts.net>
2018-02-02 13:23:57 +0000

    use money coverter class

--- src/main/features/response/routes/claim-details.ts
+++ src/main/features/response/routes/claim-details.ts
@@ -3,13 +3,14 @@ import { Paths } from 'response/paths'
 import { Claim } from 'claims/models/claim'
 import { getInterestDetails } from 'common/interest'
 import { MomentFactory } from 'common/momentFactory'
+import { MoneyConverter } from 'fees/moneyConverter'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
   .get(Paths.claimDetailsPage.uri, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
       const claim: Claim = res.locals.claim
-      const feeAmount: number = claim.claimData.feeAmountInPennies / 100
+      const feeAmount: number = MoneyConverter.convertPenniesToPounds(claim.claimData.feeAmountInPennies)
       res.render(Paths.claimDetailsPage.associatedView, {
         feeAmount: feeAmount,
         interestData: await getInterestDetails(claim),
