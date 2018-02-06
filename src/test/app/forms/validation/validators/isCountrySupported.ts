614f0fbc423cc7348656a017aafe9d41722146b0
Robert Parkinson <robert.parkinson@hmcts.net>
2018-02-06 12:12:55 +0000

    Add mocking for postcode lookup

--- src/test/app/forms/validation/validators/isCountrySupported.ts
+++ src/test/app/forms/validation/validators/isCountrySupported.ts
@@ -2,6 +2,11 @@ import { expect } from 'chai'
 import { CheckCountryConstraint } from 'forms/validation/validators/isCountrySupported'
 import { Country } from 'app/common/country'
 import { ValidationArguments } from 'class-validator'
+import * as nock from 'nock'
+import { mockAddressResponse } from '../../../../data/entity/mockAddressResponse'
+import { mockPostcodeLookupResponse, mockInvalidPostcodeLookupResponse, mockScottishPostcodeLookupResponse } from '../../../../data/entity/mockPostcodeLookupResponse'
+
+const mockPostcode = 'https://postcodeinfo.service.justice.gov.uk'
 
 describe('IsCountrySupported', () => {
   const constraint: CheckCountryConstraint = new CheckCountryConstraint()
@@ -9,7 +14,7 @@ describe('IsCountrySupported', () => {
   describe('validate', () => {
 
     describe('should return true when ', () => {
-      it('given am undefined value', async () => {
+      it('given an undefined value', async () => {
         expect(await constraint.validate(undefined)).to.equal(true)
       })
 
@@ -22,20 +27,48 @@ describe('IsCountrySupported', () => {
       })
 
       it('given a valid postcode', async () => {
+        nock(mockPostcode)
+          .get(/\/addresses\/\?postcode=.+/)
+          .reply(200, mockAddressResponse)
+
+        nock(mockPostcode)
+          .get(/\/postcodes\/.+/)
+          .reply(200, mockPostcodeLookupResponse)
         expect(await constraint.validate('SW21AN', validationArgs(Country.all()))).to.equal(true)
       })
 
       it('given a valid postcode that the postcode lookup service can not find', async () => {
+        nock(mockPostcode)
+          .get(/\/addresses\/\?postcode=.+/)
+          .reply(200, [])
+
+        nock(mockPostcode)
+          .get(/\/postcodes\/.+/)
+          .reply(200, mockInvalidPostcodeLookupResponse)
         expect(await constraint.validate('SA6 7JL', validationArgs(Country.all()))).to.equal(true)
       })
 
       it('given an invalid postcode', async () => {
-        expect(await constraint.validate('SW21AN', validationArgs(Country.all()))).to.equal(true)
+        nock(mockPostcode)
+          .get(/\/addresses\/\?postcode=.+/)
+          .reply(200, [])
+
+        nock(mockPostcode)
+          .get(/\/postcodes\/.+/)
+          .reply(200, mockInvalidPostcodeLookupResponse)
+        expect(await constraint.validate('2SW1AN', validationArgs(Country.all()))).to.equal(true)
       })
     })
 
     describe('should return false when ', () => {
       it('given a postcode that is not in the accepted list', async () => {
+        nock(mockPostcode)
+          .get(/\/addresses\/\?postcode=.+/)
+          .reply(200, [])
+
+        nock(mockPostcode)
+          .get(/\/postcodes\/.+/)
+          .reply(200, mockScottishPostcodeLookupResponse)
         expect(await constraint.validate('EH9 1SH', validationArgs(Country.defendantCountries()))).to.equal(false)
       })
     })
---
+++ src/test/data/entity/mockPostcodeLookupResponse.ts
@@ -0,0 +1,53 @@
+export const mockPostcodeLookupResponse = {
+  'valid': 'true',
+  'country': {
+    'gss_code': 'E92000001',
+    'name': 'England'
+  },
+  'local_authority': {
+    'gss_code': 'E09000033',
+    'name': 'Westminster'
+  },
+  'centre': {
+    'type': 'Point',
+    'coordinates': [
+      -0.1212324,
+      23.5022039
+    ]
+  }
+}
+
+export const mockScottishPostcodeLookupResponse = {
+  'valid': 'false',
+  'country': {
+    'gss_code': 'E92000001',
+    'name': 'Scotland'
+  },
+  'local_authority': {
+    'gss_code': 'E09000033',
+    'name': 'Edinburgh'
+  },
+  'centre': {
+    'type': 'Point',
+    'coordinates': [
+      -0.1212324,
+      23.5022039
+    ]
+  }
+}
+
+export const mockInvalidPostcodeLookupResponse = {
+  'valid': 'false',
+  'country': {
+    'gss_code': '',
+    'name': ''
+  },
+  'local_authority': {
+    'gss_code': '',
+    'name': ''
+  },
+  'centre': {
+    'type': '',
+    'coordinates': ''
+  }
+}
