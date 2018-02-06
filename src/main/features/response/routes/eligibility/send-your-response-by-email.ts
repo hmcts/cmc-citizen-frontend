a0a1710801c5bb0331646d5b1bf75ffae2c913b4
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-02-06 16:22:55 +0000

    Refactored route and added test case

--- src/main/features/response/routes/eligibility/send-your-response-by-email.ts
+++ src/main/features/response/routes/eligibility/send-your-response-by-email.ts
@@ -5,42 +5,25 @@ import { Paths } from 'response/paths'
 import { ResponseDraft } from 'response/draft/responseDraft'
 import { Draft } from '@hmcts/draft-store-client'
 import { FeesClient } from 'fees/feesClient'
-import { FeesTableViewHelper } from 'claim/helpers/feesTableViewHelper'
 import { Range } from 'fees/models/range'
-import { RangeGroup } from 'app/fees/models/rangeGroup'
 
 const supportedFeeLimitInPennies: number = 1000000
 
 async function renderView (res: express.Response, next: express.NextFunction) {
-  try {
-    Promise.all(
-      [
-        FeesClient.getIssueFeeRangeGroup(),
-        FeesClient.getHearingFeeRangeGroup()
-      ]
+  FeesClient.getIssueFeeRangeGroup().then((issueFeeRangeGroup) => {
+    const draft: Draft<ResponseDraft> = res.locals.responseDraft
+
+    const supportedIssueFees: Range[] = issueFeeRangeGroup.ranges
+      .filter(range => range.from < supportedFeeLimitInPennies)
+      .map(range => range.copy({ to: Math.min(range.to, supportedFeeLimitInPennies) }))
+
+    res.render(Paths.sendYourResponseByEmail.associatedView,
+      {
+        draft: draft.document,
+        fees: supportedIssueFees
+      }
     )
-      .then((values: any[]) => {
-        const issueFeeRangeGroup: RangeGroup = values[0]
-        const hearingFeeRangeGroup: RangeGroup = values[1]
-        const draft: Draft<ResponseDraft> = res.locals.responseDraft
-
-        const supportedIssueFees: Range[] = issueFeeRangeGroup.ranges
-          .filter(range => range.from < supportedFeeLimitInPennies)
-          .map(range => range.copy({ to: Math.min(range.to, supportedFeeLimitInPennies) }))
-        const supportedHearingFees: Range[] = hearingFeeRangeGroup.ranges
-          .filter(range => range.from < supportedFeeLimitInPennies)
-          .map(range => range.copy({ to: Math.min(range.to, supportedFeeLimitInPennies) }))
-
-        res.render(Paths.sendYourResponseByEmail.associatedView,
-          {
-            draft: draft.document,
-            rows: FeesTableViewHelper.merge(supportedIssueFees, supportedHearingFees)
-          }
-        )
-      })
-  } catch (err) {
-    next(err)
-  }
+  }).catch(next)
 }
 
 /* tslint:disable:no-default-export */
