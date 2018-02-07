00b08dfbad68ed359f9069e0bc87e7ebacd613fc
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-07 16:25:04 +0000

    Another test

--- bin/run-smoke-tests.sh
+++ bin/run-smoke-tests.sh
@@ -14,9 +14,9 @@ trap shutdownDocker INT TERM QUIT EXIT
 bash --version
 docker-compose --version
 
-#docker-compose -f ${ADDITIONAL_COMPOSE_FILE} pull
+docker-compose -f ${ADDITIONAL_COMPOSE_FILE} pull
 docker-compose -f ${ADDITIONAL_COMPOSE_FILE} up --no-color -d remote-webdriver
-#docker-compose -f ${ADDITIONAL_COMPOSE_FILE} run integration-tests
-docker-compose -f ${ADDITIONAL_COMPOSE_FILE} run integration-tests -- --grep @citizen-smoke-test
+docker-compose -f ${ADDITIONAL_COMPOSE_FILE} run integration-tests
+#docker-compose -f ${ADDITIONAL_COMPOSE_FILE} up integration-tests
 docker-compose -f ${ADDITIONAL_COMPOSE_FILE} down
 
