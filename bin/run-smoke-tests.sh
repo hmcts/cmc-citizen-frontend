4e2a4e31eb56a5f34c6b32fd92a5df676984d7b3
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-07 14:23:47 +0000

    Add debug

--- bin/run-smoke-tests.sh
+++ bin/run-smoke-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ue
 
+set -x
+
 ADDTIONAL_COMPOSE_FILE=docker-compose.integration-tests.yml
 
 function shutdownDocker() {
@@ -10,7 +12,7 @@ function shutdownDocker() {
 trap shutdownDocker INT TERM QUIT EXIT
 
 #docker-compose -f ${ADDTIONAL_COMPOSE_FILE} pull
-docker-compose -f ${ADDTIONAL_COMPOSE_FILE} up -d remote-webdriver
+docker-compose -f ${ADDTIONAL_COMPOSE_FILE} up --no-color -d remote-webdriver
 docker-compose -f ${ADDTIONAL_COMPOSE_FILE} run integration-tests test --grep '@citizen-smoke-test'
 docker-compose -f ${ADDTIONAL_COMPOSE_FILE} down
 
