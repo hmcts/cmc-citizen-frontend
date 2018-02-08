facb2e2abe1d3a02fbafef3c5db25b72cef17883
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-02-08 14:47:34 +0000

    Removed unused file

--- src/main/features/response/tasks/howMuchPaidClaimantTask.ts
+++
@@ -1,7 +0,0 @@
-import { ResponseDraft } from 'response/draft/responseDraft'
-
-export class HowMuchPaidClaimantTask {
-  static isCompleted (responseDraft: ResponseDraft): boolean {
-    return !!(responseDraft.howMuchPaidClaimant && responseDraft.howMuchPaidClaimant.option)
-  }
-}
