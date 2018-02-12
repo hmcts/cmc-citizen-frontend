8c865721032b09756ae917fa7c1324b30f0d68a7
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-12 18:02:22 +0000

    Add prod config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,23 +9,32 @@ provider "vault" {
 }
 
 data "vault_generic_secret" "s2s_secret" {
-  path = "secret/test/ccidam/service-auth-provider/api/microservice-keys/cmc"
+  path = "secret/${local.vault_section}/ccidam/service-auth-provider/api/microservice-keys/cmc"
 }
 
 data "vault_generic_secret" "draft_store_secret" {
-  path = "secret/test/cmc/draft-store/encryption-secrets/citizen-frontend"
+  path = "secret/${local.vault_section}/cmc/draft-store/encryption-secrets/citizen-frontend"
 }
 
 data "vault_generic_secret" "postcode-lookup-api-key" {
-  path = "secret/test/cmc/postcode-lookup/api-key"
+  path = "secret/${local.vault_section}/cmc/postcode-lookup/api-key"
 }
 
 data "vault_generic_secret" "oauth-client-secret" {
-  path = "secret/test/ccidam/idam-api/oauth2/client-secrets/cmc-citizen"
+  path = "secret/${local.vault_section}/ccidam/idam-api/oauth2/client-secrets/cmc-citizen"
 }
 
 locals {
   aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  vault_section = "${var.env == "prod" ? "prod" : "test"}"
+
+  idam_api_url = "${var.env == "prod" ? var.prod-idam-api-url : var.test-idam-api-url}"
+  s2s_url = "${var.env == "prod" ? var.prod-s2s-url : var.test-s2s-url}"
+
+  authentication_web_url = "${var.env == "prod" ? var.prod-authentication-web-url : var.test-authentication-web-url}"
+  payments_api_url = "${var.env == "prod" ? var.prod-payments-api-url : var.test-payments-api-url}"
+  fees_api_url = "${var.env == "prod" ? var.prod-fees-api-url : var.test-fees-api-url}"
+  draft_store_api_url = "${var.env == "prod" ? var.prod-draft-store-api-url : var.test-draft-store-api-url}"
 }
 
 module "citizen-frontend" {
@@ -54,20 +63,20 @@ module "citizen-frontend" {
     POSTCODE_LOOKUP_API_KEY = "${data.vault_generic_secret.postcode-lookup-api-key.data["value"]}"
 
     // IDAM
-    IDAM_API_URL = "${var.idam-api-url}"
-    IDAM_AUTHENTICATION_WEB_URL = "${var.authentication-web-url}"
-    IDAM_S2S_AUTH = "${var.service-2-service-auth-url}"
+    IDAM_API_URL = "${local.idam_api_url}"
+    IDAM_AUTHENTICATION_WEB_URL = "${local.authentication_web_url}"
+    IDAM_S2S_AUTH = "${local.s2s_url}"
     IDAM_S2S_TOTP_SECRET = "${data.vault_generic_secret.s2s_secret.data["value"]}"
     OAUTH_CLIENT_SECRET = "${data.vault_generic_secret.oauth-client-secret.data["value"]}"
 
     // Payments API
-    PAY_URL = "${var.payments-api-url}"
+    PAY_URL = "${local.payments_api_url}"
 
     // Fees API
-    FEES_URL = "${var.fees-api-url}"
+    FEES_URL = "${local.fees_api_url}"
 
     // Draft Store API
-    DRAFT_STORE_URL = "${var.draft-store-api-url}"
+    DRAFT_STORE_URL = "${local.draft_store_api_url}"
     DRAFT_STORE_SECRET_PRIMARY = "${data.vault_generic_secret.draft_store_secret.data["primary"]}"
     DRAFT_STORE_SECRET_SECONDARY = "${data.vault_generic_secret.draft_store_secret.data["secondary"]}"
 
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -15,28 +15,58 @@ variable "env" { }
 
 variable "ilbIp" { }
 
-variable "draft-store-api-url" {
+variable "test-draft-store-api-url" {
   default = "https://testdraftstorelb.moneyclaim.reform.hmcts.net:4302"
 }
 
-variable "payments-api-url" {
+variable "prod-draft-store-api-url" {
+  default = "https://preproddraftstorelb.moneyclaim.reform.hmcts.net:4301"
+//  default = "https://testdraftstorelb.moneyclaim.reform.hmcts.net:4301"
+}
+
+variable "test-payments-api-url" {
   default = "https://test.payments.reform.hmcts.net:4421"
 }
 
-variable "fees-api-url" {
+variable "prod-payments-api-url" {
+  default = "https://preprod.payments.reform.hmcts.net:4401"
+//  default = "https://payments.reform.hmcts.net:4401"
+}
+
+variable "test-fees-api-url" {
   default = "https://test.fees-register.reform.hmcts.net:4431"
 }
 
-variable "idam-api-url" {
+variable "prod-fees-api-url" {
+  default = "https://preprod.fees-register.reform.hmcts.net:4411"
+//  default = "https://fees-register.reform.hmcts.net:4411"
+}
+
+variable "test-idam-api-url" {
   default = "http://betaDevBccidamAppLB.reform.hmcts.net"
 }
 
-variable "authentication-web-url" {
+variable "prod-idam-api-url" {
+  default = "http://betaPreProdccidamAppLB.reform.hmcts.net:4501"
+  //  default = "http://betaProdccidamAppLB.reform.hmcts.net:4501"
+}
+
+variable "test-s2s-url" {
+  default = "http://betaDevBccidamS2SLB.reform.hmcts.net"
+}
+
+variable "prod-s2s-url" {
+  default = "http://betaPreProdccidamAppLB.reform.hmcts.net:4502"
+  //  default = "http://betaProdccidamAppLB.reform.hmcts.net:4502"
+}
+
+variable "test-authentication-web-url" {
   default = "https://idam-test.dev.ccidam.reform.hmcts.net"
 }
 
-variable "service-2-service-auth-url" {
-  default = "http://betaDevBccidamS2SLB.reform.hmcts.net"
+variable "prod-authentication-web-url" {
+  default = "https://idam.preprod.ccidam.reform.hmcts.net"
+//  default = "https://www.idam.reform.hmcts.net"
 }
 
 variable "subscription" {}
