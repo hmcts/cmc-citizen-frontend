eed5eb292f935da9c06aa4a5b5c57df7fd06b7ad
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-02-12 21:43:32 +0000

    Added missing test case

--- src/main/features/response/tasks/whenDidYouPayTask.ts
+++ src/main/features/response/tasks/whenDidYouPayTask.ts
@@ -3,9 +3,14 @@ import { Validator } from 'class-validator'
 
 const validator = new Validator()
 
+function isValid (input): boolean {
+  return input !== undefined && validator.validateSync(input).length === 0
+}
+
 export class WhenDidYouPayTask {
   static isCompleted (responseDraft: ResponseDraft): boolean {
-    return (responseDraft.whenDidYouPay.text && responseDraft.whenDidYouPay.date.year
-      && validator.validateSync(responseDraft.whenDidYouPay.date.year).length > 0)
+    return responseDraft.whenDidYouPay !== undefined
+    && isValid(responseDraft.whenDidYouPay.date)
+    && isValid(responseDraft.whenDidYouPay)
   }
 }
