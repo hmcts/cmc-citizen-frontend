53a2e15d6d37ca702417f01a6be28324c28c82f5
Robert Parkinson <robert.parkinson@hmcts.net>
2018-02-13 11:11:57 +0000

    Add in more unit tests

--- src/test/common/utils/interest.ts
+++ src/test/common/utils/interest.ts
@@ -3,26 +3,99 @@ import { getInterestDetails } from 'common/interest'
 import * as claimStoreServiceMock from '../../http-mocks/claim-store'
 import { Claim } from 'claims/models/claim'
 import { InterestType } from 'claim/form/models/interest'
-import { Moment } from 'moment'
 import { MomentFactory } from 'common/momentFactory'
+import { Moment } from 'moment'
+
+const sampleClaimObj = {
+  id: 1,
+  submitterId: '1',
+  externalId: '400f4c57-9684-49c0-adb4-4cf46579d6dc',
+  defendantId: '123',
+  referenceNumber: '000MC000',
+  createdAt: MomentFactory.currentDate(),
+  issuedOn: MomentFactory.currentDate(),
+  totalAmountTillToday: 200,
+  totalAmountTillDateOfIssue: 200,
+  claim: {
+    claimants: [
+      {
+        type: 'individual',
+        name: 'John Smith',
+        address: {
+          line1: 'line1',
+          line2: 'line2',
+          city: 'city',
+          postcode: 'bb127nq'
+        },
+        dateOfBirth: '1990-02-17'
+      }
+    ],
+    defendants: [
+      {
+        type: 'individual',
+        name: 'John Doe',
+        address: {
+          line1: 'line1',
+          line2: 'line2',
+          city: 'city',
+          postcode: 'bb127nq'
+        }
+      }
+    ],
+    payment: {
+      id: '12',
+      amount: 2500,
+      state: { status: 'failed' }
+    },
+    amount: {
+      type: 'breakdown',
+      rows: [{ reason: 'Reason', amount: 200 }]
+    },
+    interestDate: {
+      date: {
+        year: 2000,
+        month: 2,
+        day: 1
+      }
+    },
+    interest: {
+      type: InterestType.STANDARD
+    },
+    reason: 'Because I can'
+  },
+  responseDeadline: '2017-08-08'
+}
 
 describe('getInterestDetails', () => {
 
   it('should return undefined when interest has not been selected', async () => {
-    let claim: Claim = new Claim().deserialize(claimStoreServiceMock.sampleClaimObj)
+    const claim: Claim = new Claim().deserialize(sampleClaimObj)
     claim.claimData.interest.type = InterestType.NO_INTEREST
     expect(await getInterestDetails(claim)).to.be.eq(undefined)
   })
 
-  it('should return expected interestDetails containing the needed values to be displayed', async () => {
-    const claim: Claim = new Claim().deserialize(claimStoreServiceMock.sampleClaimObj)
+  it('should return 1 for number of days when interest date starts from today', async () => {
+    const claim: Claim = new Claim().deserialize(sampleClaimObj)
     claimStoreServiceMock.mockCalculateInterestRate(5)
-    const createdDate: Moment = claim.createdAt
     const interestData = {
-      numberOfDays: MomentFactory.currentDate().diff(createdDate, 'days') + 1,
+      numberOfDays: 1,
       interest: 5,
       rate: claim.claimData.interest.rate,
-      interestDate: createdDate
+      interestDate: MomentFactory.currentDate()
+    }
+    expect(await getInterestDetails(claim)).to.deep.eq(interestData)
+  })
+
+  it('should return 2 for number of days when interest date starts from yesterday', async () => {
+    const claim: Claim = new Claim().deserialize(sampleClaimObj)
+    const yesterday: Moment = MomentFactory.currentDate().subtract(1, 'days')
+    claimStoreServiceMock.mockCalculateInterestRate(5)
+    claim.createdAt = yesterday
+    const interestData = {
+      numberOfDays: 2,
+      interest: 5,
+      rate: claim.claimData.interest.rate,
+      interestDate: yesterday
     }
     expect(await getInterestDetails(claim)).to.deep.eq(interestData)
   })
