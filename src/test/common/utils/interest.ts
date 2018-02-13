5cb1e19d59e3cf8f3f5dfc3d9137efc8755c1ee1
Damian Dunajski <damian.dunajski@hmcts.net>
2018-02-13 12:21:18 +0000

    Implicitely state input conditions for interest date calculation tests

--- src/test/common/utils/interest.ts
+++ src/test/common/utils/interest.ts
@@ -1,50 +1,60 @@
+import { InterestDateType } from 'app/common/interestDateType'
 import { expect } from 'chai'
-import { getInterestDetails } from 'common/interest'
-import * as claimStoreServiceMock from '../../http-mocks/claim-store'
-import { Claim } from 'claims/models/claim'
 import { InterestType } from 'claim/form/models/interest'
+import { Claim } from 'claims/models/claim'
+import { getInterestDetails } from 'common/interest'
 import { MomentFactory } from 'common/momentFactory'
-import { Moment } from 'moment'
+import * as claimStoreServiceMock from '../../http-mocks/claim-store'
 
 const sampleClaimObj = {
+  claim: {
+    interest: {
+      type: InterestType.STANDARD
+    },
+    interestDate: {
+      type: InterestDateType.SUBMISSION
+    }
+  },
   createdAt: MomentFactory.currentDate(),
   issuedOn: MomentFactory.currentDate(),
-  claim: {
-  },
   responseDeadline: '2017-08-08'
 }
 
 describe('getInterestDetails', () => {
 
   it('should return undefined when interest has not been selected', async () => {
-    const claim: Claim = new Claim().deserialize(sampleClaimObj)
-    claim.claimData.interest.type = InterestType.NO_INTEREST
+    const claim: Claim = new Claim().deserialize({
+      ...sampleClaimObj,
+      ...{
+        claim: {
+          interest: {
+            type: InterestType.NO_INTEREST
+          },
+          interestDate: undefined
+        }
+      }
+    })
+
     expect(await getInterestDetails(claim)).to.be.eq(undefined)
   })
 
   it('should return 1 for number of days when interest date starts from today', async () => {
-    const claim: Claim = new Claim().deserialize(sampleClaimObj)
     claimStoreServiceMock.mockCalculateInterestRate(5)
-    const interestData = {
-      numberOfDays: 1,
-      interest: 5,
-      rate: claim.claimData.interest.rate,
-      interestDate: MomentFactory.currentDate()
-    }
-    expect(await getInterestDetails(claim)).to.deep.eq(interestData)
+
+    const today = MomentFactory.currentDate()
+    const claim: Claim = new Claim().deserialize({ ...sampleClaimObj, createdAt: today })
+
+    const { numberOfDays } = await getInterestDetails(claim) as any
+    expect(numberOfDays).to.deep.eq(1)
   })
 
   it('should return 2 for number of days when interest date starts from yesterday', async () => {
-    const claim: Claim = new Claim().deserialize(sampleClaimObj)
-    const yesterday: Moment = MomentFactory.currentDate().subtract(1, 'days')
     claimStoreServiceMock.mockCalculateInterestRate(5)
-    claim.createdAt = yesterday
-    const interestData = {
-      numberOfDays: 2,
-      interest: 5,
-      rate: claim.claimData.interest.rate,
-      interestDate: yesterday
-    }
-    expect(await getInterestDetails(claim)).to.deep.eq(interestData)
+
+    const yesterday = MomentFactory.currentDate().subtract(1, 'days')
+    const claim: Claim = new Claim().deserialize({ ...sampleClaimObj, createdAt: yesterday })
+
+    const { numberOfDays } = await getInterestDetails(claim) as any
+    expect(numberOfDays).to.deep.eq(2)
   })
 })
