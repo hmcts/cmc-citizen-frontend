15b8cb14fb4190a283d7a89d5b23344cf70951f5
Kiran Varma <kiranv@kainos.com>
2018-02-14 16:38:49 +0000

    added countersign agreement page

--- src/main/features/offer/paths.ts
+++ src/main/features/offer/paths.ts
@@ -8,6 +8,7 @@ export class Paths {
   static readonly settleOutOfCourtPage = new RoutablePath(`${offerPath}/settle-out-of-court`)
   static readonly responsePage = new RoutablePath(`${offerPath}/response`)
   static readonly makeAgreementPage = new RoutablePath(`${offerPath}/make-agreement`)
+  static readonly countersignAgreementPage = new RoutablePath(`${offerPath}/countersign-agreement`)
   static readonly declarationPage = new RoutablePath(`${offerPath}/declaration`)
   static readonly acceptedPage = new RoutablePath(`${offerPath}/accepted`)
   static readonly rejectedPage = new RoutablePath(`${offerPath}/rejected`)
---
+++ src/main/features/offer/routes/countersign-agreement.ts
@@ -0,0 +1,15 @@
+import * as express from 'express'
+import { Paths } from 'offer/paths'
+import { ErrorHandling } from 'common/errorHandling'
+import { Claim } from 'claims/models/claim'
+
+/* tslint:disable:no-default-export */
+export default express.Router()
+  .get(
+    Paths.countersignAgreementPage.uri,
+    ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+      const claim: Claim = res.locals.claim
+      res.render(Paths.countersignAgreementPage.associatedView, {
+        claim: claim
+      })
+    }))
