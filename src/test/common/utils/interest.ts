030d83fd6029a541f83a4a8a157888325c466b09
Robert Parkinson <robert.parkinson@hmcts.net>
2018-02-19 16:26:32 +0000

    Update to use iussedOn date

--- src/test/common/utils/interest.ts
+++ src/test/common/utils/interest.ts
@@ -42,7 +42,7 @@ describe('getInterestDetails', () => {
     claimStoreServiceMock.mockCalculateInterestRate(5)
 
     const today = MomentFactory.currentDate()
-    const claim: Claim = new Claim().deserialize({ ...sampleClaimObj, createdAt: today })
+    const claim: Claim = new Claim().deserialize({ ...sampleClaimObj, issuedOn: today })
 
     const { numberOfDays } = await getInterestDetails(claim) as any
     expect(numberOfDays).to.deep.eq(0)
