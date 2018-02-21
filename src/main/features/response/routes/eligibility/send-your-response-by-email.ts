958d46859a07071c2773fca256c30a81c089f5e8
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-02-21 13:45:14 +0000

    Change to the invocation of the fees api getIssueFeeGroup and update to the template

--- src/main/features/response/routes/eligibility/send-your-response-by-email.ts
+++ src/main/features/response/routes/eligibility/send-your-response-by-email.ts
@@ -6,18 +6,19 @@ import { ErrorHandling } from 'common/errorHandling'
 import { ResponseDraft } from 'response/draft/responseDraft'
 import { Draft } from '@hmcts/draft-store-client'
 import { FeesClient } from 'fees/feesClient'
-import { Range } from 'fees/models/range'
+import { FeeRange as MergableRange } from 'claim/helpers/feesTableViewHelper'
+import { FeeRange } from 'fees/models/feeRange'
 
-const supportedFeeLimitInPennies: number = 1000000
+const supportedFeeLimitInGBP: number = 10000
 
 /* tslint:disable:no-default-export */
 export default express.Router()
   .get(Paths.sendYourResponseByEmailPage.uri, ErrorHandling.apply(async (req: express.Request, res: express.Response) => {
     const issueFeeGroup = await FeesClient.getIssueFeeRangeGroup()
 
-    const supportedIssueFees: Range[] = issueFeeGroup.ranges
-      .filter(range => range.from < supportedFeeLimitInPennies)
-      .map(range => range.copy({ to: Math.min(range.to, supportedFeeLimitInPennies) }))
+    const supportedIssueFees: MergableRange[] = issueFeeGroup
+      .filter((range: FeeRange) => range.minRange < supportedFeeLimitInGBP)
+      .map((range: FeeRange) => new MergableRange(range.minRange, Math.min(range.maxRange, supportedFeeLimitInGBP), range.currentVersion.flatAmount.amount))
 
     const draft: Draft<ResponseDraft> = res.locals.responseDraft
     res.render(Paths.sendYourResponseByEmailPage.associatedView,
