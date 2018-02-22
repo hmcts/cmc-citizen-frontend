2c4f35d0b76d249fe21962a96ffc6b2339506fbd
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-22 14:49:08 +0000

    Pull before running - Oops

--- bin/run-smoke-tests.sh
+++ bin/run-smoke-tests.sh
@@ -19,7 +19,7 @@ trap shutdownDocker INT TERM QUIT EXIT
 
 docker-compose --version
 
-#docker-compose -f ${ADDITIONAL_COMPOSE_FILE} pull
+docker-compose -f ${ADDITIONAL_COMPOSE_FILE} pull
 docker-compose -f ${ADDITIONAL_COMPOSE_FILE} up --no-color -d remote-webdriver
 docker-compose -f ${ADDITIONAL_COMPOSE_FILE} run integration-tests
 docker-compose -f ${ADDITIONAL_COMPOSE_FILE} down
