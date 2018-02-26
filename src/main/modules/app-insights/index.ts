6e2b2396a7ab69f17ebe3c205df27e4017925abd
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2018-02-26 18:18:09 +0100

    ROC-3224: Configure App Insights (#392)

---
+++ src/main/modules/app-insights/index.ts
@@ -0,0 +1,9 @@
+import * as config from 'config'
+import * as appInsights from 'applicationinsights'
+
+export class AppInsights {
+  static enable () {
+    appInsights.setup(config.get<string>('appInsights.instrumentationKey'))
+    appInsights.start()
+  }
+}
