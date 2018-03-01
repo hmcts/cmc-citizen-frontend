5a36138e8ddfa0fc04dd831053326a86ebb76b25
Kiran Varma <varma.ap@gmail.com>
2018-03-01 18:40:01 +0000

    Revert "ROC-2892-countersign-agreement: changes as per code review and added missing defendant claim history details"
    This reverts commit f71d2a018c8f67881c989ef602e706e1bd39162b.

--- src/main/app/claims/models/claim.ts
+++ src/main/app/claims/models/claim.ts
@@ -12,11 +12,6 @@ import { ClaimStatus } from 'claims/models/claimStatus'
 import { FeatureToggles } from 'utils/featureToggles'
 import { FreeMediationOption } from 'response/form/models/freeMediation'
 
-interface State {
-  date: Moment
-  status: ClaimStatus
-}
-
 export class Claim {
   id: number
   claimantId: string
@@ -119,23 +114,6 @@ export class Claim {
       return ClaimStatus.NO_RESPONSE
     } else {
       throw new Error('Unknown Status')
-    }
-  }
-
-  get stateHistory (): State[] {
-    if (this.status === ClaimStatus.OFFER_ACCEPTED) {
-      return [{
-        date: undefined,
-        status: ClaimStatus.OFFER_ACCEPTED
-      }, {
-        date: this.respondedAt,
-        status: ClaimStatus.CLAIM_REJECTED
-      }]
-    } else {
-      return [{
-        date: undefined,
-        status: this.status
-      }]
     }
   }
 
