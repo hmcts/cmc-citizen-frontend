59f187f6d7e37727cea2af25f9b878d27680b7b8
Kiran Varma <varma.ap@gmail.com>
2018-03-02 22:32:15 +0000

    changed dashboard links to use from paths files

--- src/main/features/offer/routes/accepted.ts
+++ src/main/features/offer/routes/accepted.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 import { Paths } from 'offer/paths'
 import { ErrorHandling } from 'common/errorHandling'
 import { Claim } from 'claims/models/claim'
+import { Paths as DashboardPaths } from 'dashboard/paths'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
@@ -10,6 +11,7 @@ export default express.Router()
     ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
       const claim: Claim = res.locals.claim
       res.render(Paths.acceptedPage.associatedView, {
-        claim: claim
+        claim: claim,
+        dashboardLink: DashboardPaths.claimantPage.evaluateUri({ externalId: claim.externalId })
       })
     }))
--- src/main/features/offer/routes/settled.ts
+++ src/main/features/offer/routes/settled.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 import { Paths } from 'offer/paths'
 import { ErrorHandling } from 'common/errorHandling'
 import { Claim } from 'claims/models/claim'
+import { Paths as DashboardPaths } from 'dashboard/paths'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
@@ -11,6 +12,7 @@ export default express.Router()
       const claim: Claim = res.locals.claim
       res.render(Paths.settledPage.associatedView, {
         claim: claim,
-        agreementLink: Paths.agreementReceiver.evaluateUri({ externalId: claim.externalId })
+        agreementLink: Paths.agreementReceiver.evaluateUri({ externalId: claim.externalId }),
+        dashboardLink: DashboardPaths.defendantPage.evaluateUri({ externalId: claim.externalId })
       })
     }))
