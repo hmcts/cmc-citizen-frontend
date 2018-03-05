12e455319ac1a466ff1b31c85fbc4afa43c45781
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-03-05 15:54:05 +0000

    Remove legal frontend related files

--- src/integration-test/data/legal-test-data.ts
+++
@@ -1,9 +0,0 @@
-export const verifyPageData = {
-  organizationRefNumber: 'PBA1234567',
-  claimantOrganization: 'Abc corporation',
-  defendantOrganization: 'Def corporation',
-  emailConfirmation: 'We\'ve emailed confirmation to: ',
-  feeAccountNumber: 'PBA0000000',
-  feesPaid: '£455',
-  maxFeePaid: '£10,000'
-}
