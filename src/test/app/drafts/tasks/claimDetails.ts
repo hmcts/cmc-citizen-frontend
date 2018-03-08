8397ebb164ce27c6234a948d4fa69150a12fec12
Patryk Jar <jar.patryk@gmail.com>
2018-03-08 11:45:09 +0000

    ROC-2213: Added timeline page for claim (#407)

---
+++ src/test/app/drafts/tasks/claimDetails.ts
@@ -0,0 +1,58 @@
+import { expect } from 'chai'
+
+import { DraftClaim } from 'app/drafts/models/draftClaim'
+import { ClaimDetails } from 'drafts/tasks/claimDetails'
+
+describe('Claim details', () => {
+
+  describe('isCompleted', () => {
+
+    it('should return true when timeline has a row and reason is given', () => {
+      const input = {
+        timeline: {
+          rows: [{
+            date: 'may',
+            description: 'it is ok'
+          }]
+        },
+        reason: { reason: 'It is my reason' }
+      }
+
+      const claim: DraftClaim = new DraftClaim().deserialize(input)
+
+      expect(ClaimDetails.isCompleted(claim)).to.equal(true)
+    })
+
+    context('should return false', () => {
+
+      it('timeline has no rows and reason is valid', () => {
+        const input = {
+          timeline: {
+            rows: []
+          },
+          reason: 'It is my reason'
+        }
+
+        const claim: DraftClaim = new DraftClaim().deserialize(input)
+
+        expect(ClaimDetails.isCompleted(claim)).to.equal(false)
+      })
+
+      it('timeline has row but reason is empty', () => {
+        const input = {
+          timeline: {
+            rows: [{
+              date: 'may',
+              description: 'it is ok'
+            }]
+          },
+          reason: ''
+        }
+
+        const claim: DraftClaim = new DraftClaim().deserialize(input)
+
+        expect(ClaimDetails.isCompleted(claim)).to.equal(false)
+      })
+    })
+  })
+})
