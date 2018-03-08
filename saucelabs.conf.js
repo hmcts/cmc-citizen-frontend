cd59a830123ee7c4ce83294f97e82df1ad2306f5
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-03-08 16:51:56 +0000

    Retrieve the list of supported browsers from the new library

--- saucelabs.conf.js
+++ saucelabs.conf.js
@@ -1,4 +1,4 @@
-const supportedBrowsers = require('./src/integration-test/config/saucelabs/supported-browsers').supportedBrowsers
+import { supportedBrowsers } from '@hmcts/cmc-supported-browsers'
 
 const browser = requiredValue(process.env.SAUCELABS_BROWSER, 'SAUCELABS_BROWSER')
 const saucelabsTunnelIdentifier = requiredValue(process.env.SAUCELABS_TUNNEL_IDENTIFIER, 'SAUCELABS_TUNNEL_IDENTIFIER')
