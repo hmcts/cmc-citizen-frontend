60fe976b831ba9dc41c63792b4de9987b7d2490a
Damian Dunajski <damian.dunajski@hmcts.net>
2018-03-09 14:51:08 +0100

    Move comment disabling ts-lint rule to top of the file

--- src/main/app/pay/fees.ts
+++ src/main/app/pay/fees.ts
@@ -1,6 +1,5 @@
+/* tslint:disable variable-name allow snake_case */
+
 export class Fee {
-  constructor (public calculated_amount: number, // tslint:disable-line variable-name allow snake_case
-               public code: string,
-               public version: number) {
-  }
+  constructor (public calculated_amount: number, public code: string, public version: number) {}
 }
