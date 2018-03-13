b03aa0b5974e3b7f901678df99351940b2a31698
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-13 09:05:36 +0000

    Add claimant journey

---
+++ src/main/routes/no-claim-number.ts
@@ -0,0 +1,38 @@
+import * as express from 'express'
+import * as config from 'config'
+
+import { ErrorHandling } from 'common/errorHandling'
+import { Paths } from 'app/paths'
+import { ClaimReference } from 'forms/models/claimReference'
+import { isCCBCCaseReference } from 'common/utils/isCCBCCaseReference'
+import { Form } from 'forms/form'
+
+function renderView (form: Form<ClaimReference>, res: express.Response): void {
+  res.render(Paths.noClaimNumberPage.associatedView, { form: form })
+}
+
+const mcolUrl = config.get<string>('mcol.url')
+
+/* tslint:disable:no-default-export */
+export default express.Router()
+  .get(Paths.noClaimNumberPage.uri,
+    ErrorHandling.apply(async (req: express.Request, res: express.Response): Promise<void> => {
+      renderView(Form.empty<ClaimReference>(), res)
+    })
+  )
+  .post(
+    Paths.noClaimNumberPage.uri,
+    ErrorHandling.apply(async (req: express.Request, res: express.Response): Promise<void> => {
+      const form: Form<ClaimReference> = req.body
+
+      if (form.hasErrors()) {
+        if (isCCBCCaseReference(form.model.reference)) {
+          return res.redirect(mcolUrl)
+        }
+
+        renderView(form, res)
+      } else {
+        res.redirect(Paths.homePage.uri)
+      }
+    })
+  )
