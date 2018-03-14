4ce63f98bbe83189fb11f9bcdcfd27ff9a4248e0
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-14 10:03:04 +0000

    Finish off no claim number page

--- src/main/routes/no-claim-number.ts
+++ src/main/routes/no-claim-number.ts
@@ -3,11 +3,12 @@ import * as config from 'config'
 
 import { ErrorHandling } from 'common/errorHandling'
 import { Paths } from 'app/paths'
-import { ClaimReference } from 'forms/models/claimReference'
-import { isCCBCCaseReference } from 'common/utils/isCCBCCaseReference'
 import { Form } from 'forms/form'
+import { FormValidator } from 'forms/validation/formValidator'
+import { NoClaimNumber } from 'forms/models/noClaimNumber'
+import { Service } from 'models/service'
 
-function renderView (form: Form<ClaimReference>, res: express.Response): void {
+function renderView (form: Form<NoClaimNumber>, res: express.Response): void {
   res.render(Paths.noClaimNumberPage.associatedView, { form: form })
 }
 
@@ -17,22 +18,28 @@ const mcolUrl = config.get<string>('mcol.url')
 export default express.Router()
   .get(Paths.noClaimNumberPage.uri,
     ErrorHandling.apply(async (req: express.Request, res: express.Response): Promise<void> => {
-      renderView(Form.empty<ClaimReference>(), res)
+      renderView(Form.empty<NoClaimNumber>(), res)
     })
   )
   .post(
     Paths.noClaimNumberPage.uri,
+    FormValidator.requestHandler(undefined, NoClaimNumber.fromObject),
     ErrorHandling.apply(async (req: express.Request, res: express.Response): Promise<void> => {
-      const form: Form<ClaimReference> = req.body
+      const form: Form<NoClaimNumber> = req.body
 
       if (form.hasErrors()) {
-        if (isCCBCCaseReference(form.model.reference)) {
-          return res.redirect(mcolUrl)
-        }
+        return renderView(form, res)
+      }
 
-        renderView(form, res)
-      } else {
-        res.redirect(Paths.homePage.uri)
+      switch (form.model.service) {
+        case Service.MONEYCLAIMS:
+          res.redirect(Paths.homePage.uri)
+          break
+        case Service.MCOL:
+          res.redirect(mcolUrl)
+          break
+        default:
+          throw new Error(`Unexpected service: ${form.model.service}`)
       }
     })
   )
