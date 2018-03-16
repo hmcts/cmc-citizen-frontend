cd2bbf051b0cdf7ac0cc0e4f5aac07d81170cfea
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-16 15:37:30 +0000

    More

--- src/integration-test/tests/citizen/govukAccessRoutes/steps/accessRoutesSteps.ts
+++ src/integration-test/tests/citizen/govukAccessRoutes/steps/accessRoutesSteps.ts
@@ -1,11 +1,12 @@
 import I = CodeceptJS.I
 
 const I: I = actor()
+const returnToClaimPage = '/return-to-claim'
 
 export class AccessRoutesSteps {
   returnToClaimMcol (): void {
-    I.amOnPage('/return-to-claim')
-    I.fillField('asdasdas')
+    I.amOnPage(returnToClaimPage)
+    I.fillField('#reference', 'AA12321321')
   }
 
   returnToClaimMoneyClaims (reference: string, username: string): void {
