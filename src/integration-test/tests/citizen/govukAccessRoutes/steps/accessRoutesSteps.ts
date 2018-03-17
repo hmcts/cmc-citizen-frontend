fe674c180544f6bea9bd14650a7e26c693e06bb6
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-17 09:01:07 +0000

    First test

--- src/integration-test/tests/citizen/govukAccessRoutes/steps/accessRoutesSteps.ts
+++ src/integration-test/tests/citizen/govukAccessRoutes/steps/accessRoutesSteps.ts
@@ -3,10 +3,20 @@ import I = CodeceptJS.I
 const I: I = actor()
 const returnToClaimPage = '/return-to-claim'
 
+const fields = {
+  reference: 'input[id=reference]'
+}
+
+const buttons = {
+  submit: 'input[type=submit]'
+}
+
 export class AccessRoutesSteps {
   returnToClaimMcol (): void {
     I.amOnPage(returnToClaimPage)
-    I.fillField('#reference', 'AA12321321')
+    I.fillField(fields.reference, 'AA12321321')
+    I.click(buttons.submit)
+    I.see('Money Claim Online')
   }
 
   returnToClaimMoneyClaims (reference: string, username: string): void {
