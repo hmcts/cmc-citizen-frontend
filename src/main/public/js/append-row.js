7e2c9cc6d3a2b90f9857dbd57fb1e503b4d52a33
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-03-19 15:16:09 +0000

    Remove validation errors markup from new rows

--- src/main/public/js/append-row.js
+++ src/main/public/js/append-row.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
   $('input[name="action[addRow]"]').click(function () {
     var lastRow = $('.claim-amount-row, .multiline-row').last()
-    var parent = lastRow.parent()
     var newRow = lastRow.clone()
-    newRow.html(incrementDomNodesIds)
-    newRow.find('input, textarea').val('')
-    parent.append(newRow)
+
+    incrementDomNodesIds(newRow)
+    sanitizeContent(newRow)
+
+    lastRow.parent().append(newRow)
+
     return false
   })
 
-  function incrementDomNodesIds (index, oldHtml) {
-    return oldHtml.replace(/rows\[(\d+)\]/g, function (match, capturedRowIndex) {
-      return 'rows[' + (parseInt(capturedRowIndex) + 1) + ']'
+  function incrementDomNodesIds (newRow) {
+    newRow.html(function (index, oldHtml) {
+      return oldHtml.replace(/rows\[(\d+)\]/g, function (match, capturedRowIndex) {
+        return 'rows[' + (parseInt(capturedRowIndex) + 1) + ']'
+      })
     })
+  }
+
+  function sanitizeContent (newRow) {
+    newRow.find('input, textarea').val('')
+
+    newRow.removeClass('form-group-error')
+    newRow.find('*').removeClass('form-control-error')
+    newRow.find('.error-message').remove()
   }
 })
