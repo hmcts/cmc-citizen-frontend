d8129a5e6d55a25e47fa35cf03de66c758c8b5fe
Damian Dunajski <damian.dunajski@hmcts.net>
2018-03-19 01:09:23 +0000

    Corrrect type of value

--- src/main/features/eligibility/routes/defendant-age.ts
+++ src/main/features/eligibility/routes/defendant-age.ts
@@ -1,7 +1,6 @@
 import { Paths } from 'eligibility/paths'
 
 import { EligibilityPage } from 'eligibility/eligibilityPage'
-import { YesNoOption } from 'models/yesNoOption'
 import { EligibilityCheck, eligible, notEligible } from 'eligibility/model/eligibilityCheck'
 import { NotEligibleReason } from 'claim/helpers/eligibility/notEligibleReason'
 import { DefendantAgeOption } from 'claim/form/models/eligibility/defendantAgeOption'
@@ -10,7 +9,8 @@ class DefendantAgeEligibilityPage extends EligibilityPage<DefendantAgeOption> {
   constructor () {
     super(Paths.defendantAgePage, Paths.claimTypePage, 'defendantAge')
   }
-  checkEligibility (value: YesNoOption): EligibilityCheck {
+
+  checkEligibility (value: DefendantAgeOption): EligibilityCheck {
     switch (value) {
       case DefendantAgeOption.YES:
         return eligible()
