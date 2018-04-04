d8ab15b184823cb40d58ccd7cadd8d554228cb27
Patryk Jar <jar.patryk@gmail.com>
2018-04-04 11:17:39 +0100

    ROC-3458: cache static files

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -19,7 +19,7 @@ export class Helmet {
 
   enableFor (app: express.Express) {
     app.use(helmet())
-    app.use(helmet.noCache())
+    app.all(/^.+(?!\/public\/.*)$/, helmet.noCache())
 
     new ContentSecurityPolicy(this.developmentMode).enableFor(app)
 
