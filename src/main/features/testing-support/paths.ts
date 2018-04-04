674fd45bd0c0728e8a49315f7a879aa457e7d8a2
jacmurphy <jack.murphy@hmcts.net>
2018-04-04 10:06:25 +0100

    Renamed Create Draft -> Create Claim Draft

--- src/main/features/testing-support/paths.ts
+++ src/main/features/testing-support/paths.ts
@@ -4,5 +4,5 @@ export class Paths {
   static readonly indexPage = new RoutablePath('/testing-support/index')
   static readonly updateResponseDeadlinePage = new RoutablePath('/testing-support/update-response-deadline')
   static readonly deleteDraftsPage = new RoutablePath('/testing-support/delete-drafts')
-  static readonly createDraftPage = new RoutablePath('/testing-support/create-draft')
+  static readonly createClaimDraftPage = new RoutablePath('/testing-support/create-claim-draft')
 }
