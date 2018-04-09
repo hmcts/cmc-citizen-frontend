363262532e30308ae2b2dad439df80757328ea5b
Damian Dunajski <damian.dunajski@hmcts.net>
2018-04-09 23:57:02 +0100

    Check claim ownership before downloading claim issue receipt in first contact journey

--- src/main/features/claim/routes/receipt.ts
+++ src/main/features/claim/routes/receipt.ts
@@ -1,9 +1,13 @@
 import * as express from 'express'
 import { Paths } from 'claim/paths'
+
 import { ErrorHandling } from 'common/errorHandling'
+import { ClaimMiddleware } from 'claims/claimMiddleware'
+
 import { ClaimIssueReceiptPDFGenerator } from 'services/claimIssueReceiptPdfGenerator'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
   .get(Paths.receiptReceiver.uri,
+    ClaimMiddleware.retrieveByExternalId,
     ErrorHandling.apply(ClaimIssueReceiptPDFGenerator.requestHandler))
