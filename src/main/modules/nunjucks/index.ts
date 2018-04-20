4d8936b2ae174dbd5777cc1ed6444bdefadcd91a
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-04-20 12:45:30 +0100

    Initial claim journey views page title change

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -126,6 +126,7 @@ export class Nunjucks {
     nunjucksEnv.addGlobal('HowMuchPaidClaimantOption', HowMuchPaidClaimantOption)
     nunjucksEnv.addGlobal('Service', Service)
     nunjucksEnv.addGlobal('cookieText', `GOV.UK uses cookies make the site simpler. <a href="${AppPaths.cookiesPage.uri}">Find out more about cookies</a>`)
+    nunjucksEnv.addGlobal('moneyClaims', `Money Claims`)
   }
 
   private convertPropertiesToBoolean (featureToggles: { [key: string]: any }): { [key: string]: boolean } {
