f426f74109fbc753d6480add6f4a1f814c49254f
Tim Jacomb <tim.jacomb@hmcts.net>
2018-04-23 08:30:30 +0100

    Remove blank line

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,8 +33,7 @@ locals {
 
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
   local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.aseName}"
-
-
+  
   previewVaultName = "${var.product}-citizen-fe"
   nonPreviewVaultName = "${var.product}-citizen-fe-${var.env}"
   vaultName = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
