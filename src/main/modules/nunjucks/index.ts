2f3735801c3f962c7bf48ba0b5957d748bb8744a
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-04-24 11:12:38 +0100

    Update templates to show headings in column two thirds and added confirmation variable

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -127,6 +127,7 @@ export class Nunjucks {
     nunjucksEnv.addGlobal('Service', Service)
     nunjucksEnv.addGlobal('cookieText', `GOV.UK uses cookies make the site simpler. <a href="${AppPaths.cookiesPage.uri}">Find out more about cookies</a>`)
     nunjucksEnv.addGlobal('moneyClaims', `Money Claims`)
+    nunjucksEnv.addGlobal('confirmation_heading', `Confirmation`)
   }
 
   private convertPropertiesToBoolean (featureToggles: { [key: string]: any }): { [key: string]: boolean } {
