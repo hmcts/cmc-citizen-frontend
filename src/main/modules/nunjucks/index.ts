f6e249324b20c10760e89fd89ea3fdb7e5419b3d
rattur <rattur2002@yahoo.co.uk>
2018-05-01 16:46:17 +0100

    ROC-3374: Make page titles consistent  (#548)

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -126,6 +126,8 @@ export class Nunjucks {
     nunjucksEnv.addGlobal('HowMuchPaidClaimantOption', HowMuchPaidClaimantOption)
     nunjucksEnv.addGlobal('Service', Service)
     nunjucksEnv.addGlobal('cookieText', `GOV.UK uses cookies make the site simpler. <a href="${AppPaths.cookiesPage.uri}">Find out more about cookies</a>`)
+    nunjucksEnv.addGlobal('serviceName', `Money Claims`)
+    nunjucksEnv.addGlobal('headingVisible', true)
   }
 
   private convertPropertiesToBoolean (featureToggles: { [key: string]: any }): { [key: string]: boolean } {
