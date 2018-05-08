66d76d62fcfaa35eec2d9ab5ad34a5e1d1f35c5e
andrewwa-kainos <andrewwa@kainos.com>
2018-05-08 11:53:58 +0100

    ROC-2980 Update close ports script only to affect ports >50000 and increase timeout of routes tests

--- bin/close-idea-ports.sh
+++ bin/close-idea-ports.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-lsof -ni TCP | awk '/^idea.*\(LISTEN\)/ {system("lldb -p " $2 " -o \"call (int)close(" $4 ")\"")}'
+lsof -ni TCP | \
+  awk '{split($9,a,":"); if ($1 == "idea" && a[2] > 50000 && $10 == "(LISTEN)") { system("lldb -p " $2 " -o \"call (int)close(" $4 ")\"")} }'
