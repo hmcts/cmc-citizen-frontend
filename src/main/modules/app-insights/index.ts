00cbf138ced607c2812ca90ad2c2798a4b3d74aa
Szymon Filipczyk <s.filipczyk@kainos.com>
2018-05-15 17:22:46 +0100

    obfuscate UUID's in url's which are not static files

--- src/main/modules/app-insights/index.ts
+++ src/main/modules/app-insights/index.ts
@@ -13,6 +13,32 @@ export class AppInsights {
     appInsights.setup(appInsightsConfig.instrumentationKey)
       .setAutoCollectConsole(true, true)
     appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = appInsightsConfig.roleName
+
+    let fileRegexp = new RegExp('(\\..{2,5}$)')
+    let removeUuidRegexp = new RegExp('[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}', 'g')
+
+    appInsights.defaultClient.addTelemetryProcessor(function (envelope, contextObjects) {
+      if (contextObjects) {
+        if (contextObjects.correlationContext) {
+          // if there's operation, rename request having claim UUID to {uuid} mask to aggregate them correctly
+          if (contextObjects.correlationContext.operation) {
+            let operationName = contextObjects.correlationContext.operation.name
+
+            // if operation's name is not static file
+            if (operationName.match(fileRegexp) == null) {
+              let newOperationName = operationName.replace(removeUuidRegexp, '{uuid}')
+              // console.log(contextObjects.correlationContext.operation.name)
+              // console.log(newOperationName)
+
+              contextObjects.correlationContext.operation.name = newOperationName
+            }
+          }
+        }
+      }
+      // always send
+      return true
+    })
+
     appInsights.start()
   }
 }
