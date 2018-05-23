8f734c86034e59fd4defe6de1d1a8293967d0c53
Kiran Varma <varma.ap@gmail.com>
2018-05-23 23:11:06 +0100

    unit test updated to redirect to task list after payment date

--- src/test/features/response/routes/pay-by-set-date/payment-date.ts
+++ src/test/features/response/routes/pay-by-set-date/payment-date.ts
@@ -33,17 +33,6 @@ function nextDay () {
   }
 }
 
-function date29DaysFromToday () {
-  const dateLaterThan28DaysFromToday: moment.Moment = moment().add(29, 'days')
-  return {
-    date: {
-      year: dateLaterThan28DaysFromToday.year().toString(),
-      month: (dateLaterThan28DaysFromToday.month() + 1).toString(),
-      day: dateLaterThan28DaysFromToday.date().toString()
-    }
-  }
-}
-
 describe('Pay by set date : payment date', () => {
   attachDefaultHooks(app)
 
@@ -134,7 +123,7 @@ describe('Pay by set date : payment date', () => {
             .expect(res => expect(res).to.be.successful.withText(ValidationErrors.DATE_REQUIRED))
         })
 
-        it('should redirect to task list when data is valid and user provides a date within 28 days from today', async () => {
+        it('should redirect to task list when data is valid', async () => {
           draftStoreServiceMock.resolveFind('response')
           draftStoreServiceMock.resolveSave()
 
@@ -145,21 +134,6 @@ describe('Pay by set date : payment date', () => {
             .expect(res => expect(res).to.be.redirect
               .toLocation(
                 Paths.taskListPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId })
-              )
-            )
-        })
-
-        it('should redirect to explanation page when data is valid and user provides a date later than 28 days from today', async () => {
-          draftStoreServiceMock.resolveFind('response')
-          draftStoreServiceMock.resolveSave()
-
-          await request(app)
-            .post(pagePath)
-            .set('Cookie', `${cookieName}=ABC`)
-            .send(date29DaysFromToday())
-            .expect(res => expect(res).to.be.redirect
-              .toLocation(
-                PayBySetDatePaths.explanationPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId })
               )
             )
         })
