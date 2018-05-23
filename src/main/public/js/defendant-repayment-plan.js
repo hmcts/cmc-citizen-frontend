d8bfd8a372782b4878014eb01b6a1446e9ee705d
Kiran Varma <varma.ap@gmail.com>
2018-05-23 22:06:03 +0100

    added comments to ask questions

--- src/main/public/js/defendant-repayment-plan.js
+++ src/main/public/js/defendant-repayment-plan.js
@@ -2,12 +2,24 @@ $(document).ready(function () {
 
   $('input[name="paymentSchedule"]').click(function () {
 
+    //need total amount claimed to calculate length of payment
+    //how do i get total amount claimed to use here?
     var instalmentAmount = $('#instalmentAmount').val()
 
     function calculateLengthOfPayment(paymentScheduleVal) {
+      //wrong calculation - to be corrected
       return Math.ceil(instalmentAmount/paymentScheduleVal)
     }
 
+    //exact calculation still to be worked on
+    /*
+    total claim amount = 1000
+    regular payments = 50
+    if each week --> 1000/50 = 20 instalments = 20 weeks
+    if every two weeks --> 1000/50 = 20 instalments = 40 weeks
+    if every month --> 1000/50 = 20 instalments = 20 months
+
+     */
     switch ($(this).attr('id')) {
       case 'paymentScheduleEACH_WEEK':
         $('#lengthOfRepayment').text(calculateLengthOfPayment(7))
