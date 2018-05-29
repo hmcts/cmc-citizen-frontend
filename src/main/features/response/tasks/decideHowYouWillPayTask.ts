3fbe28e04e38af32c5fa9f9caa4be866132a8671
Damian Dunajski <damian.dunajski@hmcts.net>
2018-05-29 15:49:20 +0100

    Remove SoM validation from 'DecideHowYouWillPayTask'

--- src/main/features/response/tasks/decideHowYouWillPayTask.ts
+++ src/main/features/response/tasks/decideHowYouWillPayTask.ts
@@ -2,7 +2,6 @@ import { Validator } from 'class-validator'
 
 import { ResponseDraft } from 'response/draft/responseDraft'
 import { DefendantPaymentType } from 'response/form/models/defendantPaymentOption'
-import { StatementOfMeans } from 'response/draft/statementOfMeans'
 
 const validator = new Validator()
 
@@ -15,7 +14,6 @@ export class DecideHowYouWillPayTask {
     return responseDraft.fullAdmission
       && isValid(responseDraft.fullAdmission.defendantPaymentOption)
       && this.paymentDetailsAreProvidedFor(responseDraft)
-      && this.statementOfMeansIsCompletedIfApplicable(responseDraft)
   }
 
   private static paymentDetailsAreProvidedFor (responseDraft: ResponseDraft): boolean {
@@ -29,63 +27,5 @@ export class DecideHowYouWillPayTask {
       default:
         throw new Error(`Unknown payment option: ${responseDraft.fullAdmission.defendantPaymentOption.option}`)
     }
-  }
-
-  private static statementOfMeansIsCompletedIfApplicable (responseDraft: ResponseDraft): boolean {
-    if (StatementOfMeans.isApplicableFor(responseDraft)) {
-      const statementOfMeans: StatementOfMeans = responseDraft.statementOfMeans
-
-      return statementOfMeans !== undefined
-        && isValid(statementOfMeans.residence)
-        && DecideHowYouWillPayTask.isDependantsCompleted(statementOfMeans)
-        && DecideHowYouWillPayTask.isEmploymentCompleted(statementOfMeans)
-        && isValid(statementOfMeans.bankAccounts)
-        && isValid(statementOfMeans.debts)
-        && isValid(statementOfMeans.monthlyIncome)
-        && isValid(statementOfMeans.monthlyExpenses)
-        && isValid(statementOfMeans.courtOrders)
-    }
-
-    return true
-  }
-
-  private static isDependantsCompleted (statementOfMeans: StatementOfMeans): boolean {
-    const dependantValid = isValid(statementOfMeans.dependants)
-
-    if (!dependantValid) {
-      return false
-    }
-
-    if (statementOfMeans.dependants.hasAnyChildren && statementOfMeans.dependants.numberOfChildren.between16and19 > 0) {
-      return isValid(statementOfMeans.education)
-        && isValid(statementOfMeans.maintenance)
-        && isValid(statementOfMeans.supportedByYou)
-    }
-
-    return isValid(statementOfMeans.maintenance) && isValid(statementOfMeans.supportedByYou)
-  }
-
-  private static isEmploymentCompleted (som: StatementOfMeans): boolean {
-    const employmentValid = isValid(som.employment)
-
-    if (!employmentValid) {
-      return false
-    }
-
-    if (!som.employment.isCurrentlyEmployed) {
-      return isValid(som.unemployed)
-    }
-
-    let result: boolean = true
-
-    if (som.employment.selfEmployed) {
-      result = result && isValid(som.selfEmployed)
-    }
-
-    if (som.employment.employed) {
-      result = result && isValid(som.employers)
-    }
-
-    return result
   }
 }
