2c7496e496e0b073db14430dd28d8b1d830392e0
Kiran Varma <varma.ap@gmail.com>
2018-05-30 12:14:49 +0100

    removed defendant-repayment-plan as its replaced with paymentPlan.ts

--- src/test/common/paymentPlan.ts
+++ src/test/common/paymentPlan.ts
@@ -4,17 +4,17 @@ import { expect } from 'chai'
 import * as moment from 'moment'
 
 const TOTAL_AMOUNT = 1000
-const INSTALMENT_AMOUNT = 10
 
-describe('PaymentPlan',() => {
+describe.only('PaymentPlan',() => {
   describe('getLastPaymentDate', () => {
     it('should return the last payment date from given fromDate', () => {
+      const instalmentAmount = 10
       const fromDate = moment('2018-01-01')
-      const numberOfInstalmentsInWeeks = 100
-      const expectedLastPaymentDate = fromDate.add(numberOfInstalmentsInWeeks)
+      const numberOfInstalmentsInWeeks = 200
+      const expectedLastPaymentDate = fromDate.clone().add(numberOfInstalmentsInWeeks, 'weeks')
 
       const frequencyInWeeks = 2
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const lastPaymentDate = paymentPlan.getLastPaymentDate(fromDate)
 
       expect(lastPaymentDate.isSame(expectedLastPaymentDate)).to.be.true
@@ -22,67 +22,76 @@ describe('PaymentPlan',() => {
   })
 
   describe('getPaymentLength', () => {
-    it.only('should return a payment length that has no year showing', () => {
-      const frequencyInWeeks = 5
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+    it('should return a payment length that has no year showing', () => {
+      const instalmentAmount = 105
+      const frequencyInWeeks = 4
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const paymentLength = paymentPlan.getPaymentLength()
-      expect(paymentLength).to.equal('5 months 1 week')
+      expect(paymentLength).to.equal('8 months 3 weeks')
     })
 
     it('should return a payment length that has exactly 1 year showing', () => {
+      const instalmentAmount = 80
       const frequencyInWeeks = 5
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const paymentLength = paymentPlan.getPaymentLength()
-      expect(paymentLength).to.equal('1 year 5 months 2 weeks')
+      expect(paymentLength).to.equal('1 year 2 months 1 week')
     })
 
     it('should return a payment length that has more than 1 year showing', () => {
+      const instalmentAmount = 20
       const frequencyInWeeks = 5
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const paymentLength = paymentPlan.getPaymentLength()
-      expect(paymentLength).to.equal('2 years 5 months 2 weeks')
+      expect(paymentLength).to.equal('4 years 9 months 2 weeks')
     })
 
     it('should return a payment length that has no month showing', () => {
+      const instalmentAmount = 94
       const frequencyInWeeks = 5
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const paymentLength = paymentPlan.getPaymentLength()
-      expect(paymentLength).to.equal('1 year 2 weeks')
+      expect(paymentLength).to.equal('1 year 1 week')
     })
 
     it('should return a payment length that has exactly 1 month showing', () => {
+      const instalmentAmount = 85
       const frequencyInWeeks = 5
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const paymentLength = paymentPlan.getPaymentLength()
       expect(paymentLength).to.equal('1 year 1 month 2 weeks')
     })
 
     it('should return a payment length that has more than 1 month showing', () => {
+      const instalmentAmount = 70
       const frequencyInWeeks = 5
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const paymentLength = paymentPlan.getPaymentLength()
-      expect(paymentLength).to.equal('1 year 2 months 2 weeks')
+      expect(paymentLength).to.equal('1 year 4 months 1 week')
     })
 
     it('should return a payment length that has no week showing', () => {
+      const instalmentAmount = 25
       const frequencyInWeeks = 5
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const paymentLength = paymentPlan.getPaymentLength()
-      expect(paymentLength).to.equal('1 year 5 months')
+      expect(paymentLength).to.equal('3 years 10 months')
     })
 
     it('should return a payment length that has exactly 1 week showing', () => {
-      const frequencyInWeeks = 5
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+      const instalmentAmount = 1000
+      const frequencyInWeeks = 1
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const paymentLength = paymentPlan.getPaymentLength()
-      expect(paymentLength).to.equal('1 year 5 months 1 week')
+      expect(paymentLength).to.equal('1 week')
     })
 
     it('should return a payment length that has more than 1 week showing', () => {
-      const frequencyInWeeks = 5
-      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, INSTALMENT_AMOUNT, frequencyInWeeks)
+      const instalmentAmount = 1000
+      const frequencyInWeeks = 2
+      const paymentPlan = new PaymentPlan(TOTAL_AMOUNT, instalmentAmount, frequencyInWeeks)
       const paymentLength = paymentPlan.getPaymentLength()
-      expect(paymentLength).to.equal('1 year 5 months 2 weeks')
+      expect(paymentLength).to.equal('2 weeks')
     })
   })
 })
