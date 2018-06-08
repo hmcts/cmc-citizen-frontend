a42808421a53009bfee00844453fe5fc9eef0aea
Ellie Harrison <ellieh@kainos.com>
2018-06-08 08:59:40 +0100

    Fix issue with countersigning being blocked

--- src/main/features/offer/guards/offerAcceptedGuard.ts
+++ src/main/features/offer/guards/offerAcceptedGuard.ts
@@ -3,6 +3,7 @@ import * as express from 'express'
 import { Paths } from 'dashboard/paths'
 import { Logger } from '@hmcts/nodejs-logging'
 import { Claim } from 'claims/models/claim'
+import { User } from 'idam/user'
 
 const logger = Logger.getLogger('offer/guards/offerAcceptedGuard')
 
@@ -10,8 +11,12 @@ export class OfferAcceptedGuard {
 
   static requestHandler (req: express.Request, res: express.Response, next: express.NextFunction): void {
     const claim: Claim = res.locals.claim
+    const user: User = res.locals.user
 
-    if (claim.settlement.isOfferAccepted()) {
+    if (claim.settlementReachedAt) {
+      logger.debug('State guard: offer settlement reached, redirecting to dashboard')
+      res.redirect(Paths.dashboardPage.uri)
+    } else if (user.id === claim.claimantId && claim.settlement.isOfferResponded()) {
       logger.debug('State guard: offer already accepted, redirecting to dashboard')
       res.redirect(Paths.dashboardPage.uri)
     } else {
