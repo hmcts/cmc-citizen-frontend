6ed751943ea57d3a92c55098e0edb6282d117e12
Kiran Varma <varma.ap@gmail.com>
2018-06-18 17:44:06 +0100

    renamed to something more accurate

--- src/test/app/common/calculateMonthlyIncomeExpense.ts
+++ src/test/app/common/calculateMonthlyIncomeExpense.ts
@@ -1,49 +1,49 @@
 import { expect } from 'chai'
-import { calculateTotalAmount, IncomeExpenseSource } from 'common/monthlyIncomeExpense'
+import { calculateTotalAmount, IncomeExpenseSource } from 'common/calculateMonthlyIncomeExpense'
 import { IncomeExpenseSchedule } from 'common/incomeExpenseSchedule'
 
 describe('IncomeExpenseSource', () => {
 
   it('should calculate total amount using weekly schedule', () => {
 
-    let income: IncomeExpenseSource[] = [
+    const incomeOrExpense: IncomeExpenseSource[] = [
       { amount: 100, incomeExpenseSchedule: IncomeExpenseSchedule.WEEK }
     ]
-    expect(calculateTotalAmount(income)).to.equal(433.33)
+    expect(calculateTotalAmount(incomeOrExpense)).to.equal(433.33)
   })
 
   it('should calculate total amount using biweekly schedule', () => {
 
-    let income: IncomeExpenseSource[] = [
+    const incomeOrExpense: IncomeExpenseSource[] = [
       { amount: 100, incomeExpenseSchedule: IncomeExpenseSchedule.TWO_WEEKS }
     ]
-    expect(calculateTotalAmount(income)).to.equal(216.67)
+    expect(calculateTotalAmount(incomeOrExpense)).to.equal(216.67)
   })
 
   it('should calculate total amount using four weekly schedule', () => {
 
-    let income: IncomeExpenseSource[] = [
+    const incomeOrExpense: IncomeExpenseSource[] = [
       { amount: 100, incomeExpenseSchedule: IncomeExpenseSchedule.FOUR_WEEKS }
     ]
-    expect(calculateTotalAmount(income)).to.equal(108.33)
+    expect(calculateTotalAmount(incomeOrExpense)).to.equal(108.33)
   })
 
   it('should calculate total amount using monthly schedule', () => {
 
-    let income: IncomeExpenseSource[] = [
+    const incomeOrExpense: IncomeExpenseSource[] = [
       { amount: 100, incomeExpenseSchedule: IncomeExpenseSchedule.MONTH }
     ]
-    expect(calculateTotalAmount(income)).to.equal(100)
+    expect(calculateTotalAmount(incomeOrExpense)).to.equal(100)
   })
 
-  it('should calculate total amount using multiple amounts and different income schedules', () => {
+  it('should calculate total amount using multiple amounts and different income or expense schedules', () => {
 
-    let income: IncomeExpenseSource[] = [
+    const incomesOrExpenses: IncomeExpenseSource[] = [
       { amount: 100, incomeExpenseSchedule: IncomeExpenseSchedule.WEEK },
       { amount: 200, incomeExpenseSchedule: IncomeExpenseSchedule.TWO_WEEKS },
       { amount: 300, incomeExpenseSchedule: IncomeExpenseSchedule.FOUR_WEEKS },
       { amount: 400, incomeExpenseSchedule: IncomeExpenseSchedule.MONTH }
     ]
-    expect(calculateTotalAmount(income)).to.equal(1591.67)
+    expect(calculateTotalAmount(incomesOrExpenses)).to.equal(1591.67)
   })
 })
