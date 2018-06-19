d274cdf4582bbc7e302ba702ef1604ed7553fb26
Ronan Fitzgerald <ronan.fitzgerald@hmcts.net>
2018-06-19 15:01:15 +0100

    Removing 'why you're owed the money' content
    Further to Tim's comment on this PR.

--- src/test/features/claim/routes/reason.ts
+++ src/test/features/claim/routes/reason.ts
@@ -30,7 +30,7 @@ describe('Claim issue: reason page', () => {
       await request(app)
         .get(ClaimPaths.reasonPage.uri)
         .set('Cookie', `${cookieName}=ABC`)
-        .expect(res => expect(res).to.be.successful.withText('Why you’re owed the money'))
+        .expect(res => expect(res).to.be.successful.withText('Why you believe you’re owed the money'))
     })
 
     it('should render page when everything is fine without a name field', async () => {
@@ -40,7 +40,7 @@ describe('Claim issue: reason page', () => {
       await request(app)
         .get(ClaimPaths.reasonPage.uri)
         .set('Cookie', `${cookieName}=ABC`)
-        .expect(res => expect(res).to.be.successful.withText('Why you’re owed the money'))
+        .expect(res => expect(res).to.be.successful.withText('Why you believe you’re owed the money'))
     })
   })
 
@@ -59,7 +59,7 @@ describe('Claim issue: reason page', () => {
         await request(app)
           .post(ClaimPaths.reasonPage.uri)
           .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText('Why you’re owed the money', 'div class="error-summary"'))
+          .expect(res => expect(res).to.be.successful.withText('Why you believe you’re owed the money', 'div class="error-summary"'))
       })
 
       it('should return 500 and render error page when form is valid and cannot save draft', async () => {
