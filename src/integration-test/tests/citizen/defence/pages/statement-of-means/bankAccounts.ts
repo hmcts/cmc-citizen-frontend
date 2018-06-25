e7e83ef2a57ab7f3f43e6310e11dbd1b41139530
Damian Dunajski <damian.dunajski@hmcts.net>
2018-06-25 17:34:11 +0100

    Add E2E tests

--- src/integration-test/tests/citizen/defence/pages/statement-of-means/bankAccounts.ts
+++ src/integration-test/tests/citizen/defence/pages/statement-of-means/bankAccounts.ts
@@ -2,11 +2,25 @@ import I = CodeceptJS.I
 
 const I: I = actor()
 
+const fields = {
+  row: {
+    typeOfAccount: 'rows[0][typeOfAccount]',
+    joint: 'rows[0][joint]',
+    balance: 'rows[0][balance]'
+  }
+}
+
 const buttons = {
   submit: 'input[id="saveAndContinue"]'
 }
 
 export class BankAccountsPage {
+
+  enterBankAccount (typeOfAccount: string, joint: boolean, balance: number): void {
+    I.selectOption(fields.row.typeOfAccount, typeOfAccount)
+    I.selectOption(fields.row.joint, joint ? 'Yes' : 'No')
+    I.fillField(fields.row.balance, balance.toFixed())
+  }
 
   clickContinue (): void {
     I.click(buttons.submit)
--- src/integration-test/tests/citizen/defence/steps/statementOfMeans.ts
+++ src/integration-test/tests/citizen/defence/steps/statementOfMeans.ts
@@ -1,7 +1,11 @@
 import { DependantsPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/dependants'
+import { EducationPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/education'
+import { EmployersPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/employers'
 import { EmploymentPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/employment'
 import { MaintenancePage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/maintenance'
+import { OnTaxPaymentsPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/on-tax-payments'
 import { ResidencePage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/residence'
+import { SelfEmploymentPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/self-employment'
 import { StartPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/start'
 import { BankAccountsPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/bankAccounts'
 import { OtherDependantsPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/other-dependants'
@@ -10,37 +14,95 @@ import { DebtsPage } from 'integration-test/tests/citizen/defence/pages/statemen
 import { CourtOrdersPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/courtOrders'
 import { MonthlyIncomePage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/monthlyIncome'
 import { MonthlyExpensesPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/monthlyExpenses'
-import { CannotPayImmediatelyPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/explain-why-cannot-pay-immediately'
+import { ExplanationPage } from 'integration-test/tests/citizen/defence/pages/statement-of-means/explanation'
 
-const somStartPage: StartPage = new StartPage()
-const somResidencePage: ResidencePage = new ResidencePage()
-const somDependantsPage: DependantsPage = new DependantsPage()
-const somMaintenancePage: MaintenancePage = new MaintenancePage()
-const somEmploymentPage: EmploymentPage = new EmploymentPage()
-const somBankAccountsPage: BankAccountsPage = new BankAccountsPage()
-const somOtherDependantsPage: OtherDependantsPage = new OtherDependantsPage()
-const somUnemploymentPage: UnemploymentPage = new UnemploymentPage()
-const somDebtsPage: DebtsPage = new DebtsPage()
-const somMonthlyIncomePage: MonthlyIncomePage = new MonthlyIncomePage()
-const somMonthlyExpensesPage: MonthlyExpensesPage = new MonthlyExpensesPage()
-const somCourtOrdersPage: CourtOrdersPage = new CourtOrdersPage()
-const somCannotPayImmediatelyPage: CannotPayImmediatelyPage = new CannotPayImmediatelyPage()
+const startPage: StartPage = new StartPage()
+const bankAccountsPage: BankAccountsPage = new BankAccountsPage()
+const residencePage: ResidencePage = new ResidencePage()
+const dependantsPage: DependantsPage = new DependantsPage()
+const educationPage: EducationPage = new EducationPage()
+const maintenancePage: MaintenancePage = new MaintenancePage()
+const otherDependantsPage: OtherDependantsPage = new OtherDependantsPage()
+const employmentPage: EmploymentPage = new EmploymentPage()
+const employersPage: EmployersPage = new EmployersPage()
+const selfEmploymentPage: SelfEmploymentPage = new SelfEmploymentPage()
+const onTaxPaymentsPage: OnTaxPaymentsPage = new OnTaxPaymentsPage()
+const unemploymentPage: UnemploymentPage = new UnemploymentPage()
+const incomePage: MonthlyIncomePage = new MonthlyIncomePage()
+const expensesPage: MonthlyExpensesPage = new MonthlyExpensesPage()
+const debtsPage: DebtsPage = new DebtsPage()
+const courtOrdersPage: CourtOrdersPage = new CourtOrdersPage()
+const explanationPage: ExplanationPage = new ExplanationPage()
 
 export class StatementOfMeansSteps {
 
-  fillStatementOfMeansData (): void {
-    somStartPage.clickContinue()
-    somBankAccountsPage.clickContinue()
-    somResidencePage.selectOwnHome()
-    somDependantsPage.selectDontHaveChildren()
-    somMaintenancePage.selectDontPayMaintenance()
-    somOtherDependantsPage.selectDontSupportAnyone()
-    somEmploymentPage.selectNotWorkingCurrently()
-    somUnemploymentPage.selectRetired()
-    somDebtsPage.selectDontHaveDebts()
-    somMonthlyIncomePage.fillOutAllFieldsAndContinue()
-    somMonthlyExpensesPage.fillOutAllFieldsAndContinue()
-    somCourtOrdersPage.selectDontHaveCourtOrders()
-    somCannotPayImmediatelyPage.enterExplaination()
+  fillStatementOfMeansWithMinimalDataSet (): void {
+    startPage.clickContinue()
+    bankAccountsPage.enterBankAccount('Current account', false, 1000)
+    bankAccountsPage.clickContinue()
+    residencePage.selectOwnHome()
+    residencePage.clickContinue()
+    dependantsPage.selectNotDeclared()
+    dependantsPage.clickContinue()
+    maintenancePage.selectNotDeclared()
+    maintenancePage.clickContinue()
+    otherDependantsPage.selectNotDeclared()
+    otherDependantsPage.clickContinue()
+    employmentPage.selectNotDeclared()
+    employmentPage.clickContinue()
+    unemploymentPage.selectRetired()
+    unemploymentPage.clickContinue()
+    debtsPage.selectNotDeclared()
+    debtsPage.clickContinue()
+    incomePage.fillOutAllFields()
+    incomePage.clickContinue()
+    expensesPage.fillOutAllFields()
+    expensesPage.clickContinue()
+    courtOrdersPage.selectNotDeclared()
+    courtOrdersPage.clickContinue()
+    explanationPage.enterExplanation('I cannot pay immediately')
+    explanationPage.clickContinue()
+  }
+
+  fillStatementOfMeansWithFullDataSet (): void {
+    startPage.clickContinue()
+    bankAccountsPage.enterBankAccount('Saving account', true, 10000)
+    bankAccountsPage.clickContinue()
+    residencePage.selectOther('Special home')
+    residencePage.clickContinue()
+    dependantsPage.selectDeclared()
+    dependantsPage.enterNumberOfChildren(1, 2, 3)
+    dependantsPage.clickContinue()
+    educationPage.enterNumberOfChildren(3)
+    educationPage.clickContinue()
+    maintenancePage.selectDeclared()
+    maintenancePage.enterNumberOfChildren(4)
+    maintenancePage.clickContinue()
+    otherDependantsPage.selectDeclared()
+    otherDependantsPage.enterNumberOfPeople(5, 'Colleagues')
+    otherDependantsPage.clickContinue()
+    employmentPage.selectDeclared()
+    employmentPage.tickEmployed()
+    employmentPage.tickSelfEmployed()
+    employmentPage.clickContinue()
+    employersPage.enterDetails('Happy Dogs and Cats', 'Junior Accountant')
+    employersPage.clickContinue()
+    selfEmploymentPage.enterDetails('CEO', 1000)
+    selfEmploymentPage.clickContinue()
+    onTaxPaymentsPage.selectDeclared()
+    onTaxPaymentsPage.enterDetails(100, 'Various taxes')
+    onTaxPaymentsPage.clickContinue()
+    debtsPage.selectDeclared()
+    debtsPage.enterDebt('Wife\'s debt', 100, 10)
+    debtsPage.clickContinue()
+    incomePage.fillOutAllFields()
+    incomePage.clickContinue()
+    expensesPage.fillOutAllFields()
+    expensesPage.clickContinue()
+    courtOrdersPage.selectDeclared()
+    courtOrdersPage.enterCourtOrder('000MC001', 100, 10)
+    courtOrdersPage.clickContinue()
+    explanationPage.enterExplanation('I cannot pay immediately')
+    explanationPage.clickContinue()
   }
 }
