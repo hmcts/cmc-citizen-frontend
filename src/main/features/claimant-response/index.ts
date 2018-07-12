a70523f2993970ed9d8f9dbbf811d32100408857
Sabah u Din Irfan <sabah.irfan@gmail.com>
2018-07-12 16:15:01 +0100

    ROC-4056: Create claimant response task list (#663)

---
+++ src/main/features/claimant-response/index.ts
@@ -0,0 +1,36 @@
+import * as express from 'express'
+import * as path from 'path'
+
+import { AuthorizationMiddleware } from 'idam/authorizationMiddleware'
+import { RouterFinder } from 'shared/router/routerFinder'
+import { ClaimMiddleware } from 'claims/claimMiddleware'
+import { DraftMiddleware } from '@hmcts/cmc-draft-store-middleware'
+import { DraftService } from 'services/draftService'
+import { OnlyClaimantLinkedToClaimCanDoIt } from 'guards/onlyClaimantLinkedToClaimCanDoIt'
+import { OAuthHelper } from 'idam/oAuthHelper'
+import { DraftClaimantResponse } from 'features/claimant-response/draft/draftClaimantResponse'
+
+function requestHandler (): express.RequestHandler {
+  function accessDeniedCallback (req: express.Request, res: express.Response): void {
+    res.redirect(OAuthHelper.forLogin(req, res))
+  }
+
+  const requiredRoles = ['citizen']
+  const unprotectedPaths = []
+  return AuthorizationMiddleware.requestHandler(requiredRoles, accessDeniedCallback, unprotectedPaths)
+}
+
+export class ClaimantResponseFeature {
+  enableFor (app: express.Express) {
+    const allClaimantResponse = '/case/*/claimant-response/*'
+    app.all(allClaimantResponse, requestHandler())
+    app.all(allClaimantResponse, ClaimMiddleware.retrieveByExternalId)
+    app.all(allClaimantResponse, OnlyClaimantLinkedToClaimCanDoIt.check())
+    app.all(/^\/case\/.+\/claimant-response\/(?!confirmation).*$/,
+      DraftMiddleware.requestHandler(new DraftService(), 'claimantResponse', 100, (value: any): DraftClaimantResponse => {
+        return new DraftClaimantResponse().deserialize(value)
+      }))
+
+    app.use('/', RouterFinder.findAll(path.join(__dirname, 'routes')))
+  }
+}
