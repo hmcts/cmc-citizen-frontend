6840ee7172cbca5b3a85f076a07e584f668d2eb0
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-07-12 14:54:36 +0100

    Initial components for defendants response view

--- src/main/features/claimant-response/paths.ts
+++ src/main/features/claimant-response/paths.ts
@@ -8,4 +8,6 @@ export class Paths {
   static readonly confirmationPage = new RoutablePath(`${claimantResponsePath}/confirmation`)
   static readonly incompleteSubmissionPage = new RoutablePath(`${claimantResponsePath}/incomplete-submission`)
   static readonly notImplementedYetPage = new RoutablePath(`${claimantResponsePath}/not-implemented-yet`)
+  static readonly defendantsResponsePage = new RoutablePath(`${claimantResponsePath}/defendants-response`)
+
 }
