b3bcad76a7ff4ff59f6398f7327baeac528d198d
Sabah Irfan <sabah.irfan@gmail.com>
2018-07-12 14:19:42 +0100

    Add unit tests

--- src/test/http-mocks/draft-store.ts
+++ src/test/http-mocks/draft-store.ts
@@ -325,7 +325,7 @@ const sampleCCJDraftObj = {
   }
 }
 
-const sampleClaimantResponseDraftObj = {
+export const sampleClaimantResponseDraftObj = {
   externalId: 'fe6e9413-e804-48d5-bbfd-645917fc46e5'
 }
 
