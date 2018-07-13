ffd89c4b05d860c246e34991175fedbcf6a64517
jacmurphy <jack.murphy@hmcts.net>
2018-07-13 13:31:08 +0100

    Updated validation messages

--- src/main/features/response/form/models/howMuchDoYouOwe.ts
+++ src/main/features/response/form/models/howMuchDoYouOwe.ts
@@ -1,22 +1,29 @@
 import { IsDefined, IsPositive } from 'class-validator'
-import { Fractions } from '@hmcts/cmc-validators'
+import { Fractions, IsLessThan, Min } from '@hmcts/cmc-validators'
 import { ValidationErrors as DefaultValidationErrors } from 'forms/validation/validationErrors'
 import { toNumberOrUndefined } from 'shared/utils/numericUtils'
+import response from 'offer/routes/response'
 
 export class ValidationErrors {
   static readonly AMOUNT_NOT_VALID: string = 'Enter valid amount'
+  static readonly AMOUNT_LESS_THAN_CLAIMED: string = 'Enter a value less then the claimed amount'
 }
+
+// TODO AMOUNT_LESS_THAN_CLAIMED is a placeholder and has not been approved by PO or content. It was missing in story 3662
 
 export class HowMuchDoYouOwe {
 
   @IsDefined({ message: DefaultValidationErrors.AMOUNT_REQUIRED })
+  @IsLessThan('totalAmount', { message: ValidationErrors.AMOUNT_LESS_THAN_CLAIMED })
   @IsPositive({ message: ValidationErrors.AMOUNT_NOT_VALID })
   @Fractions(0, 2, { message: DefaultValidationErrors.AMOUNT_INVALID_DECIMALS })
   amount?: number
 
-  constructor (amount?: number) {
-    this.amount = amount
+  totalAmount?: number
 
+  constructor (amount?: number, totalAmount?: number) {
+    this.amount = amount
+    this.totalAmount = totalAmount
   }
 
   static fromObject (value?: any): HowMuchDoYouOwe {
@@ -25,13 +32,15 @@ export class HowMuchDoYouOwe {
     }
 
     const amount = toNumberOrUndefined(value.amount)
+    const totalAmount = toNumberOrUndefined(value.totalAmount)
 
-    return new HowMuchDoYouOwe(amount)
+    return new HowMuchDoYouOwe(amount, totalAmount)
   }
 
   deserialize (input: any): HowMuchDoYouOwe {
     if (input) {
       this.amount = input.amount
+      this.totalAmount = input.totalAmount
     }
 
     return this
