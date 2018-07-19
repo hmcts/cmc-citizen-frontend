52d8fef05da901e425d59afd462fe20def8f2de0
jacmurphy <jack.murphy@hmcts.net>
2018-07-19 12:02:39 +0100

    Add HowMuchDoYouOwe tests

--- src/main/app/forms/validation/validationErrors.ts
+++ src/main/app/forms/validation/validationErrors.ts
@@ -9,7 +9,7 @@ export class ValidationErrors {
   static readonly NUMBER_REQUIRED: string = 'Enter a valid number'
   static readonly VALID_OWED_AMOUNT_REQUIRED: string = 'Enter a valid amount owed'
   static readonly AMOUNT_REQUIRED: string = 'Enter an amount'
-  static readonly AMOUNT_INVALID_DECIMALS: string = 'Enter valid amount, maximum two decimal places'
+  static readonly AMOUNT_INVALID_DECIMALS: string = 'Enter a valid amount, maximum two decimal places'
   static readonly NON_NEGATIVE_NUMBER_REQUIRED: string = 'Don’t enter a negative number'
   static readonly POSITIVE_NUMBER_REQUIRED: string = 'Enter a number higher than 0'
   static readonly INTEGER_REQUIRED: string = 'Enter a numeric, for example 3'
