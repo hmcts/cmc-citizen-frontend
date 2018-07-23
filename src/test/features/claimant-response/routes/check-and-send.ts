48c33c8b137930aca038fdbb928eeef2f2cec816
Damian Dunajski <damian.dunajski@hmcts.net>
2018-07-24 00:56:08 +0100

    Add missing check-and-send route test

---
+++ src/test/features/claimant-response/routes/check-and-send.ts
@@ -0,0 +1,91 @@
+import { expect } from 'chai'
+import * as request from 'supertest'
+import * as config from 'config'
+
+import { attachDefaultHooks } from 'test/routes/hooks'
+import 'test/routes/expectations'
+
+import { checkAuthorizationGuards } from 'test/features/claimant-response/routes/checks/authorization-check'
+import { checkNotClaimantInCaseGuard } from 'test/features/claimant-response/routes/checks/not-claimant-in-case-check'
+
+import * as idamServiceMock from 'test/http-mocks/idam'
+import * as draftStoreServiceMock from 'test/http-mocks/draft-store'
+import * as claimStoreServiceMock from 'test/http-mocks/claim-store'
+
+import { fullAdmissionWithPaymentByInstalmentsData } from 'test/data/entity/responseData'
+
+import { Paths as ClaimantResponsePaths } from 'claimant-response/paths'
+
+import { app } from 'main/app'
+
+const cookieName: string = config.get<string>('session.cookieName')
+const draftType = 'claimantResponse'
+
+const pagePath = ClaimantResponsePaths.checkAndSendPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId })
+
+describe('Claimant response: check and send page', () => {
+  attachDefaultHooks(app)
+
+  describe('on GET', () => {
+    const method = 'get'
+    checkAuthorizationGuards(app, method, pagePath)
+    checkNotClaimantInCaseGuard(app, method, pagePath)
+
+    context('when user authorised', () => {
+      beforeEach(() => {
+        idamServiceMock.resolveRetrieveUserFor(claimStoreServiceMock.sampleClaimObj.submitterId, 'citizen')
+      })
+
+      context('when response submitted', () => {
+        it('should return 500 and render error page when cannot retrieve claim', async () => {
+          claimStoreServiceMock.rejectRetrieveClaimByExternalId('HTTP error')
+
+          await request(app)
+            .get(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+
+        it('should return 500 and render error page when cannot retrieve draft', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId({
+            response: fullAdmissionWithPaymentByInstalmentsData,
+            respondedAt: '2018-07-01'
+          })
+          draftStoreServiceMock.rejectFind('Error')
+
+          await request(app)
+            .get(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+
+        it('should redirect to incomplete submission when not all tasks are completed', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId({
+            response: fullAdmissionWithPaymentByInstalmentsData,
+            respondedAt: '2018-07-01'
+          })
+          draftStoreServiceMock.resolveFind(draftType, { acceptPaymentMethod: undefined })
+
+          await request(app)
+            .get(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.redirect
+              .toLocation(ClaimantResponsePaths.incompleteSubmissionPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId })))
+        })
+
+        it('should render page when everything is fine', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId({
+            response: fullAdmissionWithPaymentByInstalmentsData,
+            respondedAt: '2018-07-01'
+          })
+          draftStoreServiceMock.resolveFind(draftType)
+
+          await request(app)
+            .get(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.successful.withText('Check your answers before submitting your response'))
+        })
+      })
+    })
+  })
+})
