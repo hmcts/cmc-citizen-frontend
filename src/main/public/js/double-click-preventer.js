fa3c50dd34528f888e35eeea03d06c0cca5c4545
Surjeet Singh <surjeet.singh@hmcts.net>
2018-07-23 14:59:37 +0100

    ROC-3803: Addressing split submits by disabling submit input type

--- src/main/public/js/double-click-preventer.js
+++ src/main/public/js/double-click-preventer.js
@@ -5,8 +5,11 @@
       .forEach(function (element) {
         element.addEventListener('click', function (event) {
           if(element.className === 'button') {
-            if(!event.detail || event.detail !== 1) {
+            if(!event.detail || event.detail > 1) {
               event.preventDefault()
+            }else {
+              element.disabled = true
+              element.form.submit()
             }
           }
         })
