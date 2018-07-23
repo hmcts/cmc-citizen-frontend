2ffbf830051c7b1b1dc229ad12c9dfe544a1b979
Surjeet Singh <surjeet.singh@hmcts.net>
2018-07-23 15:36:00 +0100

    ROC-3803: Removing event overhead

--- src/main/public/js/double-click-preventer.js
+++ src/main/public/js/double-click-preventer.js
@@ -5,12 +5,8 @@
       .forEach(function (element) {
         element.addEventListener('click', function (event) {
           if(element.className === 'button') {
-            if(!event.detail || event.detail > 1) {
-              event.preventDefault()
-            }else {
-              element.disabled = true
-              element.form.submit()
-            }
+            element.disabled = true
+            element.form.submit()
           }
         })
       })
