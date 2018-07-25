e2c6dcc639cb8d55d64ae3252c77d5059f057538
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-07-25 16:01:33 +0100

    Initial test case for defendant response route

---
+++ src/test/features/claimant-response/routes/defendants-response.ts
@@ -0,0 +1,120 @@
+import { expect } from 'chai'
+import * as request from 'supertest'
+import * as config from 'config'
+
+import { attachDefaultHooks } from 'test/routes/hooks'
+import 'test/routes/expectations'
+
+import { Paths as ClaimantResponsePaths } from 'claimant-response/paths'
+
+import { app } from 'main/app'
+
+import * as idamServiceMock from 'test/http-mocks/idam'
+import * as claimStoreServiceMock from 'test/http-mocks/claim-store'
+import * as draftStoreServiceMock from 'test/http-mocks/draft-store'
+import { checkAuthorizationGuards } from 'test/features/claimant-response/routes/checks/authorization-check'
+import { checkNotClaimantInCaseGuard } from 'test/features/claimant-response/routes/checks/not-claimant-in-case-check'
+
+const cookieName: string = config.get<string>('session.cookieName')
+const externalId = claimStoreServiceMock.sampleClaimObj.externalId
+const pagePath = ClaimantResponsePaths.defendantsResponsePage.evaluateUri({ externalId: externalId })
+const taskListPagePath = ClaimantResponsePaths.taskListPage.evaluateUri({ externalId: externalId })
+
+const defendantPartialAdmissionResponse = claimStoreServiceMock.sampleDefendantPartialAdmissionResponseObj
+
+describe('Claimant response: view defendant response page', () => {
+  attachDefaultHooks(app)
+
+  describe('on GET', () => {
+    const method = 'get'
+    checkAuthorizationGuards(app, method, pagePath)
+    checkNotClaimantInCaseGuard(app, method, pagePath)
+
+    context('when user authorised', () => {
+      beforeEach(() => {
+        idamServiceMock.resolveRetrieveUserFor('1', 'citizen')
+      })
+
+      it('should return 500 and render error page when cannot retrieve claims', async () => {
+        claimStoreServiceMock.rejectRetrieveClaimByExternalId('HTTP error')
+
+        await request(app)
+          .get(pagePath)
+          .set('Cookie', `${cookieName}=ABC`)
+          .expect(res => expect(res).to.be.serverError.withText('Error'))
+      })
+
+      it('should return 500 and render error page when cannot retrieve claimantResponse draft', async () => {
+        draftStoreServiceMock.rejectFind('Error')
+        claimStoreServiceMock.resolveRetrieveClaimByExternalId(defendantPartialAdmissionResponse)
+
+        await request(app)
+          .get(pagePath)
+          .set('Cookie', `${cookieName}=ABC`)
+          .expect(res => expect(res).to.be.serverError.withText('Error'))
+      })
+    })
+
+    describe('on POST', () => {
+      const method = 'post'
+      checkAuthorizationGuards(app, method, pagePath)
+      checkNotClaimantInCaseGuard(app, method, pagePath)
+
+      context('when user authorised', () => {
+        beforeEach(() => {
+          idamServiceMock.resolveRetrieveUserFor('1', 'citizen')
+        })
+
+        context('when middleware failure', () => {
+          it('should return 500 when cannot retrieve claim by external id', async () => {
+            claimStoreServiceMock.rejectRetrieveClaimByExternalId('HTTP error')
+
+            await request(app)
+              .post(pagePath)
+              .set('Cookie', `${cookieName}=ABC`)
+              .send({ viewedDefendantResponse: true })
+              .expect(res => expect(res).to.be.serverError.withText('Error'))
+          })
+
+          it('should return 500 when cannot retrieve claimantResponse draft', async () => {
+            draftStoreServiceMock.rejectFind('Error')
+            claimStoreServiceMock.resolveRetrieveClaimByExternalId(defendantPartialAdmissionResponse)
+
+            await request(app)
+              .post(pagePath)
+              .set('Cookie', `${cookieName}=ABC`)
+              .send({ viewedDefendantResponse: true })
+              .expect(res => expect(res).to.be.serverError.withText('Error'))
+          })
+        })
+
+        context('when form is valid', async () => {
+          it('should redirect to task list page', async () => {
+            claimStoreServiceMock.resolveRetrieveClaimByExternalId(defendantPartialAdmissionResponse)
+            draftStoreServiceMock.resolveFind('claimantResponse')
+            draftStoreServiceMock.resolveSave()
+
+            await request(app)
+              .post(pagePath)
+              .set('Cookie', `${cookieName}=ABC`)
+              .send({ viewedDefendantResponse: true })
+              .expect(res => expect(res).to.be.redirect.toLocation(taskListPagePath))
+          })
+
+          it('should return 500 and render error page when cannot save claimantResponse draft', async () => {
+            claimStoreServiceMock.resolveRetrieveClaimByExternalId(defendantPartialAdmissionResponse)
+            draftStoreServiceMock.resolveFind('claimantResponse')
+            draftStoreServiceMock.rejectSave()
+
+            await request(app)
+              .post(pagePath)
+              .set('Cookie', `${cookieName}=ABC`)
+              .send({ viewedDefendantResponse: true })
+              .expect(res => expect(res).to.be.serverError.withText('Error'))
+          })
+        })
+
+      })
+    })
+  })
+})
