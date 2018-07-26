e451b0556b50bd37c8335c9b786cb768541dfce3
Patryk Jar <jar.patryk@gmail.com>
2018-07-26 18:15:09 +0100

    ROC-3926: use middleware to set amount for 'when do will you pay' page

--- src/main/features/response/routes/partial-admission/payment-option.ts
+++ src/main/features/response/routes/partial-admission/payment-option.ts
@@ -1,7 +1,20 @@
+import * as express from 'express'
+
 import { partialAdmissionPath } from 'response/paths'
 import { PaymentOptionPage } from 'response/components/payment-intention/payment-option'
 import { FeatureToggleGuard } from 'guards/featureToggleGuard'
+import { ResponseDraft } from 'response/draft/responseDraft'
+
+const setHowMuchDoYouOweAmount = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const draft: ResponseDraft = res.locals.responseDraft.document
+
+  if (draft.isResponsePartiallyAdmitted() && draft.partialAdmission.howMuchDoYouOwe) {
+    res.locals.amount = draft.partialAdmission.howMuchDoYouOwe.amount || 0
+  }
+
+  next()
+}
 
 /* tslint:disable:no-default-export */
 export default new PaymentOptionPage('partialAdmission')
-  .buildRouter(partialAdmissionPath, FeatureToggleGuard.featureEnabledGuard('admissions'))
+  .buildRouter(partialAdmissionPath, FeatureToggleGuard.featureEnabledGuard('admissions'), setHowMuchDoYouOweAmount)
