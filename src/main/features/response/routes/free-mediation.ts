abadd82d7db8d119147e7ee7deb2150daa756818
Kiran Varma <varma.ap@gmail.com>
2018-08-01 16:09:02 +0100

    fixed free mediation for response path after moving to common shared area

---
+++ src/main/features/response/routes/free-mediation.ts
@@ -0,0 +1,16 @@
+import * as express from 'express'
+
+import { AbstractFreeMediationPage } from 'shared/components/free-mediation/free-mediation'
+
+import { responsePath, Paths } from 'features/response/paths'
+
+class FreeMediationPage extends AbstractFreeMediationPage {
+  redirectUri (req: express.Request, res: express.Response): string {
+    const { externalId } = req.params
+    return Paths.taskListPage.evaluateUri({ externalId: externalId })
+  }
+}
+
+/* tslint:disable:no-default-export */
+export default new FreeMediationPage()
+  .buildRouter(responsePath)
