318832c0755a31ad50a770791e890d09d1e785f9
Christophe Kalista <christophe.kalista@gmail.com>
2018-08-02 14:26:49 +0100

    Removed unused code and tidying up

--- src/main/features/dashboard/routes/contact-them.ts
+++ src/main/features/dashboard/routes/contact-them.ts
@@ -8,7 +8,6 @@ import { ClaimStoreClient } from 'claims/claimStoreClient'
 import { User } from 'idam/user'
 
 const claimStoreClient: ClaimStoreClient = new ClaimStoreClient()
-const draftExternalId = 'draft'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
@@ -16,14 +15,6 @@ export default express.Router()
     const { externalId } = req.params
 
     res.render(Paths.contactThemPage.associatedView, {
-      claim: externalId !== draftExternalId ? await claimStoreClient.retrieveByExternalId(externalId, res.locals.user as User) : undefined
+      claim: await claimStoreClient.retrieveByExternalId(externalId, res.locals.user as User)
     })
-  }))
-  .post(Paths.contactThemPage.uri, ErrorHandling.apply(async (req: express.Request, res: express.Response): Promise<void> => {
-    const { externalId } = req.params
-
-    if (externalId === draftExternalId) {
-      throw new Error('Draft external ID is not supported')
-    }
-
   }))
