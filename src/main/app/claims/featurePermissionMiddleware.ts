d6215bf2c68673958e5bb37cb6390ec05bb6b8a9
Surjeet Singh <surjeet.singh@hmcts.net>
2018-08-06 09:23:31 +0100

    ROC-3916: Adding feature toggle

--- src/main/app/claims/featurePermissionMiddleware.ts
+++ src/main/app/claims/featurePermissionMiddleware.ts
@@ -3,10 +3,15 @@ import * as express from 'express'
 import { User } from 'idam/user'
 import { ClaimStoreClient } from 'claims/claimStoreClient'
 import { Paths as ClaimPaths } from 'claim/paths'
+import * as toBoolean from 'to-boolean'
+import * as config from 'config'
 
 export class FeaturePermissionMiddleware {
 
   static retrieveUserConsentRole (req: express.Request, res: express.Response, next: express.NextFunction): void {
+    if (!toBoolean(config.get<boolean>('featureToggles.featuresPermission'))) {
+      return next()
+    }
     const claimStoreClient: ClaimStoreClient = new ClaimStoreClient()
     const user: User = res.locals.user
     claimStoreClient.retrieveUserRoles(user).then(value => {
