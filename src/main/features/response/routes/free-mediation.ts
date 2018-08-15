56461c591010333470bf080f0653247a5cb16309
Damian Dunajski <damian.dunajski@hmcts.net>
2018-08-15 17:10:40 +0100

    Fix compilation issue

--- src/main/features/response/routes/free-mediation.ts
+++ src/main/features/response/routes/free-mediation.ts
@@ -3,8 +3,10 @@ import * as express from 'express'
 import { AbstractFreeMediationPage } from 'shared/components/free-mediation/free-mediation'
 
 import { responsePath, Paths } from 'features/response/paths'
-import { ResponseDraft } from 'response/draft/responseDraft'
+
 import { ClaimFeatureToggles } from 'utils/claimFeatureToggles'
+import { Claim } from 'claims/models/claim'
+import { ResponseDraft } from 'response/draft/responseDraft'
 
 class FreeMediationPage extends AbstractFreeMediationPage {
   buildRedirectUri (req: express.Request, res: express.Response): string {
@@ -14,6 +16,7 @@ class FreeMediationPage extends AbstractFreeMediationPage {
 }
 
 const populateAmountRequestHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const claim: Claim = res.locals.claim
   const draft: ResponseDraft = res.locals.responseDraft.document
   res.locals.amount = ClaimFeatureToggles.areAdmissionsEnabled(claim) && draft.isResponsePartiallyAdmitted() ? draft.partialAdmission.howMuchDoYouOwe.amount : 0
   next()
