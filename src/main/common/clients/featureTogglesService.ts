a175d48f1922ca6fb1559c538eec8b8c0494c62e
Tharack Ahmed <tharack@aztec.local>
2018-08-17 14:24:12 +0100

    trailing whitespaces removed

--- src/main/common/clients/featureTogglesService.ts
+++ src/main/common/clients/featureTogglesService.ts
@@ -8,12 +8,10 @@ import { User } from 'idam/user'
 const service = new FeatureToggleService(config.get<string>('feature-toggles-api.url'), requestPromiseApi)
 
 export class FeatureTogglesService {
-  
-  isToggleFeatureEnabled(user: User, roles: string[], feature: string): Promise<boolean> {
+  isToggleFeatureEnabled (user: User, roles: string[], feature: string): Promise<boolean> {
     if (!user) {
       return Promise.reject(new Error('user must be set'))
     }
-
     return service.isFeatureEnabled(feature, user.email, roles.join(','))
   }
 }
