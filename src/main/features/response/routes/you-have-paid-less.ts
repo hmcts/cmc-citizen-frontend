27911ea5a6ee57a1e560acdf01ae376a3bd7682f
andrewwa-kainos <andrewwa@kainos.com>
2018-08-17 15:17:58 +0100

    remove redundant try-catch

--- src/main/features/response/routes/you-have-paid-less.ts
+++ src/main/features/response/routes/you-have-paid-less.ts
@@ -1,19 +1,11 @@
 import * as express from 'express'
 import { Paths } from 'response/paths'
 
-async function renderView (res: express.Response, next: express.NextFunction) {
-  try {
-    res.render(Paths.youHavePaidLess.associatedView, { claim: res.locals.claim })
-  } catch (err) {
-    next(err)
-  }
-}
-
 /* tslint:disable:no-default-export */
 export default express.Router()
   .get(Paths.youHavePaidLess.uri,
     async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-      await renderView(res, next)
+      await Promise.resolve(res.render(Paths.youHavePaidLess.associatedView, { claim: res.locals.claim }))
     })
   .post(Paths.youHavePaidLess.uri,
     function (req, res) {
