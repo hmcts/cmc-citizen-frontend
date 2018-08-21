0fb598373fce29c3f9b33b8f83f736c5624fbe65
andrewwa-kainos <andrewwa@kainos.com>
2018-08-21 15:04:16 +0100

    add timeline and evidence to full defence states paid

---
+++ src/test/features/response/routes/full-rejection/timeline.ts
@@ -0,0 +1,172 @@
+import { expect } from 'chai'
+import * as request from 'supertest'
+import * as config from 'config'
+import { attachDefaultHooks } from 'test/routes/hooks'
+import 'test/routes/expectations'
+import { Paths } from 'response/paths'
+import { app } from 'main/app'
+import * as idamServiceMock from 'test/http-mocks/idam'
+import * as draftStoreServiceMock from 'test/http-mocks/draft-store'
+import * as claimStoreServiceMock from 'test/http-mocks/claim-store'
+
+import { checkAuthorizationGuards } from 'test/features/response/routes/checks/authorization-check'
+import { checkAlreadySubmittedGuard } from 'test/features/response/routes/checks/already-submitted-check'
+import { checkCountyCourtJudgmentRequestedGuard } from 'test/features/response/routes/checks/ccj-requested-check'
+import { checkNotDefendantInCaseGuard } from 'test/features/response/routes/checks/not-defendant-in-case-check'
+
+const cookieName: string = config.get<string>('session.cookieName')
+const externalId: string = claimStoreServiceMock.sampleClaimObj.externalId
+const pagePath: string = Paths.timelinePage.evaluateUri({ externalId: externalId })
+
+describe('Defendant response: timeline', () => {
+
+  attachDefaultHooks(app)
+
+  describe('on GET', () => {
+
+    const method = 'get'
+    checkAuthorizationGuards(app, method, pagePath)
+    checkNotDefendantInCaseGuard(app, method, pagePath)
+
+    context('when user authorised', () => {
+
+      beforeEach(() => {
+        idamServiceMock.resolveRetrieveUserFor(claimStoreServiceMock.sampleClaimObj.defendantId, 'citizen')
+      })
+
+      checkAlreadySubmittedGuard(app, method, pagePath)
+      checkCountyCourtJudgmentRequestedGuard(app, method, pagePath)
+
+      context('when response and CCJ not submitted', () => {
+
+        it('should return 500 and render error page when cannot retrieve claim', async () => {
+          claimStoreServiceMock.rejectRetrieveClaimByExternalId('HTTP error')
+
+          await request(app)
+            .get(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+
+        it('should return 500 and render error page when cannot retrieve draft', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+          draftStoreServiceMock.rejectFind('Error')
+
+          await request(app)
+            .get(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+
+        it('should render page when everything is fine', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+          draftStoreServiceMock.resolveFind('response')
+
+          await request(app)
+            .get(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.successful.withText('Add your timeline of events'))
+        })
+      })
+    })
+  })
+
+  describe('on POST', () => {
+
+    const method = 'post'
+    checkAuthorizationGuards(app, method, pagePath)
+    checkNotDefendantInCaseGuard(app, method, pagePath)
+
+    describe('for authorized user', () => {
+
+      beforeEach(() => {
+        idamServiceMock.resolveRetrieveUserFor(claimStoreServiceMock.sampleClaimObj.defendantId, 'citizen')
+      })
+
+      checkAlreadySubmittedGuard(app, method, pagePath)
+      checkCountyCourtJudgmentRequestedGuard(app, method, pagePath)
+
+      describe('errors are handled propely', () => {
+
+        it('should return 500 and render error page when cannot retrieve claim', async () => {
+          claimStoreServiceMock.rejectRetrieveClaimByExternalId('HTTP error')
+
+          await request(app)
+            .post(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+
+        it('should return 500 and render error page when cannot retrieve draft', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+          draftStoreServiceMock.rejectFind('Error')
+
+          await request(app)
+            .post(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .expect(res => expect(res).to.be.serverError.withText('Error'))
+        })
+      })
+
+      describe('update row action', () => {
+
+        context('valid form', () => {
+
+          ['response', 'response:partial-admission'].forEach(responseDraftType => {
+            it('should redirect to evidence page when and everything is fine', async () => {
+              claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+              draftStoreServiceMock.resolveFind(responseDraftType)
+              draftStoreServiceMock.resolveSave(100)
+
+              await request(app)
+                .post(pagePath)
+                .set('Cookie', `${cookieName}=ABC`)
+                .send({ rows: [{ date: 'Damaged roof', description: '299' }] })
+                .expect(res => expect(res).to.be.redirect
+                  .toLocation(Paths.evidencePage.evaluateUri({ externalId: externalId })))
+            })
+          })
+        })
+
+        context('invalid form', () => {
+
+          it('should render page when date undefined', async () => {
+            claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+            draftStoreServiceMock.resolveFind('response')
+
+            await request(app)
+              .post(pagePath)
+              .set('Cookie', `${cookieName}=ABC`)
+              .send({ rows: [{ date: undefined, description: '299' }] })
+              .expect(res => expect(res).to.be.successful.withText('Enter a date'))
+          })
+
+          it('should render page when description undefined', async () => {
+            claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+            draftStoreServiceMock.resolveFind('response')
+
+            await request(app)
+              .post(pagePath)
+              .set('Cookie', `${cookieName}=ABC`)
+              .send({ rows: [{ date: 'May', description: undefined }] })
+              .expect(res => expect(res).to.be.successful.withText('Enter a description of what happened'))
+          })
+        })
+      })
+
+      describe('add row action', () => {
+
+        it('should render page when valid input', async () => {
+          claimStoreServiceMock.resolveRetrieveClaimByExternalId()
+          draftStoreServiceMock.resolveFind('response')
+
+          await request(app)
+            .post(pagePath)
+            .set('Cookie', `${cookieName}=ABC`)
+            .send({ action: { addRow: 'Add row' } })
+            .expect(res => expect(res).to.be.successful.withText('Add your timeline of events'))
+        })
+      })
+    })
+  })
+})
