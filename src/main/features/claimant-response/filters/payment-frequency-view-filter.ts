33019f364b4dc4d86c821cd03218315be85b6c1f
Kiran Varma <varma.ap@gmail.com>
2018-08-21 11:49:35 +0100

    removed paymentScheduleViewFilter

--- src/main/features/claimant-response/filters/payment-frequency-view-filter.ts
+++
@@ -1,7 +0,0 @@
-import { PaymentSchedule } from 'ccj/form/models/paymentSchedule'
-
-export namespace PaymentScheduleViewFilter {
-  export function render (value: string): string {
-    return PaymentSchedule.of(value).displayValue
-  }
-}
