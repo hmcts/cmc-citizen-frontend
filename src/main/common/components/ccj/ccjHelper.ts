fa2ffb8220da81b0a7c5aa09ee794d95e5a4c851
Tim Jacomb <tim.jacomb@hmcts.net>
2018-08-22 09:41:35 +0100

    Remove unneeded check

--- src/main/common/components/ccj/ccjHelper.ts
+++ src/main/common/components/ccj/ccjHelper.ts
@@ -4,9 +4,9 @@ import { ResponseType } from 'claims/models/response/responseType'
 import { Claim } from 'claims/models/claim'
 import { DraftClaimantResponse } from 'claimant-response/draft/draftClaimantResponse'
 
-export function getAmountSettledFor (claim: Claim, draft: DraftClaimantResponse): number {
+export function getAmountSettledFor (claim: Claim, draft: DraftClaimantResponse): number | undefined {
   const settledForLessThanClaimAmount = claim.response && claim.response.responseType === ResponseType.PART_ADMISSION
-    && claim.response.paymentIntention !== undefined && draft.settleAdmitted && draft.settleAdmitted.admitted === YesNoOption.YES
+    && draft.settleAdmitted && draft.settleAdmitted.admitted === YesNoOption.YES
 
   if (settledForLessThanClaimAmount) {
     return (claim.response as PartialAdmissionResponse).amount
