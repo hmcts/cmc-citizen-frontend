a26b15a31c3cbecee5e86711df4cf4fdec715943
Tharack Ahmed <tharack@aztec.local>
2018-08-31 09:44:59 +0100

    CCJData.ts to use MomentFactory

--- src/test/data/entity/ccjData.ts
+++ src/test/data/entity/ccjData.ts
@@ -1,7 +1,7 @@
 import { MomentFactory } from 'shared/momentFactory'
 
 export const ccjIssueRequestPayImmediately = {
-  defendantDateOfBirth: '1999-01-01T00:00:00.000Z',
+  defendantDateOfBirth: MomentFactory.parse('1999-01-01').utc(true),
   paidAmount: undefined,
   paymentOption: 'IMMEDIATELY',
   payBySetDate:  MomentFactory.currentDate().add(5, 'days'),
@@ -9,19 +9,19 @@ export const ccjIssueRequestPayImmediately = {
   statementOfTruth: undefined
 }
 export const ccjIssueRequestPayBySetDate = {
-  defendantDateOfBirth: '1999-01-01T00:00:00.000Z',
+  defendantDateOfBirth: MomentFactory.parse('1999-01-01').utc(true),
   paidAmount: undefined,
   paymentOption: 'BY_SPECIFIED_DATE',
-  payBySetDate: '2050-12-31T00:00:00.000Z',
+  payBySetDate: MomentFactory.parse('2050-12-31').utc(true),
   repaymentPlan: undefined,
   statementOfTruth: undefined
 }
 export const ccjIssueRequestPayByInstalments = {
-  defendantDateOfBirth: '1999-01-01T00:00:00.000Z',
+  defendantDateOfBirth: MomentFactory.parse('1999-01-01').utc(true),
   paymentOption: 'INSTALMENTS',
   repaymentPlan: {
     instalmentAmount: 100,
-    firstPaymentDate: '2050-12-31T00:00:00.000Z',
+    firstPaymentDate: MomentFactory.parse('2050-12-31').utc(true),
     paymentSchedule: 'EACH_WEEK'
   },
   statementOfTruth: undefined
@@ -30,7 +30,7 @@ export const ccjIssueRequestPayImmediatelyForCompany = {
   defendantDateOfBirth: undefined,
   paidAmount: undefined,
   paymentOption: 'IMMEDIATELY',
-  payBySetDate: MomentFactory.currentDate().add(5, 'days'), // '2050-12-31T00:00:00.000Z',
+  payBySetDate: MomentFactory.currentDate().add(5, 'days'),
   repaymentPlan: undefined,
   statementOfTruth: undefined
 }
@@ -38,7 +38,7 @@ export const ccjIssueRequestPayBySetDateForCompany = {
   defendantDateOfBirth: undefined,
   paidAmount: undefined,
   paymentOption: 'BY_SPECIFIED_DATE',
-  payBySetDate: '2050-12-31T00:00:00.000Z',
+  payBySetDate: MomentFactory.parse('2050-12-31').utc(true),
   repaymentPlan: undefined,
   statementOfTruth: undefined
 }
@@ -47,7 +47,7 @@ export const ccjIssueRequestPayByInstalmentsForCompany = {
   paymentOption: 'INSTALMENTS',
   repaymentPlan: {
     instalmentAmount: 100,
-    firstPaymentDate: '2050-12-31T00:00:00.000Z',
+    firstPaymentDate: MomentFactory.parse('2050-12-31').utc(true),
     paymentSchedule: 'EACH_WEEK'
   },
   statementOfTruth: undefined
