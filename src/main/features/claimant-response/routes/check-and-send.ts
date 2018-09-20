99c06fe37a7ef7f2b32d4da513a06895ac0cd46f
Kiran Varma <varma.ap@gmail.com>
2018-09-20 18:18:37 +0100

    check and send working for set date

--- src/main/features/claimant-response/routes/check-and-send.ts
+++ src/main/features/claimant-response/routes/check-and-send.ts
@@ -6,38 +6,14 @@ import { ErrorHandling } from 'shared/errorHandling'
 import { DraftClaimantResponse } from 'claimant-response/draft/draftClaimantResponse'
 import { Draft } from '@hmcts/draft-store-client'
 import { Claim } from 'claims/models/claim'
-import { PaymentPlanHelper } from 'shared/helpers/paymentPlanHelper'
-import { Frequency } from 'common/frequency/frequency'
 import { User } from 'idam/user'
 import { DraftService } from 'services/draftService'
 import { OfferClient } from 'claims/offerClient'
 import { Settlement } from 'claims/models/settlement'
 import { prepareSettlement } from 'claimant-response/helpers/settlementHelper'
-import { PaymentPlan } from 'common/payment-plan/paymentPlan'
 import { FormaliseRepaymentPlanOption } from 'claimant-response/form/models/formaliseRepaymentPlanOption'
 import { CCJClient } from 'claims/ccjClient'
 import { AmountHelper } from 'claimant-response/helpers/amountHelper'
-import { PaymentType } from 'shared/components/payment-intention/model/paymentOption'
-
-function createCourtOrderPaymentPlan (draft: Draft<DraftClaimantResponse>, claim: Claim) {
-  if (draft.document.alternatePaymentMethod
-    && draft.document.alternatePaymentMethod.paymentOption
-    && draft.document.alternatePaymentMethod.paymentOption.option === PaymentType.INSTALMENTS
-  ) {
-    const claimantPaymentPlan: PaymentPlan = PaymentPlanHelper.createPaymentPlanFromDraft(draft.document)
-    const defendantPaymentPlan: PaymentPlan = PaymentPlanHelper.createPaymentPlanFromClaim(claim)
-
-    const courtOrderPaymentPlan: PaymentPlan = new PaymentPlan(
-      defendantPaymentPlan.totalAmount,
-      draft.document.courtOrderAmount,
-      Frequency.MONTHLY,
-      claimantPaymentPlan.startDate
-    )
-
-    return courtOrderPaymentPlan.convertTo(defendantPaymentPlan.frequency)
-  }
-  return undefined
-}
 
 /* tslint:disable:no-default-export */
 export default express.Router()
@@ -51,8 +27,7 @@ export default express.Router()
       res.render(Paths.checkAndSendPage.associatedView, {
         draft: draft.document,
         claim: claim,
-        totalAmount: AmountHelper.calculateTotalAmount(claim, res.locals.draft.document),
-        courtOrderPaymentPlan: createCourtOrderPaymentPlan(draft, claim)
+        totalAmount: AmountHelper.calculateTotalAmount(claim, res.locals.draft.document)
       })
     })
   )
