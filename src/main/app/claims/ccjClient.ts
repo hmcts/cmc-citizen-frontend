609b21c5fb5573723584f20b310134c3026472d5
Surjeet Singh <surjeet.singh@hmcts.net>
2018-09-27 10:35:21 +0100

    ROC:4370 - Refactoring code to use one method request with extra boolean variable

--- src/main/app/claims/ccjClient.ts
+++ src/main/app/claims/ccjClient.ts
@@ -6,15 +6,7 @@ import { Claim } from 'claims/models/claim'
 
 export class CCJClient {
 
-  static async issue (claim: Claim, countyCourtJudgment: CountyCourtJudgment, user: User): Promise<Claim> {
-    return CCJClient.save(claim.externalId, countyCourtJudgment, user, true)
-  }
-
-  static async request (externalId: string, countyCourtJudgment: CountyCourtJudgment, user: User): Promise<Claim> {
-    return CCJClient.save(externalId, countyCourtJudgment, user, false)
-  }
-
-  private static async save (externalId: string, countyCourtJudgment: CountyCourtJudgment, user: User, issue: boolean = false): Promise<Claim> {
+  static async request (externalId: string, countyCourtJudgment: CountyCourtJudgment, user: User, issue: boolean): Promise<Claim> {
     return request.post(`${claimStoreApiUrl}/${externalId}/county-court-judgment?issue=${issue}`, {
       body: countyCourtJudgment,
       headers: {
@@ -22,4 +14,5 @@ export class CCJClient {
       }
     })
   }
+
 }
