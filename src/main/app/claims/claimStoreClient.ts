15b1cce0bb1483943e2e818bfc09255c3559caa0
Lauren Taylor <l.taylor@kainos.com>
2018-10-03 16:35:49 +0100

    [WIP] Linking frontend to backend

--- src/main/app/claims/claimStoreClient.ts
+++ src/main/app/claims/claimStoreClient.ts
@@ -10,6 +10,7 @@ import { DraftClaim } from 'drafts/models/draftClaim'
 import { Draft } from '@hmcts/draft-store-client'
 import { ResponseDraft } from 'response/draft/responseDraft'
 import { Logger } from '@hmcts/nodejs-logging'
+import {DraftPaidInFull} from "paid-in-full/draft/draftPaidInFull";
 
 export const claimApiBaseUrl: string = `${config.get<string>('claim-store.url')}`
 export const claimStoreApiUrl: string = `${claimApiBaseUrl}/claims`
@@ -41,6 +42,15 @@ export class ClaimStoreClient {
       }
     }).then(caseReference => {
       return caseReference.case_reference
+    })
+  }
+
+  savePaidInFull (externalId: string, submitter: User, draft: Draft<DraftPaidInFull>): Promise<void> {
+    const datePaid = draft.document.datePaid.date;
+    return this.request.put(`${claimStoreApiUrl}/${externalId}/paid-in-full/${datePaid}`, {
+      headers: {
+        Authorization: `Bearer ${submitter.bearerToken}`
+      }
     })
   }
 
--- src/main/features/paid-in-full/routes/date-paid.ts
+++ src/main/features/paid-in-full/routes/date-paid.ts
@@ -9,6 +9,7 @@ import { FormValidator } from 'forms/validation/formValidator'
 import { User } from 'idam/user'
 import { DraftService } from 'services/draftService'
 import { DatePaid } from 'paid-in-full/form/models/datePaid'
+import {ClaimStoreClient} from "claims/claimStoreClient";
 
 function renderView (form: Form<DatePaid>, res: express.Response): void {
   res.render(Paths.datePaidPage.associatedView, { form: form })
@@ -38,6 +39,9 @@ export default express.Router()
         await new DraftService().save(draft, user.bearerToken)
 
         const { externalId } = req.params
+
+        await new ClaimStoreClient().savePaidInFull(externalId, user, draft)
+
         res.redirect(Paths.datePaidPage.uri.replace(':externalId', externalId))
       }
     }))
