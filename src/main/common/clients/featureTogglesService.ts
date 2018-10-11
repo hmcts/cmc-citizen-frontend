798b964497e4e18aa59ff7a6d7de4df5acc71312
tharacka <tharacka@kainos.com>
2018-10-11 08:19:52 +0100

    WIP memory-cache implentation

--- src/main/common/clients/featureTogglesService.ts
+++ src/main/common/clients/featureTogglesService.ts
@@ -7,6 +7,7 @@ import { User } from 'idam/user'
 const service = new FeatureToggleService(config.get<string>('feature-toggles-api.url'), requestPromiseApi)
 
 export class FeatureTogglesService {
+
   isToggleFeatureEnabled (user: User, roles: string[], feature: string): Promise<boolean> {
     if (!user) {
       return Promise.reject(new Error('user must be set'))
--- src/main/routes/shutter.ts
+++ src/main/routes/shutter.ts
@@ -2,22 +2,40 @@ import * as express from 'express'
 import { Paths as AppPaths } from 'paths'
 import { FeatureTogglesService } from 'shared/clients/featureTogglesService'
 import { User } from 'idam/user'
+import * as cache from 'memory-cache'
+
+function featureToggleCache (duration) {
+  const key = 'cmc_citizen_frontend_maintenance_unplanned'
+  let cachedValue = cache.get(key)
+  if (cachedValue) {
+    // tslint:disable-next-line
+    console.log('cached value ' + JSON.stringify(cachedValue))
+    return cachedValue
+  } else {
+    let featureTogglesService: FeatureTogglesService = new FeatureTogglesService()
+    const user: User = new User('', '', '', '', [], '', '')
+
+    cachedValue = featureTogglesService.isToggleFeatureEnabled(user, [], key)
+    cache.put(key, cachedValue, duration * 1000)
+    // tslint:disable-next-line
+    console.log('put cached value ' + JSON.stringify(cachedValue))
+    return cachedValue
+  }
+}
 
 export class Shutter {
   enableFor (app: express.Express) {
-
-    const featureTogglesService: FeatureTogglesService = new FeatureTogglesService()
-    const user: User = new User('','', '', '',[], '', '')
-
     app.all(/^((?!shutter).)*$/, (req, res, next) => {
-      featureTogglesService.isToggleFeatureEnabled(user, [], 'cmc_citizen_frontend_shutter_unplanned').then(isFeatureToggleEnabled => {
-        if (isFeatureToggleEnabled) {
-          return res.render(AppPaths.unplannedShutterPage.associatedView)
-        } else {
+      featureToggleCache(30).then(
+        isFeatureToggleEnabled => {
+          if (isFeatureToggleEnabled) {
+            // tslint:disable-next-line
+            console.log('isFeatureToggleEnabled ' + JSON.stringify(isFeatureToggleEnabled))
+            return res.render(AppPaths.unplannedShutterPage.associatedView)
+          }
           return next()
         }
-
-      })
+      )
     })
   }
 }
