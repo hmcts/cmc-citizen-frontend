b30c70e8cae2e0f3b4e83fd430a20e66ab082b3f
Sabah Irfan <sabah.irfan@gmail.com>
2018-10-23 12:11:55 +0100

    Code cleanup

--- src/main/features/paid-in-full/routes/confirmation.ts
+++ src/main/features/paid-in-full/routes/confirmation.ts
@@ -1,18 +1,11 @@
 import * as express from 'express'
 
 import { Paths } from 'paid-in-full/paths'
-import { Claim } from 'claims/models/claim'
 import { ErrorHandling } from 'shared/errorHandling'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
   .get(Paths.confirmationPage.uri,
     ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-      const claim: Claim = res.locals.claim
-
-      res.render(
-        Paths.confirmationPage.associatedView,
-        {
-          claim: claim
-        })
+      res.render(Paths.confirmationPage.associatedView, {})
     }))
