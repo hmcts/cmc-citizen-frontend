574d6724cfd578d9743d25c0ab188a12184cc57d
Kiran Varma <varma.ap@gmail.com>
2018-10-29 15:42:14 +0000

    added new test for paymentPlanHelper

---
+++ src/test/common/helpers/paymentPlanHelper.ts
@@ -0,0 +1,20 @@
+import { Claim } from 'claims/models/claim'
+import * as claimStoreServiceMock from '../../http-mocks/claim-store'
+import { expect } from 'chai'
+import { PaymentPlanHelper } from 'shared/helpers/paymentPlanHelper'
+import { PaymentPlan } from 'common/payment-plan/paymentPlan'
+import { Frequency } from 'common/frequency/frequency'
+import { MomentFactory } from 'shared/momentFactory'
+import { calculateMonthIncrement } from 'common/calculate-month-increment/calculateMonthIncrement'
+
+describe('PaymentPlanHelper', () => {
+  let claim: Claim
+  claim = new Claim().deserialize({ ...claimStoreServiceMock.sampleClaimObj, ...claimStoreServiceMock.sampleFullAdmissionWithPaymentByInstalmentsResponseObj })
+
+  context('createPaymentPlanFromDefendantFinancialStatement', () => {
+    it('should return correct paymentPlan from defendants financial statement ', () => {
+      expect(PaymentPlanHelper.createPaymentPlanFromDefendantFinancialStatement(claim)).to.deep
+        .equal(PaymentPlan.create(200,200,Frequency.WEEKLY,calculateMonthIncrement(MomentFactory.currentDate())))
+    })
+  })
+})
