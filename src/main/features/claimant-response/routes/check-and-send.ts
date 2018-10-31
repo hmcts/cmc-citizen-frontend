df70865186b9ce8eebb2601892db9770843aab80
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-10-31 10:49:23 +0000

    update convertor, template and check and send

--- src/main/features/claimant-response/routes/check-and-send.ts
+++ src/main/features/claimant-response/routes/check-and-send.ts
@@ -10,6 +10,9 @@ import { User } from 'idam/user'
 import { DraftService } from 'services/draftService'
 import { AmountHelper } from 'claimant-response/helpers/amountHelper'
 import { ClaimStoreClient } from 'claims/claimStoreClient'
+import { PartialAdmissionResponse } from 'claims/models/response/partialAdmissionResponse'
+import { FullAdmissionResponse } from 'claims/models/response/fullAdmissionResponse'
+import { PaymentIntention } from 'shared/components/payment-intention/model/paymentIntention'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
@@ -19,11 +22,20 @@ export default express.Router()
     ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
       const draft: Draft<DraftClaimantResponse> = res.locals.claimantResponseDraft
       const claim: Claim = res.locals.claim
-
+      let paymentIntention: PaymentIntention
+      if (draft.document.courtOfferedPaymentIntention) {
+        paymentIntention = draft.document.courtOfferedPaymentIntention
+      } else if (draft.document.alternatePaymentMethod) {
+        paymentIntention = draft.document.alternatePaymentMethod
+      } else {
+        const claimResponse: FullAdmissionResponse | PartialAdmissionResponse = claim.response as FullAdmissionResponse | PartialAdmissionResponse
+        paymentIntention = claimResponse.paymentIntention
+      }
       res.render(Paths.checkAndSendPage.associatedView, {
         draft: draft.document,
         claim: claim,
-        totalAmount: AmountHelper.calculateTotalAmount(claim, res.locals.draft.document)
+        totalAmount: AmountHelper.calculateTotalAmount(claim, res.locals.draft.document),
+        paymentIntention: paymentIntention
       })
     })
   )
@@ -34,7 +46,7 @@ export default express.Router()
       const claim: Claim = res.locals.claim
       const draft: Draft<DraftClaimantResponse> = res.locals.claimantResponseDraft
       const user: User = res.locals.user
-      await new ClaimStoreClient().saveClaimantResponse(claim,draft,user)
+      await new ClaimStoreClient().saveClaimantResponse(claim, draft, user)
       await new DraftService().delete(draft.id, user.bearerToken)
       res.redirect(Paths.confirmationPage.evaluateUri({ externalId: claim.externalId }))
     }))
