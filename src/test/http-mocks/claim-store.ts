b9850ac3e64b6dd099c41b7e6cef3bb79542a327
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-10-31 14:19:27 +0000

    action review comments

--- src/test/http-mocks/claim-store.ts
+++ src/test/http-mocks/claim-store.ts
@@ -16,7 +16,7 @@ import {
 } from 'test/data/entity/responseData'
 
 const serviceBaseURL: string = config.get<string>('claim-store.url')
-const externalIdRegex: string = '/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}'
+const externalIdPattern: string = '[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}'
 
 export const sampleClaimObj = {
   id: 1,
@@ -140,25 +140,25 @@ export function mockCalculateInterestRate (expected: number): mock.Scope {
 
 export function resolveRetrieveClaimByExternalId (claimOverride?: object): mock.Scope {
   return mock(`${serviceBaseURL}/claims`)
-    .get(new RegExp(externalIdRegex))
+    .get(new RegExp('/' + externalIdPattern))
     .reply(HttpStatus.OK, { ...sampleClaimObj, ...claimOverride })
 }
 
 export function resolveRetrieveClaimByExternalIdWithResponse (override?: object): mock.Scope {
   return mock(`${serviceBaseURL}/claims`)
-    .get(new RegExp(externalIdRegex))
+    .get(new RegExp('/' + externalIdPattern))
     .reply(HttpStatus.OK, { ...sampleClaimObj, ...sampleDefendantResponseObj, ...override })
 }
 
 export function rejectRetrieveClaimByExternalId (reason: string = 'Error') {
   mock(`${serviceBaseURL}/claims`)
-    .get(new RegExp(externalIdRegex))
+    .get(new RegExp('/' + externalIdPattern))
     .reply(HttpStatus.INTERNAL_SERVER_ERROR, reason)
 }
 
 export function resolveRetrieveClaimByExternalIdTo404HttpCode (reason: string = 'Claim not found') {
   mock(`${serviceBaseURL}/claims`)
-    .get(new RegExp(externalIdRegex))
+    .get(new RegExp('/' + externalIdPattern))
     .reply(HttpStatus.NOT_FOUND, reason)
 }
 
@@ -224,7 +224,7 @@ export function rejectRetrieveByDefendantId (reason: string) {
 
 export function resolvePrePaymentSave () {
   mock(`${serviceBaseURL}/claims`)
-    .post(new RegExp(externalIdRegex + '/pre-payment'))
+    .post(new RegExp('/' + externalIdPattern + '/pre-payment'))
     .reply(HttpStatus.OK, { case_reference: 1527177480274990 })
 }
 
@@ -272,7 +272,7 @@ export function rejectSaveClaimForUser (reason: string = 'HTTP error') {
 
 export function resolveSaveCcjForExternalId () {
   mock(`${serviceBaseURL}/claims`)
-    .post(new RegExp(externalIdRegex +
+    .post(new RegExp('/' + externalIdPattern +
       '/county-court-judgment'))
     .reply(HttpStatus.OK, { ...sampleClaimObj })
 }
@@ -280,7 +280,7 @@ export function resolveSaveCcjForExternalId () {
 export function resolveSaveReDeterminationForExternalId (explanation: string) {
 
   mock(`${serviceBaseURL}/claims`)
-    .post(new RegExp(externalIdRegex +
+    .post(new RegExp('/' + externalIdPattern +
       '/re-determination'))
     .reply(HttpStatus.OK, { explanation: explanation, partyType: MadeBy.CLAIMANT })
 }
@@ -329,20 +329,20 @@ export function resolveCountersignOffer (by: string = 'defendant') {
 
 export function rejectSaveCcjForExternalId (reason: string = 'HTTP error') {
   mock(`${serviceBaseURL}/claims`)
-    .post(new RegExp(externalIdRegex +
+    .post(new RegExp('/' + externalIdPattern +
       '/county-court-judgment'))
     .reply(HttpStatus.INTERNAL_SERVER_ERROR, reason)
 }
 
 export function rejectRetrieveDocument (reason: string) {
   mock(`${serviceBaseURL}/documents`)
-    .get(new RegExp(externalIdRegex))
+    .get(new RegExp('/' + externalIdPattern))
     .reply(HttpStatus.INTERNAL_SERVER_ERROR, reason)
 }
 
 export function resolveRetrieveDocument () {
   mock(`${serviceBaseURL}/documents`)
-    .get(new RegExp(externalIdRegex))
+    .get(new RegExp('/' + externalIdPattern))
     .reply(HttpStatus.OK)
 }
 
