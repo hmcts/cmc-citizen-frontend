fcf1280812575f068e8158441c9d98db384c79c8
Mark Ryan <mark.ryan@hmcts.net>
2018-11-07 09:58:18 +0000

    env variables as boolean

--- src/integration-test/tests/citizen/defence/defence_test.ts
+++ src/integration-test/tests/citizen/defence/defence_test.ts
@@ -15,17 +15,19 @@ Scenario('I can complete the journey when I fully reject the claim as I dispute 
   helperSteps.finishResponse(testData)
 })
 
-Scenario('I can complete the journey when I fully reject the claim as I have already paid @citizen @admissions', async (I: I) => {
-  const testData = await EndToEndTestData.prepareData(I, PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
-  testData.defenceType = DefenceType.FULL_REJECTION_BECAUSE_FULL_AMOUNT_IS_PAID
-  helperSteps.finishResponse(testData)
-  I.click('My account')
-  I.see(testData.claimRef)
-  I.see(`We’ve emailed ${testData.claimantName} telling them when and how you said you paid the claim`)
-})
+if (process.env.FEATURE_ADMISSIONS === 'true') {
+  Scenario('I can fill out forms for I admit part of the claim @citizen @admissions', async (I: I) => {
+    const testData = await EndToEndTestData.prepareData(I, PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
+    testData.defenceType = DefenceType.PART_ADMISSION
+    helperSteps.finishResponse(testData)
+  })
 
-Scenario('I can fill out forms for I admit part of the claim @citizen @admissions', async (I: I) => {
-  const testData = await EndToEndTestData.prepareData(I, PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
-  testData.defenceType = DefenceType.PART_ADMISSION
-  helperSteps.finishResponse(testData)
-})
+  Scenario('I can complete the journey when I fully reject the claim as I have already paid @citizen @admissions', async (I: I) => {
+    const testData = await EndToEndTestData.prepareData(I, PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
+    testData.defenceType = DefenceType.FULL_REJECTION_BECAUSE_FULL_AMOUNT_IS_PAID
+    helperSteps.finishResponse(testData)
+    I.click('My account')
+    I.see(testData.claimRef)
+    I.see(`We’ve emailed ${testData.claimantName} telling them when and how you said you paid the claim`)
+  })
+}
