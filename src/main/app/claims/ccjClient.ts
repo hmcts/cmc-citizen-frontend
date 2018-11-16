3509095361f82a4f9c5673d4cbb6ca9f94c7e7d9
Andy Walker <andrewwa@kainos.com>
2018-11-16 18:23:45 +0000

    ROC-4370 Replace CCJ admission request (#958)

--- src/main/app/claims/ccjClient.ts
+++ src/main/app/claims/ccjClient.ts
@@ -1,23 +1,15 @@
 import { User } from 'idam/user'
 import { request } from 'client/request'
-import { CCJModelConverter } from 'claims/ccjModelConverter'
 import { claimStoreApiUrl } from 'claims/claimStoreClient'
 import { CountyCourtJudgment } from 'claims/models/countyCourtJudgment'
 import { Claim } from 'claims/models/claim'
-import { DraftCCJ } from 'ccj/draft/draftCCJ'
-import { Draft } from '@hmcts/draft-store-client'
 import { ReDetermination } from 'ccj/form/models/reDetermination'
 import { MadeBy } from 'offer/form/models/madeBy'
 
 export class CCJClient {
 
-  static async request (externalId: string, draft: Draft<DraftCCJ>, user: User): Promise<Claim> {
-    const countyCourtJudgment: CountyCourtJudgment = CCJModelConverter.convertForRequest(draft.document)
-    return CCJClient.save(externalId, countyCourtJudgment, user, false)
-  }
-
-  private static async save (externalId: string, countyCourtJudgment: CountyCourtJudgment, user: User, issue: boolean = false): Promise<Claim> {
-    return request.post(`${claimStoreApiUrl}/${externalId}/county-court-judgment?issue=${issue}`, {
+  static async request (externalId: string, countyCourtJudgment: CountyCourtJudgment, user: User): Promise<Claim> {
+    return request.post(`${claimStoreApiUrl}/${externalId}/county-court-judgment`, {
       body: countyCourtJudgment,
       headers: {
         Authorization: `Bearer ${user.bearerToken}`
