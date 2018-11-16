bb382f8f93ddde94310676f037e32065c6e7b6ec
Tharack Ahmed <41899680+tharacka@users.noreply.github.com>
2018-11-16 10:41:00 +0000

    ROC-4265 Alternate payment plan start date (#913)

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -4,7 +4,7 @@ import * as path from 'path'
 import * as express from 'express'
 import * as config from 'config'
 import * as nunjucks from 'nunjucks'
-import { addDaysFilter, dateFilter, dateInputFilter } from 'modules/nunjucks/filters/dateFilter'
+import { addDaysFilter, dateFilter, dateInputFilter, monthIncrementFilter } from 'modules/nunjucks/filters/dateFilter'
 import { convertToPoundsFilter } from 'modules/nunjucks/filters/convertToPounds'
 import * as numeralFilter from 'nunjucks-numeral-filter'
 import * as numeral from 'numeral'
@@ -109,6 +109,7 @@ export class Nunjucks {
     nunjucksEnv.addFilter('inputDate', dateInputFilter)
     nunjucksEnv.addFilter('addDays', addDaysFilter)
     nunjucksEnv.addFilter('pennies2pounds', convertToPoundsFilter)
+    nunjucksEnv.addFilter('monthIncrement', monthIncrementFilter)
     nunjucksEnv.addFilter('numeral', numeralFilter)
     nunjucksEnv.addFilter('yesNo', yesNoFilter)
     nunjucksEnv.addGlobal('isAfter4pm', isAfter4pm)
