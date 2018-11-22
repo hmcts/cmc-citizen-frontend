107259c461ea2ee5066bb719b1bf1178a9a00213
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-11-22 08:11:32 +0000

    ROC-4779: Show payment plan for all payment option in repayment plan summary (#1168)

--- src/main/features/ccj/routes/repayment-plan-summary.ts
+++ src/main/features/ccj/routes/repayment-plan-summary.ts
@@ -7,25 +7,38 @@ import { PaidAmount } from 'ccj/form/models/paidAmount'
 import { Claim } from 'claims/models/claim'
 import { RepaymentPlan as CoreRepaymentPlan } from 'claims/models/response/core/repaymentPlan'
 import { PaymentSchedule } from 'ccj/form/models/paymentSchedule'
-import { RepaymentPlan } from 'claims/models/repaymentPlan'
+import { PaymentIntention } from 'claims/models/response/core/paymentIntention'
 
 function renderView (form: Form<PaidAmount>, req: express.Request, res: express.Response): void {
   const claim: Claim = res.locals.claim
+  let paymentIntention: PaymentIntention
 
-  const repaymentPlan: RepaymentPlan = claim.countyCourtJudgment.repaymentPlan
-  const coreRepaymentPlan: CoreRepaymentPlan = {
-    instalmentAmount: repaymentPlan.instalmentAmount,
-    firstPaymentDate: repaymentPlan.firstPaymentDate,
-    paymentSchedule: (repaymentPlan.paymentSchedule as PaymentSchedule).value,
-    completionDate: repaymentPlan.completionDate,
-    paymentLength: repaymentPlan.paymentLength
-  } as CoreRepaymentPlan
+  if (claim.hasClaimantAcceptedDefendantResponseWithCCJ()) {
+    const ccjRepaymentPlan = claim.countyCourtJudgment.repaymentPlan
+    paymentIntention = {
+      repaymentPlan: ccjRepaymentPlan && {
+        instalmentAmount: ccjRepaymentPlan.instalmentAmount,
+        firstPaymentDate: ccjRepaymentPlan.firstPaymentDate,
+        paymentSchedule: (ccjRepaymentPlan.paymentSchedule as PaymentSchedule).value,
+        completionDate: ccjRepaymentPlan.completionDate,
+        paymentLength: ccjRepaymentPlan.paymentLength
+      } as CoreRepaymentPlan,
+      paymentDate: claim.countyCourtJudgment.payBySetDate,
+      paymentOption: claim.countyCourtJudgment.paymentOption
+    } as PaymentIntention
+
+  } else if (claim.hasClaimantAcceptedDefendantResponseWithSettlement()) {
+    paymentIntention = claim.settlement.getLastOffer().paymentIntention
+  }
+
+  const amountPaid = claim.claimantResponse && claim.claimantResponse.amountPaid ? claim.claimantResponse.amountPaid : 0
 
   res.render(Paths.repaymentPlanSummaryPage.associatedView, {
     form: form,
     claim: claim,
-    repaymentPlan: coreRepaymentPlan,
-    madeBy: req.params.madeBy
+    paymentIntention: paymentIntention,
+    remainingAmountToPay: claim.totalAmountTillDateOfIssue - amountPaid,
+    requestedBy: req.params.madeBy
   })
 }
 
