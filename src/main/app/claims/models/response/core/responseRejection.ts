fa48887a376ab06a40cdd1410420d89162d214ce
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-11-26 10:42:29 +0000

    Add a payment intention to response rejection

--- src/main/app/claims/models/response/core/responseRejection.ts
+++ src/main/app/claims/models/response/core/responseRejection.ts
@@ -1,10 +1,12 @@
 import { ClaimantResponse } from 'claims/models/response/core/claimantResponse'
+import { PaymentIntention } from 'claims/models/response/core/paymentIntention'
 
 export class ResponseRejection implements ClaimantResponse {
   type: string
   amountPaid: number
   freeMediation?: boolean
   reason?: string
+  claimantPaymentIntention?: PaymentIntention
 
   constructor () {
     this.type = 'REJECTION'
