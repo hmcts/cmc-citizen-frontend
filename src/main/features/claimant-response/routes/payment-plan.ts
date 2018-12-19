f8613c4836f95623264dee17e1c98c6034ca4ab3
Kiran Varma <varma.ap@gmail.com>
2018-12-19 19:07:21 +0000

    added unit test and added a missing return statement

--- src/main/features/claimant-response/routes/payment-plan.ts
+++ src/main/features/claimant-response/routes/payment-plan.ts
@@ -81,7 +81,8 @@ export class PaymentPlanPage extends AbstractPaymentPlanPage<DraftClaimantRespon
         courtOfferedPaymentIntention.paymentOption = PaymentOption.INSTALMENTS
         courtOfferedPaymentIntention.repaymentPlan = {
           firstPaymentDate: paymentPlanConvertedToDefendantFrequency.startDate,
-          instalmentAmount: _.round(paymentPlanConvertedToDefendantFrequency.instalmentAmount,2),
+          instalmentAmount: paymentPlanConvertedToDefendantFrequency.instalmentAmount > admittedClaimAmount ?
+            admittedClaimAmount : _.round(paymentPlanConvertedToDefendantFrequency.instalmentAmount,2),
           paymentSchedule: Frequency.toPaymentSchedule(paymentPlanConvertedToDefendantFrequency.frequency),
           completionDate: paymentPlanConvertedToDefendantFrequency.calculateLastPaymentDate(),
           paymentLength: paymentPlanConvertedToDefendantFrequency.calculatePaymentLength()
