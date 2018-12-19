42611377c2c9ce9474b6a65395b1f76ab2a2f41a
Ranjit Rattu <rattur2002@yahoo.co.uk>
2018-12-19 09:30:56 +0000

    Added test case for task

--- src/main/features/response/tasks/viewSendCompanyFinancialDetailsTask.ts
+++ src/main/features/response/tasks/viewSendCompanyFinancialDetailsTask.ts
@@ -3,6 +3,9 @@ import { ResponseDraft } from 'response/draft/responseDraft'
 export class ViewSendCompanyFinancialDetailsTask {
 
   static isCompleted (responseDraft: ResponseDraft): boolean {
+    if (!responseDraft || !responseDraft.companyDefendantResponseViewed) {
+      return false
+    }
     return responseDraft.companyDefendantResponseViewed === true
   }
 }
