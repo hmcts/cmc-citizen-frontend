9fc76b63d4522dc46bae7a3efb239a6090534de9
markry-kainos <43344584+markry-kainos@users.noreply.github.com>
2019-01-03 11:24:14 +0000

    ROC-4956 - show court repayment plan when courts offer accepted (#1271)

--- src/main/app/claims/models/settlement.ts
+++ src/main/app/claims/models/settlement.ts
@@ -30,14 +30,12 @@ export class Settlement {
   }
 
   getLastOffer (): Offer {
-    if (!this.partyStatements) {
-      return undefined
-    }
-
-    const partyStatement: PartyStatement = this.partyStatements.reverse()
-      .find(statement => statement.type === StatementType.OFFER.value)
-
+    const partyStatement = this.getOfferedPartyStatement()
     return partyStatement ? partyStatement.offer : undefined
+  }
+
+  getLastOfferAsPartyStatement (): PartyStatement {
+    return this.getOfferedPartyStatement()
   }
 
   isOfferAccepted (): boolean {
@@ -90,5 +88,15 @@ export class Settlement {
       return settlements
     }
     return settlements.map(settlement => new PartyStatement(undefined, undefined).deserialize(settlement))
+  }
+
+  private getOfferedPartyStatement (): PartyStatement {
+    if (!this.partyStatements) {
+      return undefined
+    }
+
+    const partyStatement: PartyStatement = this.partyStatements.reverse()
+      .find(statement => statement.type === StatementType.OFFER.value)
+    return partyStatement
   }
 }
