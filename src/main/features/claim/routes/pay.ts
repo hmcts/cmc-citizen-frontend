7bcca5f8b23076c436ff98c0a24ee1a2a27d5df2
Kiran Varma <varma.ap@gmail.com>
2019-01-04 13:40:29 +0000

    ROC-4951: Consent to new features always returns true doesn't always return true but a role is always returned so removed un-necessary condition checking roles length

--- src/main/features/claim/routes/pay.ts
+++ src/main/features/claim/routes/pay.ts
@@ -82,7 +82,8 @@ async function successHandler (res, next) {
 
   if (!claimIsAlreadyFullyPersisted) {
     const roles: string[] = await claimStoreClient.retrieveUserRoles(user)
-    if (roles.length > 0 && await featureTogglesClient.isAdmissionsAllowed(user, roles)) {
+
+    if (await featureTogglesClient.isAdmissionsAllowed(user, roles)) {
       await claimStoreClient.saveClaim(draft, user, 'admissions')
     } else {
       await claimStoreClient.saveClaim(draft, user)
