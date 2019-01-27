f3cc2cb6f5e03eb4f43d91233a2288b959b0e39b
Kiran Varma <varma.ap@gmail.com>
2019-01-27 19:35:55 +0000

    changed if to switch statement

--- src/main/common/helpers/paymentIntentionHelper.ts
+++ src/main/common/helpers/paymentIntentionHelper.ts
@@ -13,43 +13,44 @@ import { RepaymentPlan } from 'claims/models/repaymentPlan'
 
 export class PaymentIntentionHelper {
 
-  static getDefendantPaymentIntention (claim: Claim): PaymentIntention {
+  static getDefendantPaymentIntention(claim: Claim): PaymentIntention {
     const claimResponse: FullAdmissionResponse | PartialAdmissionResponse
       = claim.response as FullAdmissionResponse | PartialAdmissionResponse
     return PaymentIntention.convertFromCorePaymentIntention(claimResponse.paymentIntention)
   }
 
-  static getClaimantPaymentIntention (draft: DraftClaimantResponse): PaymentIntention {
+  static getClaimantPaymentIntention(draft: DraftClaimantResponse): PaymentIntention {
     return PaymentIntention.convertFromCorePaymentIntention(draft.alternatePaymentMethod.toDomainInstance())
   }
 
-  static getCourtCalculatedPaymentIntention (draft: DraftClaimantResponse, claim: Claim): PaymentIntention {
+  static getCourtCalculatedPaymentIntention(draft: DraftClaimantResponse, claim: Claim): PaymentIntention {
     const paymentPlanFromDefendantFinancialStatement: PaymentPlan
       = PaymentPlanHelper.createPaymentPlanFromDefendantFinancialStatement(claim, draft)
 
-    if (draft.alternatePaymentMethod.paymentOption.option.value === PaymentOption.INSTALMENTS) {
-      if (paymentPlanFromDefendantFinancialStatement.startDate.isSame(MomentFactory.maxDate())) {
+    switch (draft.alternatePaymentMethod.paymentOption.option.value) {
+      case PaymentOption.INSTALMENTS:
+        if (paymentPlanFromDefendantFinancialStatement.startDate.isSame(MomentFactory.maxDate())) {
+          return new PaymentIntentionBuilder(PaymentOption.BY_SPECIFIED_DATE)
+            .setPaymentDate(MomentFactory.maxDate())
+            .build()
+        } else {
+          return new PaymentIntentionBuilder(PaymentOption.INSTALMENTS)
+            .setRepaymentPlan(new RepaymentPlan(
+              _.round(paymentPlanFromDefendantFinancialStatement.instalmentAmount, 2),
+              paymentPlanFromDefendantFinancialStatement.startDate,
+              Frequency.toPaymentSchedule(paymentPlanFromDefendantFinancialStatement.frequency),
+              paymentPlanFromDefendantFinancialStatement.calculateLastPaymentDate(),
+              paymentPlanFromDefendantFinancialStatement.calculatePaymentLength()
+            ))
+            .build()
+        }
+      case PaymentOption.IMMEDIATELY:
+      case PaymentOption.BY_SPECIFIED_DATE:
         return new PaymentIntentionBuilder(PaymentOption.BY_SPECIFIED_DATE)
-          .setPaymentDate(MomentFactory.maxDate())
+          .setPaymentDate(paymentPlanFromDefendantFinancialStatement.calculateLastPaymentDate())
           .build()
-      } else {
-        return new PaymentIntentionBuilder(PaymentOption.INSTALMENTS)
-          .setRepaymentPlan(new RepaymentPlan(
-            _.round(paymentPlanFromDefendantFinancialStatement.instalmentAmount, 2),
-            paymentPlanFromDefendantFinancialStatement.startDate,
-            Frequency.toPaymentSchedule(paymentPlanFromDefendantFinancialStatement.frequency),
-            paymentPlanFromDefendantFinancialStatement.calculateLastPaymentDate(),
-            paymentPlanFromDefendantFinancialStatement.calculatePaymentLength()
-          ))
-          .build()
-      }
-    }
-
-    if (draft.alternatePaymentMethod.paymentOption.option.value === PaymentOption.BY_SPECIFIED_DATE
-      || draft.alternatePaymentMethod.paymentOption.option.value === PaymentOption.IMMEDIATELY) {
-      return new PaymentIntentionBuilder(PaymentOption.BY_SPECIFIED_DATE)
-        .setPaymentDate(paymentPlanFromDefendantFinancialStatement.calculateLastPaymentDate())
-        .build()
+      default:
+        return undefined
     }
   }
 }
