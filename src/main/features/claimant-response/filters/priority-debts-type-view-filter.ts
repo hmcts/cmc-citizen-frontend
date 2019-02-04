35eeb4da60f6464d1b182d9ca89518592cd8bf63
tharacka <tharacka@kainos.com>
2019-02-04 09:44:55 +0000

    unit tests

--- src/main/features/claimant-response/filters/priority-debts-type-view-filter.ts
+++ src/main/features/claimant-response/filters/priority-debts-type-view-filter.ts
@@ -2,6 +2,9 @@ import { PriorityDebtType } from 'response/form/models/statement-of-means/priori
 
 export namespace PriorityDebtTypeViewFilter {
   export function render (value: string): string {
+    if (!value) {
+      throw new Error('Must be a valid priority debt type')
+    }
     return PriorityDebtType.valueOf(value).displayValue
   }
 }
