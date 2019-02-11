836b50e8753365aedb12ad1ab5208f452c6c658c
Kiran Varma <varma.ap@gmail.com>
2019-02-11 16:01:57 +0000

    disposable income should be <= 0

--- src/main/features/claimant-response/helpers/courtOfferedInstalmentHelper.ts
+++ src/main/features/claimant-response/helpers/courtOfferedInstalmentHelper.ts
@@ -2,8 +2,11 @@ import { PaymentPlan } from 'common/payment-plan/paymentPlan'
 import { Frequency } from 'common/frequency/frequency'
 
 export class CourtOfferedInstalmentHelper {
-  static getCourtOfferedInstalmentAmount (paymentPlanConvertedToDefendantFrequency: PaymentPlan, defendantPaymentPlan: PaymentPlan): number {
-    if (paymentPlanConvertedToDefendantFrequency.instalmentAmount < Frequency.WEEKLY.monthlyRatio && defendantPaymentPlan.frequency === Frequency.MONTHLY) {
+  static getCourtOfferedInstalmentAmount (
+    paymentPlanConvertedToDefendantFrequency: PaymentPlan,
+    defendantPaymentPlan: PaymentPlan): number {
+    if (paymentPlanConvertedToDefendantFrequency.instalmentAmount < Frequency.WEEKLY.monthlyRatio
+      && defendantPaymentPlan.frequency === Frequency.MONTHLY) {
       return paymentPlanConvertedToDefendantFrequency.instalmentAmount
     } else {
       return defendantPaymentPlan.instalmentAmount
