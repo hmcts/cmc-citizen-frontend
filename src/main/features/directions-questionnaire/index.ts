0dbcc136f9588f40d848dda66f34eef306f163a6
tharacka <tharacka@kainos.com>
2019-03-08 09:12:02 +0000

    local and ff4J toggle set up

---
+++ src/main/features/directions-questionnaire/index.ts
@@ -0,0 +1,49 @@
+import * as express from 'express'
+import * as path from 'path'
+import {OAuthHelper} from 'idam/oAuthHelper'
+import {AuthorizationMiddleware} from 'idam/authorizationMiddleware'
+import {ClaimMiddleware} from 'claims/claimMiddleware'
+import {CountyCourtJudgmentRequestedGuard} from 'response/guards/countyCourtJudgmentRequestedGuard'
+import {DraftMiddleware} from '@hmcts/cmc-draft-store-middleware'
+import {DraftService} from 'services/draftService'
+import {MediationDraft} from 'mediation/draft/mediationDraft'
+import {ResponseDraft} from 'response/draft/responseDraft'
+import {RouterFinder} from 'shared/router/routerFinder'
+
+function requestHandler (): express.RequestHandler {
+  function accessDeniedCallback (req: express.Request, res: express.Response): void {
+    res.redirect(OAuthHelper.forLogin(req, res))
+  }
+
+  const requiredRoles = ['citizen']
+  const unprotectedPaths = []
+  return AuthorizationMiddleware.requestHandler(requiredRoles, accessDeniedCallback, unprotectedPaths)
+}
+
+export class DirectionsQuestionnaireFeature {
+  enableFor (app: express.Express) {
+    const allDQs = '/case/*/directions-questionnaire/*'
+    app.all(allDQs, requestHandler())
+    app.all(allDQs, ClaimMiddleware.retrieveByExternalId)
+    app.all(allDQs, CountyCourtJudgmentRequestedGuard.requestHandler)
+    app.all(allDQs,
+      DraftMiddleware.requestHandler(new DraftService(), 'mediation', 100, (value: any): MediationDraft => {
+        return new MediationDraft().deserialize(value)
+      }),
+      (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        res.locals.draft = res.locals.mediationDraft
+        next()
+      })
+    app.all(allDQs,
+      DraftMiddleware.requestHandler(new DraftService(), 'response', 100, (value: any): ResponseDraft => {
+        return new ResponseDraft().deserialize(value)
+      }),
+      (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        res.locals.draft = res.locals.responseDraft
+        next()
+      }
+    )
+
+    app.use('/', RouterFinder.findAll(path.join(__dirname, 'routes')))
+  }
+}
