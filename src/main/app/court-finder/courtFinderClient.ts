9b5c928a746af1633a7d57a9b2523e7d4279c3c0
Ellie Harrison <ellieh@kainos.com>
2019-03-12 15:51:44 +0000

    Cut down client for only CMC related data

--- src/main/app/court-finder/courtFinderClient.ts
+++ src/main/app/court-finder/courtFinderClient.ts
@@ -1,7 +1,6 @@
 import * as requestDefault from 'request'
 import * as requestPromise from 'request-promise-native'
 import { Address } from './address'
-import { AreaOfLaw } from './areaOfLaw'
 import { Court } from './court'
 import { CourtFinderResponse } from './courtFinderResponse'
 import { Spoe } from './spoe'
@@ -15,28 +14,13 @@ export class CourtFinderClient {
   ) {
   }
 
-  public findCourtByPostcode (postcode: string, areaOfLaw: AreaOfLaw, spoe?: Spoe): Promise<CourtFinderResponse> {
+  public findMoneyClaimCourtsByPostcode (postcode: string, spoe: Spoe = Spoe.NEAREST): Promise<CourtFinderResponse> {
     if (!postcode) {
       return Promise.reject('Missing postcode')
     }
-    if (!areaOfLaw) {
-      return Promise.reject('Missing area of law')
-    }
 
-    let uri: string = `${this.apiUrl}/search/results.json?postcode=${postcode}&aol=${areaOfLaw.name}`
-    if (spoe) {
-      uri += `&spoe=${spoe}`
-    }
+    let uri: string = `${this.apiUrl}/search/results.json?postcode=${postcode}&aol=Money claims&spoe=${spoe}`
 
-    return this.performRequest(uri)
-  }
-
-  public findCourtByAddress (address: string): Promise<CourtFinderResponse> {
-    if (!address) {
-      return Promise.reject('Missing address')
-    }
-
-    const uri: string = `${this.apiUrl}/search/results.json?q=${address}`
     return this.performRequest(uri)
   }
 
@@ -58,25 +42,13 @@ export class CourtFinderClient {
         responseBody.map((court: any) => {
           return new Court(
             court.name,
-            court.lat,
-            court.lon,
-            court.number,
-            court.cci_code,
-            court.magistrate_code,
-            court.slug,
-            court.types,
-            court.dx_number,
             court.distance,
             new Address(
               court.address.address_lines,
               court.address.postcode,
               court.address.town,
-              court.address.type),
-            court.areas_of_law.map((aol: any) => {
-              return new AreaOfLaw(aol.name, aol.external_link, aol.external_link_desc)
-            }))
-        })
-      )
+              court.address.type))
+        }))
       return courtFinderResponse
     })
   }
--- src/test/app/court-finder/courtFinderClient.ts
+++ src/test/app/court-finder/courtFinderClient.ts
@@ -3,7 +3,6 @@ import * as nock from 'nock'
 import * as path from 'path'
 import * as chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
-import { AreaOfLaw } from 'court-finder-client/areaOfLaw'
 import { CourtFinderClient } from 'court-finder-client/courtFinderClient'
 import { CourtFinderResponse } from 'court-finder-client/courtFinderResponse'
 
@@ -22,23 +21,8 @@ const expectedResponse = {
         town: 'Birmingham',
         type: 'Visiting'
       },
-      areasOfLaw: [
-        {
-          externalLink: 'https%3A//www.gov.uk/wills-probate-inheritance',
-          externalLinkDescription: 'Information about wills and probate',
-          name: 'Probate'
-        }
-      ],
-      cciCode: null,
-      courtNumber: null,
       distance: 1,
-      dxNumber: '701990 Birmingham 7',
-      lat: 52.4816613587661,
-      lon: -1.89552893773996,
-      magistrateCode: null,
-      name: 'Birmingham District Probate Registry',
-      slug: 'birmingham-district-probate-registry',
-      types: []
+      name: 'Birmingham District Probate Registry'
     }
   ], statusCode: 200, valid: true
 }
@@ -47,13 +31,13 @@ describe('CourtFinderClient', () => {
 
   chai.use(chaiAsPromised)
 
-  describe('findCourtByPostcode', () => {
+  describe('findMoneyClaimCourtsByPostcode', () => {
     it('should return valid false if no court found', () => {
       nock(mockClient)
           .get(/\/search\/results.json\?postcode=.+&aol=.+/)
           .reply(404, [])
 
-      return courtFinderClient.findCourtByPostcode('A111AA', AreaOfLaw.ALL)
+      return courtFinderClient.findMoneyClaimCourtsByPostcode('A111AA')
           .then((courtResponse: CourtFinderResponse) => {
             chai.expect(courtResponse).eql({ courts: [], statusCode: 404, valid: false })
           })
@@ -63,41 +47,14 @@ describe('CourtFinderClient', () => {
       nock(mockClient)
           .get(/\/search\/results.json\?postcode=.+&aol=.+/)
           .reply(200, fs.readFileSync(path.join(__dirname, 'courtFinderClientAll.json')))
-      return courtFinderClient.findCourtByPostcode('A111AA', AreaOfLaw.ALL)
+      return courtFinderClient.findMoneyClaimCourtsByPostcode('A111AA')
           .then((courtResponse: CourtFinderResponse) => {
             chai.expect(courtResponse).eql(expectedResponse)
           })
     })
 
     it('should reject promise if no postcode', () =>
-      chai.expect(courtFinderClient.findCourtByPostcode('', AreaOfLaw.ALL)).rejectedWith('Missing postcode')
-    )
-  })
-
-  describe('findCourtByAddress', () => {
-    it('should return valid false if no court found', () => {
-      nock(mockClient)
-          .get(/\/search\/results.json\?q=.+/)
-          .reply(404, [])
-
-      return courtFinderClient.findCourtByAddress('invalid address')
-          .then((courtResponse: CourtFinderResponse) => {
-            chai.expect(courtResponse).eql({ courts: [], statusCode: 404, valid: false })
-          })
-    })
-
-    it('should return found courts', () => {
-      nock(mockClient)
-          .get(/\/search\/results.json\?q=.+/)
-          .reply(200, fs.readFileSync(path.join(__dirname, 'courtFinderClientAll.json')))
-      return courtFinderClient.findCourtByAddress('A111AA')
-          .then((courtResponse: CourtFinderResponse) => {
-            chai.expect(courtResponse).eql(expectedResponse)
-          })
-    })
-
-    it('should reject promise if no address', () =>
-      chai.expect(courtFinderClient.findCourtByAddress('')).rejectedWith('Missing address')
+      chai.expect(courtFinderClient.findMoneyClaimCourtsByPostcode('')).rejectedWith('Missing postcode')
     )
   })
 })
