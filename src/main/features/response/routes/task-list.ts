b9128a81c926c252bb36eb60055739ab6dfd7971
tharacka <tharacka@kainos.com>
2019-03-12 16:26:56 +0000

    review comments

--- src/main/features/response/routes/task-list.ts
+++ src/main/features/response/routes/task-list.ts
@@ -9,7 +9,6 @@ import { ResponseDraft } from 'response/draft/responseDraft'
 import { Draft } from '@hmcts/draft-store-client'
 import { MomentFactory } from 'shared/momentFactory'
 import { MediationDraft } from 'mediation/draft/mediationDraft'
-// import { DirectionsQuestionnaireDraft } from 'directions-questionnaire/draft/directionsQuestionnaireDraft'
 
 /* tslint:disable:no-default-export */
 export default express.Router()
@@ -17,7 +16,6 @@ export default express.Router()
     try {
       const draft: Draft<ResponseDraft> = res.locals.responseDraft
       const draftMediation: Draft<MediationDraft> = res.locals.mediationDraft
-      // const draftDirectionsQuestionnaire: Draft<DirectionsQuestionnaireDraft> = res.locals.directionsQuestionnaireDraft
       const claim: Claim = res.locals.claim
 
       const beforeYouStartSection = TaskListBuilder
