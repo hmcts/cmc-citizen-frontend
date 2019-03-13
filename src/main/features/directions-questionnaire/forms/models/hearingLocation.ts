cc85071e3af5b432fbf31a92005a9b3e92d5ec16
Ellie Harrison <ellieh@kainos.com>
2019-03-13 22:50:05 +0000

    Allow loop of hearing location route to search for postcode

--- src/main/features/directions-questionnaire/forms/models/hearingLocation.ts
+++ src/main/features/directions-questionnaire/forms/models/hearingLocation.ts
@@ -1,23 +1,54 @@
-import { IsDefined } from '@hmcts/class-validator'
+import { IsDefined, IsIn, IsNotEmpty, ValidateIf } from '@hmcts/class-validator'
 import { ValidationErrors as GlobalValidationErrors } from 'forms/validation/validationErrors'
-import { CompletableTask } from 'models/task'
 import { YesNoOption } from 'models/yesNoOption'
+import { IsValidPostcode } from '@hmcts/cmc-validators'
 
-export class HearingLocation implements CompletableTask {
+export class AlternativeCourtOption {
+  static readonly BY_NAME: string = 'name'
+  static readonly BY_POSTCODE: string = 'postcode'
+
+  static all (): string[] {
+    return [this.BY_NAME, this.BY_POSTCODE]
+  }
+}
+
+export class HearingLocation {
   @IsDefined({ message: GlobalValidationErrors.YES_NO_REQUIRED })
   courtAccepted?: YesNoOption
+
+  @IsDefined()
   courtName?: string
+
+  @IsDefined()
   courtPostcode?: string
+
+  @ValidateIf(o => o.courtAccepted.option === YesNoOption.NO.option)
+  @IsDefined()
+  @IsIn(AlternativeCourtOption.all())
+  alternativeOption?: string
+
+  @ValidateIf(o => o.alternativeOption === 'name')
+  @IsDefined()
+  @IsNotEmpty()
   alternativeCourtName?: string
+
+  @ValidateIf(o => o.alternativeOption === 'postcode')
+  @IsDefined()
+  @IsValidPostcode()
+  alternativePostcode?: string
 
   constructor (courtAccepted?: YesNoOption,
                courtName?: string,
                courtPostcode?: string,
-               alternativeCourtName?: string) {
+               alternativeOption?: string,
+               alternativeCourtName?: string,
+               alternativePostcode?: string) {
     this.courtAccepted = courtAccepted
     this.courtName = courtName
     this.courtPostcode = courtPostcode
+    this.alternativeOption = alternativeOption
     this.alternativeCourtName = alternativeCourtName
+    this.alternativePostcode = alternativePostcode
   }
 
   static fromObject (value?: any): HearingLocation {
@@ -25,10 +56,13 @@ export class HearingLocation implements CompletableTask {
       return value
     }
 
-    return new HearingLocation(YesNoOption.fromObject(value.courtAccepted),
+    return new HearingLocation(
+      YesNoOption.fromObject(value.courtAccepted),
       value.courtName,
       value.courtPostcode,
-      value.alternativeCourtName)
+      value.alternativeOption,
+      value.alternativeCourtName,
+      value.alternativePostcode)
   }
 
   deserialize (input?: any): HearingLocation {
@@ -36,12 +70,10 @@ export class HearingLocation implements CompletableTask {
       this.courtAccepted = input.courtAccepted
       this.courtName = input.courtName
       this.courtPostcode = input.courtPostcode
+      this.alternativeOption = input.alternativeOption
       this.alternativeCourtName = input.alternativeCourtName
+      this.alternativePostcode = input.alternativePostcode
     }
     return this
-  }
-
-  isCompleted (): boolean {
-    return true
   }
 }
