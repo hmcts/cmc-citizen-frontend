25021f447ca2f5bf13f36b015740b245d4f93b10
Ellie Harrison <ellieh@kainos.com>
2019-03-14 13:42:04 +0000

    Add exceptional circumstances page

--- src/main/features/directions-questionnaire/paths.ts
+++ src/main/features/directions-questionnaire/paths.ts
@@ -7,4 +7,5 @@ export class Paths {
   static readonly selfWitnessPage = new RoutablePath(`${directionsQuestionnairePath}/self-witness`)
   static readonly otherWitnessesPage = new RoutablePath(`${directionsQuestionnairePath}/other-witnesses`)
   static readonly datesPage = new RoutablePath(`${directionsQuestionnairePath}/dates`)
+  static readonly hearingExceptionalCircumstancesPage = new RoutablePath(`${directionsQuestionnairePath}/hearing-exceptional-circumstances`)
 }
