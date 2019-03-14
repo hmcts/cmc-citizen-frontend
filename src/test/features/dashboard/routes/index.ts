8ed98aab612a30dec95a14efb28770003cf2ce5b
Silvia Mandala <silvia.mandala@hmcts.net>
2019-03-14 10:34:49 +0000

    ROC-5625 Add route tests for clam issued and admissions

--- src/test/features/dashboard/routes/index.ts
+++ src/test/features/dashboard/routes/index.ts
@@ -11,8 +11,26 @@ import { app } from 'main/app'
 
 import * as idamServiceMock from 'test/http-mocks/idam'
 import * as draftStoreServiceMock from 'test/http-mocks/draft-store'
-import * as claimStoreServiceMock from 'test/http-mocks/claim-store'
 import { checkAuthorizationGuards } from 'test/features/dashboard/routes/checks/authorization-check'
+import {
+  rejectRetrieveByClaimantId,
+  resolveRetrieveByClaimantId,
+  resolveRetrieveByClaimantIdToEmptyList,
+  resolveRetrieveByDefendantId,
+  resolveRetrieveByDefendantIdToEmptyList,
+  sampleClaimIssueObj,
+  sampleClaimObj
+} from 'test/http-mocks/claim-store'
+import { MomentFactory } from 'shared/momentFactory'
+import {
+  baseFullAdmissionData,
+  basePartialAdmissionData,
+  basePayByInstalmentsData,
+  basePayBySetDateData,
+  basePayImmediatelyData,
+  baseResponseData
+} from 'test/data/entity/responseData'
+import { baseAcceptationClaimantResponseData } from 'test/data/entity/claimantResponseData'
 
 const cookieName: string = config.get<string>('session.cookieName')
 
@@ -29,7 +47,7 @@ describe('Dashboard page', () => {
 
       it('should return 500 and render error page when cannot retrieve claims', async () => {
         draftStoreServiceMock.resolveFind('claim')
-        claimStoreServiceMock.rejectRetrieveByClaimantId('HTTP error')
+        rejectRetrieveByClaimantId('HTTP error')
 
         await request(app)
           .get(Paths.dashboardPage.uri)
@@ -39,8 +57,8 @@ describe('Dashboard page', () => {
 
       context('when no claims issued', () => {
         beforeEach(() => {
-          claimStoreServiceMock.resolveRetrieveByClaimantIdToEmptyList()
-          claimStoreServiceMock.resolveRetrieveByDefendantIdToEmptyList()
+          resolveRetrieveByClaimantIdToEmptyList()
+          resolveRetrieveByDefendantIdToEmptyList()
         })
 
         it('should render page with start claim button when everything is fine', async () => {
@@ -55,8 +73,8 @@ describe('Dashboard page', () => {
 
       context('when at least one claim issued', () => {
         beforeEach(() => {
-          claimStoreServiceMock.resolveRetrieveByClaimantId()
-          claimStoreServiceMock.resolveRetrieveByDefendantId('000MC001', '1')
+          resolveRetrieveByClaimantId()
+          resolveRetrieveByDefendantId('000MC001', '1')
         })
 
         it('should render page with continue claim button when everything is fine', async () => {
@@ -77,7 +95,292 @@ describe('Dashboard page', () => {
             .expect(res => expect(res).to.be.successful.withText('Your money claims account', 'Make a new money claim'))
         })
       })
-    })
 
+      context('when checking the status of a claim', () => {
+        beforeEach(() => {
+          draftStoreServiceMock.resolveFindNoDraftFound()
+        })
+
+        context('as a claimant', () => {
+          beforeEach(() => {
+            resolveRetrieveByDefendantIdToEmptyList()
+          })
+
+          it('should show claim issued statuses when the claim is issued', async () => {
+            resolveRetrieveByClaimantId(sampleClaimIssueObj, {
+              responseDeadline: MomentFactory.currentDate().add(1, 'days')
+            })
+            await request(app)
+              .get(Paths.dashboardPage.uri)
+              .set('Cookie', `${cookieName}=ABC`)
+              .expect(res => expect(res).to.be.successful.withText('Your claim has been sent.'))
+          })
+
+          context('when the claim is in full admission', () => {
+            const fullAdmissionClaim = {
+              ...sampleClaimObj,
+              responseDeadline: MomentFactory.currentDate().add(1, 'days'),
+              response: {
+                ...baseResponseData,
+                ...baseFullAdmissionData
+              }
+            }
+            it('should show pay immediately statuses when the claim is marked as pay immediately', async () => {
+              resolveRetrieveByClaimantId(fullAdmissionClaim, {
+                response: { ...fullAdmissionClaim.response, ...basePayImmediatelyData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('The defendant admits they owe all the money. They’ve said that they will pay immediately.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay immediately and payment is past the deadline', async () => {
+              resolveRetrieveByClaimantId(fullAdmissionClaim, {
+                responseDeadline: MomentFactory.currentDate().subtract(1, 'days'),
+                response: { ...fullAdmissionClaim.response, ...basePayImmediatelyData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('The defendant has not responded to your claim. You can request a County Court Judgment against them.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay by set date', async () => {
+              resolveRetrieveByClaimantId(fullAdmissionClaim, {
+                response: { ...fullAdmissionClaim.response, ...basePayBySetDateData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('The defendant has offered to pay by a set date. You can accept or reject their offer.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay by instalments', async () => {
+              resolveRetrieveByClaimantId(fullAdmissionClaim, {
+                response: { ...fullAdmissionClaim.response, ...basePayByInstalmentsData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('The defendant has offered to pay in instalments. You can accept or reject their offer.'))
+            })
+          })
+          context('when the claim is in partial admission', () => {
+            const partAdmissionClaim = {
+              ...sampleClaimObj,
+              responseDeadline: MomentFactory.currentDate().add(1, 'days'),
+              response: {
+                ...baseResponseData,
+                ...basePartialAdmissionData,
+                amount: 30
+              }
+            }
+            it('should show pay immediately statuses when the claim is marked as pay immediately', async () => {
+              resolveRetrieveByClaimantId(partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayImmediatelyData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('The defendant believes they owe you £30. You can accept or reject that this is the amount owed.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay immediately and the offer is accepted', async () => {
+              resolveRetrieveByClaimantId(partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayImmediatelyData },
+                claimantResponse: baseAcceptationClaimantResponseData
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('You’ve accepted the defendant’s part admission. They said they’d pay immediately.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay immediately and the offer is accepted and response is after the deadline', async () => {
+              resolveRetrieveByClaimantId(partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayImmediatelyData },
+                claimantResponse: baseAcceptationClaimantResponseData,
+                responseDeadline: MomentFactory.currentDate().subtract(1, 'days')
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('The defendant has not responded to your claim. You can request a County Court Judgment against them.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay by set date', async () => {
+              resolveRetrieveByClaimantId(partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayBySetDateData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('The defendant believes they owe you £30. You can accept or reject that this is the amount owed.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay by instalments', async () => {
+              resolveRetrieveByClaimantId(partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayByInstalmentsData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('The defendant believes they owe you £30. You can accept or reject that this is the amount owed.'))
+            })
+          })
+        })
+
+        context('as a defendant', () => {
+          beforeEach(() => {
+            resolveRetrieveByClaimantIdToEmptyList()
+          })
+
+          it('should show claim issued statuses when the claim is issued', async () => {
+            resolveRetrieveByDefendantId('000MC001', '1', sampleClaimIssueObj, {
+              responseDeadline: MomentFactory.currentDate().add(1, 'days')
+            })
+
+            await request(app)
+              .get(Paths.dashboardPage.uri)
+              .set('Cookie', `${cookieName}=ABC`)
+              .expect(res => expect(res).to.be.successful.withText('Respond to claim.', '(1 day remaining)'))
+          })
+
+          context('when the claim is in full admission', () => {
+            const fullAdmissionClaim = {
+              ...sampleClaimObj,
+              responseDeadline: MomentFactory.currentDate().add(1, 'days'),
+              response: {
+                ...baseResponseData,
+                ...baseFullAdmissionData
+              }
+            }
+            it('should show pay immediately statuses when the claim is marked as pay immediately', async () => {
+              resolveRetrieveByDefendantId('000MC001', '1', fullAdmissionClaim, {
+                response: { ...fullAdmissionClaim.response, ...basePayImmediatelyData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('You’ve admitted all of the claim and said you’ll pay the full amount immediately.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay immediately and payment is past the deadline', async () => {
+              resolveRetrieveByDefendantId('000MC001', '1', fullAdmissionClaim, {
+                response: { ...fullAdmissionClaim.response, ...basePayImmediatelyData },
+                responseDeadline: MomentFactory.currentDate().subtract(1, 'days')
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('You haven’t responded to the claim.', 'John Smith can now ask for a County Court Judgment (CCJ) against you.', 'You can still respond to this claim before they ask for a CCJ.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay by set date', async () => {
+              resolveRetrieveByDefendantId('000MC001', '1', fullAdmissionClaim, {
+                response: { ...fullAdmissionClaim.response, ...basePayBySetDateData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('You’ve admitted all of the claim and offered to pay the full amount by 31 December 2050.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay by instalments', async () => {
+              resolveRetrieveByDefendantId('000MC001', '1', fullAdmissionClaim, {
+                response: { ...fullAdmissionClaim.response, ...basePayByInstalmentsData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('You’ve admitted all of the claim and offered to pay the full amount in instalments.'))
+            })
+          })
+          context('when the claim is in partial admission', () => {
+            const partAdmissionClaim = {
+              ...sampleClaimObj,
+              responseDeadline: MomentFactory.currentDate().add(1, 'days'),
+              response: {
+                ...baseResponseData,
+                ...basePartialAdmissionData,
+                amount: 30
+              }
+            }
+            it('should show pay immediately statuses when the claim is marked as pay immediately', async () => {
+              resolveRetrieveByDefendantId('000MC001', '1', partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayImmediatelyData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('You’ve admitted part of the claim.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay immediately and the offer is accepted', async () => {
+              resolveRetrieveByDefendantId('000MC001', '1', partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayImmediatelyData },
+                claimantResponse: baseAcceptationClaimantResponseData
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('John Smith accepted your admission of 30'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay immediately and the offer is accepted and response is after the deadline', async () => {
+              resolveRetrieveByDefendantId('000MC001', '1', partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayImmediatelyData },
+                claimantResponse: baseAcceptationClaimantResponseData,
+                responseDeadline: MomentFactory.currentDate().subtract(1, 'days')
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('You haven’t responded to the claim.', 'John Smith can now ask for a County Court Judgment (CCJ) against you.', 'You can still respond to this claim before they ask for a CCJ.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay by set date', async () => {
+              resolveRetrieveByDefendantId('000MC001', '1', partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayBySetDateData }
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('You’ve admitted part of the claim.'))
+            })
+
+            it('should show pay immediately statuses when the claim is marked as pay by instalments', async () => {
+              resolveRetrieveByDefendantId('000MC001', '1', partAdmissionClaim, {
+                response: { ...partAdmissionClaim.response, ...basePayByInstalmentsData }
+
+              })
+
+              await request(app)
+                .get(Paths.dashboardPage.uri)
+                .set('Cookie', `${cookieName}=ABC`)
+                .expect(res => expect(res).to.be.successful.withText('You’ve admitted part of the claim.'))
+            })
+          })
+        })
+      })
+    })
   })
 })
