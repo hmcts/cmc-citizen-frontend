9ae6d0020c8213c32d00e16a0c8c9e79c05827c5
andrewwa-kainos <andrewwa@kainos.com>
2019-03-15 09:21:49 +0000

    bare bones

---
+++ src/main/features/directions-questionnaire/routes/hearing-dates.ts
@@ -0,0 +1,39 @@
+import * as express from 'express'
+import { Paths } from 'directions-questionnaire/paths'
+import { Paths as DashboardPaths } from 'dashboard/paths'
+import { FormValidator } from 'forms/validation/formValidator'
+import { Form } from 'forms/form'
+import { DirectionsQuestionnaireDraft } from 'directions-questionnaire/draft/directionsQuestionnaireDraft'
+import { Draft } from '@hmcts/draft-store-client'
+import { ErrorHandling } from 'shared/errorHandling'
+import { DraftService } from 'services/draftService'
+import { Availability } from 'directions-questionnaire/forms/models/availability'
+
+function renderPage (res: express.Response, form: Form<Availability>) {
+  res.render(Paths.hearingDatesPage.associatedView, { form: form })
+}
+
+/* tslint:disable:no-default-export */
+export default express.Router()
+  .get(Paths.hearingDatesPage.uri,
+    (req: express.Request, res: express.Response) => {
+      const draft: Draft<DirectionsQuestionnaireDraft> = res.locals.draft
+      renderPage(res, new Form<Availability>(draft.document.availability))
+    })
+  .post(Paths.hearingDatesPage.uri, FormValidator.requestHandler(Availability, Availability.fromObject),
+    ErrorHandling.apply(async (req: express.Request, res: express.Response) => {
+      const form: Form<Availability> = req.body
+
+      if (form.hasErrors()) {
+        renderPage(res, form)
+      } else {
+        const draft: Draft<DirectionsQuestionnaireDraft> = res.locals.draft
+        const user: User = res.locals.user
+
+        draft.document.availability = form.model
+
+        await new DraftService().save(draft, user.bearerToken)
+
+        res.redirect(DashboardPaths.dashboardPage.evaluateUri({ externalId: res.locals.claim.externalId }))
+      }
+    }))
