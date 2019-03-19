a95fbe73c2633821ad9634b3ac915bc82b9dc75d
Ellie Harrison <ellieh@kainos.com>
2019-03-19 14:01:58 +0000

    add support required page

--- src/main/features/directions-questionnaire/draft/directionsQuestionnaireDraft.ts
+++ src/main/features/directions-questionnaire/draft/directionsQuestionnaireDraft.ts
@@ -1,14 +1,17 @@
 import { DraftDocument } from '@hmcts/cmc-draft-store-middleware'
 import { SelfWitness } from 'directions-questionnaire/forms/models/selfWitness'
 import { OtherWitnesses } from 'directions-questionnaire/forms/models/otherWitnesses'
+import { SupportRequired } from 'directions-questionnaire/forms/models/supportRequired'
 
 export class DirectionsQuestionnaireDraft extends DraftDocument {
 
   selfWitness?: SelfWitness
   otherWitnesses?: OtherWitnesses
+  supportRequired?: SupportRequired
 
   constructor (selfWitness: SelfWitness = new SelfWitness(),
-               otherWitnesses: OtherWitnesses = new OtherWitnesses()) {
+               otherWitnesses: OtherWitnesses = new OtherWitnesses(),
+               supportRequired: SupportRequired = new SupportRequired()) {
     super()
   }
 
@@ -17,6 +20,7 @@ export class DirectionsQuestionnaireDraft extends DraftDocument {
       this.externalId = input.externalId
       this.selfWitness = input.selfWitness
       this.otherWitnesses = input.otherWitnesses
+      this.supportRequired = input.supportRequired
     }
     return this
   }
---
+++ src/main/features/directions-questionnaire/helpers/directionsQuestionnaireHelper.ts
@@ -0,0 +1,32 @@
+import { MadeBy } from 'offer/form/models/madeBy'
+import { Claim } from 'main/app/claims/models/claim'
+import { User } from 'main/app/idam/user'
+
+export function getPreferredParty (claim: Claim): MadeBy {
+  const isClaimantBusiness: boolean = claim.claimData.claimant.isBusiness()
+  const isDefendantBusiness: boolean = claim.claimData.defendant.isBusiness()
+
+  if (isClaimantBusiness) {
+    if (isDefendantBusiness) {
+      return MadeBy.CLAIMANT
+    } else {
+      return MadeBy.DEFENDANT
+    }
+  } else {
+    if (isDefendantBusiness) {
+      return MadeBy.CLAIMANT
+    } else {
+      return MadeBy.DEFENDANT
+    }
+  }
+}
+
+export function getUsersRole (claim: Claim, user: User): MadeBy {
+  if (claim.claimantId === user.id && claim.response !== undefined) {
+    return MadeBy.CLAIMANT
+  } else if (claim.defendantId === user.id && claim.response === undefined) {
+    return MadeBy.DEFENDANT
+  } else {
+    throw Error('User has no role in claim')
+  }
+}
