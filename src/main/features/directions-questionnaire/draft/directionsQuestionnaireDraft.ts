a16e56afb1e707616d83d737efe1c1b83c606d15
Ellie Harrison <ellieh@kainos.com>
2019-03-26 09:04:47 +0000

    ROC-5243 - Defendant Hearing - Do you want witnesses to give evidence? (#1495)

--- src/main/features/directions-questionnaire/draft/directionsQuestionnaireDraft.ts
+++ src/main/features/directions-questionnaire/draft/directionsQuestionnaireDraft.ts
@@ -1,14 +1,22 @@
 import { DraftDocument } from '@hmcts/cmc-draft-store-middleware'
+import { SelfWitness } from 'directions-questionnaire/forms/models/selfWitness'
+import { OtherWitnesses } from 'directions-questionnaire/forms/models/otherWitnesses'
 
 export class DirectionsQuestionnaireDraft extends DraftDocument {
 
-  constructor () {
+  selfWitness?: SelfWitness
+  otherWitnesses?: OtherWitnesses
+
+  constructor (selfWitness: SelfWitness = new SelfWitness(),
+               otherWitnesses: OtherWitnesses = new OtherWitnesses()) {
     super()
   }
 
   deserialize (input: any): DirectionsQuestionnaireDraft {
     if (input) {
       this.externalId = input.externalId
+      this.selfWitness = input.selfWitness
+      this.otherWitnesses = input.otherWitnesses
     }
     return this
   }
