f656c177eb79bbc72be940d3a52c8a176dca4ed4
Ellie Harrison <ellieh@kainos.com>
2019-03-26 09:05:32 +0000

    Fix court option validation when not selected

--- src/main/features/directions-questionnaire/forms/models/hearingLocation.ts
+++ src/main/features/directions-questionnaire/forms/models/hearingLocation.ts
@@ -33,12 +33,12 @@ export class HearingLocation {
   @IsIn(AlternativeCourtOption.all(), { message: ValidationErrors.SELECT_ALTERNATIVE_OPTION })
   alternativeOption?: string
 
-  @ValidateIf(o => o.alternativeOption === 'name' || !o.courtName)
+  @ValidateIf(o => o.courtAccepted && o.courtAccepted.option === YesNoOption.NO.option && o.alternativeOption === 'name' || !o.courtName)
   @IsDefined({ message: ValidationErrors.NO_ALTERNATIVE_COURT_NAME })
   @IsNotEmpty({ message: ValidationErrors.NO_ALTERNATIVE_COURT_NAME })
   alternativeCourtName?: string
 
-  @ValidateIf(o => o.alternativeOption === 'postcode')
+  @ValidateIf(o => o.courtAccepted && o.courtAccepted.option === YesNoOption.NO.option || o.alternativeOption === 'postcode')
   @IsDefined({ message: ValidationErrors.NO_ALTERNATIVE_POSTCODE })
   @IsValidPostcode({ message: ValidationErrors.NO_ALTERNATIVE_POSTCODE })
   alternativePostcode?: string
