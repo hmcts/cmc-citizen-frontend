beac6901f88da86060eb443dd80f9cd54d07e500
Sumit Shrivastava <sumitsavit@hotmail.com>
2019-03-29 12:13:15 +0000

    ROC-4699-DefenCandS- Update as per @RobertParkinson and @Andrewwa-kainos

--- src/test/data/draft/partyDetails.ts
+++ src/test/data/draft/partyDetails.ts
@@ -1,23 +1,27 @@
 import { PartyType } from 'common/partyType'
 
-export const individualDetails = {
-  type: PartyType.INDIVIDUAL.value,
-  name: 'John Smith',
+export const addressCorrespondenceAddress = {
   address: {
-    line1: 'Flat 1',
-    line2: 'Street 1',
+    line1: 'Flat 2',
+    line2: 'Street 2',
     line3: 'Cool house name',
     city: 'London',
-    postcode: 'E1 8FA'
+    postcode: 'E2 8FA'
   },
   hasCorrespondenceAddress: true,
   correspondenceAddress: {
-    line1: 'Flat 10',
-    line2: 'Street 10',
+    line1: 'Flat 20',
+    line2: 'Street 20',
     line3: 'Cooler house name',
     city: 'Belfast',
-    postcode: 'BT1 5GB'
-  },
+    postcode: 'BT2 5GB'
+  }
+}
+
+export const individualDetails = {
+  type: PartyType.INDIVIDUAL.value,
+  name: 'John Smith',
+  ...addressCorrespondenceAddress,
   dateOfBirth: {
     known: true,
     date: {
@@ -32,105 +36,35 @@ export const soleTraderDetails = {
   type: PartyType.SOLE_TRADER_OR_SELF_EMPLOYED.value,
   name: 'SoleTrader Smith',
   businessName: 'SoleTrader Ltd.',
-  address: {
-    line1: 'Flat 2',
-    line2: 'Street 2',
-    line3: 'Cool house name',
-    city: 'London',
-    postcode: 'E2 8FA'
-  },
-  hasCorrespondenceAddress: true,
-  correspondenceAddress: {
-    line1: 'Flat 20',
-    line2: 'Street 20',
-    line3: 'Cooler house name',
-    city: 'Belfast',
-    postcode: 'BT2 5GB'
-  }
+  ...addressCorrespondenceAddress
 }
 
 export const claimantSoleTraderDetails = {
   type: PartyType.SOLE_TRADER_OR_SELF_EMPLOYED.value,
   name: 'Claimant SoleTrader',
   businessName: 'Claimant SoleTrader Ltd.',
-  address: {
-    line1: 'Flat 2',
-    line2: 'Street 2',
-    line3: 'Cool house name',
-    city: 'London',
-    postcode: 'E2 8FA'
-  },
-  hasCorrespondenceAddress: true,
-  correspondenceAddress: {
-    line1: 'Flat 20',
-    line2: 'Street 20',
-    line3: 'Cooler house name',
-    city: 'Belfast',
-    postcode: 'BT2 5GB'
-  }
+  ...addressCorrespondenceAddress
 }
 
 export const defendantSoleTraderDetails = {
   type: PartyType.SOLE_TRADER_OR_SELF_EMPLOYED.value,
   name: 'Defendant SoleTrader',
   businessName: 'Defendant SoleTrader Ltd.',
-  address: {
-    line1: 'Flat 2',
-    line2: 'Street 2',
-    line3: 'Cool house name',
-    city: 'London',
-    postcode: 'E2 8FA'
-  },
-  hasCorrespondenceAddress: true,
-  correspondenceAddress: {
-    line1: 'Flat 20',
-    line2: 'Street 20',
-    line3: 'Cooler house name',
-    city: 'Belfast',
-    postcode: 'BT2 5GB'
-  }
+  ...addressCorrespondenceAddress
 }
 
 export const companyDetails = {
   type: PartyType.COMPANY.value,
   name: 'Company Ltd.',
   contactPerson: 'Company Smith',
-  address: {
-    line1: 'Flat 3',
-    line2: 'Street 3',
-    line3: 'Cool house name',
-    city: 'London',
-    postcode: 'E3 8FA'
-  },
-  hasCorrespondenceAddress: true,
-  correspondenceAddress: {
-    line1: 'Flat 30',
-    line2: 'Street 30',
-    line3: 'Cooler house name',
-    city: 'Belfast',
-    postcode: 'BT3 5GB'
-  }
+  ...addressCorrespondenceAddress
 }
 
 export const organisationDetails = {
   type: PartyType.ORGANISATION.value,
   name: 'Organisation.',
   contactPerson: 'Organisation Smith',
-  address: {
-    line1: 'Flat 4',
-    line2: 'Street 4',
-    line3: 'Cool house name',
-    city: 'London',
-    postcode: 'E4 8FA'
-  },
-  hasCorrespondenceAddress: true,
-  correspondenceAddress: {
-    line1: 'Flat 40',
-    line2: 'Street 40',
-    line3: 'Cooler house name',
-    city: 'Belfast',
-    postcode: 'BT4 5GB'
-  }
+  ...addressCorrespondenceAddress
 }
 
 export function partyDetails (partyType: string) {
