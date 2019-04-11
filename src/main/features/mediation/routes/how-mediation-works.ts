cf4e18750ac891f3445d2d9bd8719a90b616b9a7
Andrew Walker <andrewwa@kainos.com>
2019-04-11 13:43:26 +0100

    Save mediation option to `willYouTryMediation`

--- src/main/features/mediation/routes/how-mediation-works.ts
+++ src/main/features/mediation/routes/how-mediation-works.ts
@@ -2,6 +2,11 @@ import * as express from 'express'
 
 import { Paths } from 'mediation/paths'
 import { ErrorHandling } from 'main/common/errorHandling'
+import { Draft } from '@hmcts/draft-store-client'
+import { MediationDraft } from 'mediation/draft/mediationDraft'
+import { FreeMediation, FreeMediationOption } from 'forms/models/freeMediation'
+import { DraftService } from 'services/draftService'
+import { User } from 'idam/user'
 
 function renderView (res: express.Response): void {
   res.render(Paths.howMediationWorksPage.associatedView)
@@ -15,6 +20,14 @@ export default express.Router()
   .post(
     Paths.howMediationWorksPage.uri,
     ErrorHandling.apply(async (req: express.Request, res: express.Response) => {
+      const user: User = res.locals.user
+      const draft: Draft<MediationDraft> = res.locals.mediationDraft
+      draft.document.willYouTryMediation = new FreeMediation(
+        req.body.mediationYes ? FreeMediationOption.YES : FreeMediationOption.NO
+      )
+
+      await new DraftService().save(draft, user.bearerToken)
+
       const { externalId } = req.params
       res.redirect(Paths.willYouTryMediation.evaluateUri({ externalId: externalId }))
     }))
