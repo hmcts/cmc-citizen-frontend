87dfe8565e65431e1aca0c91b2fa282e58d74f59
Karim Lawani <k7lawani@gmail.com>
2019-04-25 13:49:05 +0100

    refactor to initialise LaunchDarkly client as Singleton
    Signed-off-by: Karim Lawani <k7lawani@gmail.com>

--- src/main/common/clients/launchDarklyClient.ts
+++ src/main/common/clients/launchDarklyClient.ts
@@ -3,16 +3,26 @@ import { User } from 'idam/user'
 import * as ld from 'ldclient-node'
 
 const sdkKeys: string = config.get<string>('featureFlags.keys.sdk')
-const client: ld.LDClient = ld.init(sdkKeys)
 
 export class LaunchDarklyClient {
+  private static client: ld.LDClient
+
+  constructor () {
+    LaunchDarklyClient.initClient()
+  }
+
+  static initClient () {
+    if (!LaunchDarklyClient.client) {
+      LaunchDarklyClient.client = ld.init(sdkKeys)
+    }
+  }
 
   callFeatureFlag (user: User, featureKey: string, f?: () => void): void {
     const ldUser: ld.LDUser = {
       key: user.id
     }
-    client.on('ready', function () {
-      client.variation(featureKey, ldUser, false, (value: ld.LDFlagValue) => {
+    LaunchDarklyClient.client.on('ready', function () {
+      LaunchDarklyClient.client.variation(featureKey, ldUser, false, (value: ld.LDFlagValue) => {
         if (value) {
           f()
         }
