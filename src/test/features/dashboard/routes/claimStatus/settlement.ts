0c7c6aa0a4d2899e8bff527aff38aa446a2122f0
Andrew Walker <andrewwa@kainos.com>
2019-04-30 10:48:41 +0100

    Refactor dashboard & claim status tests to match established structure

--- src/test/features/dashboard/routes/claimStatus/settlement.ts
+++ src/test/features/dashboard/routes/claimStatus/settlement.ts
@@ -10,7 +10,6 @@ import { app } from 'main/app'
 
 import * as idamServiceMock from 'test/http-mocks/idam'
 import * as claimStoreServiceMock from 'test/http-mocks/claim-store'
-
 import { sampleClaimDraftObj } from 'test/http-mocks/draft-store'
 import * as data from 'test/data/entity/settlement'
 
@@ -20,156 +19,118 @@ const externalId: string = sampleClaimDraftObj.externalId
 const claimantPagePath = Paths.claimantPage.evaluateUri({ externalId })
 const defendantPagePath = Paths.defendantPage.evaluateUri({ externalId })
 
-interface TestContext {
-  party: string
-  id: string
-  url: string
-}
-
-const claimantContext: TestContext = {
+const claimantContext = {
   party: 'claimant',
   id: claimStoreServiceMock.sampleClaimObj.submitterId,
   url: claimantPagePath
 }
 
-const defendantContext: TestContext = {
+const defendantContext = {
   party: 'defendant',
   id: claimStoreServiceMock.sampleClaimObj.defendantId,
   url: defendantPagePath
 }
 
-const bothParties = [claimantContext, defendantContext]
+const testData = [
+  {
+    status: 'Should show case settled when settlement reached',
+    claim: {
+      ...data.claim,
+      ...data.partialAdmission,
+      ...data.payBySetDateSettlementReachedPartyStatements
+    },
+    claimantAssertions: [
+      'You’ve both signed a settlement agreement',
+      'Download the settlement agreement'
+    ],
+    defendantAssertions: [
+      'You’ve both signed a settlement agreement',
+      'Download the settlement agreement'
+    ]
+  },
+  {
+    status: 'Should show offer settlement reached',
+    claim: {
+      ...data.claim,
+      ...data.partialAdmission,
+      ...data.claimantResponses.acceptBySettlement,
+      ...data.nonMonetaryOfferSettlementReachedPartyStatements
+    },
+    claimantAssertions: [
+      'Agreement signed',
+      'You’ve both signed a legal agreement. The claim is now settled.',
+      'Download the settlement agreement'
+    ],
+    defendantAssertions: [
+      'Agreement signed',
+      'You’ve both signed a legal agreement. The claim is now settled.',
+      'Download the settlement agreement'
+    ]
+  },
+  {
+    status: 'Should show settlement rejected',
+    claim: {
+      ...data.claim,
+      ...data.partialAdmission,
+      ...data.claimantResponses.acceptWithNewPlan,
+      ...data.defendantRejectsSettlementPartyStatements
+    },
+    claimantAssertions: [
+      'The defendant has rejected your settlement agreement',
+      'You can request a County Court Judgment (CCJ) against them',
+      'Request a County Court Judgment (CCJ)'
+    ],
+    defendantAssertions: [
+      'You rejected the settlement agreement',
+      `${claimStoreServiceMock.sampleClaimObj.claim.claimants[0].name} can request a County Court Judgment (CCJ) against you`,
+      'Download your response'
+    ]
+  },
+  {
+    status: 'Should show claimant accepted court plan settlement',
+    claim: {
+      ...data.claim,
+      ...data.partialAdmission,
+      ...data.claimantResponses.acceptsWithCourtPlan,
+      ...data.claimantAcceptsCourtOfferPartyStatements
+    },
+    claimantAssertions: [
+      'You’ve signed a settlement agreement',
+      `We’ve emailed ${claimStoreServiceMock.sampleClaimObj.claim.defendants[0].name} the repayment plan and the settlement agreement for them to sign.`,
+      'If they do not respond you can request a County Court Judgment.'
+    ],
+    defendantAssertions: [
+      'They asked you to sign a settlement agreement to formalise the plan.',
+      'If you sign the agreement, they can’t request a County Court Judgment against you unless you break the terms.',
+      'View the repayment plan'
+    ]
+  }
+]
 
-describe('settlement claim statuses', () => {
+describe('Settlement claim statuses', () => {
   beforeEach(() => app.locals.csrf = 'dummy-token')
 
-  context('Settlement reached', () => {
-    bothParties.forEach(test => {
-      context(test.party, () => {
-        beforeEach(() => {
-          idamServiceMock.resolveRetrieveUserFor(test.id, 'citizen')
-          claimStoreServiceMock.resolveRetrieveClaimByExternalId({
-            ...data.claim,
-            ...data.partialAdmission,
-            ...data.payBySetDateSettlementReachedPartyStatements
-          })
-        })
-
-        it('should show settled dashboard status', async () => {
-          await request(app)
-            .get(test.url)
-            .set('Cookie', `${cookieName}=ABC`)
-            .expect(res => expect(res).to.be.successful.withText(
-              'You’ve both signed a settlement agreement',
-              'Download the settlement agreement'
-            ))
-        })
-      })
-    })
-  })
-
-  context('Offer settlement reached', () => {
-    bothParties.forEach(test => {
-      context(test.party, () => {
-        it('should show offer settlement reached dashboard status', async () => {
-          idamServiceMock.resolveRetrieveUserFor(test.id, 'citizen')
-          claimStoreServiceMock.resolveRetrieveClaimByExternalId({
-            ...data.claim,
-            ...data.partialAdmission,
-            ...data.claimantResponses.acceptBySettlement,
-            ...data.nonMonetaryOfferSettlementReachedPartyStatements
-          })
-
-          await request(app)
-            .get(test.url)
-            .set('Cookie', `${cookieName}=ABC`)
-            .expect(res => expect(res).to.be.successful.withText(
-              'Agreement signed',
-              'You’ve both signed a legal agreement. The claim is now settled.',
-              'Download the settlement agreement'
-            ))
-        })
-      })
-    })
-  })
-
-  context('Settlement agreement rejected', () => {
-    beforeEach(() => {
-      claimStoreServiceMock.resolveRetrieveClaimByExternalId({
-        ...data.claim,
-        ...data.partialAdmission,
-        ...data.claimantResponses.acceptWithNewPlan,
-        ...data.defendantRejectsSettlementPartyStatements
-      })
-    })
-
-    context(claimantContext.party, () => {
-      it('should show settlement rejected dashboard status', async () => {
+  testData.forEach(data => {
+    context(data.status, () => {
+      it(claimantContext.party, async () => {
         idamServiceMock.resolveRetrieveUserFor(claimantContext.id, 'citizen')
+        claimStoreServiceMock.resolveRetrieveByExternalId(data.claim)
 
         await request(app)
           .get(claimantContext.url)
           .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText(
-            'The defendant has rejected your settlement agreement',
-            'You can request a County Court Judgment (CCJ) against them',
-            'Request a County Court Judgment (CCJ)'
-          ))
+          .expect(res => expect(res).to.be.successful.withText(...data.claimantAssertions))
       })
-    })
 
-    context(defendantContext.party, () => {
-      it('should show settlement rejected dashboard status', async () => {
+      it(defendantContext.party, async () => {
         idamServiceMock.resolveRetrieveUserFor(defendantContext.id, 'citizen')
+        claimStoreServiceMock.resolveRetrieveByExternalId(data.claim)
 
-        const claimantName = claimStoreServiceMock.sampleClaimObj.claim.claimants[0].name
         await request(app)
           .get(defendantContext.url)
           .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText(
-            'You rejected the settlement agreement',
-            `${claimantName} can request a County Court Judgment (CCJ) against you`,
-            'Download your response'
-          ))
+          .expect(res => expect(res).to.be.successful.withText(...data.defendantAssertions))
       })
-    })
-  })
-
-  context('Claimant accepted court plan settlement', () => {
-    beforeEach(() => {
-      claimStoreServiceMock.resolveRetrieveClaimByExternalId({
-        ...data.claim,
-        ...data.partialAdmission,
-        ...data.claimantResponses.acceptsWithCourtPlan,
-        ...data.claimantAcceptsCourtOfferPartyStatements
-      })
-    })
-
-    it(claimantContext.party, async () => {
-      idamServiceMock.resolveRetrieveUserFor(claimantContext.id, 'citizen')
-
-      const defendantName = claimStoreServiceMock.sampleClaimObj.claim.defendants[0].name
-      await request(app)
-        .get(claimantContext.url)
-        .set('Cookie', `${cookieName}=ABC`)
-        .expect(res => expect(res).to.be.successful.withText(
-          'You’ve signed a settlement agreement',
-          `We’ve emailed ${defendantName} the repayment plan and the settlement agreement for them to sign.`,
-          'If they do not respond you can request a County Court Judgment.'
-        ))
-    })
-
-    it(defendantContext.party, async () => {
-      idamServiceMock.resolveRetrieveUserFor(defendantContext.id, 'citizen')
-
-      await request(app)
-        .get(defendantContext.url)
-        .set('Cookie', `${cookieName}=ABC`)
-        .expect(res => expect(res).to.be.successful.withText(
-          'They asked you to sign a settlement agreement to formalise the plan.',
-          'If you sign the agreement, they can’t request a County Court Judgment against you unless you break the terms.',
-          'View the repayment plan'
-        ))
     })
   })
 })
--- src/test/features/dashboard/routes/dashboard/settlement.ts
+++ src/test/features/dashboard/routes/dashboard/settlement.ts
@@ -7,32 +7,24 @@ import 'test/routes/expectations'
 import { Paths } from 'dashboard/paths'
 
 import { app } from 'main/app'
-
-import { attachDefaultHooks } from 'test/routes/hooks'
 import * as idamServiceMock from 'test/http-mocks/idam'
 import * as claimStoreServiceMock from 'test/http-mocks/claim-store'
 import * as draftStoreMock from 'test/http-mocks/draft-store'
 import * as data from 'test/data/entity/settlement'
+import { attachDefaultHooks } from 'test/routes/hooks'
 
 const cookieName: string = config.get<string>('session.cookieName')
 
 const pagePath = Paths.dashboardPage.uri
 
-interface TestContext {
-  party: string
-  id: string
-  ownMock: (override: any) => void
-  otherMock: () => void
-}
-
-const claimantContext: TestContext = {
+const claimantContext = {
   party: 'claimant',
   id: claimStoreServiceMock.sampleClaimObj.submitterId,
   ownMock: claimStoreServiceMock.resolveRetrieveByClaimantId,
   otherMock: claimStoreServiceMock.resolveRetrieveByDefendantIdToEmptyList
 }
 
-const defendantContext: TestContext = {
+const defendantContext = {
   party: 'defendant',
   id: claimStoreServiceMock.sampleClaimObj.defendantId,
   ownMock: override => claimStoreServiceMock.resolveRetrieveByDefendantId(
@@ -43,149 +35,99 @@ const defendantContext: TestContext = {
   otherMock: claimStoreServiceMock.resolveRetrieveByClaimantIdToEmptyList
 }
 
-const bothParties = [claimantContext, defendantContext]
-
-describe('settlement dashboard statuses', () => {
-  attachDefaultHooks(app)
-
-  beforeEach(() => draftStoreMock.resolveFindNoDraftFound())
-
-  context('Settlement reached', () => {
-    bothParties.forEach(test => {
-      context(test.party, () => {
-        beforeEach(() => {
-          idamServiceMock.resolveRetrieveUserFor(test.id, 'citizen')
-          test.ownMock({
-            ...data.claim,
-            ...data.partialAdmission,
-            ...data.payBySetDateSettlementReachedPartyStatements
-          })
-          test.otherMock()
-        })
-
-        it('should show settled dashboard status', async () => {
-          await request(app)
-            .get(pagePath)
-            .set('Cookie', `${cookieName}=ABC`)
-            .expect(res => expect(res).to.be.successful.withText(
-              'You’ve both signed a settlement agreement.'
-            ))
-        })
-      })
-    })
-  })
-
-  context('Offer settlement reached', () => {
-    bothParties.forEach(test => {
-      context(test.party, () => {
-        it('should show offer settlement reached dashboard status', async () => {
-          idamServiceMock.resolveRetrieveUserFor(test.id, 'citizen')
-          test.ownMock({
-            ...data.claim,
-            ...data.partialAdmission,
-            ...data.claimantResponses.acceptBySettlement,
-            ...data.nonMonetaryOfferSettlementReachedPartyStatements
-          })
-          test.otherMock()
-
-          await request(app)
-            .get(pagePath)
-            .set('Cookie', `${cookieName}=ABC`)
-            .expect(res => expect(res).to.be.successful.withText(
-              'You’ve both signed a legal agreement.',
-              'The claim is now settled.'
-            ))
-        })
-      })
-    })
-  })
-
-  context('Settlement agreement rejected', () => {
-    const override = {
+const testData = [
+  {
+    status: 'Should show case settled when settlement reached',
+    claim: {
+      ...data.claim,
+      ...data.partialAdmission,
+      ...data.payBySetDateSettlementReachedPartyStatements
+    },
+    claimantAssertions: [
+      'You’ve both signed a settlement agreement.'
+    ],
+    defendantAssertions: [
+      'You’ve both signed a settlement agreement'
+    ]
+  },
+  {
+    status: 'Should show offer settlement reached',
+    claim: {
+      ...data.claim,
+      ...data.partialAdmission,
+      ...data.claimantResponses.acceptBySettlement,
+      ...data.nonMonetaryOfferSettlementReachedPartyStatements
+    },
+    claimantAssertions: [
+      'You’ve both signed a legal agreement.',
+      'The claim is now settled.'
+    ],
+    defendantAssertions: [
+      'You’ve both signed a legal agreement.',
+      'The claim is now settled.'
+    ]
+  },
+  {
+    status: 'Should show settlement rejected',
+    claim: {
       ...data.claim,
       ...data.partialAdmission,
       ...data.claimantResponses.acceptWithNewPlan,
       ...data.defendantRejectsSettlementPartyStatements
-    }
-
-    context(claimantContext.party, () => {
-      beforeEach(() => {
-        claimantContext.ownMock(override)
-        claimantContext.otherMock()
-        idamServiceMock.resolveRetrieveUserFor(claimantContext.id, 'citizen')
-      })
-
-      it('should show settlement rejected dashboard status', async () => {
-        const defendantName = claimStoreServiceMock.sampleClaimObj.claim.defendants[0].name
-        await request(app)
-          .get(pagePath)
-          .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText(
-            defendantName + ' has rejected your settlement agreement',
-            'You can request a County Court Judgment against them'
-          ))
-      })
-    })
-
-    context(defendantContext.party, () => {
-      beforeEach(() => {
-        defendantContext.ownMock(override)
-        defendantContext.otherMock()
-        idamServiceMock.resolveRetrieveUserFor(defendantContext.id, 'citizen')
-      })
-
-      it('should show settlement rejected dashboard status', async () => {
-        await request(app)
-          .get(pagePath)
-          .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText(
-            'You rejected the settlement agreement'
-          ))
-      })
-    })
-  })
-
-  context('Claimant accepted court plan settlement', () => {
-    const override = {
+    },
+    claimantAssertions: [
+      `${claimStoreServiceMock.sampleClaimObj.claim.defendants[0].name} has rejected your settlement agreement`,
+      'You can request a County Court Judgment against them'
+    ],
+    defendantAssertions: [
+      'You rejected the settlement agreement'
+    ]
+  },
+  {
+    status: 'Should show claimant accepted court plan settlement',
+    claim: {
       ...data.claim,
       ...data.partialAdmission,
       ...data.claimantResponses.acceptsWithCourtPlan,
       ...data.claimantAcceptsCourtOfferPartyStatements
-    }
+    },
+    claimantAssertions: [
+      'You’ve signed a settlement agreement.',
+      'The defendant can choose to sign it or not.'
+    ],
+    defendantAssertions: [
+      `${claimStoreServiceMock.sampleClaimObj.claim.claimants[0].name} asked you to sign a settlement agreement`
+    ]
+  }
+]
 
-    context(claimantContext.party, () => {
-      beforeEach(() => {
-        claimantContext.ownMock(override)
+describe('Settlement claim statuses', () => {
+  attachDefaultHooks(app)
+
+  testData.forEach(data => {
+    context(data.status, () => {
+      beforeEach(() => draftStoreMock.resolveFindNoDraftFound())
+
+      it(claimantContext.party, async () => {
+        claimantContext.ownMock(data.claim)
         claimantContext.otherMock()
         idamServiceMock.resolveRetrieveUserFor(claimantContext.id, 'citizen')
-      })
 
-      it('should show settlement agreement signed and pending countersignature status', async () => {
         await request(app)
           .get(pagePath)
           .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText(
-            'You’ve signed a settlement agreement.',
-            'The defendant can choose to sign it or not.'
-          ))
+          .expect(res => expect(res).to.be.successful.withText(...data.claimantAssertions))
       })
-    })
 
-    context(defendantContext.party, () => {
-      beforeEach(() => {
-        defendantContext.ownMock(override)
+      it(defendantContext.party, async () => {
+        defendantContext.ownMock(data.claim)
         defendantContext.otherMock()
         idamServiceMock.resolveRetrieveUserFor(defendantContext.id, 'citizen')
-      })
 
-      it('should show settlement agreement signed and pending countersignature status', async () => {
-        const claimantName = claimStoreServiceMock.sampleClaimObj.claim.claimants[0].name
         await request(app)
           .get(pagePath)
           .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText(
-            claimantName + ' asked you to sign a settlement agreement'
-          ))
+          .expect(res => expect(res).to.be.successful.withText(...data.defendantAssertions))
       })
     })
   })
