bee2100cd448697c9e5d78da8727aaa15bd41112
Robert Parkinson <robert.parkinson@hmcts.net>
2019-06-07 14:46:35 +0100

    Refactor mock data to separate file

--- src/test/features/response/routes/confirmation.ts
+++ src/test/features/response/routes/confirmation.ts
@@ -11,6 +11,7 @@ import { app } from 'main/app'
 
 import * as idamServiceMock from 'test/http-mocks/idam'
 import * as claimStoreServiceMock from 'test/http-mocks/claim-store'
+import * as fullDefenceMock from 'test/data/entity/fullDefenceData'
 
 import { checkCountyCourtJudgmentRequestedGuard } from 'test/common/checks/ccj-requested-check'
 import { checkNotDefendantInCaseGuard } from 'test/common/checks/not-defendant-in-case-check'
@@ -47,7 +48,7 @@ describe('Defendant response: confirmation page', () => {
       })
 
       it('should render mediation option when mediation is yes', async () => {
-        claimStoreServiceMock.resolveRetrieveClaimBySampleExternalId(claimStoreServiceMock.sampleClaimWithFullDefenceMediation)
+        claimStoreServiceMock.resolveRetrieveClaimBySampleExternalId(fullDefenceMock.sampleClaimWithFullDefenceMediation)
 
         await request(app)
           .get(ResponsePaths.confirmationPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId }))
@@ -61,7 +62,7 @@ describe('Defendant response: confirmation page', () => {
       })
 
       it('should render full defence paper DQ page when no mediation is chosen', async () => {
-        claimStoreServiceMock.resolveRetrieveClaimBySampleExternalId(claimStoreServiceMock.sampleClaimWithFullDefenceNoMediation)
+        claimStoreServiceMock.resolveRetrieveClaimBySampleExternalId(fullDefenceMock.sampleClaimWithFullDefenceNoMediation)
 
         await request(app)
           .get(ResponsePaths.confirmationPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId }))
--- src/test/http-mocks/claim-store.ts
+++ src/test/http-mocks/claim-store.ts
@@ -365,26 +365,6 @@ export const sampleDefendantResponseObj = {
   }
 }
 
-export const sampleDefendantResponseNoMediationObj = {
-  respondedAt: '2017-07-25T22:45:51.785',
-  response: {
-    responseType: 'FULL_DEFENCE',
-    defenceType: 'DISPUTE',
-    defence: 'I reject this money claim',
-    freeMediation: 'no',
-    defendant: {
-      type: 'individual',
-      name: 'full name',
-      address: {
-        line1: 'line1',
-        line2: 'line2',
-        city: 'city',
-        postcode: 'bb127nq'
-      }
-    }
-  }
-}
-
 export const samplePartialAdmissionWithPaymentBySetDateResponseObj = {
   respondedAt: '2017-07-25T22:45:51.785',
   claimantRespondedAt: '2017-07-25T22:45:51.785',
@@ -451,17 +431,6 @@ export const sampleFullDefenceRejectEntirely = {
 export const sampleFullDefenceWithStatesPaidGreaterThanClaimAmount = {
   respondedAt: '2017-07-25T22:45:51.785',
   response: fullDefenceWithStatesPaidGreaterThanClaimAmount
-}
-
-export const sampleClaimWithFullDefenceNoMediation = {
-  ...sampleClaimIssueObj,
-  ...sampleDefendantResponseNoMediationObj,
-  directionsQuestionnaireDeadline: MomentFactory.currentDate().add(14, 'days')
-}
-
-export const sampleClaimWithFullDefenceMediation = {
-  ...sampleClaimIssueObj,
-  ...sampleDefendantResponseObj
 }
 
 export function mockCalculateInterestRate (expected: number): mock.Scope {
