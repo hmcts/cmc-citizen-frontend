8d278ea3e58949fbb3e3429cc7294eafa71a7fea
Kiran Varma <varma.ap@gmail.com>
2019-06-19 07:54:08 +0100

    master merge issues resolved and reverted DQ confirmation changes from this PR and there is different story for it

--- src/test/features/directions-questionnaire/routes/hearing-location.ts
+++ src/test/features/directions-questionnaire/routes/hearing-location.ts
@@ -76,6 +76,7 @@ describe('Directions Questionnaire - hearing location', () => {
           claimStoreServiceMock.resolveRetrieveClaimByExternalId(claim)
           draftStoreServiceMock.resolveFind('directionsQuestionnaire')
           draftStoreServiceMock.resolveFind('response')
+          courtFinderMock.rejectFind()
 
           await request(app)
             .get(pagePath)
@@ -89,6 +90,7 @@ describe('Directions Questionnaire - hearing location', () => {
           claimStoreServiceMock.resolveRetrieveClaimByExternalId(claim)
           draftStoreServiceMock.resolveFind('directionsQuestionnaire')
           draftStoreServiceMock.resolveFind('response')
+          courtFinderMock.resolveFind()
 
           await request(app)
             .get(pagePath)
--- src/test/features/response/routes/confirmation.ts
+++ src/test/features/response/routes/confirmation.ts
@@ -11,7 +11,6 @@ import { app } from 'main/app'
 
 import * as idamServiceMock from 'test/http-mocks/idam'
 import * as claimStoreServiceMock from 'test/http-mocks/claim-store'
-import * as fullDefenceMock from 'test/data/entity/fullDefenceData'
 
 import { checkCountyCourtJudgmentRequestedGuard } from 'test/common/checks/ccj-requested-check'
 import { checkNotDefendantInCaseGuard } from 'test/common/checks/not-defendant-in-case-check'
@@ -34,51 +33,13 @@ describe('Defendant response: confirmation page', () => {
 
       checkCountyCourtJudgmentRequestedGuard(app, method, pagePath)
 
-      it('should render page when DQs enabled and user said no to mediation', async () => {
-        claimStoreServiceMock.resolveRetrieveClaimBySampleExternalId(fullDefenceMock.sampleClaimWithFullDefenceNoMediationDQsEnabled)
+      it('should render page when everything is fine', async () => {
+        claimStoreServiceMock.resolveRetrieveClaimByExternalIdWithResponse()
 
         await request(app)
           .get(ResponsePaths.confirmationPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId }))
           .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText('You’ve submitted your response',
-            'accepts your response the claim will be settled. We’ll contact you by post when they respond.',
-            'If they reject your response the court will review the case. You might have to go to a hearing.',
-            'We’ll contact you by post if we set a hearing date to tell you how to prepare.',
-            'Settle out of court',
-            'For example you could offer to repair goods you sold the claimant or suggest a payment.',
-            'You can avoid getting a County Court Judgment if the claimant accepts your offer.'
-          ))
-      })
-
-      it('should render mediation option when mediation is yes', async () => {
-        claimStoreServiceMock.resolveRetrieveClaimBySampleExternalId(fullDefenceMock.sampleClaimWithFullDefenceMediation)
-
-        await request(app)
-          .get(ResponsePaths.confirmationPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId }))
-          .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText('You’ve submitted your response',
-            'if they want to try mediation. If they agree, we’ll contact you with a date for an appointment. If not, we’ll tell you what to do.',
-            'Settle out of court',
-            'For example you could offer to repair goods you sold the claimant or suggest a payment.',
-            'You can avoid getting a County Court Judgment if the claimant accepts your offer.'
-          ))
-      })
-
-      it('should render full defence paper DQ page when no mediation is chosen', async () => {
-        claimStoreServiceMock.resolveRetrieveClaimBySampleExternalId(fullDefenceMock.sampleClaimWithFullDefenceNoMediation)
-
-        await request(app)
-          .get(ResponsePaths.confirmationPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId }))
-          .set('Cookie', `${cookieName}=ABC`)
-          .expect(res => expect(res).to.be.successful.withText('You’ve submitted your response',
-            'You’ll have to go to a hearing.',
-            'complete a directions questionnaire form',
-            'Your defence will be cancelled if you don’t complete and return the form by 4pm on',
-            'We’ll contact you when we set a hearing date to tell you how to prepare.',
-            'Settle out of court',
-            'For example you could offer to repair goods you sold the claimant or suggest a payment.',
-            'You can avoid getting a County Court Judgment if the claimant accepts your offer.'
-          ))
+          .expect(res => expect(res).to.be.successful.withText('You’ve submitted your response'))
       })
 
       it('should return 500 and render error page when cannot retrieve claim', async () => {
