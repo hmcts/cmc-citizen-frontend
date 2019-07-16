6674755b4dbcd961bf047d5cc852e6f0bf437971
sudheer <sudheer.ajax@gmail.com>
2019-07-16 13:31:43 +0100

    Merged mediation missing test cases

--- src/test/features/response/routes/partial-admission/how-much-have-you-paid.ts
+++ src/test/features/response/routes/partial-admission/how-much-have-you-paid.ts
@@ -66,6 +66,8 @@ describe(`Defendant: partial admission - ${header}`, () => {
               type: ResponseType.PART_ADMISSION
             }
           })
+          draftStoreServiceMock.resolveFind('mediation')
+
           await request(app)
             .get(pagePath)
             .set('Cookie', `${cookieName}=ABC`)
@@ -109,6 +111,7 @@ describe(`Defendant: partial admission - ${header}`, () => {
           it('should return 500 and render error page when cannot save response draft', async () => {
             claimStoreServiceMock.resolveRetrieveClaimByExternalId()
             draftStoreServiceMock.resolveFind('response:partial-admission')
+            draftStoreServiceMock.resolveFind('mediation')
             draftStoreServiceMock.rejectSave()
 
             await request(app)
@@ -127,6 +130,7 @@ describe(`Defendant: partial admission - ${header}`, () => {
                 type: ResponseType.PART_ADMISSION
               }
             })
+            draftStoreServiceMock.resolveFind('mediation')
           })
 
           it('when form is valid should render page', async () => {
--- src/test/features/response/routes/partial-admission/payment-date.ts
+++ src/test/features/response/routes/partial-admission/payment-date.ts
@@ -70,6 +70,7 @@ describe('Pay by set date: payment date', () => {
 
         it('should render page when everything is fine', async () => {
           draftStoreServiceMock.resolveFind('response:partial-admission', draft)
+          draftStoreServiceMock.resolveFind('mediation')
 
           await request(app)
             .get(pagePath)
@@ -110,6 +111,7 @@ describe('Pay by set date: payment date', () => {
 
         it('should render error page when unable to save draft', async () => {
           draftStoreServiceMock.resolveFind('response:partial-admission', draft)
+          draftStoreServiceMock.resolveFind('mediation')
           draftStoreServiceMock.rejectSave()
 
           await request(app)
@@ -121,6 +123,7 @@ describe('Pay by set date: payment date', () => {
 
         it('should trigger validation when invalid data is given', async () => {
           draftStoreServiceMock.resolveFind('response:partial-admission', draft)
+          draftStoreServiceMock.resolveFind('mediation')
 
           await request(app)
             .post(pagePath)
@@ -131,6 +134,7 @@ describe('Pay by set date: payment date', () => {
 
         it('should redirect to task list when data is valid and user provides a date within 28 days from today', async () => {
           draftStoreServiceMock.resolveFind('response:partial-admission', draft)
+          draftStoreServiceMock.resolveFind('mediation')
           draftStoreServiceMock.resolveSave()
 
           await request(app)
