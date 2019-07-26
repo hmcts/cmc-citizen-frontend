d01c8314a12f8e58c57456591cb1632f78a248eb
Mark Ryan <mark.ryan@hmcts.net>
2019-07-26 08:59:25 +0100

    fix single quote in expectation stmt

--- src/integration-test/tests/citizen/claimantResponse/reject_test.ts
+++ src/integration-test/tests/citizen/claimantResponse/reject_test.ts
@@ -103,7 +103,7 @@ if (process.env.FEATURE_ADMISSIONS === 'true') {
       // as defendant
       defendantResponseSteps.disputeClaimAsAlreadyPaid(testData, claimantResponseTestData, true)
       I.see(testData.claimRef)
-      I.see(`You told us you've paid £125. We've sent ${testData.claimantName} this response`)
+      I.see(`You told us you’ve paid £125. We’ve sent ${testData.claimantName} this response`)
       // check dashboard
       I.click('My account')
       I.see(`We’ve emailed ${testData.claimantName} telling them when and how you said you paid the claim.`)
