7083ab6624d7380e9851241f498debd3bb45594b
Andrew Walker <andrewwa@kainos.com>
2019-07-29 15:38:02 +0100

    Reverting removal of unused method; it was extracted from a large PR for ease-of-review purposes

--- src/main/common/utils/freeMediationUtil.ts
+++ src/main/common/utils/freeMediationUtil.ts
@@ -3,6 +3,7 @@ import { YesNoOption } from 'main/app/claims/models/response/core/yesNoOption'
 import { ResponseDraft } from 'main/features/response/draft/responseDraft'
 import { Claim } from 'main/app/claims/models/claim'
 import { FreeMediationOption } from 'main/app/forms/models/freeMediation'
+import { CompanyDetails } from 'forms/models/companyDetails'
 import { FreeMediation } from 'forms/models/freeMediation'
 
 export class FreeMediationUtil {
@@ -40,6 +41,21 @@ export class FreeMediationUtil {
         }
       } else {
         return mediationDraft.canWeUse.mediationPhoneNumber
+      }
+    }
+    return undefined
+  }
+
+  static getMediationContactPerson (claim: Claim, mediationDraft: MediationDraft, draft?: ResponseDraft): string {
+    if (mediationDraft.canWeUseCompany) {
+      if (mediationDraft.canWeUseCompany.option === FreeMediationOption.YES) {
+        if (!claim.isResponseSubmitted() && draft) {
+          return (draft.defendantDetails.partyDetails as CompanyDetails).contactPerson || undefined
+        } else {
+          return (claim.claimData.claimant as CompanyDetails).contactPerson
+        }
+      } else {
+        return mediationDraft.canWeUseCompany.mediationContactPerson
       }
     }
     return undefined
