084802734cf7c7a4ac09696e51d947240d8900d3
Kiran Varma <varma.ap@gmail.com>
2019-08-09 20:16:42 +0100

    added unit test for failing scenario

--- src/test/app/claims/models/directions-questionnaire/directionsQuestionnaire.ts
+++ src/test/app/claims/models/directions-questionnaire/directionsQuestionnaire.ts
@@ -56,6 +56,23 @@ describe('DirectionsQuestionnaire', () => {
       expect(DirectionsQuestionnaire.deserialize(directionsQuestionnaireDraftSampleData)).to.deep.equal(expectedData)
     })
 
+    it('should deserialize directions questionnaire correctly when no expert reports and no expert evidence', () => {
+      let directionsQuestionnaireDraftSampleData: DirectionsQuestionnaireDraft
+      // @ts-ignore
+      directionsQuestionnaireDraftSampleData = new DirectionsQuestionnaireDraft().deserialize({...sampleDirectionsQuestionnaireDraftObj, ...{
+        expertReports: {
+          declared: true,
+          rows: [{}]
+        },
+        expertEvidence: undefined
+      }})
+
+      expect(DirectionsQuestionnaire.deserialize(directionsQuestionnaireDraftSampleData)).to.deep.equal({...expectedData, ...{
+        expertReports: undefined,
+        expertRequest: undefined
+      }})
+    })
+
     it('should deserialize undefined directions questionnaire and it should return undefined', () => {
       expect(DirectionsQuestionnaire.deserialize(undefined)).to.be.equal(undefined)
     })
