1c0c515b365151799674ea136337f3388f3dca31
Ben Doherty <ben.doherty@hmcts.net>
2019-08-20 12:55:16 +0100

    fixed tests for content change

--- src/test/features/response/routes/confirmation.ts
+++ src/test/features/response/routes/confirmation.ts
@@ -53,10 +53,7 @@ describe('Defendant response: confirmation page', () => {
           .get(ResponsePaths.confirmationPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId }))
           .set('Cookie', `${cookieName}=ABC`)
           .expect(res => expect(res).to.be.successful.withText('You’ve submitted your response',
-            'We’ll contact you when John Smith responds, to tell you what to do next.',
-            'If John Smith accepts your response the claim will be ended.',
-            'If they reject your response and agree to try mediation we’ll contact you to arrange a call with the mediator.',
-            'If they reject your response and don’t want to try mediation, the court will review the case. You might have to go to a hearing.'))
+            'We’ll ask John Smith if they want to try mediation. If they agree, we’ll contact you with a date for an appointment. If not, we’ll tell you what to do.'))
       })
 
       it('should render page when no for mediation and DQ', async () => {
@@ -66,9 +63,10 @@ describe('Defendant response: confirmation page', () => {
           .get(ResponsePaths.confirmationPage.evaluateUri({ externalId: claimStoreServiceMock.sampleClaimObj.externalId }))
           .set('Cookie', `${cookieName}=ABC`)
           .expect(res => expect(res).to.be.successful.withText('You’ve submitted your response',
-            'We’ll contact you when John Smith responds, to tell you what to do next.',
-            'If John Smith accepts your response the claim will be ended.',
-            'If they reject your response the court will review the case. You might have to go to a hearing.'))
+            'You’ll have to go to a hearing.',
+            'You need to complete a directions questionnaire form to tell us more about the claim.',
+            'Your defence will be cancelled if you don’t complete and return the form by 4pm on ',
+            'We’ll contact you when we set a hearing date to tell you how to prepare.'))
       })
 
       it('when full defence already paid with mediation should render page when everything is fine', async () => {
