24ddbe021cf279cb2f9ec87d84768cbf6c6fab99
Robert Parkinson <robert.parkinson@hmcts.net>
2019-09-02 13:28:13 +0100

    Remove deletion as blocked by idam

--- src/integration-test/bootstrap/teardown.ts
+++ src/integration-test/bootstrap/teardown.ts
@@ -1,11 +1,12 @@
 /* tslint:disable:no-console */
 
-import { IdamClient } from 'integration-test/helpers/clients/idamClient'
+// import { IdamClient } from 'integration-test/helpers/clients/idamClient'
 
 module.exports = async function (done: () => void) {
   try {
-    await IdamClient.deleteUser('civilmoneyclaims+citizen-claimant@gmail.com')
-    await IdamClient.deleteUser('civilmoneyclaims+citizen-defendant@gmail.com')
+    // await IdamClient.deleteUser('civilmoneyclaims+citizen-claimant@gmail.com')
+    // await IdamClient.deleteUser('civilmoneyclaims+citizen-defendant@gmail.com')
+    console.log('Teardown finished')
   } catch (error) {
     handleError(error)
   }
