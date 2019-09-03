6cd36e1248badd20d112bfe57cb403116506d758
Robert Parkinson <robert.parkinson@hmcts.net>
2019-09-03 13:03:42 +0100

    Remove comment section once deadline has passed

--- src/main/app/claims/models/reviewOrder.ts
+++ src/main/app/claims/models/reviewOrder.ts
@@ -1,12 +1,12 @@
-import { Moment } from 'moment'
+import * as moment from 'moment'
 import { MomentFactory } from 'shared/momentFactory'
 
 export class ReviewOrder {
   reason: string
   requestedBy: string
-  requestedAt: Moment
+  requestedAt: moment.Moment
 
-  constructor (reason?: string, requestedBy?: string, requestedAt?: Moment) {
+  constructor (reason?: string, requestedBy?: string, requestedAt?: moment.Moment) {
     this.reason = reason
     this.requestedBy = requestedBy
     this.requestedAt = requestedAt
@@ -20,5 +20,13 @@ export class ReviewOrder {
     }
 
     return this
+  }
+
+  isReconsiderationDeadlinePassed (deadline: moment.Moment): boolean {
+    if (!deadline) {
+      return false
+    }
+
+    return MomentFactory.currentDateTime().isAfter(deadline.set({ h: 16, m: 0 }))
   }
 }
