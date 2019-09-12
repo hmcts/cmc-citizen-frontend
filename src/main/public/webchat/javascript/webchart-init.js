fa4af2a69b55977cb49adaa05850e2966d5fc56e
Dharmendra kumar <k_dharmendra@hotmail.com>
2019-09-12 09:07:56 +0100

    use config for the webchat

--- src/main/public/webchat/javascript/webchart-init.js
+++ src/main/public/webchat/javascript/webchart-init.js
@@ -1,3 +1,38 @@
+var _paq = _paq || []
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+
+var xhttp
+xhttp=new XMLHttpRequest
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var json = JSON.parse(this.response);
+
+    webchat_init({
+      uuid: 'script_' + json.chatId,
+      tenant: json.tenant,
+      channel: 'CMC',
+      btnNoAgents: '/aG1jdHNzdGFnaW5nMDE/button_'+json.buttonNoAgents+'/img/logo',
+      btnAgentsBusy: '/aG1jdHNzdGFnaW5nMDE/button_'+json.buttonAgentsBusy+'/img/logo',
+      btnServiceClosed: '/aG1jdHNzdGFnaW5nMDE/button_'+json.buttonServiceClosed+'/img/logo',
+      chatDownAction: 'showMessage',
+      chatLinkFocusable: false,
+      textChatDown: 'The web chat service is temporarily unavailable, please try again later.',
+      textChatClosed: 'Web chat is now closed.\nOpening hours are Monday to Friday, 9:30am to 5pm.',
+      textChatWithAnAgent: 'Chat online with an agent',
+      textNoAgentsAvailable: 'No agents are available, please try again later.',
+      textAllAgentsBusy: 'All our web chat agents are busy helping other people. Please try again later or contact us using one of the ways above.',
+      textChatAlreadyOpen: 'A web chat window is already open.',
+      textAdditional: 'Monday to Friday, 9am to 5pm.',
+      stylesheetURL: 'https://' + getUrl() + '/webchat/css/hmcts-webchat-gds-v3.css',
+      busHandlerURL: 'https://' + getUrl() + '/webchat/javascript/hmcts-webchat-busHandler.js',
+      gdsMajorVersion: 3
+    })
+  }
+};
+xhttp.open("GET", '/webchat', true)
+xhttp.send()
+
+
 function getUrl () {
   if (window.location.port) {
     return window.location.hostname + ":" + window.location.port
@@ -5,24 +40,3 @@ function getUrl () {
     return window.location.hostname
   }
 }
-
-webchat_init({
-  uuid: 'script_17284281475d5519274e25a3.69472655',
-  tenant: 'aG1jdHNzdGFnaW5nMDE',
-  channel: 'CMC',
-  btnNoAgents: '/aG1jdHNzdGFnaW5nMDE/button_7732814745cac6f4603c4d1.53357933/img/logo',
-  btnAgentsBusy: '/aG1jdHNzdGFnaW5nMDE/button_2042157415cc19c95669039.65793052/img/logo',
-  btnServiceClosed: '/aG1jdHNzdGFnaW5nMDE/button_20199488815cc1a89e0861d5.73103009/img/logo',
-  chatDownAction: 'showMessage',
-  chatLinkFocusable: false,
-  textChatDown: 'The web chat service is temporarily unavailable, please try again later.',
-  textChatClosed: 'Web chat is now closed.\nOpening hours are Monday to Friday, 9:30am to 5pm.',
-  textChatWithAnAgent: 'Chat online with an agent',
-  textNoAgentsAvailable: 'No agents are available, please try again later.',
-  textAllAgentsBusy: 'All our web chat agents are busy helping other people. Please try again later or contact us using one of the ways above.',
-  textChatAlreadyOpen: 'A web chat window is already open.',
-  textAdditional: 'Monday to Friday, 9am to 5pm.',
-  stylesheetURL: 'https://' + getUrl() + '/webchat/css/hmcts-webchat-gds-v3.css',
-  busHandlerURL: 'https://' + getUrl() + '/webchat/javascript/hmcts-webchat-busHandler.js',
-  gdsMajorVersion: 3
-})
---
+++ src/main/routes/webchat.ts
@@ -0,0 +1,14 @@
+import * as express from 'express'
+import * as config from 'config'
+
+class Paths {
+  static main: string = '/webchat'
+}
+
+/* tslint:disable:no-default-export */
+export default express.Router()
+  .get(Paths.main, (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    const site = config.get('webChat')
+    res.send(JSON.stringify(site))
+  })
