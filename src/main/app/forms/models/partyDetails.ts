f1dba89c29e31e698857a57d71a004ab302b282e
sudheer <sudheer.ajax@gmail.com>
2019-10-01 09:45:19 +0100

    Fixed validation issue

--- src/main/app/forms/models/partyDetails.ts
+++ src/main/app/forms/models/partyDetails.ts
@@ -31,6 +31,11 @@ export class PartyDetails {
   @ValidateNested({ groups: ['claimant', 'defendant', 'response'] })
   correspondenceAddress?: CorrespondenceAddress
 
+  firstName?: string
+
+  lastName?: string
+
+  title?: string
   constructor (name?: string,
                address: Address = new Address(),
                hasCorrespondenceAddress: boolean = false,
