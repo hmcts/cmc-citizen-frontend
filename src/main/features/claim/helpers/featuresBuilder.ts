c0fdda1edd47cc0bf33ab0de801f7a269ef43b78
Dave Oxtoby <david.oxtoby@hmcts.net>
2019-11-04 15:34:56 +0000

    ROC-6776: Remove interest from feature calculation

--- src/main/features/claim/helpers/featuresBuilder.ts
+++ src/main/features/claim/helpers/featuresBuilder.ts
@@ -1,5 +1,4 @@
 import { FeatureToggles } from 'utils/featureToggles'
-import { draftClaimAmountWithInterest } from 'shared/interestUtils'
 import { FeatureTogglesClient } from 'shared/clients/featureTogglesClient'
 import { ClaimStoreClient } from 'claims/claimStoreClient'
 import { User } from 'idam/user'
@@ -18,16 +17,14 @@ export class FeaturesBuilder {
       features = 'admissions'
     }
 
-    if (draft.document.amount.totalAmount() <= 300 && FeatureToggles.isEnabled('directionsQuestionnaire')) {
-      if (await featureTogglesClient.isFeatureToggleEnabled(user, roles, 'cmc_directions_questionnaire')) {
-        features += (features === '') ? 'directionsQuestionnaire' : ', directionsQuestionnaire'
+    const PILOT_AMOUNT = 300
+    if (draft.document.amount.totalAmount() <= PILOT_AMOUNT) {
+      if (FeatureToggles.isEnabled('directionsQuestionnaire') && await featureTogglesClient.isFeatureToggleEnabled(user, roles, 'cmc_directions_questionnaire')) {
+        features += features === '' ? 'directionsQuestionnaire' : ', directionsQuestionnaire'
       }
-    }
 
-    const totalAmount = await draftClaimAmountWithInterest(draft.document)
-    if (totalAmount <= 300) {
       if (await featureTogglesClient.isFeatureToggleEnabled(user, roles, 'cmc_mediation_pilot')) {
-        features += (features === '') ? 'mediationPilot' : ', mediationPilot'
+        features += features === '' ? 'mediationPilot' : ', mediationPilot'
       }
     }
     return (features === '') ? undefined : features
--- src/test/features/claim/helpers/featuresBuilder.ts
+++ src/test/features/claim/helpers/featuresBuilder.ts
@@ -13,6 +13,7 @@ import { claimDraft as claimDraftData } from 'test/data/draft/claimDraft'
 import * as moment from 'moment'
 import { attachDefaultHooks } from 'test/routes/hooks'
 import { app } from 'main/app'
+import { YesNoOption } from 'models/yesNoOption'
 
 function mockFeatureFlag (feature: string, enabled: boolean): mock.Scope {
   return mock(`${config.get<string>('feature-toggles-api.url')}/api/ff4j/check`)
@@ -20,68 +21,113 @@ function mockFeatureFlag (feature: string, enabled: boolean): mock.Scope {
     .reply(HttpStatus.OK, enabled ? { some: 'value' } : null)
 }
 
-function mockInterestCalculation (amount: number = 10): mock.Scope {
-  return mock(`${config.get<string>('claim-store.url')}/interest`)
-    .get(`/calculate`)
-    .query(true)
-    .reply(HttpStatus.OK, { amount: amount })
-}
 const user = new User('1', 'user@example.com', 'John', 'Smith', [], 'citizen', '')
 const elegibleDraft = new Draft<DraftClaim>(123, 'claim', new DraftClaim().deserialize(claimDraftData), moment(), moment())
+const pilotLimit = 300
+const limitDraft = new Draft<DraftClaim>(123, 'claim', new DraftClaim().deserialize({
+  ...claimDraftData,
+  amount: {
+    rows: [
+      {
+        reason: 'Valid reason',
+        amount: pilotLimit
+      }
+    ]
+  },
+  interest: {
+    option: YesNoOption.NO
+  }
+}), moment(), moment())
+
+const overLimitDraft = new Draft<DraftClaim>(123, 'claim', new DraftClaim().deserialize({
+  ...claimDraftData,
+  amount: {
+    rows: [
+      {
+        reason: 'Valid reason',
+        amount: pilotLimit + 1
+      }
+    ]
+  },
+  interest: {
+    option: YesNoOption.NO
+  }
+}), moment(), moment())
+
+const limitPlusInterestDraft = new Draft<DraftClaim>(123, 'claim', new DraftClaim().deserialize({
+  ...claimDraftData,
+  amount: {
+    rows: [
+      {
+        reason: 'Valid reason',
+        amount: pilotLimit
+      }
+    ]
+  }
+}), moment(), moment())
 
 describe('FeaturesBuilder', () => {
   attachDefaultHooks(app)
 
   beforeEach(() => {
     claimStoreServiceMock.resolveRetrieveUserRoles()
-    mockInterestCalculation()
-  })
-  it('should add admissions to features if flag is set', async () => {
-    mockFeatureFlag('cmc_admissions', true)
-    mockFeatureFlag('cmc_directions_questionnaire', false)
-    const features = await FeaturesBuilder.features(elegibleDraft, user)
-    expect(features).to.equal('admissions')
   })
 
-  it('should add dq to features if flag is set and draft is <= 300', async () => {
+  describe('Admissions Feature', () => {
+    it('should add admissions to features if flag is set', async () => {
+      mockFeatureFlag('cmc_admissions', true)
+      mockFeatureFlag('cmc_directions_questionnaire', false)
+      const features = await FeaturesBuilder.features(elegibleDraft, user)
+      expect(features).to.equal('admissions')
+    })
+  })
+
+  describe('Directions Questionnaire Feature', () => {
+    it('should add dq to features if flag is set and draft is <= 300', async () => {
+      mockFeatureFlag('cmc_directions_questionnaire', true)
+      const features = await FeaturesBuilder.features(limitDraft, user)
+      expect(features).to.equal('directionsQuestionnaire')
+    })
+
+    it('should add dq to features if principal amount <= 300 but with interest is > 300 and flag is set', async () => {
+      mockFeatureFlag('cmc_directions_questionnaire', true)
+      mockFeatureFlag('cmc_mediation_pilot', false)
+      const features = await FeaturesBuilder.features(limitPlusInterestDraft, user)
+      expect(features).to.equal('directionsQuestionnaire')
+    })
+
+    it('should not dd dq to features if principal amount is > 300', async () => {
+      const features = await FeaturesBuilder.features(overLimitDraft, user)
+      expect(features).to.be.undefined
+    })
+
+  })
+
+  describe('Mediation Pilot Feature', () => {
+    it('should add mediation pilot to features if principal amount <= 300 and flag is set', async () => {
+      mockFeatureFlag('cmc_directions_questionnaire', false)
+      mockFeatureFlag('cmc_mediation_pilot', true)
+      const features = await FeaturesBuilder.features(limitDraft, user)
+      expect(features).to.equal('mediationPilot')
+    })
+
+    it('should add mediation pilot to features if principal amount <= 300 but with interest is > 300 and flag is set', async () => {
+      mockFeatureFlag('cmc_directions_questionnaire', false)
+      mockFeatureFlag('cmc_mediation_pilot', true)
+      const features = await FeaturesBuilder.features(limitPlusInterestDraft, user)
+      expect(features).to.equal('mediationPilot')
+    })
+
+    it('should not add mediation pilot to features if principal amount is > 300', async () => {
+      const features = await FeaturesBuilder.features(overLimitDraft, user)
+      expect(features).to.be.undefined
+    })
+  })
+
+  it('should add dq and mediation pilot to features if principal amount <= 300 and flag is set', async () => {
     mockFeatureFlag('cmc_directions_questionnaire', true)
-    const features = await FeaturesBuilder.features(elegibleDraft, user)
-    expect(features).to.equal('directionsQuestionnaire')
-  })
-
-  it('should not dd dq to features if flag is set and draft is > 300', async () => {
-    const claimDraft = new Draft<DraftClaim>(123, 'claim', new DraftClaim().deserialize({
-      ...claimDraftData,
-      amount: {
-        rows: [
-          {
-            reason: 'Valid reason',
-            amount: 500
-          }
-        ]
-      }
-    }), moment(), moment())
-
-    const features = await FeaturesBuilder.features(claimDraft, user)
-    expect(features).to.be.undefined
-  })
-
-  it('should add mediation pilot to features if total amount <= 300 and flag is set', async () => {
-    mockFeatureFlag('cmc_directions_questionnaire', false)
     mockFeatureFlag('cmc_mediation_pilot', true)
-    const features = await FeaturesBuilder.features(elegibleDraft, user)
-    expect(features).to.equal('mediationPilot')
-  })
-
-  it('should not add mediation pilot to features if flag is set but claim amount is > 300', async () => {
-    const features = await FeaturesBuilder.features(elegibleDraft, user)
-    expect(features).to.be.undefined
-  })
-
-  it('should add dq and mediation pilot to features if total amount <= 300 and flag is set', async () => {
-    mockFeatureFlag('cmc_directions_questionnaire', true)
-    mockFeatureFlag('cmc_mediation_pilot', true)
-    const features = await FeaturesBuilder.features(elegibleDraft, user)
+    const features = await FeaturesBuilder.features(limitDraft, user)
     expect(features).to.equal('directionsQuestionnaire, mediationPilot')
   })
 
