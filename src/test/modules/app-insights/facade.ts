7f341be741c412539eb0a6c135a81e59508f497b
Andrew Walker <andrewwa@kainos.com>
2019-12-04 13:54:48 +0000

    Introduce a facade to application insights and test the telemetry processor configuration

---
+++ src/test/modules/app-insights/facade.ts
@@ -0,0 +1,32 @@
+// tslint:disable:no-unused-expression
+import { AppInsights } from 'modules/app-insights'
+import { expect } from 'chai'
+import { TelemetryClient } from 'applicationinsights'
+
+describe('Application Insights facade', () => {
+  context('error logging telemetry processor', () => {
+    class TelemetryClientStub extends TelemetryClient {
+      public telemetryProcessors: any[] = []
+
+      addTelemetryProcessor (telemetryProcessor): void {
+        this.telemetryProcessors.push(telemetryProcessor)
+      }
+    }
+
+    let client: TelemetryClientStub
+
+    beforeEach(() => {
+      client = new TelemetryClientStub()
+    })
+
+    it('should not add the error logging telemetry processor if instrumentation key is not STDOUT', () => {
+      new AppInsights('instrumentation key', client).prepareTelemetryProcessors()
+      expect(client.telemetryProcessors).to.have.length(1)
+    })
+
+    it('should add the error logging telemetry processor if instrumentation key is STDOUT', () => {
+      new AppInsights('STDOUT', client).prepareTelemetryProcessors()
+      expect(client.telemetryProcessors).to.have.length(2)
+    })
+  })
+})
