838bc21c522a754cf09a652d8ae23ead28076d31
Karim Lawani <k7lawani@gmail.com>
2019-12-06 10:53:50 +0000

    update nodejs version in app settings

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -108,6 +108,9 @@ module "citizen-frontend" {
   asp_rg = "${local.asp_name}"
   instance_size = "${local.sku_size}"
   website_local_cache_sizeinmb = "${var.website_local_cache_sizeinmb}"
+  app_settings           = {
+    WEBSITE_NODE_DEFAULT_VERSION = "10.15.2"
+  }
 
   app_settings = {
     // Node specific vars
