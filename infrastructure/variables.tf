b3d98b47dfd95662b6cdd31bf4b9479333eee2aa
Tim Jacomb <tim.jacomb@hmcts.net>
2020-03-26 07:44:33 +0000

    Remove unneeded variable - fixes build

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -107,10 +107,6 @@ variable "tenant_id" {
   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
 }
 
-variable "client_id" {
-  description = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies. This is usually sourced from environment variables and not normally required to be specified."
-}
-
 variable "node_env" {
   // https://www.dynatrace.com/news/blog/the-drastic-effects-of-omitting-node_env-in-your-express-js-applications/
   default = "production"
