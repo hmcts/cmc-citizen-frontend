8188deb3305524e85abf00239c040297600e236d
Karim Lawani <k7lawani@gmail.com>
2020-03-31 16:12:56 +0100

    update terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,5 @@
-provider "vault" {
-  //  # It is strongly recommended to configure this provider through the
-  //  # environment variables described above, so that each user can have
-  //  # separate credentials set in the environment.
-  //  #
-  //  # This will default to using $VAULT_ADDR
-  //  # But can be set explicitly
-  address = "https://vault.reform.hmcts.net:6200"
+provider "azurerm" {
+  version = "1.44.0"
 }
 
 locals {
