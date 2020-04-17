2f27bd30b66437b9119a23bc212cc9a9a9442c2a
Ronan Fitzgerald <ronan.fitzgerald@hmcts.net>
2020-04-17 13:22:55 +0100

    Content - ROC-7871
    https://tools.hmcts.net/jira/browse/ROC-7871
    
    Added a line making it clear the claim now continues online, as per this ticket.

--- src/test/features/dashboard/routes/claimStatus/ccj.ts
+++ src/test/features/dashboard/routes/claimStatus/ccj.ts
@@ -368,14 +368,16 @@ function testData () {
         'A County Court Judgment has been issued against the defendant.',
         'We’ll post a copy of the judgment to you and to John Doe',
         'A judge will decide what John Doe can afford to pay, based on their financial details.',
-        'We’ll contact you to tell you what to do next.'
+        'We’ll contact you by post to tell you what to do next.'
+        'Your online account won’t be updated - any further updates will be by post.'
       ],
       defendantAssertions: ['000MC050',
         'John Smith requested a County Court Judgment (CCJ) against you',
         'They rejected your repayment plan.',
         'They also rejected a repayment plan determined by the court, based on the financial details you provided.',
         'When we’ve processed the request we’ll post a copy of the judgment to you and to John Smith.',
-        'A judge will make a repayment plan. We’ll contact you to tell you what to do next.',
+        'A judge will make a repayment plan. We’ll contact you by post to to tell you what to do next.',
+        'Your online account won’t be updated - any further updates will be by post.'
         'Download your response'
       ]
     },
