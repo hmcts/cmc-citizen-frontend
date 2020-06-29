e72560a6c2302712191e6ca603247dfe2e547722
Prathap Mathiyalagan <prathap.mathiyalagan@cognizant.com>
2020-06-29 21:58:24 +0100

    Update tokenGenerator.ts

--- src/main/app/utils/tokenGenerator.ts
+++ src/main/app/utils/tokenGenerator.ts
@@ -15,8 +15,8 @@ export class TokenGenerator {
     if (tokenKey) {
       const key = crypto.scryptSync(tokenKey, 'salt', 32)
       Object.keys(params).forEach(p => {
-      params[p] = String(params[p])
-      });
+        params[p] = String(params[p])
+      })
       const strParams = JSON.stringify(params)
       const cipher = crypto.createCipheriv(algorithm, key, iv)
       encrypted = cipher.update(strParams, 'utf8', 'hex')
