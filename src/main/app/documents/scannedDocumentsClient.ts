b48d7a6781b3227abdbd9a80851ea3a8e99fc37a
justin-r-oliver <justin-r-oliver@users.noreply.github.com>
2020-06-30 10:37:53 +0100

    ROC-7807 view scanned defence response (#2339)
    * ROC-7939: Paper response data model initial commit
    
    * ROC-7939: ResponseMethod as enum
    
    * ROC-7939: Use British spelling
    
    * ROC-7939: Update enum options
    
    * ROC-7939: Update response method in responseModelConverter
    
    * ROC-7939: Fix test
    
    * ROC-7939: Partial admission response
    
    * ROC-7939: Fix lint
    
    * ROC-7939: Add offline defaults for LA pilot and JDDO
    
    * Determine defendant response page
    
    * Remove debug
    
    * Initial commit
    
    * Fixed lint
    
    * Improved test coverage
    
    * Improved test coverage
    
    * Fixed a11y
    
    * Remove debug
    
    * Remove duplicate test
    
    * Update src/main/app/documents/scannedDocumentsClient.ts
    
    Co-authored-by: Andy Walker <andrewwa@kainos.com>
    
    * Added unit test assertion
    
    * Fixed typo
    
    Co-authored-by: Dave Oxtoby <david.oxtoby@hmcts.net>
    Co-authored-by: dharmendra kumar <dharmendra.kumar@hmcts.net>
    Co-authored-by: Andy Walker <andrewwa@kainos.com>
    Co-authored-by: Dave Oxtoby <dave.oxtoby@gmail.com>

---
+++ src/main/app/documents/scannedDocumentsClient.ts
@@ -0,0 +1,42 @@
+import * as config from 'config'
+import { StringUtils } from 'utils/stringUtils'
+import * as requestDefault from 'request'
+import * as requestPromise from 'request-promise-native'
+
+const claimStoreBaseUrl = config.get<string>('claim-store.url')
+
+export class ScannedDocumentsClient {
+
+  constructor (public documentsUrl: string = `${claimStoreBaseUrl}/scanned-documents`,
+               private readonly request: requestDefault.RequestAPI<requestPromise.RequestPromise,
+                 requestPromise.RequestPromiseOptions,
+                 requestDefault.RequiredUriUrl> = requestPromise) {
+  }
+
+  public getScannedResponseFormPDF (claimExternalId: string, bearerToken: string): Promise<Buffer> {
+    return this.getPDF(claimExternalId, 'OCON9X', bearerToken)
+  }
+
+  private getPDF (claimExternalId: string, documentTemplate: string, bearerToken: string): Promise<Buffer> {
+    if (StringUtils.isBlank(claimExternalId)) {
+      throw new Error('Claim external ID cannot be blank')
+    }
+    if (StringUtils.isBlank(documentTemplate)) {
+      throw new Error('Document template cannot be blank')
+    }
+    if (StringUtils.isBlank(bearerToken)) {
+      throw new Error('User authorisation cannot be blank')
+    }
+
+    const options = {
+      uri: `${this.documentsUrl}/${claimExternalId}/${documentTemplate}`,
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+        Accept: 'application/pdf'
+      },
+      encoding: null
+    }
+
+    return this.request(options)
+  }
+}
