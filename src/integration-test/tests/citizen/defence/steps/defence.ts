0a8f087cb9a2c7f1a2f8c10e05c1ac713f77a5c1
Prathap Mathiyalagan <prathap.mathiyalagan@cognizant.com>
2020-07-01 01:09:14 +0100

    Removed the Promise<Void>

--- src/integration-test/tests/citizen/defence/steps/defence.ts
+++ src/integration-test/tests/citizen/defence/steps/defence.ts
@@ -363,14 +363,14 @@ export class DefenceSteps {
     I.see('You’ve submitted your response')
   }
 
-  makeFullAdmission (
+  async makeFullAdmission (
     defendantParty: Party,
     defendantType: PartyType,
     paymentOption: PaymentOption,
     claimantName: string,
     statementOfMeansFullDataSet: boolean = true,
     respondToPCQ?: boolean
-  ): void {
+  ) {
     this.confirmYourDetails(defendantParty)
 
     this.requestMoreTimeToRespond()
@@ -404,8 +404,8 @@ export class DefenceSteps {
     }
 
     defendantSteps.selectCheckAndSubmitYourDefence()
-
-    if (respondToPCQ) {
+    const pcqHealth = await I.checkPCQHealth()
+    if (respondToPCQ && pcqHealth) {
       I.rejectAnsweringPCQ()
     } else {
       I.bypassPCQ()
