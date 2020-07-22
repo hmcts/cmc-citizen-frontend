3a1794958b44102c9393576251e25cfa4374fd0f
Thiru <Thirumurugan.Devarajan@cognizant.com>
2020-07-22 10:33:29 +0100

    Unit test coverage for HWF Draft claim pdf

--- src/main/features/claim/routes/receipt.ts
+++ src/main/features/claim/routes/receipt.ts
@@ -23,6 +23,7 @@ export default express.Router()
     }))
 
   .get(Paths.draftReceiptReceiver.uri,
+    ClaimMiddleware.retrieveByExternalId,
     ErrorHandling.apply(async (req: express.Request, res: express.Response): Promise<void> => {
       const { externalId } = req.params
       const pdf: Buffer = await documentsClient.getDraftClaimReceiptPDF(externalId, res.locals.user.bearerToken)
