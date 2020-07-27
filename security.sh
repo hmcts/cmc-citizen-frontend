a5f9c54214987cd810ed6b60bc17e9cc9f08e3ab
541839 <sushant.choudhari@hmcts.net>
2020-07-27 12:44:40 +0100

    changes   to  test security

--- security.sh
+++ security.sh
@@ -1,13 +1,22 @@
-#!/usr/bin/env bash
-echo ${TEST_URL}
+#!/bin/bash
+#echo "${SECURITYCONTEXT}" > /zap/security.context
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
-cat zap.out
-echo "ZAP has successfully started"
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-mkdir -p functional-output
-chmod a+wx functional-output
-curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-cp /zap/api-report.html functional-output/
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
+zap-x.sh -d -host 0.0.0.0 -port 1001 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true /dev/null 2>&1 &
+i=0
+while !(curl -s http://0.0.0.0:1001) > /dev/null
+  do
+    i=$(( (i+1) %4 ))
+    sleep .1
+  done
+  echo "ZAP has successfully started"
+  zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
+  zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
+  zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
+  zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
+  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
+  curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+  echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
+  chown -R $(id -u):$(id -u) activescan.html
+  cp *.html functional-output/
+  zap-cli -p 1001 alerts -l Informational
