80660d43bb9cddddeec51e9b792d1a2cec2ee7c8
Rob Doherty <robdohertykelly@gmail.com>
2020-08-25 08:47:19 +0100

    feature: add a11y 'warning' checks to a11y tests

--- src/test/a11y/a11y.ts
+++ src/test/a11y/a11y.ts
@@ -47,6 +47,7 @@ interface TestsOnSpecificPages {
 
 async function runPa11y (url: string): Promise<Issue[]> {
   const result = await pa11y(url, {
+    includeWarnings: true,
     headers: {
       Cookie: `${cookieName}=ABC`
     },
@@ -70,9 +71,17 @@ function check (uri: string, customTests: CustomChecks = [], requestDetails: Req
     })
 
     describe(`Pa11y tests for ${uri}`, () => {
-      it('should have no accessibility errors', async () => {
-        const issues: Issue[] = await runPa11y(agent.get(uri).url)
-        ensureNoAccessibilityErrors(issues)
+      let issues: Issue[]
+      before(async () => {
+        issues = await runPa11y(agent.get(uri).url)
+      })
+
+      it('should have no accessibility errors', () => {
+        ensureNoAccessibilityAlerts('error', issues)
+      })
+
+      it('should have no accessibility warnings', () => {
+        ensureNoAccessibilityAlerts('warning', issues)
       })
     })
   })
@@ -100,9 +109,9 @@ async function extractPageContent (url: string, requestDetails: RequestDetails =
   return res.text
 }
 
-function ensureNoAccessibilityErrors (issues: Issue[]): void {
-  const errors: Issue[] = issues.filter((issue: Issue) => issue.type === 'error')
-  expect(errors, `\n${JSON.stringify(errors, null, 2)}\n`).to.be.empty
+function ensureNoAccessibilityAlerts (issueType: string, issues: Issue[]): void {
+  const alerts: Issue[] = issues.filter((issue: Issue) => issue.type === issueType)
+  expect(alerts, `\n${JSON.stringify(alerts, null, 2)}\n`).to.be.empty
 }
 
 const excludedPaths: Paths[] = [
