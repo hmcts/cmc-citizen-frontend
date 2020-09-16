c8618fe72aea5ecd58906db86317b0b83b4605be
pradhap87 <41957682+pradhap87@users.noreply.github.com>
2020-09-16 08:59:54 +0100

    Features/roc 7786 accessibility non descriptive form elements issue (#2469)
    * DAC_Non-Descriptive_Form_Elements_Issue1
    
    * Issue ID: DAC_Non-Descriptive_Form_Elements_Issue2
    
    * Issue ID: DAC_Non-Descriptive_Form_Elements_Issue3
    
    * Issue ID: DAC_Non-Descriptive_Form_Elements_Issue4
    
    * DAC_Non-Descriptive_Form_Elements_Issue2
    
    * Merge branch 'Features/Accessibility_Issue_Fix' into Features/ROC-7786-Accessibility-Non_Descriptive_Form_Elements_Issue
    
    * Revert "Issue ID: DAC_Non-Descriptive_Form_Elements_Issue4"
    
    This reverts commit b94e0236ecd325561d26246a9163dd4447a8ac72.
    
    * Update form.njk
    
    * Update incomeExpenseSource.njk
    
    * Updated the textinput macro
    
    * Bug fix
    
    Co-authored-by: sunilkumar-cts <sunil.kumar@hmcts.net>

--- src/main/public/js/append-row.js
+++ src/main/public/js/append-row.js
@@ -11,18 +11,32 @@ $(document).ready(function () {
     return false
   })
 
-  function incrementDomNodesIds (newRow) {
+  function incrementDomNodesIds(newRow) {
+    var newIndex = 0
     newRow.html(function (index, oldHtml) {
       return oldHtml.replace(/rows\[(\d+)\]/g, function (match, capturedRowIndex) {
-        if(parseInt(capturedRowIndex) === 99) {
+        if (parseInt(capturedRowIndex) === 99) {
           removeEventButton()
         }
+        newIndex = parseInt(capturedRowIndex) + 1        
         return 'rows[' + (parseInt(capturedRowIndex) + 1) + ']'
       })
     })
+    newRow.html(function (index, oldHtml) {
+      return oldHtml.replace('<label class="form-label-bold mobile-show visually-hidden" for="rows['+ newIndex +'][reason]">'+ newIndex +' . What you’re claiming for</label>',
+        function (match, capturedRowIndex) {
+          return '<label class="form-label-bold mobile-show visually-hidden" for="rows['+ newIndex +'][reason]">'+ (parseInt(newIndex) + 1) +' . What you’re claiming for</label>'
+        })
+    })
+    newRow.html(function (index, oldHtml) {
+      return oldHtml.replace('<label class="form-label-bold mobile-show visually-hidden" for="rows['+ newIndex +'][amount]">'+ newIndex +' . Amount</label>',
+        function (match, capturedRowIndex) {
+          return '<label class="form-label-bold mobile-show visually-hidden" for="rows['+ newIndex +'][amount]">'+ (parseInt(newIndex) + 1) +' . Amount</label>'
+        })
+    })
   }
 
-  function sanitizeContent (newRow) {
+  function sanitizeContent(newRow) {
     newRow.find('input, textarea').val('')
 
     newRow.removeClass('form-group-error')
@@ -30,7 +44,7 @@ $(document).ready(function () {
     newRow.find('.error-message').remove()
   }
 
-  function removeEventButton () {
+  function removeEventButton() {
     $('input[name="action[addRow]"]').remove()
   }
 })
