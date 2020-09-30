2b66263d37040a03f7d5e03aa86841123ca3a47c
pradhap87 <prathap.mathiyalagan@hmcts.net>
2020-09-30 13:02:11 +0100

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -36,3 +36,7 @@ variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
   default = ""
 }
+
+variable "common_tags" {
+  type = "map"	  type = map(string)
+}	}
