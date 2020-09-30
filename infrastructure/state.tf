ea04fea4636ccb6f895a119cc042d09d1bb4778d
pradhap87 <prathap.mathiyalagan@hmcts.net>
2020-09-30 11:58:41 +0100

    TF update

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,5 +1,15 @@
 terraform {
   backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.25"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 }
 
 
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -21,7 +21,6 @@ variable "ilbIp" { }
 variable "subscription" {}
 
 variable "jenkins_AAD_objectId" {
-  type                        = "string"
   description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
