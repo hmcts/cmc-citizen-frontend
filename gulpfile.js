1a4bfc9ab0dffd75775db42dd534df535d62311e
pradhap87 <prathap.mathiyalagan@hmcts.net>
2020-10-14 18:56:42 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -97,7 +97,7 @@ function copyWebChatTemplate () {
     .pipe(gulp.dest(`${webChatDirectory}/css/`))
 
     gulp.src([
-      'src/main/public/assets/locale/*.json'
+      'src/main/public/assets/locale/**/*.*'
     ])
       .pipe(gulp.dest(`${antennaWebChatDirectory}/locale/`))
 }
