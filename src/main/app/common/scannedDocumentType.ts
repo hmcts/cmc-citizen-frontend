f4b6a1c9b3486d98c3c3f94bc53db223dda0f467
Tanveer Singh <tanveer.singh@hmcts.net>
2020-10-21 16:29:05 +0100

    updated download link text

--- src/main/app/common/scannedDocumentType.ts
+++ src/main/app/common/scannedDocumentType.ts
@@ -4,19 +4,19 @@ export class ScannedDocumentType {
     uri: 'form-ocon9x'
   }
   static readonly FORM_N9 = {
-    text: 'Download N9 Form',
+    text: 'Download the defendant\'s paper response',
     uri: 'form-ocon9x'
   }
   static readonly FORM_N9A = {
-    text: 'Download N9a Form',
+    text: 'Download the defendant\'s paper response',
     uri: 'form-N9a'
   }
   static readonly FORM_N9B = {
-    text: 'Download N9b Form',
+    text: 'Download the defendant\'s paper response',
     uri: 'form-N9b'
   }
   static readonly FORM_N11 = {
-    text: 'Download N11 Form',
+    text: 'Download the defendant\'s paper response',
     uri: 'form-N11'
   }
   static readonly PAPER_RESPONSE_FORMS = ['N9', 'N9a', 'N9b', 'N11']
