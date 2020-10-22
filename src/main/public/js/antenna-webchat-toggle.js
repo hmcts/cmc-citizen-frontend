bb4017e771e2a1ec3e035e36d2b246e92e88cbfc
pradhap87 <prathap.mathiyalagan@hmcts.net>
2020-10-22 12:25:06 +0100

    Final web-chat code

--- src/main/public/js/antenna-webchat-toggle.js
+++ src/main/public/js/antenna-webchat-toggle.js
@@ -1,17 +1,11 @@
 (function() {        
         const button = document.querySelector('.chat-button');
-        const webChat = document.querySelector('web-chat');
         button.addEventListener('click', () => {
-            webChat.classList.remove('hidden');
+            window.open("/web-chat", "Popup", "location,status,scrollbars,resizable,width=360, height=610");
         });
-        /**
-         * When a user clicks the 'Hide' button on the chat client,
-         * an event is dispatched on the web-chat component.
-         * To listen for this event, we use the addEventListener DOM API
-         * and register a callback.
-         */
-        webChat.addEventListener('hide', () => {
-            webChat.classList.add('hidden');
-        });
-      
+
+        const buttonChat = document.querySelector('.webChat');
+        buttonChat.addEventListener('click', () => {
+            window.open("/web-chat", "Popup", "location,status,scrollbars,resizable,width=360, height=610");
+        });      
     }).call(this);
