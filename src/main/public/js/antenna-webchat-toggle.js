8cac2a23ffd7a16534f06af8d904d4ab6385557b
pradhap87 <prathap.mathiyalagan@hmcts.net>
2020-10-22 18:32:50 +0100

    added the web-chat metics

--- src/main/public/js/antenna-webchat-toggle.js
+++ src/main/public/js/antenna-webchat-toggle.js
@@ -7,5 +7,24 @@
         const buttonChat = document.querySelector('.webChat');
         buttonChat.addEventListener('click', () => {
             window.open("/web-chat", "Popup", "location,status,scrollbars,resizable,width=360, height=610");
-        });      
+        });
+
+        const webChat = document.querySelector('web-chat');
+        webChat.addEventListener('metrics', function (metrics) {
+            const metricsDetail = metrics.detail;
+            //const ewt = metricsDetail.ewt;
+            const ccState = metricsDetail.contactCenterState;
+            //const availableAgents = metricsDetail.availableAgents;
+            if (ccState == "Open") {
+                document.getElementById("metrics").textContent = 'Web chat is now closed. Come back Monday to Thursday 8.30am to 5pm and Friday 8.30am to 4pm, or contact us by phone or email.';
+                button.classList.add('hidden');
+                buttonChat.classList.add('hidden');
+            } else {
+                document.getElementById("metrics").textContent = '';
+                button.classList.remove('hidden');
+                buttonChat.classList.remove('hidden');
+            }
+
+          });
+      
     }).call(this);
