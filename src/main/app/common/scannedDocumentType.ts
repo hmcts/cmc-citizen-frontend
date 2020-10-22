26758c17b16ff7a9d52fed0d4eb73724033c2369
Tanveer Singh <tanveer.singh@hmcts.net>
2020-10-22 14:26:06 +0100

    Removed N9 from list as it does not need any special handling

--- src/main/app/common/scannedDocumentType.ts
+++ src/main/app/common/scannedDocumentType.ts
@@ -3,10 +3,6 @@ export class ScannedDocumentType {
     text: 'Download OCON9x Form',
     uri: 'form-ocon9x'
   }
-  static readonly FORM_N9 = {
-    text: 'Download the defendant\'s paper response',
-    uri: 'form-ocon9x'
-  }
   static readonly FORM_N9A = {
     text: 'Download the defendant\'s paper response',
     uri: 'form-N9a'
@@ -19,5 +15,5 @@ export class ScannedDocumentType {
     text: 'Download the defendant\'s paper response',
     uri: 'form-N11'
   }
-  static readonly PAPER_RESPONSE_FORMS = ['N9', 'N9a', 'N9b', 'N11']
+  static readonly PAPER_RESPONSE_FORMS = ['N9a', 'N9b', 'N11']
 }
