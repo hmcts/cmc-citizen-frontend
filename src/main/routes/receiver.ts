d1df07bd2ffd59c1c14ca30388dea01b21a7fc51
sunilkumar-cts <sunil.kumar@hmcts.net>
2020-11-11 13:08:03 +0000

    Update receiver.ts

--- src/main/routes/receiver.ts
+++ src/main/routes/receiver.ts
@@ -84,7 +84,7 @@ function loginErrorHandler (req: express.Request,
 }
 
 async function retrieveRedirectForLandingPage (req: express.Request, res: express.Response): Promise<string> {
-  return DashboardPaths.dashboardPage.uri;
+  return DashboardPaths.dashboardPage.uri
   const eligibility: Eligibility = eligibilityStore.read(req, res)
   if (eligibility.eligible) {
     return ClaimPaths.taskListPage.uri
