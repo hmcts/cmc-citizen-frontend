ba6870d08439dd15146eddd6d49d1867ba08f6ce
pradhap87 <prathap.mathiyalagan@hmcts.net>
2020-11-12 11:42:38 +0000

    Updated the states

--- src/main/app/claims/models/claim-states/initial-states.ts
+++ src/main/app/claims/models/claim-states/initial-states.ts
@@ -6,9 +6,5 @@ export enum InitialStates {
   HWF_APPLICATION_PENDING = 'help-with-fees',
   HWF_INVALID_REFERENCE = 'help-with-fees-invalid',
   HWF_AWAITING_RESPONSE_HWF = 'awaiting_response_hwf',
-  HWF_Rejected = 'help-with-fees-rejected',
-  HWF_FEES_REQUIREMENT_NOT_MET = 'fees_requirement_not_met',
-  HWF_NOT_QUALIFY_FEE_ASSISTANCE ='not_qualify_fee_assistance',
-  HWF_INCORRECT_EVIDENCE = 'incorrect_evidence',
-  HWF_INSUFFICIENT_EVIDENCE='insufficient_evidence'
+  HWF_Rejected = 'help-with-fees-rejected'
 }
