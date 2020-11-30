8e32465bce94cbb8fe72eee3295ed71eaa89e02a
pradhap87 <prathap.mathiyalagan@hmcts.net>
2020-11-30 11:13:52 +0000

    Error message issue fix
    FOT-1483 & FOT-1480

--- src/main/public/js/antenna-webchat-toggle.js
+++ src/main/public/js/antenna-webchat-toggle.js
@@ -8,22 +8,30 @@
         buttonChat.addEventListener('click', () => {
             window.open("/web-chat", "Popup", "location,status,scrollbars,resizable,width=360, height=610");
         });
-
+        
         const webChat = document.querySelector('web-chat');
         webChat.addEventListener('metrics', function (metrics) {
+            var today = new Date();
             const metricsDetail = metrics.detail;
+            const ewt = metricsDetail.ewt;
+            const waitTimeInMinutes = ewt/60;
             const ccState = metricsDetail.contactCenterState;
             const availableAgents = metricsDetail.availableAgents;
-            if (ccState != "Open") {
-                document.getElementById("metrics").textContent = 'Web chat is now closed. Come back Monday to Thursday 8.30am to 5pm and Friday 8.30am to 4pm, or contact us by phone or email.';
+            if (ccState != "Open" || (today.getHours() < 9 || today.getHours() > 17)) {
+                document.getElementById("metrics").textContent = 'Web chat is now closed. Come back Monday to Friday 9am to 5pm or contact us by phone or email.';
                 button.classList.add('hidden');
                 buttonChat.classList.add('hidden');
             } else if (availableAgents == 0)
             {
                 document.getElementById("metrics").textContent = 'No agents are currently available, Come back later.';
-                button.classList.remove('hidden');
-                buttonChat.classList.remove('hidden');
-            } 
+                button.classList.add('hidden');
+                buttonChat.classList.add('hidden');
+            } else if (waitTimeInMinutes > 5 && waitTimeInMinutes < 60 )
+            {
+                document.getElementById("metrics").textContent = 'All our agents are busy helping other people. Please try again later or contact us by phone or email.';
+                button.classList.add('hidden');
+                buttonChat.classList.add('hidden');
+            }
             else {
                 document.getElementById("metrics").textContent = '';
                 button.classList.remove('hidden');
