d2370a59863bbb508beed32d73b6cfd7652af150
Aloknath Datta <alokdatta.uit@gmail.com>
2021-01-07 23:41:03 +0000

    Unit test coverage

--- src/test/modules/i18n/backend.ts
+++ src/test/modules/i18n/backend.ts
@@ -33,9 +33,21 @@ describe('A gettext backend for i18next', () => {
       loadPath: '/tmp/non-existing-file'
     })
 
-    backend.read('cy', 'translation', (err, translation) => {
-      expect(err).to.be.an('error')
+    backend.read('cy', 'translation', (error, translation) => {
+      expect(error).to.be.an('error')
       expect(translation).to.be.null
+      done()
+    })
+  })
+
+  it('should fail with an exception when translation fails', function (done) {
+    this.timeout(6000)
+    backend.init(null, {
+      loadPath: path.join(__dirname, 'fixtures/invalid_translation.po')
+    })
+
+    backend.read('cy', 'translation', (error, translation) => {
+      expect(error).not.to.be.null
       done()
     })
   })
