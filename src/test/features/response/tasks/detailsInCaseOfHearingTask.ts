c3a64acf8a91821b1ef22c132ee3cdb80467737a
Aloknath Datta <alokdatta.uit@gmail.com>
2021-01-12 20:13:08 +0000

    Unit test coverage

--- src/test/features/response/tasks/detailsInCaseOfHearingTask.ts
+++ src/test/features/response/tasks/detailsInCaseOfHearingTask.ts
@@ -227,6 +227,23 @@ describe('Details In case of hearing task', () => {
     expect(DetailsInCaseOfHearingTask.isCompleted(draft, directionsQuestionnaireDraft, claim)).to.be.false
   })
 
+  it('should not be completed when expert evidence is not completed', () => {
+    const draft = new ResponseDraft()
+    const directionsQuestionnaireDraft = new DirectionsQuestionnaireDraft()
+    const claim: Claim = new Claim().deserialize({
+      ...claimStoreMock.sampleClaimObj, ...{ features: ['directionsQuestionnaire'] }
+    })
+    directionsQuestionnaireDraft.hearingLocation.courtName = 'London'
+    directionsQuestionnaireDraft.selfWitness = new SelfWitness().deserialize({ option: 'yes' })
+    directionsQuestionnaireDraft.expertRequired = new ExpertRequired().deserialize({ option: 'yes' })
+    directionsQuestionnaireDraft.expertReports = new ExpertReports().deserialize({ declared: { option: 'no' }, rows: [] })
+    directionsQuestionnaireDraft.permissionForExpert = new PermissionForExpert().deserialize({ option: { option: 'yes' } })
+    directionsQuestionnaireDraft.expertEvidence = new ExpertEvidence().deserialize({
+      expertEvidence: { option: 'yes' }
+    })
+    expect(DetailsInCaseOfHearingTask.isCompleted(draft, directionsQuestionnaireDraft, claim)).to.be.false
+  })
+
   it('should not be completed when export evidence is not selected', () => {
     const draft = new ResponseDraft()
     const directionsQuestionnaireDraft = new DirectionsQuestionnaireDraft()
