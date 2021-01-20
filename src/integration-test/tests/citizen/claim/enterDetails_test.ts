d65df4e53f2dbda9a1cf00ea96d5f27b06f0492d
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-01-20 18:04:50 +0000

    Revert "Removed the @smoke in the HWF E2E test case"
    This reverts commit 543c17a7246cc7585ee54a10f68745022e72416f.

--- src/integration-test/tests/citizen/claim/enterDetails_test.ts
+++ src/integration-test/tests/citizen/claim/enterDetails_test.ts
@@ -158,10 +158,10 @@ Scenario('I should be redirected to PCQ if "Your details" are filled in while ma
 
 })
 
-// running tests with pre-registered user for help with fees
+// The @citizen-smoke-test tag used for running smoke tests with pre-registered user for help with fees
 
 if (process.env.FEATURE_HELP_WITH_FEES) {
-  Scenario('I can enter a claim details and i can create a claim with Help With Fees reference number @citizen', { retries: 3 }, (I: I) => {
+  Scenario('I can enter a claim details and i can create a claim with Help With Fees reference number @smoke-test', { retries: 3 }, (I: I) => {
     claimSteps.makeAHwfClaimAndSubmit()
   })
 }
