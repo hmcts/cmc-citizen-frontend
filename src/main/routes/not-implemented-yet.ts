b1d1fdd53394039c038ba091bd9cef7d5538da89
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-02-12 11:46:19 +0000

    removed the pages for testing

--- src/main/routes/not-implemented-yet.ts
+++
@@ -1,7 +0,0 @@
-import * as express from 'express'
-
-/* tslint:disable:no-default-export */
-export default express.Router()
-  .get('/not-implemented-yet', (req: express.Request, res: express.Response) => {
-    res.render('not-implemented-yet')
-  })
--- src/main/routes/resolve-before-claim.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express'
-
-import { Paths } from 'paths'
-
-/* tslint:disable:no-default-export */
-export default express.Router()
-  .get(Paths.resolveBeforeClaimPage.uri, function (req, res) {
-    res.render(Paths.resolveBeforeClaimPage.associatedView)
-  })
