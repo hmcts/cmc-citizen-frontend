d1d7caad83fc64341dfe6a409a67426c9cc7a5f6
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-02-12 15:38:36 +0000

    routes updated

--- src/main/app.ts
+++ src/main/app.ts
@@ -137,14 +137,15 @@ moment.prototype.toISOString = function () {
   return this.format('YYYY-MM-DD[T]HH:mm:ss.SSS')
 }
 
-logger.info('Loading routes')
+logger.info('Loading routes (health & receiver)')
 app.use('/', RouterFinder.findAll(path.join(__dirname, 'routes')))
-logger.info('Loading routes-public-second')
-app.use('/', RouterFinder.findAll(path.join(__dirname, 'routes-public-second')))
 logger.info('Loading routes-public-third')
 app.use('/', RouterFinder.findAll(path.join(__dirname, 'routes-public-third')))
-logger.info('Loading routes-public')
-app.use('/', RouterFinder.findAll(path.join(__dirname, 'routes-public-first')))
+logger.info('Loading routes-public-second')
+app.use('/', RouterFinder.findAll(path.join(__dirname, 'routes-public-second')))
+logger.info('Loading routes-public-first')
+app.use('/', RouterFinder.findAll(path.join(__dirname, 'routes-public')))
+logger.info('All the routes are loaded')
 
 // Below will match all routes not covered by the router, which effectively translates to a 404 response
 app.use((req, res, next) => {
