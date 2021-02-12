c676e45ecc8bed49f6d52f08b5c856c19a92448e
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-02-12 10:14:50 +0000

    removed Hwf Miscellaneous

--- src/main/features/dashboard/claims-state-machine/initial-transitions.ts
+++ src/main/features/dashboard/claims-state-machine/initial-transitions.ts
@@ -71,11 +71,6 @@ export function initialTransitions (claim: Claim): StateMachine {
         to: InitialStates.HWF_Part_Remitted
       },
       {
-        name: 'checkHwfMiscellaneous',
-        from: InitialStates.HWF_AWAITING_RESPONSE_HWF,
-        to: InitialStates.HWF_Part_Remitted
-      },
-      {
         name: 'checkHwfInvalid',
         from: [InitialStates.INIT, InitialStates.NO_RESPONSE, InitialStates.HWF_APPLICATION_PENDING],
         to: InitialStates.HWF_INVALID_REFERENCE
@@ -138,10 +133,6 @@ export function initialTransitions (claim: Claim): StateMachine {
 
       onBeforeCheckHwfInvalid () {
         return !claim.response && claim.helpWithFeesNumber !== null && claim.state === 'AWAITING_RESPONSE_HWF' && claim.lastEventTriggeredForHwfCase === 'InvalidHWFReference'
-      },
-
-      onBeforecheckHwfMiscellaneous () {
-        return !claim.response && claim.helpWithFeesNumber !== null && claim.state === 'AWAITING_RESPONSE_HWF' && (claim.lastEventTriggeredForHwfCase === 'CreateHelpWithFeesClaim' || claim.lastEventTriggeredForHwfCase === 'UpdateHWFNumber')
       },
 
       findState (currentSate: StateMachine) {
