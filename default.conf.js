81dc020af46ed41f07da6e317c00d517850fbebe
PhaniP <phani.perla@hmcts.net>
2021-02-19 13:19:15 +0000

    TA-457-Increased Threads count

--- default.conf.js
+++ default.conf.js
@@ -15,7 +15,7 @@ exports.config = {
   timeout: 20000,
   multiple: {
     parallel: {
-      chunks: parseInt(process.env.CHUNKS || '5')
+      chunks: parseInt(process.env.CHUNKS || '4')
     }
   },
   helpers: {
