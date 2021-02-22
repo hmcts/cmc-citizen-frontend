1bd66694a98cd05f52621919d25037f304ec6d15
PhaniP <phani.perla@hmcts.net>
2021-02-22 18:39:59 +0000

    TA-459-Offers E2E Test Code Improvements

--- src/integration-test/tests/citizen/offers/offers_test.ts
+++ src/integration-test/tests/citizen/offers/offers_test.ts
@@ -6,15 +6,17 @@ import I = CodeceptJS.I
 
 const userSteps: UserSteps = new UserSteps()
 const offerSteps: OfferSteps = new OfferSteps()
+let claimantEmail
+let defendantEmail
+let claimRef
 
 Feature('Offers')
 
-Scenario('I can as a defendant make an offer, accept offer and counter sign the agreement @citizen', { retries: 3 }, async (I: I) => {
-  const claimantEmail: string = userSteps.getClaimantEmail()
-  const defendantEmail: string = userSteps.getDefendantEmail()
+BeforeSuite(async (I: I) => {
+  claimantEmail = userSteps.getClaimantEmail()
+  defendantEmail = userSteps.getDefendantEmail()
 
-  const claimRef: string = await I.createClaim(createClaimData(PartyType.INDIVIDUAL, PartyType.INDIVIDUAL), claimantEmail)
-
+  claimRef = await I.createClaim(createClaimData(PartyType.INDIVIDUAL, PartyType.INDIVIDUAL), claimantEmail)
   I.respondToClaim(claimRef, claimantEmail, createResponseData(PartyType.INDIVIDUAL), defendantEmail)
 
   userSteps.login(defendantEmail)
@@ -23,6 +25,10 @@ Scenario('I can as a defendant make an offer, accept offer and counter sign the 
   I.click('Sign out')
 
   userSteps.login(claimantEmail)
+
+})
+
+Scenario('I can as a defendant make an offer, accept offer and counter sign the agreement @citizen @nightly', { retries: 3 }, async (I: I) => {
   offerSteps.acceptOfferFromDashboard(claimRef)
   I.click('Sign out')
 
@@ -32,20 +38,7 @@ Scenario('I can as a defendant make an offer, accept offer and counter sign the 
   I.see('You’ve both signed a legal agreement. The claim is now settled.')
 })
 
-Scenario('I can make an offer as a defendant to a claimant and have the claimant reject it @nightly', { retries: 3 }, async (I: I) => {
-  const claimantEmail: string = userSteps.getClaimantEmail()
-  const defendantEmail: string = userSteps.getDefendantEmail()
-
-  const claimRef: string = await I.createClaim(createClaimData(PartyType.INDIVIDUAL, PartyType.INDIVIDUAL), claimantEmail)
-
-  I.respondToClaim(claimRef, claimantEmail, createResponseData(PartyType.INDIVIDUAL), defendantEmail)
-
-  userSteps.login(defendantEmail)
-  offerSteps.makeOfferFromDashboard(claimRef)
-  I.see('We’ve sent your offer to ' + createClaimant(PartyType.INDIVIDUAL).name)
-  I.click('Sign out')
-
-  userSteps.login(claimantEmail)
+Scenario('I can make an offer as a defendant to a claimant and have the claimant reject it @citizen @nightly', { retries: 3 }, async (I: I) => {
   offerSteps.rejectOfferFromDashboard(claimRef)
   I.click('Sign out')
 
--- types/steps.d.ts
+++ types/steps.d.ts
@@ -4,6 +4,7 @@ declare const actor: () => CodeceptJS.I;
 declare const Feature: (string: string) => void;
 declare const Scenario: (string: string, callback: ICodeceptCallback) => void;
 declare const Before: (callback: ICodeceptCallback) => void;
+declare const BeforeSuite: (callback: ICodeceptCallback) => void;
 declare const After: (callback: ICodeceptCallback) => void;
 declare const within: (selector: string, callback: Function) => void;
 
