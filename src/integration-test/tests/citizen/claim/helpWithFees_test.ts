9a5567f389c0ba9010b6379c29938c2330d39273
phaniperla <45269063+phaniperla@users.noreply.github.com>
2021-02-22 15:26:07 +0000

    Ta 457 claimant help with fee e2 e (#2845)
    * TA-457-HelpWithFeeE2E-Tests Coderefactoring
    
    * TA-457-Increased Threads count
    
    * TA-457-Increased Threads count
    
    * TA-457 update
    
    * TA-457 HelpWithFee Tests
    
    * TA-457-Claimant-HelpWith Fee Tests

---
+++ src/integration-test/tests/citizen/claim/helpWithFees_test.ts
@@ -0,0 +1,19 @@
+import I = CodeceptJS.I
+import { ClaimSteps } from 'integration-test/tests/citizen/claim/steps/claim'
+const claimSteps: ClaimSteps = new ClaimSteps()
+
+Feature('Help With Fee E2E Tests...')
+
+Scenario('Submit Claim via HWF Reference... @citizen', { retries: 3 }, async (I: I) => {
+  claimSteps.makeAHwfClaimAndSubmit()
+})
+
+// The @citizen-smoke-test tag used for running smoke tests with pre-registered user
+Scenario('Navigate Payment Page by providing claim details... @smoke-test', { retries: 3 }, async (I: I) => {
+  claimSteps.makeAClaimAndNavigateUpToPayment()
+})
+
+// The @citizen-smoke-test tag used for running smoke tests with pre-registered user for help with fees
+Scenario(' Navigate Payment Page via Providing HWF reference...  @smoke-test', { retries: 3 }, async (I: I) => {
+  claimSteps.makeAHwfClaimAndNavigateUpToPayment()
+})
