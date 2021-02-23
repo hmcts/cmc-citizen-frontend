494cd27ea97ba34f187633e871b3d9227f77ef73
Rob Doherty <robdohertykelly@gmail.com>
2021-02-23 16:04:07 +0000

    TA-370: add run-crossbrowser-tests.sh runner and extend to run against chrome and firefox browsers. Includes async/await refactor of e2e test bootstrapAll to better wait for setup before starting tests.

---
+++ bin/run-crossbrowser-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+if [[ "$BROWSER_GROUP" == "" ]]
+then
+    EXIT_STATUS=0
+    BROWSER_GROUP=chrome yarn test:crossbrowser-verbose || EXIT_STATUS=$?
+    BROWSER_GROUP=firefox yarn test:crossbrowser-verbose || EXIT_STATUS=$?
+    echo EXIT_STATUS: $EXIT_STATUS
+    exit $EXIT_STATUS
+else
+    # Compatible with Jenkins parallel crossbrowser pipeline
+    yarn test:crossbrowser-verbose
+fi
