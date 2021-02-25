f9d18a86fa0f2a47497316a3502a3f84ed9c51f9
Aloknath Datta <alokdatta.uit@gmail.com>
2021-02-25 00:48:33 +0000

    Actual fix - with LD variation under autoEnroll feature toggle

--- src/main/features/claim/helpers/featuresBuilder.ts
+++ src/main/features/claim/helpers/featuresBuilder.ts
@@ -32,9 +32,13 @@ export class FeaturesBuilder {
     let features = []
     for (const feature of FEATURES) {
       if (feature.validForAmount(amount)) {
-        const offlineDefault = config.get<boolean>(`featureToggles.${feature.setting}`) || false
-        const ldVariation = await this.launchDarklyClient.userVariation(user, roles, feature.toggle, offlineDefault)
-        if (ldVariation) {
+        if (!autoEnrollFeatureEnabled) {
+          const offlineDefault = config.get<boolean>(`featureToggles.${feature.setting}`) || false
+          const ldVariation = await this.launchDarklyClient.userVariation(user, roles, feature.toggle, offlineDefault)
+          if (ldVariation) {
+            features.push(feature.feature)
+          }
+        } else {
           features.push(feature.feature)
         }
       }
