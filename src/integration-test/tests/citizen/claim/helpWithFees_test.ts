c966ac03630df7768355ea3dc7c29eb4012b1d2e
Rob Doherty <robdohertykelly@gmail.com>
2021-02-25 17:29:43 +0000

    e2e refactor: add missing awaits to helpWithFees_test.ts

--- src/integration-test/tests/citizen/claim/helpWithFees_test.ts
+++ src/integration-test/tests/citizen/claim/helpWithFees_test.ts
@@ -5,15 +5,15 @@ const claimSteps: ClaimSteps = new ClaimSteps()
 Feature('Help With Fee E2E Tests...')
 
 Scenario('Submit Claim via HWF Reference... @citizen', { retries: 3 }, async (I: I) => {
-  claimSteps.makeAHwfClaimAndSubmit(I)
+  await claimSteps.makeAHwfClaimAndSubmit(I)
 })
 
 // The @citizen-smoke-test tag used for running smoke tests with pre-registered user
 Scenario('Navigate Payment Page by providing claim details... @smoke-test', { retries: 3 }, async (I: I) => {
-  claimSteps.makeAClaimAndNavigateUpToPayment(I)
+  await claimSteps.makeAClaimAndNavigateUpToPayment(I)
 })
 
 // The @citizen-smoke-test tag used for running smoke tests with pre-registered user for help with fees
 Scenario(' Navigate Payment Page via Providing HWF reference...  @smoke-test', { retries: 3 }, async (I: I) => {
-  claimSteps.makeAHwfClaimAndNavigateUpToPayment(I)
+  await claimSteps.makeAHwfClaimAndNavigateUpToPayment(I)
 })
