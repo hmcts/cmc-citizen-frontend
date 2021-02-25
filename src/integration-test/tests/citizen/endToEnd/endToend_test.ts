826d72303729c6bb77c6e1b18dcfe0fb6f52cb0d
Rob Doherty <robdohertykelly@gmail.com>
2021-02-25 18:53:31 +0000

    TA-371 & TA-373: e2e test work-around for IE11 bug until it's been fixed, and extra waiting for Safari

--- src/integration-test/tests/citizen/endToEnd/endToend_test.ts
+++ src/integration-test/tests/citizen/endToEnd/endToend_test.ts
@@ -10,25 +10,25 @@ Feature('E2E tests for Claim and Defence response')
 // Warning : Changing the text description of this scenario, could cause failure when running ZAP security test
 Scenario('I can as an Individual make a claim against an Individual Without a defendant email address and are able to pay on the Gov Pay page @citizen @crossbrowser', { retries: 3 }, async (I: I) => {
   const testData = await EndToEndTestData.prepareDataWithNoDefendantEmail(I, PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
-  helperSteps.finishResponse(testData, false, false)
+  await helperSteps.finishResponse(testData, false, false)
 })
 
 Scenario('I can as Sole Trader make a claim against an Individual and are able to pay on the Gov Pay page @nightly', { retries: 3 }, async (I: I) => {
   const testData = await EndToEndTestData.prepareData(I, PartyType.SOLE_TRADER, PartyType.INDIVIDUAL)
-  helperSteps.finishResponse(testData)
+  await helperSteps.finishResponse(testData)
 })
 
 Scenario('I can as a Individual make a claim against a Company and are able to pay on the Gov Pay page @nightly', { retries: 3 }, async (I: I) => {
   const testData = await EndToEndTestData.prepareData(I, PartyType.COMPANY, PartyType.INDIVIDUAL)
-  helperSteps.finishResponse(testData)
+  await helperSteps.finishResponse(testData)
 })
 
 Scenario('I can as a Company make a claim against a company and are able to pay on the Gov Pay page @nightly', { retries: 3 }, async (I: I) => {
   const testData = await EndToEndTestData.prepareData(I, PartyType.COMPANY, PartyType.COMPANY)
-  helperSteps.finishResponse(testData)
+  await helperSteps.finishResponse(testData)
 })
 
 Scenario('I can as a Organisation make a claim against an Individual and are able to pay on the Gov Pay page @nightly', { retries: 3 }, async (I: I) => {
   const testData = await EndToEndTestData.prepareData(I, PartyType.ORGANISATION, PartyType.INDIVIDUAL)
-  helperSteps.finishResponse(testData)
+  await helperSteps.finishResponse(testData)
 })
