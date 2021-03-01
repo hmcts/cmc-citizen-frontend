49628029cbfad18f4ed2b21cd91f476ad7c43316
Aloknath Datta <alokdatta.uit@gmail.com>
2021-03-01 20:29:31 +0000

    Wildcard removed

--- src/main/modules/helmet/modules/contentSecurityPolicy.ts
+++ src/main/modules/helmet/modules/contentSecurityPolicy.ts
@@ -12,8 +12,8 @@ export class ContentSecurityPolicy {
     const inlineJsEnabledBodyClassName = '\'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU=\''
     const inlineJsWindowGOVUKClassName = '\'sha256-G29/qSW/JHHANtFhlrZVDZW1HOkCDRc78ggbqwwIJ2g=\''
     const additionalClassName = '\'sha256-AaA9Rn5LTFZ5vKyp3xOfFcP4YbyOjvWn2up8IKHVAKk=\''
-    const scriptSrc = [inlineJsEnabledBodyClassName, additionalClassName, inlineJsWindowGOVUKClassName, self, '*.google-analytics.com', 'vcc-eu4.8x8.com','vcc-eu4b.8x8.com','www.apply-for-probate.service.gov.uk']
-    const connectSrc = [self, '*.gov.uk']
+    const scriptSrc = [inlineJsEnabledBodyClassName, additionalClassName, inlineJsWindowGOVUKClassName, self, 'www.google-analytics.com', 'vcc-eu4.8x8.com','vcc-eu4b.8x8.com','www.apply-for-probate.service.gov.uk']
+    const connectSrc = [self, 'www.gov.uk']
 
     if (this.developmentMode) {
       scriptSrc.push('https://localhost:35729')
@@ -33,7 +33,7 @@ export class ContentSecurityPolicy {
         ],
         imgSrc: [
           '\'self\'',
-          '*.google-analytics.com',
+          'www.google-analytics.com',
           'vcc-eu4.8x8.com',
           'vcc-eu4b.8x8.com'
         ],
