931b2a795e9a6385eba34ce767dcd0a44e56540b
Aloknath Datta <alokdatta.uit@gmail.com>
2021-03-02 16:39:47 +0000

    Security file updated to exclude the wildcards

--- security.sh
+++ security.sh
@@ -12,6 +12,8 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   echo "ZAP has successfully started"
   zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
   zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
+  zap-cli --zap-url http://0.0.0.0 -p 1001 exclude "*gov.uk"
+  zap-cli --zap-url http://0.0.0.0 -p 1001 exclude "*.google-analytics.com"
   zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
   zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
   zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
