4e24e2a3dee8cd797d76b3a6553405b5be6f65db
PhaniP <phani.perla@hmcts.net>
2021-03-03 16:09:39 +0000

    TA-494-updated Full Admission tests

--- default.conf.js
+++ default.conf.js
@@ -30,7 +30,7 @@ exports.config = {
       desiredCapabilities: {
         proxy: new ProxySettings(),
         chromeOptions: {
-          args: [ "--headless", "--disable-dev-shm-usage", "--window-size=1280,960", "--no-sandbox" ]
+          args: [ "--headless", "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1280,960"]
         }
       }
     },
--- src/integration-test/tests/citizen/defence/full_admission_test.ts
+++ src/integration-test/tests/citizen/defence/full_admission_test.ts
@@ -9,36 +9,35 @@ import { UserSteps } from 'integration-test/tests/citizen/home/steps/user'
 const helperSteps: Helper = new Helper()
 const defenceSteps: DefenceSteps = new DefenceSteps()
 const userSteps: UserSteps = new UserSteps()
+let claimantEmail
+let defendantEmail
+let claimData
+let claimRef
 
-async function prepareClaim (I: I) {
-  const claimantEmail: string = userSteps.getClaimantEmail()
-  const defendantEmail: string = userSteps.getDefendantEmail()
+Before(async (I: I) => {
+  claimantEmail = userSteps.getClaimantEmail()
+  defendantEmail = userSteps.getDefendantEmail()
 
-  const claimData: ClaimData = createClaimData(PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
-  const claimRef: string = await I.createClaim(claimData, claimantEmail)
+  claimData = createClaimData(PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
+  claimRef = await I.createClaim(claimData, claimantEmail)
 
   await helperSteps.enterPinNumber(claimRef, claimantEmail)
   helperSteps.linkClaimToDefendant(defendantEmail)
   helperSteps.startResponseFromDashboard(claimRef)
-
-  return { data: claimData }
-}
+})
 
 if (process.env.FEATURE_ADMISSIONS === 'true') {
   Feature('Fully admit all of the claim')
 
   Scenario('I can complete the journey when I fully admit all of the claim with immediate payment @nightly @admissions', { retries: 3 }, async (I: I) => {
-    const claimData = await prepareClaim(I)
     defenceSteps.makeFullAdmission(claimData.data.defendants[0], PartyType.INDIVIDUAL, PaymentOption.IMMEDIATELY, claimData.data.claimants[0].name, false)
   })
 
   Scenario('I can complete the journey when I fully admit all of the claim with payment by set date @nightly @admissions', { retries: 3 }, async (I: I) => {
-    const claimData = await prepareClaim(I)
     defenceSteps.makeFullAdmission(claimData.data.defendants[0], PartyType.INDIVIDUAL, PaymentOption.BY_SET_DATE, claimData.data.claimants[0].name, false)
   })
 
   Scenario('I can complete the journey when I fully admit all of the claim with full payment by instalments and also see PCQ in my journey @citizen @admissions', { retries: 3 }, async (I: I) => {
-    const claimData = await prepareClaim(I)
     defenceSteps.makeFullAdmission(claimData.data.defendants[0], PartyType.INDIVIDUAL, PaymentOption.INSTALMENTS, claimData.data.claimants[0].name, false, true)
   })
 }
