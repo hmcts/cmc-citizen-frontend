fc20eef05d511caeb20b03c446b3ca4ffd9c46f4
Aloknath Datta <alokdatta.uit@gmail.com>
2021-03-03 00:50:57 +0000

    checking

--- security.sh
+++ security.sh
@@ -23,4 +23,4 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   chown -R $(id -u):$(id -u) activescanReport.xml
   curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
   cp *.* functional-output/
-  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
+  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
