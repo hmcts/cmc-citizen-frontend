a09c6204473ede876823bd133b5e5fe6d79d0d55
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-03-09 14:05:01 +0000

    Revert "ROC-8912"
    This reverts commit e187e27e8ef2c0ad2006023efa026bb54aa65aec.

--- src/integration-test/tests/citizen/claim/helpWithFees_test.ts
+++ src/integration-test/tests/citizen/claim/helpWithFees_test.ts
@@ -7,3 +7,13 @@ Feature('Help With Fee E2E Tests...')
 Scenario('Submit Claim via HWF Reference... @citizen', { retries: 3 }, async (I: I) => {
   await claimSteps.makeAHwfClaimAndSubmit(I)
 })
+
+// The @citizen-smoke-test tag used for running smoke tests with pre-registered user
+Scenario('Navigate Payment Page by providing claim details... @smoke-test', { retries: 3 }, async (I: I) => {
+  await claimSteps.makeAClaimAndNavigateUpToPayment(I)
+})
+
+// The @citizen-smoke-test tag used for running smoke tests with pre-registered user for help with fees
+Scenario(' Navigate Payment Page via Providing HWF reference...  @smoke-test', { retries: 3 }, async (I: I) => {
+  await claimSteps.makeAHwfClaimAndNavigateUpToPayment(I)
+})
