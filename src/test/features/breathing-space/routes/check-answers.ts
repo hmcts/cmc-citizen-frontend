b2f8c6ef79c6cd4639732565443fba32ff4ee30f
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-03-28 16:03:08 +0100

    Revert "removed un-used mocks"
    This reverts commit f2a98f73935f88f12250c0fb0beac634373ff515.

--- src/test/features/breathing-space/routes/check-answers.ts
+++ src/test/features/breathing-space/routes/check-answers.ts
@@ -83,6 +83,7 @@ describe('Breathing Space: check-answer page', () => {
       context('when response not submitted', () => {
         it('should redirect to dashboard-claimant details page', async () => {
           idamServiceMock.resolveRetrieveUserFor('1', 'citizen')
+          draftStoreServiceMock.resolveFind('claim')
 
           await request(app)
             .post(pagePath)
@@ -94,6 +95,8 @@ describe('Breathing Space: check-answer page', () => {
 
         it('should render page when everything is fine', async () => {
           idamServiceMock.resolveRetrieveUserFor('1', 'citizen')
+          draftStoreServiceMock.resolveFind('claim')
+
           await request(app)
             .post(pagePath)
             .send({ type: SignatureType.BASIC })
