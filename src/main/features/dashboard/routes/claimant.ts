5f378752da4cd034c1316b3ae9b58dde7e80dae4
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-03-30 10:22:28 +0100

    fixing lint issue

--- src/main/features/dashboard/routes/claimant.ts
+++ src/main/features/dashboard/routes/claimant.ts
@@ -20,11 +20,6 @@ export default express.Router()
   .get(Paths.claimantPage.uri,
     ErrorHandling.apply(async (req: express.Request, res: express.Response): Promise<void> => {
       const { externalId } = req.params
-      res.app.locals.breathingSpaceExternalId = externalId
-      res.app.locals.breathingSpaceEndDate = null
-      res.app.locals.breathingSpaceEnteredDate = null
-      res.app.locals.breathingSpaceReferenceNumber = ''
-      res.app.locals.breathingSpaceType = null
 
       const claim = externalId !== draftExternalId ? await claimStoreClient.retrieveByExternalId(externalId, res.locals.user as User) : undefined
       const mediationDeadline: Moment = claim ? await claim.respondToMediationDeadline() : undefined
@@ -33,6 +28,11 @@ export default express.Router()
       const judgePilot: boolean = claim ? claim.features !== undefined && claim.features.includes('judgePilotEligible') : false
 
       const respondToReviewOrderDeadline: Moment = claim ? await claim.respondToReviewOrderDeadline() : undefined
+      res.app.locals.breathingSpaceExternalId = claim.externalId
+      res.app.locals.breathingSpaceEndDate = null
+      res.app.locals.breathingSpaceEnteredDate = null
+      res.app.locals.breathingSpaceReferenceNumber = ''
+      res.app.locals.breathingSpaceType = null
 
       if (claim && claim.claimantId !== res.locals.user.id) {
         throw new ForbiddenError()
--- src/test/app/claims/claimStoreClient.ts
+++ src/test/app/claims/claimStoreClient.ts
@@ -363,7 +363,6 @@ describe('ClaimStoreClient', () => {
           draft.breathingSpace.breathingSpaceEndDate = moment('9999-09-09')
           draft.breathingSpace.breathingSpaceExternalId = 'bbb89313-7e4c-4124-8899-34389312033a'
           draft.breathingSpace.breathingSpaceReferenceNumber = 'BS12345678'
-  
           await claimStoreClient.saveBreatingSpace(draft, claimant)
         } catch (err) {
           expect(err.statusCode).to.equal(HttpStatus.NOT_FOUND)
