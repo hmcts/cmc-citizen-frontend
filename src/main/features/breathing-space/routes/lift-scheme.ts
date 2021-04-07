1d5874be77480e8a787630f368dc19de76ce752b
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-04-07 17:43:29 +0100

    Added aditional tests

--- src/main/features/breathing-space/routes/lift-scheme.ts
+++ src/main/features/breathing-space/routes/lift-scheme.ts
@@ -5,35 +5,26 @@ import { Paths as DashboardPaths } from 'dashboard/paths'
 import { Form } from 'forms/form'
 import { FormValidator } from 'forms/validation/formValidator'
 import { ErrorHandling } from 'shared/errorHandling'
-import { BreathingSpace } from 'features/claim/form/models/breathingSpace'
 import { ClaimStoreClient } from 'claims/claimStoreClient'
 import { DraftClaim } from 'drafts/models/draftClaim'
+import { BreathingSpaceLiftDate } from '../models/bsLiftDate'
 
-function renderView (form: Form<BreathingSpace>, res: express.Response, next: express.NextFunction) {
-  let bsEndDate: any
-  if (res.app.locals.breathingSpaceLiftedbyInsolvencyTeamDate) {
-    if (res.app.locals.breathingSpaceLiftedbyInsolvencyTeamDate.day !== undefined) {
-      bsEndDate = res.app.locals.breathingSpaceLiftedbyInsolvencyTeamDate.toMoment().format('DD MMMM YYYY')
-    }
-  }
-
+function renderView (form: Form<BreathingSpaceLiftDate>, res: express.Response, next: express.NextFunction) {
   res.render(Paths.bsLiftCheckAnswersPage.associatedView, {
-    form: form,
-    breathingSpaceExternalId: res.app.locals.breathingSpaceExternalId,
-    breathingSpaceLiftedbyInsolvencyTeamDate: bsEndDate
+    form: form
   })
 }
 
 /*  tslint:disable:no-default-export */
 export default express.Router()
     .get(Paths.bsLiftCheckAnswersPage.uri, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-      renderView(new Form(new BreathingSpace()), res, next)
+      renderView(new Form(new BreathingSpaceLiftDate(res.app.locals.breathingSpaceLiftedbyInsolvencyTeamDate.toMoment().format('DD MMMM YYYY'))), res, next)
     })
     .post(
       Paths.bsLiftCheckAnswersPage.uri,
-      FormValidator.requestHandler(BreathingSpace),
+      FormValidator.requestHandler(BreathingSpaceLiftDate),
       ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-        const form: Form<BreathingSpace> = req.body
+        const form: Form<BreathingSpaceLiftDate> = req.body
         let draft: DraftClaim = new DraftClaim()
         draft.breathingSpace.breathingSpaceLiftedFlag = 'YES'
         draft.breathingSpace.breathingSpaceExternalId = res.app.locals.breathingSpaceExternalId
--- src/test/features/breathing-space/routes/lift-check-answers.ts
+++ src/test/features/breathing-space/routes/lift-check-answers.ts
@@ -8,12 +8,34 @@ import { Paths as BreathingSpacePaths } from 'breathing-space/paths'
 import { app } from 'main/app'
 import * as idamServiceMock from 'test/http-mocks/idam'
 import * as claimStoreServiceMock from 'test/http-mocks/claim-store'
+import { MomentFactory } from 'shared/momentFactory'
+import { Moment } from 'moment'
 
 const cookieName: string = config.get<string>('session.cookieName')
 
 const pagePath = BreathingSpacePaths.bsLiftCheckAnswersPage.uri
 
 describe('Breathing Space: Lift check-answer page', () => {
+  context('when user authorised', () => {
+    beforeEach(() => {
+      idamServiceMock.resolveRetrieveUserFor(claimStoreServiceMock.sampleClaimObj.defendantId, 'citizen')
+    })
+
+    context('Breathing Space Lift check your answer', () => {
+      it('should render the page with all the values', function (done) {
+        this.timeout(0)
+        idamServiceMock.resolveRetrieveUserFor('1', 'citizen')
+        const date: Moment = MomentFactory.currentDate().subtract(1, 'year')
+        request(app)
+          .get(pagePath)
+          .set('Cookie', `${cookieName}=ABC`)
+          .send({ respiteLiftDate: { day: date.date(), month: date.month() - 1, year: date.year() } })
+          .expect(res => expect(res).to.be.successful.withText('Are you sure you want to lift the debt respite scheme?'))
+        done()
+      })
+    })
+  })
+
   describe('on POST', () => {
 
     context('when user authorised', () => {
@@ -21,13 +43,13 @@ describe('Breathing Space: Lift check-answer page', () => {
         idamServiceMock.resolveRetrieveUserFor(claimStoreServiceMock.sampleClaimObj.defendantId, 'citizen')
       })
 
-      context('when response not submitted', () => {
+      context('when response submitted', () => {
         it('should redirect to dashboard-claimant details page', async () => {
           idamServiceMock.resolveRetrieveUserFor('1', 'citizen')
 
           await request(app)
             .post(pagePath)
-            .send({ breathingSpaceLiftedbyInsolvencyTeamDate: '2025-01-01', breathingSpaceExternalId: 'bbb89313-7e4c-4124-8899-34389312033a' })
+            .send({ breathingSpaceLiftedbyInsolvencyTeamDate: '2021-01-01', breathingSpaceExternalId: 'bbb89313-7e4c-4124-8899-34389312033a' })
             .set('Cookie', `${cookieName}=ABC`)
             .expect(res => expect(res).has.redirect)
         })
