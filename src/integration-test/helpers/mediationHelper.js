267b76c863b69920a989133cdcbf9d10c2376787
Aloknath Datta <alokdatta.uit@gmail.com>
2021-04-26 16:15:42 +0100

    async fix

--- src/integration-test/helpers/mediationHelper.js
+++ src/integration-test/helpers/mediationHelper.js
@@ -12,16 +12,16 @@ class mediationHelper extends codecept_helper {
     console.log(para[0]);
     console.log(para[1]);
     console.log('new journey::', para.includes('BETA This is a new service – your feedback (opens in a new window) will help us to improve it.'))
-    if (para.includes('BETA This is a new service – your feedback (opens in a new window) will help us to improve it.')) {
+    if (para[1].includes('We have automatically registered you for free telephone mediation from HM Courts and Tribunals Service.')) {
       console.log('enhanced mediation journey is enabled');
-      return true
+      return Promise.resolve(true)
     } else if (heading === 'Free telephone mediation') {
       console.log('enhanced mediation journey is disabled');
-      return false
+      return Promise.resolve(false)
     } else if (heading === 'Sorry, there is a problem with the service') {
       // silently move on.
       console.log('Error in Mediation Service');
-      return false
+      return Promise.resolve(false)
     }
   }
 }
