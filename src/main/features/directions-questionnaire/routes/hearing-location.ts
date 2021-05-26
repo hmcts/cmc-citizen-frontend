a3ce435311160d8972b89c9f6c199cfd815cb080
Thiru <Thirumurugan.Devarajan@cognizant.com>
2021-05-26 15:37:00 +0100

    lint issue

--- src/main/features/directions-questionnaire/routes/hearing-location.ts
+++ src/main/features/directions-questionnaire/routes/hearing-location.ts
@@ -84,7 +84,7 @@ export default express.Router()
 
       if (form.hasErrors()) {
         handleFormError(res, form, true, undefined)
-      } else if (form.model.courtAccepted === undefined && form.model.alternativeCourtSelected === undefined && 
+      } else if (form.model.courtAccepted === undefined && form.model.alternativeCourtSelected === undefined &&
         form.model.courtName) {
         await handleFormError(res, form, true, ValidationErrors.SELECT_ALTERNATIVE_OPTION)
       } else {
