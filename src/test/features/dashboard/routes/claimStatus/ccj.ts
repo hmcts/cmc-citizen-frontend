0753848b42cfc5fa954bee4040a01c6aa54bc2c2
Aloknath Datta <alokdatta.uit@gmail.com>
2021-06-24 13:09:45 +0100

    defendant page updated

--- src/test/features/dashboard/routes/claimStatus/ccj.ts
+++ src/test/features/dashboard/routes/claimStatus/ccj.ts
@@ -113,11 +113,11 @@ function testData () {
         'If we discover this has happened, we’ll reject your request.'
       ],
       defendantAssertions: ['000MC000',
-        'We’ll contact you',
-        'John Smith has requested a County Court Judgment (CCJ) against you because the deadline for your response has passed.',
-        'It’s possible we received your postal response before the deadline but hadn’t processed it when they requested judgment.',
-        'If we discover this has happened, we’ll reject their request.',
-        'Otherwise we’ll post a copy of the CCJ to you and to John Smith and explain what to do next.'
+        'Claimant has requested a County Court Judgment (CCJ)',
+        'John Smith has requested a CCJ against you because the deadline for your response has passed.',
+        'Your online account will not be updated with the progress of this claim - any further updates will be by post.',
+        'If your deadline has passed, but the CCJ has not been issued, you can still respond. Contact us for more information.',
+        'Otherwise we’ll post a copy of the CCJ to you and to Jan Clark and explain what to do next.'
       ]
     },
     {
