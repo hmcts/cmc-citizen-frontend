4cc21a093d76f9087700daf29924577be3f2cc32
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-06-25 10:48:36 +0100

    update

--- bin/run-crossbrowser-tests.sh
+++ bin/run-crossbrowser-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-if [[ "$BROWSER_GROUP" == "" ]]
+if [[ "$BROWSER_GROUP" == "--all" ]]
 then
     EXIT_STATUS=0
     BROWSER_GROUP=chrome yarn test:crossbrowser-verbose || EXIT_STATUS=$?
@@ -9,7 +9,6 @@ then
     BROWSER_GROUP=microsoft yarn test:crossbrowser-verbose || EXIT_STATUS=$?
     BROWSER_GROUP=safari yarn test:crossbrowser-verbose || EXIT_STATUS=$?
     echo EXIT_STATUS: $EXIT_STATUS
-    exit $EXIT_STATUS
 else
     # Compatible with Jenkins parallel crossbrowser pipeline
     yarn test:crossbrowser-verbose
