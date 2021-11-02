d8ae1f433e176180fd4e777c8ce50f7beb49c1d2
James <james.idowu@outlook.com>
2021-11-02 16:31:30 +0000

    ROC-5319 * Adding validation to separate fields in party names for the claimant section of the claim

--- src/main/app/forms/models/splitNamedPartyDetails.ts
+++ src/main/app/forms/models/splitNamedPartyDetails.ts
@@ -6,34 +6,34 @@ import { NameFormatter } from 'utils/nameFormatter'
 export class ValidationErrors {
   static readonly FIRSTNAME_REQUIRED: string = 'Enter first name'
   static readonly LASTNAME_REQUIRED: string = 'Enter last name'
-  static errorTooLong (input: string): string {
+  static errorTooLong(input: string): string {
     return `${input} must be no longer than $constraint1 characters`
   }
 }
 
 export class SplitNamedPartyDetails extends PartyDetails {
 
-  @ValidateIf(o => o.title !== undefined, { groups: ['defendant', 'response'] })
-  @MaxLength(35, { message: ValidationErrors.errorTooLong('Title'), groups: ['defendant', 'response'] })
+  @ValidateIf(o => o.title !== undefined, { groups: ['defendant', 'response', 'claimant'] })
+  @MaxLength(35, { message: ValidationErrors.errorTooLong('Title'), groups: ['defendant', 'response', 'claimant'] })
   title?: string
 
   @ValidateIf(o => o.firstName !== undefined, { groups: ['response'] })
-  @IsDefined({ message: ValidationErrors.FIRSTNAME_REQUIRED, groups: ['defendant', 'response'] })
-  @IsNotBlank({ message: ValidationErrors.FIRSTNAME_REQUIRED, groups: ['defendant', 'response'] })
-  @MaxLength(255, { message: ValidationErrors.errorTooLong('First name'), groups: ['defendant', 'response'] })
+  @IsDefined({ message: ValidationErrors.FIRSTNAME_REQUIRED, groups: ['defendant', 'response', 'claimant'] })
+  @IsNotBlank({ message: ValidationErrors.FIRSTNAME_REQUIRED, groups: ['defendant', 'response', 'claimant'] })
+  @MaxLength(255, { message: ValidationErrors.errorTooLong('First name'), groups: ['defendant', 'response', 'claimant'] })
   firstName?: string
 
   @ValidateIf(o => o.lastName !== undefined, { groups: ['response'] })
-  @IsDefined({ message: ValidationErrors.LASTNAME_REQUIRED, groups: ['defendant', 'response'] })
-  @IsNotBlank({ message: ValidationErrors.LASTNAME_REQUIRED, groups: ['defendant', 'response'] })
-  @MaxLength(255, { message: ValidationErrors.errorTooLong('Last name'), groups: ['defendant', 'response'] })
+  @IsDefined({ message: ValidationErrors.LASTNAME_REQUIRED, groups: ['defendant', 'response', 'claimant'] })
+  @IsNotBlank({ message: ValidationErrors.LASTNAME_REQUIRED, groups: ['defendant', 'response', 'claimant'] })
+  @MaxLength(255, { message: ValidationErrors.errorTooLong('Last name'), groups: ['defendant', 'response', 'claimant'] })
   lastName?: string
 
-  constructor () {
+  constructor() {
     super()
   }
 
-  static fromObject (input?: any): SplitNamedPartyDetails {
+  static fromObject(input?: any): SplitNamedPartyDetails {
     if (input == null) {
       return input
     }
@@ -52,7 +52,7 @@ export class SplitNamedPartyDetails extends PartyDetails {
     return deserialized
   }
 
-  deserialize (input?: any): SplitNamedPartyDetails {
+  deserialize(input?: any): SplitNamedPartyDetails {
     if (input) {
       Object.assign(this, new PartyDetails().deserialize(input))
       this.title = input.title
