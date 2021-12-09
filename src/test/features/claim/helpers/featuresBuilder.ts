a73f8b2e47324b9733680ce9c1e424ab554a63da
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2021-12-09 15:48:20 +0000

    ROC-9208: revert changes that are not needed

--- src/test/features/claim/helpers/featuresBuilder.ts
+++ src/test/features/claim/helpers/featuresBuilder.ts
@@ -6,7 +6,6 @@ import { attachDefaultHooks } from 'test/routes/hooks'
 import { app } from 'main/app'
 
 const featuresBuilder = new FeaturesBuilder()
-const LOW_CLAIM_AMOUNT = 300
 
 const MIN_THRESHOLD = Math.min(
   FeaturesBuilder.JUDGE_PILOT_THRESHOLD,
@@ -30,7 +29,7 @@ describe('FeaturesBuilder', () => {
     it(`should not add dq to features if amount > ${FeaturesBuilder.ONLINE_DQ_THRESHOLD}`, async () => {
       const featuresBuilder = new FeaturesBuilder()
       const features = await featuresBuilder.features(FeaturesBuilder.ONLINE_DQ_THRESHOLD + 0.01)
-      expect(features).to.be.equal('judgePilotEligible')
+      expect(features).to.be.undefined
     })
 
   })
@@ -55,9 +54,9 @@ describe('FeaturesBuilder', () => {
       expect(features).to.equal('judgePilotEligible, directionsQuestionnaire')
     })
 
-    it(`should not add judge pilot eligible to features if amount < ${FeaturesBuilder.LA_PILOT_THRESHOLD}`, async () => {
-      const features = await featuresBuilder.features(LOW_CLAIM_AMOUNT)
-      expect(features).to.equal('LAPilotEligible, directionsQuestionnaire')
+    it(`should not add judge pilot eligible to features if amount > ${FeaturesBuilder.JUDGE_PILOT_THRESHOLD}`, async () => {
+      const features = await featuresBuilder.features(FeaturesBuilder.JUDGE_PILOT_THRESHOLD + 1)
+      expect(features).to.be.undefined
     })
   })
 
