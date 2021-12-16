3d7db70955360ba4f8a2ee75ea918d4edfd19efd
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2021-12-16 16:12:54 +0000

    CIV-490 add keywords in uri

--- src/main/app/fees/feesClient.ts
+++ src/main/app/fees/feesClient.ts
@@ -30,10 +30,10 @@ export class FeesClient {
    */
   static async F (claimValue: number): Promise<number> {
     if (await featureToggles.isNewClaimFeesEnabled()) {
-      return this.calculateFee(issueFeeEvent, claimValue, paperChannel)
+      return this.calculateFee(issueFeeEvent, claimValue, paperChannel, issueFeeKeyWord)
       .then((outcome: FeeOutcome) => outcome.amount)
     } else {
-      return this.calculateFee(issueFeeEvent, claimValue, onlineChannel)
+      return this.calculateFee(issueFeeEvent, claimValue, onlineChannel, issueFeeKeyWord)
       .then((outcome: FeeOutcome) => outcome.amount)
     }
   }
@@ -45,7 +45,7 @@ export class FeesClient {
    * @returns {Promise.<string>} promise containing the fee code
    */
   static async retrieveClaimIssuanceFeeCode (claimValue: number): Promise<string> {
-    return this.calculateFee(issueFeeEvent, claimValue, paperChannel)
+    return this.calculateFee(issueFeeEvent, claimValue, paperChannel, issueFeeKeyWord)
     .then((outcome: FeeOutcome) => outcome.code)
   }
 
@@ -56,7 +56,7 @@ export class FeesClient {
    * @returns {Promise.<number>} promise containing the fee amount in pounds
    */
   static calculateHearingFee (claimValue: number): Promise<number> {
-    return this.calculateFee(hearingFeeEvent, claimValue, paperChannel)
+    return this.calculateFee(hearingFeeEvent, claimValue, paperChannel, hearingFeeKeyWord)
       .then((outcome: FeeOutcome) => outcome.amount)
   }
 
@@ -68,15 +68,18 @@ export class FeesClient {
    * @param channel online or paper/default
    * @returns {Promise.<FeeOutcome>} promise containing the Fee outcome (including fee amount in GBP)
    */
-  static async calculateFee (eventType: string, amount: number, channel: string): Promise<FeeOutcome> {
+  static async calculateFee (eventType: string, amount: number, channel: string, keyword: string): Promise<FeeOutcome> {
     if (StringUtils.isBlank(eventType)) {
       throw new Error('Fee eventType is required')
     }
     if (StringUtils.isBlank(channel)) {
       throw new Error('Fee channel is required')
     }
+    if(StringUtils.isBlank(keyword)) {
+      throw new Error('Keyword is required')
+    }
     ClaimValidator.claimAmount(amount)
-    const feeUri: string = `${feesUrl}/fees-register/fees/lookup?service=${service}&jurisdiction1=${jurisdiction1}&jurisdiction2=${jurisdiction2}&channel=${channel}&event=${eventType}&amount_or_volume=${amount}`
+    const feeUri: string = `${feesUrl}/fees-register/fees/lookup?service=${service}&jurisdiction1=${jurisdiction1}&jurisdiction2=${jurisdiction2}&channel=${channel}&event=${eventType}&keyword=${keyword}&amount_or_volume=${amount}`
     const options = {
       uri: feeUri
     }
