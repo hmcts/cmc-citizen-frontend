d5cf519278f63a9ec5230a7b3f8e0199f1de53b8
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-01-17 14:22:54 +0000

    CIV-472 another attempt to fix the issue with a11y

--- src/test/a11y/a11y.ts
+++ src/test/a11y/a11y.ts
@@ -61,7 +61,8 @@ async function runPa11y (url: string): Promise<Issue[]> {
       Cookie: `${cookieName}=ABC`
     },
     chromeLaunchConfig: {
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless'],
+      ignoreHTTPSErrors: false
     }
   })
   return result.issues
