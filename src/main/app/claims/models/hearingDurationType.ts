e2fe16535dda072b6f387b13d68e50b58377b81e
JackDevN <jack.nicholson@version1.com>
2022-01-18 15:58:16 +0000

    CIV-512: Reordered enum for consistency

--- src/main/app/claims/models/hearingDurationType.ts
+++ src/main/app/claims/models/hearingDurationType.ts
@@ -2,9 +2,9 @@ export enum HearingDurationType {
   ONE_DAY = 'One day',
   FOUR_HOURS = 'Four hours',
   THREE_HOURS = 'Three hours',
+  TWO_AND_HALF_HOURS = 'Two and half hours',
   TWO_HOURS = 'Two hours',
-  HALF_HOUR = '30 minutes',
   ONE_AND_HALF_HOUR = 'One and half hour',
   ONE_HOUR = 'One hour',
-  TWO_AND_HALF_HOURS = 'Two and half hours'
+  HALF_HOUR = '30 minutes'
 }
