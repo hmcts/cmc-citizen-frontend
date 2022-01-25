fb89d4f7543b18d1b00590f93df25dbf7b019a60
JackDevN <jack.nicholson@version1.com>
2022-01-25 11:16:29 +0000

    CIV-

--- src/main/app/claims/models/hearingDurationType.ts
+++ src/main/app/claims/models/hearingDurationType.ts
@@ -2,7 +2,6 @@ export enum HearingDurationType {
   ONE_DAY = 'One day',
   FOUR_HOURS = 'Four hours',
   THREE_HOURS = 'Three hours',
-  TWO_AND_HALF_HOURS = 'Two and half hours',
   TWO_HOURS = 'Two hours',
   ONE_AND_HALF_HOUR = 'One and half hour',
   ONE_HOUR = 'One hour',
