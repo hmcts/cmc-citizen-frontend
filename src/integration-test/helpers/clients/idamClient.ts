31c40632d56180cef18af6479638f06feb8ba9f3
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2022-03-09 17:15:19 +0000

    Update

--- src/integration-test/helpers/clients/idamClient.ts
+++ src/integration-test/helpers/clients/idamClient.ts
@@ -1,6 +1,9 @@
 import { request } from 'integration-test/helpers/clients/base/request'
 import * as url from 'url'
 import * as urlencode from 'urlencode'
+const NodeCache = require('node-cache');
+
+const idamTokenCache = new NodeCache({ stdTTL: 25200, checkperiod: 1800 });
 
 const baseURL: string = process.env.IDAM_URL
 
@@ -86,6 +89,18 @@ export class IdamClient {
     })
   }
 
+  static async authenticateUser (username: string, password: string = undefined): Promise<string> {
+    if (idamTokenCache.get(username) != null) {
+        console.log('User access token coming from cache', username);
+        return idamTokenCache.get(username);
+    } else {
+        const accessToken = await IdamClient.getAccessTokenFromIdam(username, password);
+        idamTokenCache.set(username, accessToken);
+        console.log('user access token coming from idam', username);
+        return accessToken;
+    }
+  }
+
   /**
    * Authenticate user
    *
@@ -93,7 +108,7 @@ export class IdamClient {
    * @param password the users password (optional, default will be used if none provided)
    * @returns {Promise<string>} the users access token
    */
-  static async authenticateUser (username: string, password: string = undefined): Promise<string> {
+  static async getAccessTokenFromIdam (username: string, password: string = undefined): Promise<string> {
     const base64Authorisation: string = IdamClient.toBase64(`${username}:${password || defaultPassword}`)
     const oauth2Params: string = IdamClient.toUrlParams(oauth2)
 
