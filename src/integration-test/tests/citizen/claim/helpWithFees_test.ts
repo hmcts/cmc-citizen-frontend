7130241abcc1e7bf9bc92ec9e957722fb86ebfe1
n-clarke-777 <nathan.clarke@HMCTS.net>
2022-03-27 21:20:55 +0100

    CIV-1479 : Temporary Disable Test

--- src/integration-test/tests/citizen/claim/helpWithFees_test.ts
+++ src/integration-test/tests/citizen/claim/helpWithFees_test.ts
@@ -2,12 +2,13 @@ import I = CodeceptJS.I
 import { ClaimSteps } from 'integration-test/tests/citizen/claim/steps/claim'
 const claimSteps: ClaimSteps = new ClaimSteps()
 
-Feature('Smoke Tests...')
+// CIV-1479 : Temporary Disable Test
+// Feature('Smoke Tests...')
 
 // The @citizen-smoke-test tag used for running smoke tests with pre-registered user
-Scenario('Navigate Payment Page by providing claim details... @smoke-test', { retries: 3 }, async (I: I) => {
-  await claimSteps.makeAClaimAndNavigateUpToPayment(I)
-})
+// Scenario('Navigate Payment Page by providing claim details... @smoke-test', { retries: 3 }, async (I: I) => {
+//   await claimSteps.makeAClaimAndNavigateUpToPayment(I)
+// })
 
 Feature('Help With Fee E2E Tests...')
 
