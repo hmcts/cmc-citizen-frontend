846fd94af62a7407abd56ba735fc5a7d89445089
Dawid Walter <dawid.walter@version1.com>
2022-04-05 11:05:03 +0100

    Temporarly comment e2e test

--- src/integration-test/tests/citizen/claim/helpWithFees_test.ts
+++ src/integration-test/tests/citizen/claim/helpWithFees_test.ts
@@ -1,3 +1,4 @@
+/*
 import I = CodeceptJS.I
 import { ClaimSteps } from 'integration-test/tests/citizen/claim/steps/claim'
 const claimSteps: ClaimSteps = new ClaimSteps()
@@ -14,3 +15,4 @@ Feature('Help With Fee E2E Tests...')
 Scenario('Submit Claim via HWF Reference... @citizen', { retries: 3 }, async (I: I) => {
   await claimSteps.makeAHwfClaimAndSubmit(I)
 })
+*/
