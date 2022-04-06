e232edcfcd05300cd1e998ba591542304997558c
n-clarke-777 <nathan.clarke@HMCTS.net>
2022-04-06 16:03:10 +0100

    CIV-1168 : update hearing duration type

--- src/main/app/claims/models/hearingDurationType.ts
+++ src/main/app/claims/models/hearingDurationType.ts
@@ -2,9 +2,9 @@ export enum HearingDurationType {
   ONE_DAY = 'One day',
   FOUR_HOURS = 'Four hours',
   THREE_HOURS = 'Three hours',
-  TWO_AND_HALF_HOURS = 'Two and half hours',
+  TWO_AND_HALF_HOURS = 'Two and a half hours',
   TWO_HOURS = 'Two hours',
-  ONE_AND_HALF_HOURS = 'One and half hours',
+  ONE_AND_HALF_HOURS = 'One and a half hours',
   ONE_HOUR = 'One hour',
   HALF_HOUR = '30 minutes'
 }
