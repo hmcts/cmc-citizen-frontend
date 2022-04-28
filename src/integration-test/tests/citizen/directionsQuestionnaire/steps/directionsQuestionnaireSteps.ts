524f4d42c7d9dfc4254f7b59ebca8fefcab96e93
jeejavr <jeejamol.vijayarajan@hmcts.net>
2022-04-28 15:51:58 +0100

    CIV-1418: fixing test cases

--- src/integration-test/tests/citizen/directionsQuestionnaire/steps/directionsQuestionnaireSteps.ts
+++ src/integration-test/tests/citizen/directionsQuestionnaire/steps/directionsQuestionnaireSteps.ts
@@ -7,6 +7,7 @@ import { SelfWitnessPage } from 'integration-test/tests/citizen/directionsQuesti
 import { OtherWitnessPage } from 'integration-test/tests/citizen/directionsQuestionnaire/pages/other-wtiness'
 import { HearingExceptionalCircumstancesPage } from 'integration-test/tests/citizen/directionsQuestionnaire/pages/hearing-exceptional-circumstances'
 import { PartyType } from 'integration-test/data/party-type'
+import { VulnerabilityQuestionsPage } from 'integration-test/tests/citizen/directionsQuestionnaire/pages/vulnerability-questions'
 
 const supportRequiredPage: SupportRequiredPage = new SupportRequiredPage()
 const hearingLocationPage: HearingLocationPage = new HearingLocationPage()
@@ -16,6 +17,7 @@ const expertReportsPage: ExpertReportsPage = new ExpertReportsPage()
 const selfWitnessPage: SelfWitnessPage = new SelfWitnessPage()
 const otherWitnessPage: OtherWitnessPage = new OtherWitnessPage()
 const hearingDatesPage: HearingDatesPage = new HearingDatesPage()
+const vulnerabilityQuestionsPage: VulnerabilityQuestionsPage = new VulnerabilityQuestionsPage()
 
 export class DirectionsQuestionnaireSteps {
   async acceptDirectionsQuestionnaireYesJourney (defendantType: PartyType = PartyType.INDIVIDUAL): Promise<void> {
@@ -30,6 +32,7 @@ export class DirectionsQuestionnaireSteps {
         '2019-01-01')
       selfWitnessPage.chooseYes()
       otherWitnessPage.chooseYes()
+      vulnerabilityQuestionsPage.chooseYes()
       await hearingDatesPage.chooseYes()
     }
   }
@@ -46,6 +49,7 @@ export class DirectionsQuestionnaireSteps {
       selfWitnessPage.chooseNo()
       otherWitnessPage.chooseNo()
       hearingDatesPage.chooseNo()
+      vulnerabilityQuestionsPage.chooseNo()
     }
   }
 
@@ -61,6 +65,7 @@ export class DirectionsQuestionnaireSteps {
       selfWitnessPage.chooseNo()
       otherWitnessPage.chooseNo()
       hearingDatesPage.chooseNo()
+      vulnerabilityQuestionsPage.chooseNo()
     }
   }
 }
