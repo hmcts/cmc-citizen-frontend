a18331d05d89f348afe4497736f6fa0affe73e06
darren <darren.boyd@hmcts.net>
2022-05-11 17:46:14 +0100

    Fixing array error

--- src/main/app/utils/keywordIssueEventUtils.ts
+++ src/main/app/utils/keywordIssueEventUtils.ts
@@ -7,22 +7,22 @@ export class KeywordIssueEventUtils {
     if (amount >= 0.01 && amount <= 300) {
       keyword = listIssueKeywords[0]
     } else if (amount >= 300.01 && amount <= 500) {
-      keyword = listIssueKeywords[0]
+      keyword = listIssueKeywords[1]
     } else if (amount >= 500.01 && amount <= 1000) {
-      keyword = listIssueKeywords[0]
+      keyword = listIssueKeywords[2]
     } else if (amount >= 1000.01 && amount <= 1500) {
-      keyword = listIssueKeywords[0]
+      keyword = listIssueKeywords[3]
     } else if (amount >= 1500.01 && amount <= 3000) {
-      keyword = listIssueKeywords[0]
+      keyword = listIssueKeywords[4]
     } else if (amount >= 3000.01 && amount <= 5000) {
-      keyword = listIssueKeywords[0]
+      keyword = listIssueKeywords[5]
     } else if (amount >= 5000.01 && amount <= 10000) {
-      keyword = listIssueKeywords[0]
+      keyword = listIssueKeywords[6]
     } else if (amount >= 10000.01 && amount <= 200000) {
-      keyword = listIssueKeywords[0]
+      keyword = listIssueKeywords[7]
     } else if (amount >= 200000.01) {
-      keyword = listIssueKeywords[0]
-    } else keyword = listIssueKeywords[0]
+      keyword = listIssueKeywords[8]
+    } else keyword = listIssueKeywords[9]
 
     return keyword
   }
