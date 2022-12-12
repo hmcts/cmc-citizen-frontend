3069cb118545dd1ce335d43a7f8853b8a7794ed4
Sabah Irfan <sabah.irfan@gmail.com>
2022-12-12 22:08:55 +0000

    Code cleanup

--- src/main/app/utils/listHearingKeywords.ts
+++
@@ -1,8 +0,0 @@
-export const listHearingKeywords: string[] = [
-  'HearingFeeUpTo300',
-  'HearingFeeUpTo500',
-  'HearingFeeUpTo1000',
-  'HearingFeeUpTo1500',
-  'HearingFeeUpTo3k',
-  'HearingFeeAbove3k'
-]
