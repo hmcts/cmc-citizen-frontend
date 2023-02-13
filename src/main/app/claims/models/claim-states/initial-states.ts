a172ae4d8b264d70340dc55af9f6a867a02cbba9
Sabah Irfan <sabah.irfan@gmail.com>
2023-02-13 10:22:27 +0000

    Remove newly added state in dashboard

--- src/main/app/claims/models/claim-states/initial-states.ts
+++ src/main/app/claims/models/claim-states/initial-states.ts
@@ -1,6 +1,5 @@
 export enum InitialStates {
   INIT = 'init',
-  SETTLED = 'settled',
   NO_RESPONSE = 'no-response',
   MORE_TIME_REQUESTED = 'more-time-requested',
   NO_RESPONSE_PAST_DEADLINE = 'no-response-past-deadline',
--- src/main/features/dashboard/claims-state-machine/initial-transitions.ts
+++ src/main/features/dashboard/claims-state-machine/initial-transitions.ts
@@ -68,11 +68,6 @@ export function initialTransitions (claim: Claim): StateMachine {
         name: 'checkHwfInvalid',
         from: [InitialStates.INIT, InitialStates.NO_RESPONSE, InitialStates.HWF_APPLICATION_PENDING],
         to: InitialStates.HWF_INVALID_REFERENCE
-      },
-      {
-        name: 'checkSettled',
-        from: [InitialStates.HWF_APPLICATION_PENDING, InitialStates.HWF_INVALID_REFERENCE, InitialStates.HWF_Part_Remitted, InitialStates.HWF_CLOSED, InitialStates.HWF_More_Info, InitialStates.HWF_Rejected],
-        to: InitialStates.SETTLED
       }
     ],
     data: {
@@ -100,10 +95,6 @@ export function initialTransitions (claim: Claim): StateMachine {
 
       onBeforeCheckCCJEnabled () {
         return this.state !== 'init' && claim.responseDeadline && isPastDeadline(MomentFactory.currentDateTime(), claim.responseDeadline)
-      },
-
-      onBeforeCheckSettled () {
-        return claim.state && (claim.state === 'settled' || claim.state === 'SETTLED')
       },
 
       onBeforeCheckHwf () {
