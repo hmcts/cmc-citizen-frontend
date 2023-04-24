c44ad781ec44380b1786cb7a91d98b8145f85843
Your Name <james.ferrari@version1.com>
2023-04-24 10:36:20 +0100

    amend env urls

--- bin/variables/load-preview-env-variables.sh
+++ bin/variables/load-preview-env-variables.sh
@@ -9,5 +9,5 @@ echo 'export ENVIRONMENT=preview'
 # urls
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
-echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.preview.platform.hmcts.net/oauth2redirect"
+echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://ccd-definition-store-cmc-citizen-frontend-pr-${pr}.preview.platform.hmcts.net"
