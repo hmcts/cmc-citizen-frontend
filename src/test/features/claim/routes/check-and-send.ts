e2a3f328ed0c2a45ea6a41676dbc047e3ea34018
Sabah Irfan <sabah.irfan@gmail.com>
2023-05-22 12:34:45 +0100

    Revert fix for the test

--- src/test/features/claim/routes/check-and-send.ts
+++ src/test/features/claim/routes/check-and-send.ts
@@ -594,7 +594,6 @@ describe('Claim issue: check and send page', () => {
           declared: YesNoOption.YES,
           helpWithFeesNumber: 'HWF123456'
         } })
-        draftStoreServiceMock.resolveUpdate()
         feesServiceMock.rejectRetreiveClaimIssuanceFeeCode()
         const nextPage = ClaimPaths.taskListPage.uri
         await request(app)
