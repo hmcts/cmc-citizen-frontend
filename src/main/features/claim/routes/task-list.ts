13565bd210c48b9332e30b8014afca5f290bcf16
GorilaNaranja <woodworkerdeveloper@gmail.com>
2023-05-23 11:49:18 +0200

    Revert "Fix: add try/catch tasklist controller"
    This reverts commit dbfa41775ff1fb7a9791f73a430af5549ad729da.

--- src/main/features/claim/routes/task-list.ts
+++ src/main/features/claim/routes/task-list.ts
@@ -1,31 +1,28 @@
 import * as express from 'express'
+
 import { Paths } from 'claim/paths'
+
 import { TaskList } from 'drafts/tasks/taskList'
+
 import { TaskListBuilder } from 'claim/helpers/taskListBuilder'
 import { Draft } from '@hmcts/draft-store-client'
 import { DraftClaim } from 'drafts/models/draftClaim'
-import { Logger } from '@hmcts/nodejs-logging'
-
-const logger = Logger.getLogger('claims/claimStoreClient')
 
 /* tslint:disable:no-default-export */
 export default express.Router()
   .get(Paths.taskListPage.uri, async (req: express.Request, res: express.Response) => {
-    try {
-      const draft: Draft<DraftClaim> = res.locals.claimDraft
-      const beforeYouStartSection: TaskList = TaskListBuilder.buildBeforeYouStartSection(draft.document)
-      const prepareYourClaimSection: TaskList = await TaskListBuilder.buildPrepareYourClaimSection(draft.document)
-      const submitSection: TaskList = TaskListBuilder.buildSubmitSection()
-      const status = TaskListBuilder.getTaskStatus([beforeYouStartSection, prepareYourClaimSection, submitSection])
-  
-      res.render(Paths.taskListPage.associatedView,
-        {
-          beforeYouStart: beforeYouStartSection,
-          prepareYourClaim: prepareYourClaimSection,
-          submit: submitSection,
-          status
-        })
-    } catch (error) {
-      logger.err('Error in TaskList controller')
-    }
+    const draft: Draft<DraftClaim> = res.locals.claimDraft
+
+    const beforeYouStartSection: TaskList = TaskListBuilder.buildBeforeYouStartSection(draft.document)
+    const prepareYourClaimSection: TaskList = await TaskListBuilder.buildPrepareYourClaimSection(draft.document)
+    const submitSection: TaskList = TaskListBuilder.buildSubmitSection()
+    const status = TaskListBuilder.getTaskStatus([beforeYouStartSection, prepareYourClaimSection, submitSection])
+
+    res.render(Paths.taskListPage.associatedView,
+      {
+        beforeYouStart: beforeYouStartSection,
+        prepareYourClaim: prepareYourClaimSection,
+        submit: submitSection,
+        status
+      })
   })
