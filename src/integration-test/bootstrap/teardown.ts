f25e99d69daa92324df2e89f9894a21006332bd4
zikrurR <workzikr@gmail.com>
2023-06-02 14:37:44 +0100

    modify try statement

--- src/integration-test/bootstrap/teardown.ts
+++ src/integration-test/bootstrap/teardown.ts
@@ -5,18 +5,14 @@ import { IdamClient } from 'integration-test/helpers/clients/idamClient'
 module.exports = {
   teardownAll: async function (claimantEmail, defendantEmail) {
     console.log('teardownAll...')
-    try {
-      if (process.env.IDAM_URL) {
-        if (process.env.SMOKE_TEST_CITIZEN_USERNAME) {
-          console.log('Deleting test users...')
-          claimantEmail !== undefined ? await IdamClient.deleteUser(claimantEmail) : console.log("claimantEmail is undefined")
-          defendantEmail !== undefined ? await IdamClient.deleteUser(defendantEmail) : console.log("defendantEmail is undefined")
-          claimantEmail !== undefined && defendantEmail !== undefined ?
-           await IdamClient.deleteUsers([claimantEmail, defendantEmail]) : console.log("claimantEmail and defendantEmail is undefined")
-        }
+    if (process.env.IDAM_URL) {
+      if (process.env.SMOKE_TEST_CITIZEN_USERNAME) {
+        console.log('Deleting test users...')
+        claimantEmail !== undefined ? await IdamClient.deleteUser(claimantEmail) : console.log("claimantEmail is undefined")
+        defendantEmail !== undefined ? await IdamClient.deleteUser(defendantEmail) : console.log("defendantEmail is undefined")
+        claimantEmail !== undefined && defendantEmail !== undefined ?
+          await IdamClient.deleteUsers([claimantEmail, defendantEmail]) : console.log("claimantEmail and defendantEmail is undefined")
       }
-    } catch (error) {
-      console.error('Error during teardown, exiting', error)
     }
   }
 }
