996ff6c3928e85b989d8b3475426e529726604fa
MMNycz <maciej.nycz@version1.com>
2023-07-19 11:58:26 +0100

    update correspondence address to uk postcode

--- src/test/data/draft/partyDetails.ts
+++ src/test/data/draft/partyDetails.ts
@@ -10,11 +10,11 @@ export const addressCorrespondenceAddress = {
   },
   hasCorrespondenceAddress: true,
   correspondenceAddress: {
-    line1: 'Flat 20',
-    line2: 'Street 20',
-    line3: 'Cooler house name',
+    line1: 'Flat 2',
+    line2: 'Street 2',
+    line3: 'Cool house name',
     city: 'London',
-    postcode: 'SE1 8ST'
+    postcode: 'E2 8FA'
   }
 }
 
