3a300a826068dd2e7864a7da800fdc7d80e642a9
zikrurR <workzikr@gmail.com>
2024-03-07 15:51:49 +0000

    update az acr login

--- bin/local/run-local-functional-tests.sh
+++ bin/local/run-local-functional-tests.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 source $(dirname "${BASH_SOURCE[0]}")/setup-environment.sh
-docker logout hmctspublic.azurecr.io &>/dev/null
-az acr login --name hmctsprivate --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 
 
 if [[ "$1" = "PULL" ]];
 then
     PULL=true
-    if [ ${#} -gt 0 && ${PULL}=true]
+    if [[ ${#} -gt 0 && ${PULL}=true ]]
     then
+    docker logout hmctspublic.azurecr.io &>/dev/null
+    az acr login --name hmctsprivate --subscription 8999dec3-0104-4a27-94ee-6588559729d1
+    sleep 1
     docker-compose ${COMPOSE_FILES} pull ${@}
     else
     docker-compose ${COMPOSE_FILES} pull
