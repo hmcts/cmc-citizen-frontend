afd8fdd9227ba6dc05f19c22dfde7af5cdda4f7d
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2024-09-12 21:12:51 +0100

    test

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -145,7 +145,7 @@ export class Nunjucks {
     nunjucksEnv.addGlobal('reportProblemSurveyUrl', config.get('feedback.reportProblemSurvey.url'))
     nunjucksEnv.addGlobal('customerSurveyUrl', config.get('feedback.serviceSurvey.url'))
     nunjucksEnv.addGlobal('cuiDashboardUrl', config.get('civil-citizen-ui.url'))
-    // nunjucksEnv.addGlobal('redirectToCUI', config.get('civil-citizen-ui.dashboard-redirect'))
+    nunjucksEnv.addGlobal('redirectToCUI', false)
 
     nunjucksEnv.addGlobal('featureToggles', this.convertPropertiesToBoolean(config.get('featureToggles')))
     nunjucksEnv.addGlobal('RejectAllOfClaimOption', RejectAllOfClaimOption)
