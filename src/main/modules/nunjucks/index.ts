593508ed7653735f1198927fe08f84c679ec3a1f
Velican <duzgunce.vd@gmail.com>
2024-09-13 11:09:37 +0100

    pipeline fix attempt 3

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -145,7 +145,7 @@ export class Nunjucks {
     nunjucksEnv.addGlobal('reportProblemSurveyUrl', config.get('feedback.reportProblemSurvey.url'))
     nunjucksEnv.addGlobal('customerSurveyUrl', config.get('feedback.serviceSurvey.url'))
     nunjucksEnv.addGlobal('cuiDashboardUrl', config.get<string>('cui.url'))
-    nunjucksEnv.addGlobal('redirectToCUI', config.get<boolean>('cui.dashboardRedirect'))
+    nunjucksEnv.addGlobal('redirectToCUI', toBoolean(config.get<boolean>('cui.dashboardRedirect')))
 
     nunjucksEnv.addGlobal('featureToggles', this.convertPropertiesToBoolean(config.get('featureToggles')))
     nunjucksEnv.addGlobal('RejectAllOfClaimOption', RejectAllOfClaimOption)
