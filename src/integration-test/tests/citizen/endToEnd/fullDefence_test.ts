7ada06c306aecae8495b525a1bf739b5f9070b32
Velican <duzgunce.vd@gmail.com>
2024-09-16 11:55:04 +0100

    test if FT runs with env variable condition

--- src/integration-test/tests/citizen/endToEnd/fullDefence_test.ts
+++ src/integration-test/tests/citizen/endToEnd/fullDefence_test.ts
@@ -17,38 +17,40 @@ const checkAndSendPage: ClaimantCheckAndSendPage = new ClaimantCheckAndSendPage(
 let testData
 let claimData
 
-Feature('Full Defence E2E')
+if (!process.env.CUI_DASHBOARD_REDIRECT) {
+  Feature('Full Defence E2E')
 
-Before(async (I: I) => {
-  testData = await EndToEndTestData.prepareData(I, PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
-  claimData = await createClaimData(I, PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
-  testData.defenceType = DefenceType.FULL_REJECTION_WITH_DISPUTE
-  testData.defendantClaimsToHavePaidInFull = true
-  await helperSteps.finishResponse(testData)
+  Before(async (I: I) => {
+    testData = await EndToEndTestData.prepareData(I, PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
+    claimData = await createClaimData(I, PartyType.INDIVIDUAL, PartyType.INDIVIDUAL)
+    testData.defenceType = DefenceType.FULL_REJECTION_WITH_DISPUTE
+    testData.defendantClaimsToHavePaidInFull = true
+    await helperSteps.finishResponse(testData)
 
-  I.see(testData.claimRef)
-  I.click('My account')
-  I.click(testData.claimRef)
-  I.see(testData.claimRef)
-  I.see('You have rejected the claim')
-  defendantDetails.clickViewClaim()
-  defendantDetails.checkClaimData(testData.claimRef, claimData)
-  I.click('Sign out')
+    I.see(testData.claimRef)
+    I.click('My account')
+    I.click(testData.claimRef)
+    I.see(testData.claimRef)
+    I.see('You have rejected the claim')
+    defendantDetails.clickViewClaim()
+    defendantDetails.checkClaimData(testData.claimRef, claimData)
+    I.click('Sign out')
 
-  userSteps.login(testData.claimantEmail)
-  claimantResponseSteps.viewClaimFromDashboard(testData.claimRef)
-  I.see(testData.claimRef)
-  I.see('Decide whether to proceed')
-  I.see('Mrs. Rose Smith has rejected your claim.')
-  I.click('View and respond')
-})
+    userSteps.login(testData.claimantEmail)
+    claimantResponseSteps.viewClaimFromDashboard(testData.claimRef)
+    I.see(testData.claimRef)
+    I.see('Decide whether to proceed')
+    I.see('Mrs. Rose Smith has rejected your claim.')
+    I.click('View and respond')
+  })
 
-Scenario('Reject/Dispute all of the claim @citizen', { retries: 3 }, async (I: I) => {
-  claimantResponseSteps.decideToProceed()
-  checkAndSendPage.checkFactsTrueAndSubmit(testData.defenceType)
-  I.see('You’ve rejected their response')
-})
+  Scenario('Reject/Dispute all of the claim @citizen', {retries: 3}, async (I: I) => {
+    claimantResponseSteps.decideToProceed()
+    checkAndSendPage.checkFactsTrueAndSubmit(testData.defenceType)
+    I.see('You’ve rejected their response')
+  })
 
-Scenario('Claimant Stop the claim /Case Stayed E2E @citizen @nightly', { retries: 3 }, async (I: I) => {
-  claimantResponseSteps.decideNotToProceed()
-})
+  Scenario('Claimant Stop the claim /Case Stayed E2E @citizen @nightly', {retries: 3}, async (I: I) => {
+    claimantResponseSteps.decideNotToProceed()
+  })
+}
