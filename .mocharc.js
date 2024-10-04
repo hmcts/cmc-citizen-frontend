3ac83b802952292eff3002119ac107ceea07813a
Velican <duzgunce.vd@gmail.com>
2024-10-04 17:05:50 +0100

    ignore tests for pages related to when feature toggle is on

--- .mocharc.js
+++ .mocharc.js
@@ -5,5 +5,11 @@ module.exports = {
     'ts-node/register',
     'tsconfig-paths/register'
   ],
+  ignore: process.env.FEATURE_DISABLE_PAGES === 'true' ?
+    [
+      'src/test/features/claim/**/*.ts',
+      'src/test/features/eligibility/**/*.ts',
+      'src/test/features/dashboard/**/*.ts'
+    ] : [],
   reporter: 'mochawesome'
 };
