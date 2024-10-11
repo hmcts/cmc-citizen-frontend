48b20f23cefde2ebb722ed0a87fcbb1e601d165f
Velican <duzgunce.vd@gmail.com>
2024-10-11 13:21:53 +0100

    clean up

--- src/main/features/claim-documents/routes/sealed-claim.ts
+++ src/main/features/claim-documents/routes/sealed-claim.ts
@@ -1,16 +1,4 @@
-import * as express from 'express'
-import { Paths } from 'claim/paths'
-
-import { ErrorHandling } from 'shared/errorHandling'
-import { ClaimMiddleware } from 'claims/claimMiddleware'
-
-import { SealedClaimPdfGenerator } from 'services/sealedClaimPdfGenerator'
-import { OnlyDefendantLinkedToClaimCanDoIt } from 'guards/onlyDefendantLinkedToClaimCanDoIt'
+import SealedClaimRoute from 'claim/routes/sealed-claim'
 
 /* tslint:disable:no-default-export */
-export default express.Router()
-  .get(
-    Paths.sealedClaimPdfReceiver.uri,
-    ClaimMiddleware.retrieveByExternalId,
-    OnlyDefendantLinkedToClaimCanDoIt.check(),
-    ErrorHandling.apply(SealedClaimPdfGenerator.requestHandler))
+export default SealedClaimRoute
