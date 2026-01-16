4b5164fb93213183aa8a4ed146c4a8635e3a45df
Ruban <ruban72@hotmail.com>
2026-01-16 14:28:41 +0000

    DTSCCI-3203: migrate from Jitpack

---
+++ src/main/routes/configurableInfoContributor.ts
@@ -0,0 +1,29 @@
+import * as requestPromise from 'request-promise-native'
+import { RequestPromiseOptions } from 'request-promise-native'
+import { InfoContributor } from '@hmcts/info-provider'
+
+export class ConfigurableInfoContributor extends InfoContributor {
+  constructor (serviceUrl: string, private readonly requestOptions?: RequestPromiseOptions) {
+    super(serviceUrl)
+  }
+
+  async call (): Promise<object> {
+    if (!this.requestOptions) {
+      return super.call()
+    }
+
+    try {
+      return await requestPromise.get({
+        uri: this.url,
+        json: true,
+        ...this.requestOptions
+      })
+    } catch (error) {
+      return {
+        error: `Error calling ${this.url}`,
+        statusText: error?.message,
+        body: error?.response?.body
+      }
+    }
+  }
+}
