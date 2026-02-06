6af5bfca31baf8de8d0579779145fd0ad1d01dac
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2026-02-06 08:43:51 +0000

    Updating Terraform Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
- features {}
+  features {}
 }
 
 locals {
@@ -7,85 +7,85 @@ locals {
 }
 
 data "azurerm_key_vault" "cmc_key_vault" {
-  name = "${local.vaultName}"
-  resource_group_name = "${local.vaultName}"
+  name                = local.vaultName
+  resource_group_name = local.vaultName
 }
 
 data "azurerm_key_vault_secret" "cookie_encryption_key" {
-  name = "citizen-cookie-encryption-key"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "citizen-cookie-encryption-key"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "s2s_secret" {
-  name = "cmc-s2s-secret"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "cmc-s2s-secret"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "draft_store_primary" {
-  name = "citizen-draft-store-primary"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "citizen-draft-store-primary"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "draft_store_secondary" {
-  name = "citizen-draft-store-secondary"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "citizen-draft-store-secondary"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "os_postcode_lookup_api_key" {
-  name = "os-postcode-lookup-api-key"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "os-postcode-lookup-api-key"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "oauth_client_secret" {
-  name = "citizen-oauth-client-secret"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "citizen-oauth-client-secret"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "staff_email" {
-  name = "staff-email"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "staff-email"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "launch_darkly_sdk_key" {
-  name = "launchDarkly-sdk-key"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "launchDarkly-sdk-key"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "cmc_webchat_id" {
-  name = "cmc-webchat-id"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "cmc-webchat-id"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "cmc_webchat_tenant" {
-  name = "cmc-webchat-tenant"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "cmc-webchat-tenant"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "cmc_webchat_button_no_agents" {
-  name = "cmc-webchat-button-no-agents"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "cmc-webchat-button-no-agents"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 data "azurerm_key_vault_secret" "cmc_webchat_button_busy" {
-  name = "cmc-webchat-button-busy"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "cmc-webchat-button-busy"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "cmc_webchat_button_service_closed" {
-  name = "cmc-webchat-button-service-closed"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "cmc-webchat-button-service-closed"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "pcq_token_key" {
-  name = "pcq-token-key"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "pcq-token-key"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "app_insights_instrumental_key" {
-  name = "AppInsightsInstrumentationKey"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "AppInsightsInstrumentationKey"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "draft_store_access_key" {
-  name = "draft-store-access-key"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  name         = "draft-store-access-key"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,7 +13,7 @@ variable "location" {
   default = "UK South"
 }
 
-variable "env" { }
+variable "env" {}
 
 variable "ilbIp" {
   default = ""
@@ -22,7 +22,7 @@ variable "ilbIp" {
 variable "subscription" {}
 
 variable "jenkins_AAD_objectId" {
-  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
 variable "tenant_id" {
@@ -35,7 +35,7 @@ variable "node_env" {
 
 variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
-  default = ""
+  default     = ""
 }
 
 variable "common_tags" {
