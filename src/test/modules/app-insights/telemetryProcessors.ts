58575d406348e97a4e19dd4b7ce9a397e843369f
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2026-03-09 17:57:29 +0000

    DTSCCI-4033: update

--- src/test/modules/app-insights/telemetryProcessors.ts
+++ src/test/modules/app-insights/telemetryProcessors.ts
@@ -2,7 +2,7 @@
 import * as chai from 'chai'
 import * as telemetryProcessors from 'modules/app-insights/telemetryProcessors'
 import { LoggerInstance } from 'winston'
-import Envelope = require('applicationinsights/out/Declarations/Contracts/Generated/Envelope')
+import Envelope from 'applicationinsights/out/Declarations/Contracts/Generated/Envelope'
 
 const expect = chai.expect
 
