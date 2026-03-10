d70630c359dcec221ec0f0f4db7753c9157794ae
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2026-03-10 13:27:59 +0000

    DTSCCI-4033: fix linting issue

--- src/main/app/claims/claimStoreClient.ts
+++ src/main/app/claims/claimStoreClient.ts
@@ -98,54 +98,50 @@ export class ClaimStoreClient {
   }
 
   saveBreatingSpace (draft: DraftClaim, claimant: User): Promise<Claim> {
-    try {
-      let endDate = moment('9999-09-09').format('YYYY-MM-DD')
-      let StartDate = moment('9999-09-09').format('YYYY-MM-DD')
-      let endDateByInsolvencyTeam = moment('9999-09-09').format('YYYY-MM-DD')
-      let startDateByInsolvencyTeam = moment('9999-09-09').format('YYYY-MM-DD')
+    let endDate = moment('9999-09-09').format('YYYY-MM-DD')
+    let StartDate = moment('9999-09-09').format('YYYY-MM-DD')
+    let endDateByInsolvencyTeam = moment('9999-09-09').format('YYYY-MM-DD')
+    let startDateByInsolvencyTeam = moment('9999-09-09').format('YYYY-MM-DD')
 
-      if (draft.breathingSpace.breathingSpaceEndDate !== undefined && draft.breathingSpace.breathingSpaceEndDate !== null) {
-        endDate = moment(draft.breathingSpace.breathingSpaceEndDate).format('YYYY-MM-DD')
-      }
-
-      if (draft.breathingSpace.breathingSpaceEnteredDate !== undefined && draft.breathingSpace.breathingSpaceEnteredDate !== null) {
-        StartDate = moment(draft.breathingSpace.breathingSpaceEnteredDate).format('YYYY-MM-DD')
-      }
-
-      if (draft.breathingSpace.breathingSpaceEnteredbyInsolvencyTeamDate !== undefined && draft.breathingSpace.breathingSpaceEnteredbyInsolvencyTeamDate !== null) {
-        startDateByInsolvencyTeam = moment(draft.breathingSpace.breathingSpaceEnteredbyInsolvencyTeamDate).format('YYYY-MM-DD')
-      }
-
-      if (draft.breathingSpace.breathingSpaceLiftedbyInsolvencyTeamDate !== undefined && draft.breathingSpace.breathingSpaceLiftedbyInsolvencyTeamDate !== null) {
-        endDateByInsolvencyTeam = moment(draft.breathingSpace.breathingSpaceLiftedbyInsolvencyTeamDate).format('YYYY-MM-DD')
-      }
-
-      return this.request
-        .post(`${claimStoreApiUrl}/${draft.breathingSpace.breathingSpaceExternalId.toString()}/breathingSpace`, {
-          body: {
-            'bs_entered_date_by_insolvency_team': startDateByInsolvencyTeam,
-            'bs_entered_date': StartDate,
-            'bs_expected_end_date': endDate,
-            'bs_reference_number': draft.breathingSpace.breathingSpaceReferenceNumber !== undefined ? draft.breathingSpace.breathingSpaceReferenceNumber.toString() : '',
-            'bs_type': draft.breathingSpace.breathingSpaceType.toString(),
-            'bs_lifted_flag': draft.breathingSpace.breathingSpaceLiftedFlag.toString(),
-            'bs_lifted_date_by_insolvency_team': endDateByInsolvencyTeam
-          },
-          headers: {
-            Authorization: `Bearer ${claimant.bearerToken}`
-          }
-        })
-        .then(claim => new Claim().deserialize(claim))
-        .catch(err => {
-          if (err.statusCode === HttpStatus.CONFLICT) {
-            logger.warn(`Claim ${draft.externalId} appears to have been saved successfully on initial timed out attempt, retrieving the saved instance`)
-            return this.retrieveByExternalId(draft.externalId, claimant)
-          }
-          throw err
-        })
-    } catch (error) {
-      throw error
+    if (draft.breathingSpace.breathingSpaceEndDate !== undefined && draft.breathingSpace.breathingSpaceEndDate !== null) {
+      endDate = moment(draft.breathingSpace.breathingSpaceEndDate).format('YYYY-MM-DD')
     }
+
+    if (draft.breathingSpace.breathingSpaceEnteredDate !== undefined && draft.breathingSpace.breathingSpaceEnteredDate !== null) {
+      StartDate = moment(draft.breathingSpace.breathingSpaceEnteredDate).format('YYYY-MM-DD')
+    }
+
+    if (draft.breathingSpace.breathingSpaceEnteredbyInsolvencyTeamDate !== undefined && draft.breathingSpace.breathingSpaceEnteredbyInsolvencyTeamDate !== null) {
+      startDateByInsolvencyTeam = moment(draft.breathingSpace.breathingSpaceEnteredbyInsolvencyTeamDate).format('YYYY-MM-DD')
+    }
+
+    if (draft.breathingSpace.breathingSpaceLiftedbyInsolvencyTeamDate !== undefined && draft.breathingSpace.breathingSpaceLiftedbyInsolvencyTeamDate !== null) {
+      endDateByInsolvencyTeam = moment(draft.breathingSpace.breathingSpaceLiftedbyInsolvencyTeamDate).format('YYYY-MM-DD')
+    }
+
+    return this.request
+      .post(`${claimStoreApiUrl}/${draft.breathingSpace.breathingSpaceExternalId.toString()}/breathingSpace`, {
+        body: {
+          'bs_entered_date_by_insolvency_team': startDateByInsolvencyTeam,
+          'bs_entered_date': StartDate,
+          'bs_expected_end_date': endDate,
+          'bs_reference_number': draft.breathingSpace.breathingSpaceReferenceNumber !== undefined ? draft.breathingSpace.breathingSpaceReferenceNumber.toString() : '',
+          'bs_type': draft.breathingSpace.breathingSpaceType.toString(),
+          'bs_lifted_flag': draft.breathingSpace.breathingSpaceLiftedFlag.toString(),
+          'bs_lifted_date_by_insolvency_team': endDateByInsolvencyTeam
+        },
+        headers: {
+          Authorization: `Bearer ${claimant.bearerToken}`
+        }
+      })
+      .then(claim => new Claim().deserialize(claim))
+      .catch(err => {
+        if (err.statusCode === HttpStatus.CONFLICT) {
+          logger.warn(`Claim ${draft.externalId} appears to have been saved successfully on initial timed out attempt, retrieving the saved instance`)
+          return this.retrieveByExternalId(draft.externalId, claimant)
+        }
+        throw err
+      })
   }
 
   updateHelpWithFeesClaim (draft: Draft<DraftClaim>, claimant: User, ...features: string[]): Promise<Claim> {
