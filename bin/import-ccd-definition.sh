eca4d0fc30042f049adfa914f714ba6608f059dd
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2026-03-19 15:54:29 +0000

    DTSCCI-4033: update the ACR

--- bin/import-ccd-definition.sh
+++ bin/import-ccd-definition.sh
@@ -25,7 +25,7 @@ docker run --rm --name json2xlsx \
   -v ${definition_input_dir}:/tmp/ccd-definition \
   -v ${definition_output_file}:/tmp/ccd-definition.xlsx \
   -e CCD_DEF_CLAIM_STORE_BASE_URL=${IMPORTER_CLAIM_STORE_URL:-http://docker.for.mac.localhost:4000} \
-  hmctspublic.azurecr.io/ccd/definition-processor:${definition_processor_version} \
+  hmctsprod.azurecr.io/ccd/definition-processor:${definition_processor_version} \
   json2xlsx -D /tmp/ccd-definition -o /tmp/ccd-definition.xlsx ${params}
 
 ${dir}/ccd-import-definition.sh ${definition_output_file}
