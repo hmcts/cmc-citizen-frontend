0e437f171eb715382317cc106d5a2aed845e9848
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-09-16 16:59:26 +0100

    DTSCCI-0000 Revert skip test after platops fix

--- playwright/tests/api-functional/claim-lifecycle.spec.ts
+++ playwright/tests/api-functional/claim-lifecycle.spec.ts
@@ -22,8 +22,7 @@ import { buildClaimData, buildResponseData, generateTestEmail } from '../../help
  *   6. Defendant responds via POST /responses/claim/{externalId}/defendant/{defendantId}
  *   7. Verify claim has response attached
  */
-test.describe.skip('Claim Lifecycle - Create, Respond, Verify', () => {
-  test.describe.configure({ mode: 'serial' });
+test.describe.serial('Claim Lifecycle - Create, Respond, Verify', () => {
 
   const claimantEmail = generateTestEmail('pw-claimant');
   const defendantEmail = generateTestEmail('pw-defendant');
